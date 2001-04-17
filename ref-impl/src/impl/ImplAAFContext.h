beba21eb2718ec566336bc3ed1f71880b494936f
tbingham <>
2001-04-17 03:03:55 +0000

    Changing AAFSession to AAFContext (no new functionality).

--- ref-impl/src/impl/ImplAAFContext.h
+++ ref-impl/src/impl/ImplAAFContext.h
@@ -1,7 +1,7 @@
 //@doc
-//@class    AAFSession | Implementation class for AAFSession
-#ifndef __ImplAAFSession_h__
-#define __ImplAAFSession_h__
+//@class    AAFContext | Implementation class for AAFContext
+#ifndef __ImplAAFContext_h__
+#define __ImplAAFContext_h__
 
 /******************************************\
 *                                          *
@@ -23,7 +23,7 @@ class ImplAAFFile;
 class ImplAAFPluginManager;
 
 
-class ImplAAFSession : public ImplAAFRoot
+class ImplAAFContext : public ImplAAFRoot
 {
 public:
 
@@ -32,80 +32,15 @@ public:
   //
   // Allows clients to get the single instance of this class.
   //
-  static ImplAAFSession * GetInstance ();
-
-
-  //****************
-  // EndSession()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    EndSession ();
-
-
-  //****************
-  // CreateFile()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    CreateFile
-        (aafWChar *  filePath,   //@parm [in] File path [replace with object later]
-		 aafFileRev_t  rev,   //@parm [in] File revision to create
-         ImplAAFFile ** file);  //@parm [out] Current AAF file
-
-
-  //****************
-  // OpenReadFile()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    OpenReadFile
-        (aafWChar *  filePath,   //@parm [in] File path [replace with object later]
-		 ImplAAFFile ** file);  //@parm [out] Current AAF file
-
-
-  //****************
-  // OpenModifyFile()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    OpenModifyFile
-        (aafWChar *  filePath,   //@parm [in] File path [replace with object later]
-		 ImplAAFFile ** file);  //@parm [out] Current AAF file
-
-  //***********************************************************
-  // METHOD NAME: SetCurrentIdentification()
-  //
-  // DESCRIPTION:
-  // @mfunc AAFRESULT | AAFSession | SetCurrentIdentification |
-  // Sets the object which identifies the creator of the file.
-  // @end
-  // 
-  virtual AAFRESULT STDMETHODCALLTYPE
-  SetDefaultIdentification (
-    // @parm aafProductIdentification | ident | [in] a struct from which it is initialized
-    aafProductIdentification_t  *ident
-  );
-
-
-  //***********************************************************
-  // METHOD NAME: GetIdentification()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-  GetIdentification (
-    aafProductIdentification_t  *ident
-  );
-
-  virtual AAFRESULT STDMETHODCALLTYPE
-  BeginSession (
-    aafProductIdentification_t  *ident
-  );
+  static ImplAAFContext * GetInstance ();
 
 
 public:
   // Declare the module test method. The implementation of the will be be
-  // in /test/ImplAAFSessionTest.cpp.
+  // in /test/ImplAAFContextTest.cpp.
   static AAFRESULT test();
 
-  ImplAAFFile *GetTopFile(void);
-  void SetTopFile(ImplAAFFile *file);
-  aafProductIdentification_t *GetDefaultIdent(void);
+
   void InitPluginManager (void);
   ImplAAFPluginManager *GetPluginManager (void);
 
@@ -115,17 +50,20 @@ private:
   // Constructor/destructor
   //
   //********
-  ImplAAFSession ();
-  virtual ~ImplAAFSession ();
+  ImplAAFContext ();
+  virtual ~ImplAAFContext ();
 
   // single instance of this class
-  static ImplAAFSession * _singleton;
+  static ImplAAFContext * _singleton;
 
-  ImplAAFFile	*_topFile;
-  aafProductIdentification_t	*_defaultIdent;
-  class ImplAAFPluginManager	*_plugins;
+  ImplAAFPluginManager	*_plugins;
 
+  // Make private helper class a friend so that it
+  // may call the destructor. This helper class is used
+  // to ensure that the singleton context instance is 
+  // cleaned up properly.
+  friend class ImplAAFContextHelper;
 };
 
-#endif // ! __ImplAAFSession_h__
+#endif // ! __ImplAAFContext_h__
 
