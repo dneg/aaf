bc2b88376a850ba8c8871886550542042f9f580f
tbingham <>
2001-04-17 03:37:00 +0000

    Re-order makedepend stuff.

--- ref-impl/bld_cfg/FreeBSD.mk
+++ ref-impl/bld_cfg/FreeBSD.mk
@@ -9,6 +9,9 @@
 #
 # Contains elements for FreeBSD/i386 builds.
 #
+# The inclusion of depend.mk requires that DEPEND_INCLUDES and SOURCES
+# be already defined.
+#
 
 PTFM_SUFX = 86
 OBJ = o${PTFM_SUFX}
@@ -23,3 +26,12 @@ EXEFILE = -o unittest.${EXE}
 
 SH_PREFIX = 
 SH_SUFFIX =
+
+
+#
+# Some platforms (which do their own makedepend generation) may
+# require that DEPEND_INCLUDES and SOURCES be already defined in order
+# for depend.mk to function properly.  That should not be a problem if
+# Clearcase dependency checking is used.
+#
+include $(BLD_CFG_DIR)/depend.mk
