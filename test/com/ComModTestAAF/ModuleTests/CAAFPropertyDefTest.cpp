76f97e57dfe5109e8d566eeec0877576b77f274f
tbingham <>
2001-04-17 07:35:32 +0000

    Added tests ... reviewd by transdel

--- test/com/ComModTestAAF/ModuleTests/CAAFPropertyDefTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFPropertyDefTest.cpp
@@ -1,93 +1,400 @@
 // @doc INTERNAL
 // @com This file implements the module test for CAAFPropertyDef
 /***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+*
+*              Copyright (c) 1998-1999 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying 
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted, 
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+* prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
 
-
-
-
-
- 
-
-
-#include "AAFTypes.h" //Use #include "AAF.h" for functional module test.
+#include "AAF.h"
 #include "AAFResult.h"
+#include "AAFStoredObjectIDs.h"
+#include "AAFDataDefs.h"
+#include "AAFDefUIDs.h"
+
+#include <iostream.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "CAAFBuiltinDefs.h"
+
+typedef IAAFSmartPointer<IAAFClassDef>				IAAFClassDefSP;
+typedef IAAFSmartPointer<IAAFPropertyDef>			IAAFPropertyDefSP;
+typedef IAAFSmartPointer<IAAFTypeDef>				IAAFTypeDefSP;
+typedef IAAFSmartPointer<IAAFPropertyValue>			IAAFPropertyValueSP;
+typedef IAAFSmartPointer<IAAFTypeDefInt>			IAAFTypeDefIntSP;
+typedef IAAFSmartPointer<IUnknown>					IUnknownSP;
+typedef IAAFSmartPointer<IAAFDefObject>				IAAFDefObjectSP;
+
+//// Our TEST (gold) values ....
+
+// {CADEF777-6D3C-11d3-8449-00600832ACB8}
+static const aafUID_t propDef_id = 
+{ 0xcadef777, 0x6d3c, 0x11d3, { 0x84, 0x49, 0x0, 0x60, 0x8, 0x32, 0xac, 0xb8 } };
+
+#define TEST_NAME L"Test: Property Def Name"
+#define TEST_DESCRIPTION L"Test: Property Def Description"
+
+static const aafUInt8 TEST_INT_BYTE = 0xAB;
+static const aafUID_t TEST_UID = kAAFTypeID_UInt8;
+
+//Initialize our Non-Implemented flags to False ...
+static aafBoolean_t	 bSetDefaultValue_NI = kAAFFalse;       
+static aafBoolean_t  bSetIsSearchable_NI = kAAFFalse;       
+static aafBoolean_t  bGetDefaultValue_NI = kAAFFalse;       
+static aafBoolean_t  bGetIsSearchable_NI = kAAFFalse;       
 
 
-extern "C" HRESULT CAAFPropertyDef_test()
+// convenient error handlers.
+inline void checkResult(HRESULT r)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+	if (FAILED(r))
+		throw r;
+}
+inline void checkExpression(bool expression, HRESULT r)
+{
+	if (!expression)
+		throw r;
+}
+
+// Cross-platform utility to delete a file.
+static void RemoveTestFile(const wchar_t* pFileName)
+{
+	const size_t kMaxFileName = 512;
+	char cFileName[kMaxFileName];
+	
+	size_t status = wcstombs(cFileName, pFileName, kMaxFileName);
+	if (status != (size_t)-1)
+	{ // delete the file.
+		remove(cFileName);
+	}
 }
 
 
+static HRESULT CreateAAFFile(aafWChar *  pFileName )
+{
+	HRESULT hr = AAFRESULT_SUCCESS;
+	
+	aafProductIdentification_t ProductInfo;
+	IAAFFile* pFile = NULL;
+	IAAFHeader * pHeader = NULL;
+	IAAFDictionary * pDict = NULL;
+	
+	try
+	{
+		ProductInfo.companyName = L"AAF Developers Desk";
+		ProductInfo.productName = L"AAFPropertyDef Test";
+		ProductInfo.productVersion.major = 1;
+		ProductInfo.productVersion.minor = 0;
+		ProductInfo.productVersion.tertiary = 0;
+		ProductInfo.productVersion.patchLevel = 0;
+		ProductInfo.productVersion.type = kAAFVersionUnknown;
+		ProductInfo.productVersionString = NULL;
+		ProductInfo.productID = UnitTestProductID;
+		ProductInfo.platform = NULL;
+		
+		//Do the usual ...
+		RemoveTestFile (pFileName);
+		checkResult (AAFFileOpenNewModify(pFileName, 0, &ProductInfo, &pFile));
+		assert (pFile);
+		checkResult (pFile->GetHeader (&pHeader));
+		assert (pHeader);
+		checkResult (pHeader->GetDictionary (&pDict));
+		assert (pDict);
+		CAAFBuiltinDefs defs (pDict);
+		
+		
+		//Look up the Type def based on Unsigned 8-bit integer
+		IAAFTypeDefSP spTypeDef;
+		checkResult(pDict->LookupTypeDef (TEST_UID, &spTypeDef));
+		
+		//Look up a Class based on a "sequence" or any other object for that matter
+		IAAFClassDefSP spSomeClass;
+		checkResult(pDict->LookupClassDef (AUID_AAFSequence, &spSomeClass));
+		
+		//OK, lets register (and get) a property def, give it a name + typedef, and
+		//		get back a property def
+		IAAFPropertyDefSP spPropDef;
+		checkResult(spSomeClass->RegisterOptionalPropertyDef (propDef_id,
+			TEST_NAME,
+			spTypeDef,
+			&spPropDef));
+		
+		
+		//SetDescription
+		checkResult(spPropDef->SetDescription(TEST_DESCRIPTION));
+		
+		//SetDefaultValue
+		//	//first, create a value for integer
+		IAAFTypeDefIntSP  spTypeDefInt;
+		checkResult(spTypeDef->QueryInterface(IID_IAAFTypeDefInt, (void **) &spTypeDefInt));
+		IAAFPropertyValueSP  spPropVal;
+		checkResult(spTypeDefInt->CreateValue((aafMemPtr_t)&TEST_INT_BYTE, 1, &spPropVal));
+		//	//now, set the default value
+		hr = spPropDef->SetDefaultValue(spPropVal);
+		if (FAILED(hr)) 
+		{
+			if (hr == AAFRESULT_NOT_IMPLEMENTED)
+			{
+				//If not implemented, then mark as NI and move on ...
+				//Flag this NI
+				bSetDefaultValue_NI = kAAFTrue;
+				//Reset the hr
+				hr = AAFRESULT_SUCCESS;
+				//Move on
+			}
+			else
+				//If it failed, but was not an NI, then "what the hell is this problem ??!!"
+				throw hr;
+		}//if Failed hr
+		
+		//SetIsSearchable
+		hr = spPropDef->SetIsSearchable(kAAFTrue);
+		if (FAILED(hr)) 
+		{
+			if (hr == AAFRESULT_NOT_IMPLEMENTED)
+			{
+				//If not implemented, then mark as NI and move on ...
+				//Flag this NI
+				bSetIsSearchable_NI = kAAFTrue;
+				//Reset the hr
+				hr = AAFRESULT_SUCCESS;
+				//Move on
+			}
+			else
+				//If it failed, but was not an NI, then "what the hell is this problem ??!!"
+				throw hr;
+		}//if Failed hr
+		
+		
+	}
+	catch (HRESULT & rResult)
+	{
+		hr = rResult;
+	}
+	
+	if (pDict) pDict->Release();
+	if (pHeader) pHeader->Release();
+	if (pFile)
+	{
+		AAFRESULT temphr = pFile->Save();
+		if (! SUCCEEDED (temphr)) return temphr;
+		temphr = pFile->Close();
+		if (! SUCCEEDED (temphr)) return temphr;
+		pFile->Release();
+	}
+	
+	return hr;
+}
 
 
+static HRESULT  ReadAAFFile(aafWChar *  pFileName )
+{
+	HRESULT hr = AAFRESULT_SUCCESS;
+	
+	IAAFFile* pFile = NULL;
+	IAAFHeader * pHeader = NULL;
+	IAAFDictionary * pDict = NULL;
+	aafBoolean_t  bFileOpen = kAAFFalse;
+	
+	IUnknownSP    pUnk1, pUnk2;		
+	
+	try
+	{
+		
+		// Open the file
+		checkResult(AAFFileOpenExistingRead(pFileName, 0, &pFile));
+		bFileOpen = kAAFTrue;
+		
+		checkResult (pFile->GetHeader (&pHeader));
+		assert (pHeader);
+		checkResult (pHeader->GetDictionary (&pDict));
+		assert (pDict);
+		CAAFBuiltinDefs defs (pDict);
+		
+		//Now, get our (extended) Sequence class
+		IAAFClassDefSP spSomeClass;
+		checkResult(pDict->LookupClassDef (AUID_AAFSequence, &spSomeClass));
+		
+		IAAFPropertyDefSP spPropDef;
+		checkResult(spSomeClass->LookupPropertyDef(propDef_id, &spPropDef));
+		
+		//Now we have a prop def ... so deal with the Methods ...
 
+		aafUInt32 bufSize = 0;
+		
+		//GetName	
+		//GetNameBufLen
+		checkResult( spPropDef->GetNameBufLen  (&bufSize) );
+		aafCharacter *nameBuf = new aafCharacter[bufSize];
+		checkResult( spPropDef->GetName(nameBuf, bufSize) );
+		checkExpression( wcscmp(TEST_NAME, nameBuf) == 0, AAFRESULT_TEST_FAILED );
+		delete [] nameBuf;
+		
+		//GetDescription	
+		//GetDescriptionBufLen
+		checkResult( spPropDef->GetDescriptionBufLen  (&bufSize) );
+		aafCharacter *descBuf = new aafCharacter[bufSize];
+		checkResult( spPropDef->GetDescription(descBuf, bufSize) );
+		checkExpression( wcscmp(TEST_DESCRIPTION, descBuf) == 0, AAFRESULT_TEST_FAILED );
+		delete [] descBuf;
+		
+		
+		//GetIsOptional
+		aafBoolean_t bIsOptional = kAAFFalse;
+		checkResult( spPropDef->GetIsOptional(&bIsOptional) );
+		checkExpression( bIsOptional == kAAFTrue, AAFRESULT_TEST_FAILED );
+		
+		//GetTypeDef
+		IAAFTypeDefSP  spTypeDef;
+		checkResult( spPropDef->GetTypeDef(&spTypeDef) );
 
+		//check AUID
+		IAAFDefObjectSP  spDefObj;
+		aafUID_t checkUID = {0};
+		checkResult(spTypeDef->QueryInterface(IID_IAAFDefObject, (void**)&spDefObj));
+		checkResult(spDefObj->GetAUID (&checkUID));
+		checkExpression(memcmp(&checkUID, &TEST_UID, sizeof(checkUID)) == 0, AAFRESULT_TEST_FAILED);
+		
+		//check Category of the typedef - WE HAPPEN TO KNOW THAT ITS AN "INTEGER" ... 
+		//			.....  REMOVE if we ever change the type to something other than INTEGER
+		eAAFTypeCategory_t typeCat = kAAFTypeCatUnknown;
+		checkResult(spTypeDef->GetTypeCategory(&typeCat));
+		checkExpression( typeCat == kAAFTypeCatInt, AAFRESULT_TEST_FAILED );
+		
+		//NOt implemented yet ... GetDefaultValue() and GetIsSearchable()
+		
+		//GetDefaultValue
+		IAAFPropertyValueSP  spPropVal;
+		IAAFTypeDefSP  spTypeDef2;
+		hr = spPropDef->GetDefaultValue(&spPropVal);
+		if (FAILED(hr)) 
+		{
+			if (hr == AAFRESULT_NOT_IMPLEMENTED)
+			{
+				//If not implemented, then mark as NI and move on ...
+				//Flag this NI
+				bGetDefaultValue_NI = kAAFTrue;
+				//Reset the hr
+				hr = AAFRESULT_SUCCESS;
+				//Move on
+				goto _skip_GetDefaultValue;
+			}
+			else
+				//If it failed, but was not an NI, then "what the hell is this problem ??!!"
+				throw hr;
+		}//if Failed hr
+		checkResult(spPropVal->GetType(&spTypeDef2));
+		//Compare the two TypeDef's ...
+		checkResult(spTypeDef->QueryInterface(IID_IUnknown, (void **)&pUnk1));
+		checkResult(spTypeDef2->QueryInterface(IID_IUnknown, (void **)&pUnk2));
+		checkExpression( pUnk1 == pUnk2, AAFRESULT_TEST_FAILED );
+_skip_GetDefaultValue:
+		
+		//GetIsSearchable
+		aafBoolean_t bIsSearchable = kAAFFalse;
+		hr = spPropDef->GetIsSearchable(&bIsSearchable);
+		if (FAILED(hr)) 
+		{
+			if (hr == AAFRESULT_NOT_IMPLEMENTED)
+			{
+				//If not implemented, then mark as NI and move on ...
+				//Flag this NI
+				bGetIsSearchable_NI = kAAFTrue;
+				//Reset the hr
+				hr = AAFRESULT_SUCCESS;
+				//Move on
+				goto _skip_GetIsSearchable;
+			}
+			else
+				//If it failed, but was not an NI, then "what the hell is this problem ??!!"
+				throw hr;
+		}//if Failed hr
+		checkExpression( bIsSearchable == kAAFTrue, AAFRESULT_TEST_FAILED );
+_skip_GetIsSearchable:
+		;
+		
+		
+	}//try
+	catch (HRESULT & rResult)
+	{
+		hr = rResult;
+	}
+	
+	if (pDict) pDict->Release();
+	if (pHeader) pHeader->Release();
+	
+	if (bFileOpen && pFile)
+	{
+		AAFRESULT temphr = pFile->Close();
+		if (FAILED(temphr)) 
+			return temphr;
+		pFile->Release();
+	}
+	
+	return hr;
+	
+}
 
+extern "C" HRESULT CAAFPropertyDef_test()
+{
+	HRESULT hr = AAFRESULT_SUCCESS;
+	aafWChar * pFileName = L"AAFPropertyDefTest.aaf";
+	
+	try
+	{
+		hr = CreateAAFFile(	pFileName );
+		if(hr == AAFRESULT_SUCCESS)
+			hr = ReadAAFFile( pFileName );
+		
+		//Check for partial success
+		if (bSetDefaultValue_NI || bSetIsSearchable_NI || bGetDefaultValue_NI || bGetIsSearchable_NI )
+		{
+			//if any one of these are set, we have only partial success ... damn! ...
+			hr = AAFRESULT_TEST_PARTIAL_SUCCESS;
+			
+			//print out the appropriate NI message(s) ...
+			if (bSetDefaultValue_NI)
+				cerr << "\t  SetDefaultValue  method is Not Implemented" << endl;
+			if (bSetIsSearchable_NI)
+				cerr << "\t  SetIsSearchable  method is Not Implemented" << endl;
+			if (bGetDefaultValue_NI)
+				cerr << "\t  GetDefaultValue  method is Not Implemented" << endl;
+			if (bGetIsSearchable_NI)
+				cerr << "\t  GetIsSearchable  method is Not Implemented" << endl;
+			
+		}//if partial success stuff
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	}//try
+	catch (...)
+	{
+		cerr << "CAAFPropertyDef_test...Caught general C++"
+			" exception!" << endl; 
+		hr = AAFRESULT_TEST_FAILED;
+	}
+	
+	
+	return hr;
+}
+	
--- test/com/ComModTestAAF/ModuleTests/CAAFPropertyTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFPropertyTest.cpp
@@ -1,186 +1,317 @@
 // @doc INTERNAL
 // @com This file implements the module test for CAAFProperty
 /***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+*
+*              Copyright (c) 1998-1999 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying 
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted, 
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+* prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
 
 #include "AAF.h"
-
 #include "AAFResult.h"
 #include "AAFStoredObjectIDs.h"
+#include "AAFDataDefs.h"
 #include "AAFDefUIDs.h"
-
-#include "CAAFBuiltinDefs.h"
 
 #include <iostream.h>
 #include <stdio.h>
 #include <assert.h>
 
+#include "CAAFBuiltinDefs.h"
 
+//Define some smart pointers
+typedef IAAFSmartPointer<IAAFTypeDef>				IAAFTypeDefSP;
+typedef IAAFSmartPointer<IAAFDefObject>				IAAFDefObjectSP;
+typedef IAAFSmartPointer<IAAFTypeDefString>			IAAFTypeDefStringSP;
 
-// {669349B1-EBC3-11d2-842C-00600832ACB8}
-static aafUID_t PropID_Local1 = 
-{ 0x669349b1, 0xebc3, 0x11d2, { 0x84, 0x2c, 0x0, 0x60, 0x8, 0x32, 0xac, 0xb8 } };
-
-// {669349B2-EBC3-11d2-842C-00600832ACB8}
-static aafUID_t TypeID_Local1 = 
-{ 0x669349b2, 0xebc3, 0x11d2, { 0x84, 0x2c, 0x0, 0x60, 0x8, 0x32, 0xac, 0xb8 } };
-
-
+#define TEST_NAME   L"Test: Some Name"
 
 // convenient error handlers.
 inline void checkResult(HRESULT r)
 {
-  if (FAILED(r))
-    throw r;
+	if (FAILED(r))
+		throw r;
 }
 inline void checkExpression(bool expression, HRESULT r)
 {
-  if (!expression)
-    throw r;
+	if (!expression)
+		throw r;
 }
-
-static wchar_t testFileName[] = L"AAFPropertyTest.aaf";
-
 
 // Cross-platform utility to delete a file.
 static void RemoveTestFile(const wchar_t* pFileName)
 {
-  const size_t kMaxFileName = 512;
-  char cFileName[kMaxFileName];
-
-  size_t status = wcstombs(cFileName, pFileName, kMaxFileName);
-  if (status != (size_t)-1)
-  { // delete the file.
-    remove(cFileName);
-  }
+	const size_t kMaxFileName = 512;
+	char cFileName[kMaxFileName];
+	
+	size_t status = wcstombs(cFileName, pFileName, kMaxFileName);
+	if (status != (size_t)-1)
+	{ // delete the file.
+		remove(cFileName);
+	}
 }
 
 
-// not ready for prime time
-#if 0
-
-static HRESULT ObjectTest ()
+static HRESULT CreateAAFFile(aafWChar *  pFileName )
 {
-  HRESULT hr = AAFRESULT_SUCCESS;
-
-  aafProductIdentification_t ProductInfo;
-  IAAFFile* pFile = NULL;
-  IAAFHeader * pHeader = NULL;
-  IAAFDictionary * pDict = NULL;
-  IAAFPropertyDef * pPropDef = NULL;
-  IAAFPropertyValue * pPropVal = NULL;
-  IAAFProperty * pProp = NULL;
-
-  try
+	HRESULT hr = AAFRESULT_SUCCESS;
+	
+	aafProductIdentification_t ProductInfo;
+	IAAFFile* pFile = NULL;
+	IAAFHeader * pHeader = NULL;
+	IAAFDictionary * pDict = NULL;
+	IAAFCompositionMob * pCMob = NULL;
+	IAAFMob * pMob = NULL;
+	IAAFObject * pObj = NULL;
+	
+	try
 	{
-	  ProductInfo.companyName = L"AAF Developers Desk";
-	  ProductInfo.productName = L"AAFProperty Test";
-	  ProductInfo.productVersion.major = 1;
-	  ProductInfo.productVersion.minor = 0;
-	  ProductInfo.productVersion.tertiary = 0;
-	  ProductInfo.productVersion.patchLevel = 0;
-	  ProductInfo.productVersion.type = kAAFVersionUnknown;
-	  ProductInfo.productVersionString = NULL;
-	  ProductInfo.productID = -1;
-	  ProductInfo.platform = NULL;
-
-	  RemoveTestFile (testFileName);
-	  checkResult (AAFFileOpenNewModify(testFileName, 0, &ProductInfo, &pFile));
-	  assert (pFile);
-	  checkResult (pFile->GetHeader (&pHeader));
-	  assert (pHeader);
-	  checkResult (pHeader->GetDictionary (&pDict));
-	  assert (pDict);
-	  CAAFBuiltinDefs defs (pDict);
-
-	  checkResult (pDict->CreateInstance (AUID_IAAFTypeDefInt,
-										  IID_IAAFTypeDefInt,
-										  (IUnknown **) &pTypeDefInt));
-	  assert (pTypeDefInt);
-	  checkResult (pTypeDefInt->Initialize (&TypeID_Local1,
-											1,
-											kAAFTrue,
-											L"Fake local 1-byte signed int type");
-	  
-	  checkResult (pDict->CreateInstance (defs.cdPropertyDef(),
-										  IID_IAAFPropertyDef,
-										  (IUnknown **) &pPropDef));
-	  assert (pPropDef);
-	  checkResult (pPropDef->Initialize (&PropID_Local1,
-										 pTypeDefInt,
-										 L"Fake local property def");										 
-	  checkResult (pDict->CreateInstance (defs.cdProperty(),
-										  IID_IAAFProperty,
-										  (IUnknown **) &pProp));
-	  assert (pProp);
+		ProductInfo.companyName = L"AAF Developers Desk";
+		ProductInfo.productName = L"AAFProperty Test";
+		ProductInfo.productVersion.major = 1;
+		ProductInfo.productVersion.minor = 0;
+		ProductInfo.productVersion.tertiary = 0;
+		ProductInfo.productVersion.patchLevel = 0;
+		ProductInfo.productVersion.type = kAAFVersionUnknown;
+		ProductInfo.productVersionString = NULL;
+		ProductInfo.productID = UnitTestProductID;
+		ProductInfo.platform = NULL;
+		
+		//Do the usual ...
+		RemoveTestFile (pFileName);
+		checkResult (AAFFileOpenNewModify(pFileName, 0, &ProductInfo, &pFile));
+		assert (pFile);
+		checkResult (pFile->GetHeader (&pHeader));
+		assert (pHeader);
+		checkResult (pHeader->GetDictionary (&pDict));
+		assert (pDict);
+		CAAFBuiltinDefs defs (pDict);
+		
+		//Create a composition ...
+		checkResult (defs.cdCompositionMob()->
+			CreateInstance (IID_IAAFCompositionMob,
+			(IUnknown **) &pCMob));
+		assert (pCMob);
+		checkResult (pCMob->Initialize (TEST_NAME));
+		
+		//... Get its mob, and add it to the header info
+		checkResult (pCMob->QueryInterface (IID_IAAFMob,
+										  (void **) &pMob));
+		assert (pMob);
+		checkResult (pHeader->AddMob (pMob));
+		
 	}
-  catch (HRESULT & rResult)
+	catch (HRESULT & rResult)
 	{
-	  hr = rResult;
+		hr = rResult;
 	}
-
-  if (pProp) pProp->Release ();
-  if (pPropVal) pPropVal->Release ();
-  if (pPropDef) pPropVal->Release ();
-  if (pDict) pDict->Release ();
-  if (pHeader) pHeader->Release ();
-  if (pFile)
+	
+	if (pCMob) pCMob->Release();
+	if (pMob) pCMob->Release();
+	if (pObj) pObj->Release();
+	if (pDict) pDict->Release();
+	if (pHeader) pHeader->Release();
+	if (pFile)
 	{
-	  AAFRESULT temphr = pFile->Close();
-	  if (! SUCCEEDED (temphr)) return temphr;
-	  pFile->Release();
+		AAFRESULT temphr = pFile->Save();
+		if (! SUCCEEDED (temphr)) return temphr;
+		temphr = pFile->Close();
+		if (! SUCCEEDED (temphr)) return temphr;
+		pFile->Release();
 	}
-  
-  return hr;
+	
+	return hr;
 }
-#endif // 0
+
+
+static HRESULT  ReadAAFFile(aafWChar *  pFileName )
+{
+	HRESULT hr = AAFRESULT_SUCCESS;
+	
+	IAAFFile *					pFile = NULL;
+	IAAFHeader *				pHeader = NULL;
+	IEnumAAFMobs*				pMobIter = NULL;
+	IAAFCompositionMob *		pCMob = NULL;
+	IAAFMob*					pMob = NULL;
+	bool bFileOpen = false;
+	aafNumSlots_t				numMobs;
+	IAAFObject*					pObj = NULL;
+	
+	IEnumAAFProperties * pEnum = NULL;
+	IAAFProperty *pProp = NULL;
+	
+	IAAFPropertyDef* pPropDef = NULL;
+	IAAFPropertyValue* pPropVal = NULL;
+
+	IAAFTypeDef*	pTypeDef  = NULL;
+
+	try
+	{
+		// Open the file
+		checkResult(AAFFileOpenExistingRead(pFileName, 0, &pFile));
+		bFileOpen = true;
+		
+		// We can't really do anthing in AAF without the header.
+		checkResult(pFile->GetHeader(&pHeader));
+		
+		// Validate that there is only one composition mob.
+		checkResult(pHeader->CountMobs(kAAFCompMob, &numMobs));
+		checkExpression(1 == numMobs, AAFRESULT_TEST_FAILED);
+		
+		// Enumerate over all Composition Mobs
+		aafSearchCrit_t				criteria;
+		criteria.searchTag = kAAFByMobKind;
+		criteria.tags.mobKind = kAAFCompMob;
+		checkResult(pHeader->GetMobs(&criteria, &pMobIter));
+		while (AAFRESULT_SUCCESS == pMobIter->NextOne(&pMob))
+		{		
+			//Get the Composition mob
+			checkResult (pMob->QueryInterface (IID_IAAFCompositionMob,
+				(void **) &pCMob));
+			
+			//Get the Object
+			checkResult (pCMob->QueryInterface (IID_IAAFObject,
+				(void **) &pObj));
+			
+			//From the Object, we can get the properties ....
+			aafUInt32 propCount = 0;
+			checkResult (pObj->CountProperties (&propCount));
+			//A composition should have at least 6 properties ...
+			checkExpression(propCount>=6, AAFRESULT_TEST_FAILED);
+			
+			//Get Enumeration over properties
+			checkResult (pObj->GetProperties (&pEnum));
+			checkExpression (pEnum != 0, AAFRESULT_TEST_FAILED);
+			while (propCount--)
+			{
+				//Check for property validity
+				checkResult (pEnum->NextOne (&pProp));
+				checkExpression (pProp != NULL, AAFRESULT_TEST_FAILED);
+				//Now call prop methods
+				
+				//1.  Get Definition
+				checkResult( pProp->GetDefinition(&pPropDef) );
+				//2. Get Value
+				checkResult( pProp->GetValue(&pPropVal) );
+
+				//deal with prop value
+				checkResult( pPropVal->GetType(&pTypeDef) );
+
+				//From propval, get the typedef!!!!!!!!!!!!!!!!!!!!!
+				IAAFTypeDefSP spTypeDef;
+				checkResult( pPropVal->GetType(&spTypeDef) );
+
+				//get category
+				eAAFTypeCategory_t typeCat = kAAFTypeCatUnknown;
+				checkResult (spTypeDef->GetTypeCategory(&typeCat));
+				//make sure it is not unknown
+				checkExpression(kAAFTypeCatUnknown != typeCat, AAFRESULT_TEST_FAILED);
+				
+				//Deal with our Name  String .... 
+				if (typeCat == kAAFTypeCatString)
+				{
+					IAAFTypeDefStringSP  spTypeDefString;
+					checkResult( spTypeDef->QueryInterface(IID_IAAFTypeDefString, 
+												(void**)&spTypeDefString) );
+
+					aafUInt32 bufSize = 
+						//don't forget the NULL char, and factoring in the size of a wchar
+						   (wcslen(TEST_NAME)+1) * sizeof(aafCharacter); 
+
+					aafCharacter *nameBuf = new aafCharacter[bufSize];
+					checkResult( spTypeDefString->GetElements(pPropVal, (aafMemPtr_t)nameBuf, bufSize) );
+					checkExpression( wcscmp(TEST_NAME, nameBuf) == 0, AAFRESULT_TEST_FAILED );
+					delete [] nameBuf;
+				}
+
+				//Done ...........
+
+				//release typedef
+				pTypeDef->Release();
+
+				//release defintion + value
+				pPropDef->Release();
+				pPropVal->Release();
+				
+				//release this prop
+				pProp->Release();
+				pProp = NULL;
+
+			}//while prop count
+		}//while mob iter
+	}//try
+	
+	catch (HRESULT & rResult)
+	{
+		hr = rResult;
+	}
+	
+	//Release the rest ...
+	if (pEnum) 
+		pEnum->Release();
+	
+	if (pObj) 
+		pObj->Release();
+	if (pCMob) 
+		pCMob->Release();
+	if (pMob)
+		pMob->Release();
+	if (pMobIter)
+		pMobIter->Release();
+	if (pHeader)
+		pHeader->Release();
+	
+	if (pFile) 
+	{
+		if (bFileOpen)
+			pFile->Close();
+		pFile->Release();
+	}
+	
+	return hr;
+	
+}
 
 extern "C" HRESULT CAAFProperty_test()
 {
-  HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
+	HRESULT hr = AAFRESULT_SUCCESS;
+	aafWChar * pFileName = L"AAFPropertyTest.aaf";
+	
+	try
+	{
+		hr = CreateAAFFile(	pFileName );
+		if(hr == AAFRESULT_SUCCESS)
+			hr = ReadAAFFile( pFileName );
 
-  try
-    {
-	  // hr = PropertyTest ();
-
-      // if (FAILED(hr))
-      // {
-      //  cerr << "CAAFProperty_test...FAILED!";
-      //  return hr;
-      // }
-
-      return AAFRESULT_NOT_IMPLEMENTED;
-
-    }
-  catch (...)
-    {
-      cerr << "CAAFProperty_test...Caught general C++"
-        " exception!" << endl; 
-    }
-
-  return hr;
+	}
+	catch (...)
+	{
+		cerr << "CAAFProperty_test...Caught general C++"
+			" exception!" << endl; 
+		hr = AAFRESULT_TEST_FAILED;
+	}
+	
+	
+	return hr;
 }
+
