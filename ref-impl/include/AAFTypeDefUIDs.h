4c5cafaabd331a18f2ad0891d96cb25f3a06a1ec
tbingham <>
2001-04-17 07:29:24 +0000

    Added TypeDefIndirect: enabled for ConstantValue, ControlPoint and TaggedValue. The AAFTypeDefIndirect module test is incomplete...

--- ref-impl/include/AAFTypeDefUIDs.h
+++ ref-impl/include/AAFTypeDefUIDs.h
@@ -53,6 +53,8 @@
   const aafUID_t TYPE_GUID_NAME(name) = id;
 #define AAF_TYPE_DEFINITION_STRING(name, id, type) \
   const aafUID_t TYPE_GUID_NAME(name) = id;
+#define AAF_TYPE_DEFINITION_INDIRECT(name, id) \
+  const aafUID_t TYPE_GUID_NAME(name) = id;
 #define AAF_TYPE_DEFINITION_EXTENDIBLE_ENUMERATION(name, id) \
   const aafUID_t TYPE_GUID_NAME(name) = id;
 #define AAF_TYPE_DEFINITION_CHARACTER(name, id, size) \
