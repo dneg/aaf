13ff8385599dfb2455f3a3d71acec77ed2c66a53
tbingham <>
2001-04-17 05:54:46 +0000

    Newly implemented class.

---
+++ test/com/ComModTestAAF/ModuleTests/CEnumAAFClassDefsTest.cpp
@@ -0,0 +1,33 @@
+// @doc INTERNAL
+// @com This file implements the module test for CEnumAAFClassDefs
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
+
+#include "AAFTypes.h" //Use #include "AAF.h" for functional module test.
+#include "AAFResult.h"
+
+// Required function prototype.
+extern "C" HRESULT CEnumAAFClassDefs_test(void);
+
+HRESULT CEnumAAFClassDefs_test()
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+
+
+
+
+
+
+
+
+
+
