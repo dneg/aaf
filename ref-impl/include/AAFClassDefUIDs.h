0966e30976bb5c0716f328e6089d7492cdd356f0
tbingham <>
2001-04-17 04:03:44 +0000

    Well-known class definition UIDs.

---
+++ ref-impl/include/AAFClassDefUIDs.h
@@ -0,0 +1,21 @@
+#ifndef __AAFClassDefUIDs_h__
+#define __AAFClassDefUIDs_h__
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
+
+//
+// GUIDs for built-in class definitions
+//
+
+// {3D7BE941-022E-11d3-842D-00600832ACB8}
+const aafUID_t kAAFClassID_Object = 
+{ 0x3d7be941, 0x22e, 0x11d3, { 0x84, 0x2d, 0x0, 0x60, 0x8, 0x32, 0xac, 0xb8 } };
+
+
+#endif // ! __AAFClassDefUIDs_h__
