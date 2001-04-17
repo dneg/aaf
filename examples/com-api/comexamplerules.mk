51895a11ee2b4ecae308aef59f32461353f768b7
tbingham <>
2001-04-17 11:55:48 +0000

    Use new platform/compiler definitions from cdefs-* and pdefs-* files.

--- examples/com-api/comexamplerules.mk
+++ examples/com-api/comexamplerules.mk
@@ -1,26 +1,33 @@
-
 PACKAGE = Examples
 
 AAFBASE ?= ../../..
 
-include $(AAFBASE)/build/defs.mk
 
+# Common definitions
+include $(AAFBASE)/build/common.mk
+
+
+# Include directories
 INCLUDES = -I../../../ref-impl/include \
 	   -I../../../ref-impl/include/ref-api \
 	   -I../../../ref-impl/include/com-api \
 	   -I../../../Utilities/Include
 
-.PHONY : all
-all : $(BINDIR)/$(EXAMPLE)$(EXE_SUFFIX)
 
-$(BINDIR)/$(EXAMPLE)$(EXE_SUFFIX) : $(BINDIR) $(OBJDIR) $(CXXOBJS)
-	$(CC) -n32 -Wl,-wall $(CXXOBJS) $(CC2LDOPT) \
-	$(LD_DYN_LIB_PATH_FLAG) $(CC2LDOPT) $(LIBDIR) \
-	-L$(LIBDIR) $(CC2LDOPT) -laaflib -laafiid -o $@ -lC -laafutils
+.PHONY : all
+all : $(BINDIR)/$(EXAMPLE)$(EXE)
+
+
+$(BINDIR)/$(EXAMPLE)$(EXE) : $(BINDIR) $(OBJDIR) $(CXXOBJS)
+	$(LD) $(CXXOBJS) $(RPATH_OPT) \
+	-L$(LIBDIR) -laaflib -laafiid -o $@
+
 
 .PHONY : clean
 clean :
-	@rm -f $(CXXOBJS)
-	@rm -f $(BINDIR)/$(EXAMPLE)$(EXE_SUFFIX)
+	@$(RM) $(CXXOBJS)
+	@$(RM) $(BINDIR)/$(EXAMPLE)$(EXE)
+
 
 include $(AAFBASE)/build/rules.mk
+
