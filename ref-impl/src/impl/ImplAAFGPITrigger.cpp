0f42c445366bdcccd2de78398f3894e1b1f1e0da
tbingham <>
2001-04-17 04:24:24 +0000

    First version with OM code integrated but with no corresponding module test.

--- ref-impl/src/impl/ImplAAFGPITrigger.cpp
+++ ref-impl/src/impl/ImplAAFGPITrigger.cpp
@@ -1,30 +1,27 @@
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
-
-
-
-
-
-
-
-#include "AAFStoredObjectIDs.h"
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
 
 #ifndef __ImplAAFGPITrigger_h__
 #include "ImplAAFGPITrigger.h"
 #endif
 
-#include <assert.h>
-#include <string.h>
+
+#include "AAFStoredObjectIDs.h"
+#include "AAFPropertyIDs.h"
+#include <AAFResult.h>
 
 
-ImplAAFGPITrigger::ImplAAFGPITrigger ()
-{}
+ImplAAFGPITrigger::ImplAAFGPITrigger () :
+  _activeState(PID_GPITrigger_ActiveTrigger, "ActiveState")
+{
+  _persistentProperties.put(_activeState.address());
+}
 
 
 ImplAAFGPITrigger::~ImplAAFGPITrigger ()
@@ -33,18 +30,23 @@ ImplAAFGPITrigger::~ImplAAFGPITrigger ()
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFGPITrigger::GetActiveState (
-      aafBool *  /*pActiveState*/)
+      aafBool *pActiveState)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+  if (NULL == pActiveState)
+    return AAFRESULT_NULL_PARAM;
+
+  *pActiveState = _activeState;
+  return AAFRESULT_SUCCESS;
 }
 
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFGPITrigger::SetActiveState (
-      aafBool  /*ActiveState*/)
+      aafBool  ActiveState)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+  _activeState = ActiveState;
+  return AAFRESULT_SUCCESS;
 }
 
 
