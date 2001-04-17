90941bdba7d581e1fc7829a3ef52749d194c65f7
tbingham <>
2001-04-17 07:44:34 +0000

    Check in impl and test for AAFTypeDefVariableArray, with shorter names.

---
+++ ref-impl/src/impl/ImplAAFTypeDefVariableArray.h
@@ -0,0 +1,191 @@
+//@doc
+//@class    AAFTypeDefVariableArray | Implementation class for AAFTypeDefVariableArray
+#ifndef __ImplAAFTypeDefVariableArray_h__
+#define __ImplAAFTypeDefVariableArray_h__
+
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+
+
+
+
+#ifndef __ImplAAFTypeDef_h__
+#include "ImplAAFTypeDef.h"
+#endif
+
+
+class ImplAAFTypeDefVariableArray : public ImplAAFTypeDef
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplAAFTypeDefVariableArray ();
+
+protected:
+  virtual ~ImplAAFTypeDefVariableArray ();
+
+public:
+
+  //****************
+  // Initialize()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Initialize
+        (// @parm [in] auid to be used to identify this type
+         aafUID_t *  pID,
+
+         // @parm [in] type of each element to be contained in this array
+         ImplAAFTypeDef * pTypeDef,
+
+         // @parm [in] friendly name of this type definition
+         wchar_t *  pTypeName);
+
+
+  //****************
+  // GetType()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetType
+        // @parm [out] type of elements in this array
+        (ImplAAFTypeDef ** ppTypeDef);
+
+
+  //****************
+  // GetCount()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetCount
+        (// @parm [in] property value of array
+         ImplAAFPropertyValue * pPropVal,
+
+         // @parm [out] count of elements in the specified array property value
+         aafUInt32 *  pCount);
+
+
+  //****************
+  // AppendElement()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    AppendElement
+        (// @parm [in] property value corresponding to array to which element is appended
+         ImplAAFPropertyValue * pInPropVal,
+
+         // @parm [in] value to be appended to this array
+         ImplAAFPropertyValue * pMemberPropVal);
+
+
+  //****************
+  // CreateValueFromValues()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    CreateValueFromValues
+        (// @parm [in, size_is(numElements)] array of property values for elements of array value which
+    // is to be created.
+         ImplAAFPropertyValue * pElementValues,
+
+         // @parm [in] size of pElementValues array.
+         aafUInt32  numElements,
+
+         // @parm [out] newly-created property value
+         ImplAAFPropertyValue ** ppPropVal);
+
+
+  //****************
+  // CreateValueFromCArray()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    CreateValueFromCArray
+        (// @parm [in, size_is(initDataSize)] pointer to compile-time C array containing data to use
+         aafMemPtr_t  pInitData,
+
+         // @parm [in] size of data in pInitData, in bytes
+         aafUInt32  initDataSize,
+
+         // @parm [out] newly created property value
+         ImplAAFPropertyValue ** ppPropVal);
+
+
+  //****************
+  // GetElementValue()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetElementValue
+        (// @parm [in] property value to read
+         ImplAAFPropertyValue * pInPropVal,
+
+         // @parm [in] zero-based index into elements in this array type
+         aafUInt32  index,
+
+         // @parm [out] value that is read
+         ImplAAFPropertyValue ** ppOutPropVal);
+
+
+  //****************
+  // GetCArray()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetCArray
+        (// @parm [in] property value to read
+         ImplAAFPropertyValue * pPropVal,
+
+         // @parm [out, size_is(dataSize)] buffer into which C array data should be written
+         aafMemPtr_t  pData,
+
+         // @parm [in] size of pData buffer in bytes
+         aafUInt32  dataSize);
+
+
+  //****************
+  // SetElementValue()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetElementValue
+        (// @parm [in] property value to write
+         ImplAAFPropertyValue * pPropVal,
+
+         // @parm [in] zero-based index into members in this array type
+         aafUInt32  index,
+
+         // @parm [in] value to be placed into this array
+         ImplAAFPropertyValue * pMemberPropVal);
+
+
+  //****************
+  // SetCArray()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetCArray
+        (// @parm [in] property value to write
+         ImplAAFPropertyValue * pPropVal,
+
+         // @parm [in, size_is(dataSize)] buffer from which C array data should be read
+         aafMemPtr_t  pData,
+
+         // @parm [in] size of pData buffer in bytes
+         aafUInt32  dataSize);
+
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFTypeDefVariableArray)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFTypeDefVariableArrayTest.cpp.
+  static AAFRESULT test();
+};
+
+#endif // ! __ImplAAFTypeDefVariableArray_h__
+
+
