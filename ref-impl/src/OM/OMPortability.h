221d3a0469db167915631858e02eca5e05f8886c
tbingham <>
2001-04-17 07:43:15 +0000

    Remove (unused) contents - this file will be repurposed later.

--- ref-impl/src/OM/OMPortability.h
+++ ref-impl/src/OM/OMPortability.h
@@ -29,19 +29,7 @@
 #ifndef OMPORTABILITY_H
 #define OMPORTABILITY_H
 
-#if defined (_MSC_VER)      // Microsoft Visual C++
-
-#if _MSC_VER <= 1000
-// <= 1000 means earlier than version 5.0
-
-#define typename class
-typedef int bool;
-const bool false = 0;
-const bool true = 1;
-
-#endif
-
-#endif
+// Contents TBS
 
 #endif
 
