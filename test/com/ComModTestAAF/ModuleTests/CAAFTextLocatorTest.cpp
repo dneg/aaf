09d95faa03576e5d110dc21a44b8b1bf53448c7c
tbingham <>
2001-04-17 05:49:30 +0000

    Initial implementation, based on NetworkLocator test.

---
+++ test/com/ComModTestAAF/ModuleTests/CAAFTextLocatorTest.cpp
@@ -0,0 +1,353 @@
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+// @doc INTERNAL
+// @com This file implements the module test for CAAFTextLocator interface
+/************************************************\
+*                                                *
+* Advanced Authoring Format                      * 
+*                                                *
+* Copyright (c) 1998-1999 Avid Technology, Inc.  *
+* Copyright (c) 1998-19999 Microsoft Corporation * 
+*                                                *
+\************************************************/
+
+#ifndef __CAAFTextLocator_h__
+#include "CAAFTextLocator.h"
+#endif
+
+#include <iostream.h>
+#include <stdio.h>
+
+#include "AAFStoredObjectIDs.h"
+#include "AAFResult.h"
+#include "AAFDefUIDs.h"
+
+#define TEST_NAME	L"This is a text locator"
+
+// Temporarily necessary global declarations.
+extern "C" const CLSID CLSID_AAFTextLocator; // generated
+
+// Cross-platform utility to delete a file.
+static void RemoveTestFile(const wchar_t* pFileName)
+{
+  const size_t kMaxFileName = 512;
+  char cFileName[kMaxFileName];
+
+  size_t status = wcstombs(cFileName, pFileName, kMaxFileName);
+  if (status != (size_t)-1)
+  { // delete the file.
+    remove(cFileName);
+  }
+}
+
+// convenient error handlers.
+inline void checkResult(HRESULT r)
+{
+  if (FAILED(r))
+    throw r;
+}
+inline void checkExpression(bool expression, HRESULT r)
+{
+  if (!expression)
+    throw r;
+}
+
+static HRESULT CreateAAFFile(aafWChar * pFileName)
+{
+	IAAFFile*					pFile = NULL;
+	bool						bFileOpen = false;
+	IAAFHeader*					pHeader = NULL;
+	IAAFDictionary*				 pDictionary = NULL;
+	IAAFLocator*				pLocator = NULL;
+	IAAFTextLocator*			pTextLocator = NULL;
+	aafProductIdentification_t	ProductInfo;
+	IAAFSourceMob				*pSourceMob = NULL;
+	IAAFMob						*pMob = NULL;
+	IAAFEssenceDescriptor		*edesc = NULL;
+	aafUID_t					newUID;
+	aafInt32					numLocators;
+	HRESULT						hr = AAFRESULT_SUCCESS;
+	aafRational_t	audioRate = { 44100, 1 };
+
+
+	ProductInfo.companyName = L"AAF Developers Desk";
+	ProductInfo.productName = L"AAFTextLocator Test";
+	ProductInfo.productVersion.major = 1;
+	ProductInfo.productVersion.minor = 0;
+	ProductInfo.productVersion.tertiary = 0;
+	ProductInfo.productVersion.patchLevel = 0;
+	ProductInfo.productVersion.type = kVersionUnknown;
+	ProductInfo.productVersionString = NULL;
+	ProductInfo.productID = -1;
+	ProductInfo.platform = NULL;
+
+
+	try
+	{
+		// Remove the previous test file if any.
+		RemoveTestFile(pFileName);
+
+
+		// Create the file.
+		checkResult(AAFFileOpenNewModify(pFileName, 0, &ProductInfo, &pFile));
+		bFileOpen = true;
+ 
+		// We can't really do anthing in AAF without the header.
+		checkResult(pFile->GetHeader(&pHeader));
+
+		// Get the AAF Dictionary so that we can create valid AAF objects.
+		checkResult(pHeader->GetDictionary(&pDictionary));
+ 		
+		//Make the first mob
+		// Create a Mob
+		checkResult(pDictionary->CreateInstance(&AUID_AAFSourceMob,
+								IID_IAAFSourceMob, 
+								(IUnknown **)&pSourceMob));
+
+		checkResult(pSourceMob->QueryInterface (IID_IAAFMob, (void **)&pMob));
+		checkResult(CoCreateGuid((GUID *)&newUID));
+		checkResult(pMob->SetMobID(&newUID));
+		checkResult(pMob->SetName(L"TextLocatorTestSourceMOB"));
+		
+		checkResult(pDictionary->CreateInstance(&AUID_AAFEssenceDescriptor,
+								IID_IAAFEssenceDescriptor, 
+								(IUnknown **)&edesc));
+										
+ 		checkResult(pSourceMob->SetEssenceDescriptor(edesc));
+
+			// Verify that there are no locators
+		checkResult(edesc->GetNumLocators(&numLocators));
+		checkExpression(0 == numLocators, AAFRESULT_TEST_FAILED);
+
+  
+		// Make a locator, and attach it to the EssenceDescriptor
+		checkResult(pDictionary->CreateInstance(&AUID_AAFTextLocator,
+								IID_IAAFTextLocator, 
+								(IUnknown **)&pTextLocator));		
+		checkResult(pTextLocator->QueryInterface (IID_IAAFLocator, (void **)&pLocator));
+
+		checkResult(pTextLocator->SetName (TEST_NAME));
+		
+		checkResult(edesc->AppendLocator(pLocator));
+ 		checkResult(pSourceMob->SetEssenceDescriptor (edesc));
+
+		// Verify that there is now one locator
+		checkResult(edesc->GetNumLocators(&numLocators));
+		checkExpression(1 == numLocators, AAFRESULT_TEST_FAILED);
+
+		// Add the source mob into the tree
+		checkResult(pHeader->AppendMob(pMob));
+
+	}
+	catch (HRESULT& rResult)
+	{
+		hr = rResult;
+	}	
+
+	// cleanup
+	if (pLocator)
+		pLocator->Release();
+
+	if (pTextLocator)
+		pTextLocator->Release();
+
+	if (edesc)
+		edesc->Release();
+
+	if (pMob)
+		pMob->Release();
+
+	if (pSourceMob)
+		pSourceMob->Release();
+
+	if (pDictionary)
+		pDictionary->Release();
+
+	if (pHeader)
+		pHeader->Release();
+
+	if (pFile)
+	{
+		if (bFileOpen)
+		  {
+			pFile->Save();
+			pFile->Close();
+		  }
+		pFile->Release();
+	}
+	// hr = pSession->EndSession();
+ 	// if (AAFRESULT_SUCCESS != hr)
+	//	return hr;
+
+	// if (pSession) pSession->Release();
+
+
+	return hr;
+}
+
+static HRESULT ReadAAFFile(aafWChar * pFileName)
+{
+	// IAAFSession *				pSession = NULL;
+	IAAFFile*				pFile = NULL;
+	IAAFHeader*				pHeader = NULL;
+	IAAFEssenceDescriptor*	pEdesc = NULL;
+	IAAFSourceMob*			pSourceMob = NULL;
+	IEnumAAFLocators*		pEnum = NULL;
+	IAAFLocator*			pLocator = NULL;
+	IAAFTextLocator*		pTextLocator = NULL;
+	IEnumAAFMobs*			pMobIter = NULL;
+	IAAFMob*				pMob = NULL;
+	aafInt32				numLocators, readLen;
+	aafNumSlots_t			numMobs, n;
+
+	HRESULT					hr = AAFRESULT_SUCCESS;
+	aafWChar					readBuf[1024];
+	bool bFileOpen = false;
+
+
+	try
+	{	  
+	    // Open the file.
+		checkResult(AAFFileOpenExistingRead(pFileName, 0, &pFile));
+		bFileOpen = true;
+
+  		checkResult(pFile->GetHeader(&pHeader));
+
+		checkResult(pHeader->GetNumMobs(kAllMob, &numMobs));
+		if (1 != numMobs )
+			checkResult(AAFRESULT_TEST_FAILED);
+
+
+		//!!!	aafSearchCrit_t		criteria;
+		//!!!	criteria.searchTag = kNoSearch;
+
+		checkResult(pHeader->EnumAAFAllMobs (NULL, &pMobIter));
+		for(n = 0; n < numMobs; n++)
+		{
+			aafWChar		name[500];
+			aafUID_t		mobID;
+
+			checkResult(pMobIter->NextOne (&pMob));
+			checkResult(pMob->GetName (name, sizeof(name)));
+			checkResult(pMob->GetMobID (&mobID));
+
+			checkResult(pMob->QueryInterface (IID_IAAFSourceMob, (void **)&pSourceMob));
+			checkResult(pSourceMob->GetEssenceDescriptor (&pEdesc));
+
+			// Verify that there is now one locator
+			checkResult(pEdesc->GetNumLocators(&numLocators));
+			if (1 != numLocators)
+				checkResult(AAFRESULT_TEST_FAILED);
+		
+			checkResult(pEdesc->EnumAAFAllLocators(&pEnum));
+
+			// This should read the one real locator
+			checkResult(pEnum->NextOne(&pLocator));
+			checkResult(pLocator->QueryInterface(IID_IAAFTextLocator, (void **)&pTextLocator));
+			checkResult(pTextLocator->GetNameBufLen (&readLen));
+	//		if(readLen != strlen(TEST_PATH))
+				
+			checkResult(pTextLocator->GetName (readBuf, readLen));
+
+		// This should run off the end
+			pLocator->Release();
+			pLocator = NULL;
+			pTextLocator->Release();
+			pTextLocator = NULL;
+			hr = pEnum->NextOne(&pLocator);
+			if (AAFRESULT_NO_MORE_OBJECTS != hr)
+				checkResult(hr);
+			else
+				hr = AAFRESULT_SUCCESS; // reset result
+		}
+
+
+	}
+	catch (HRESULT& rResult)
+	{
+		hr = rResult;
+	}
+
+	
+	// Cleanup...
+	if (pLocator)
+		pLocator->Release();
+
+	if (pTextLocator)
+		pTextLocator->Release();
+
+	if (pEnum)
+		pEnum->Release();
+
+	if (pEdesc)
+		pEdesc->Release();
+
+	if (pSourceMob)
+		pSourceMob->Release();
+
+	if (pMob)
+		pMob->Release();
+
+	if (pMobIter)
+		pMobIter->Release();
+
+	if (pHeader)
+		pHeader->Release();
+
+	if (pFile)
+	{
+		if (bFileOpen)
+			pFile->Close();
+		pFile->Release();
+	}
+
+	// hr = pSession->EndSession();
+	// if (AAFRESULT_SUCCESS != hr)
+	//	return hr;
+
+	// if (pSession) pSession->Release();
+	
+	return 	hr;
+}
+ 
+HRESULT CAAFTextLocator::test()
+{
+  HRESULT	hr = AAFRESULT_NOT_IMPLEMENTED;
+  aafWChar*	pFileName = L"TextLocatorTest.aaf";
+
+  try
+	{
+		hr = CreateAAFFile(	pFileName );
+		if (AAFRESULT_SUCCESS != hr)
+			return hr;
+		hr = ReadAAFFile( pFileName );
+	}
+  catch (...)
+	{
+	  cerr << "CAAFTextLocator::test...Caught general C++"
+		" exception!" << endl; 
+	}
+
+
+  return hr;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
