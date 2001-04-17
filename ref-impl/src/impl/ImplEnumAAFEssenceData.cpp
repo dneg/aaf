76d91d1a81a15d84237eacdef8b4b943075137b1
tbingham <>
2001-04-17 04:47:46 +0000

    implemented but untested

---
+++ ref-impl/src/impl/ImplEnumAAFEssenceData.cpp
@@ -0,0 +1,175 @@
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+#ifndef __ImplAAFEssenceData_h__
+#include "ImplAAFEssenceData.h"
+#endif
+#ifndef __ImplAAFContentStorage_h__
+#include "ImplAAFContentStorage.h"
+#endif
+
+
+
+
+
+
+#ifndef __ImplEnumAAFEssenceData_h__
+#include "ImplEnumAAFEssenceData.h"
+#endif
+
+#include <assert.h>
+#include <string.h>
+#include "AAFResult.h"
+#include "ImplAAFObjectCreation.h"
+
+extern "C" const aafClassID_t CLSID_EnumAAFEssenceData;
+
+
+ImplEnumAAFEssenceData::ImplEnumAAFEssenceData ()
+{
+  _current = 0;
+  _contentStorage = NULL;
+}
+
+
+ImplEnumAAFEssenceData::~ImplEnumAAFEssenceData ()
+{
+  if (_contentStorage)
+  {
+    _contentStorage->ReleaseReference();
+    _contentStorage = NULL;
+  }
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFEssenceData::NextOne (ImplAAFEssenceData ** ppEssenceData)
+{
+  AAFRESULT hr = AAFRESULT_SUCCESS;
+  aafUInt32 cur = _current, siz = 0;
+
+  hr = _contentStorage->GetNumEssenceData (&siz);
+  if (AAFRESULT_SUCCESS == hr)
+  {    
+    if (cur < siz)
+    {
+      hr = _contentStorage->GetNthEssenceData (cur, ppEssenceData);
+      if (AAFRESULT_SUCCESS == hr)
+        _current = ++cur;
+    }
+  }
+  return hr;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFEssenceData::Next (aafUInt32  count,
+                           ImplAAFEssenceData ** ppEssenceData,
+                           aafUInt32 *  pFetched)
+{
+	aafUInt32		numEssenceData = 0;
+	AAFRESULT		result = AAFRESULT_SUCCESS;
+
+	if ((pFetched == NULL) || (NULL == ppEssenceData))
+		return AAFRESULT_NULL_PARAM;
+
+	for (numEssenceData = 0; numEssenceData < count; numEssenceData++)
+	{
+		result = NextOne(&ppEssenceData[numEssenceData]);
+		if (AAFRESULT_SUCCESS != result)
+			break;
+	}
+	
+	*pFetched = numEssenceData;
+
+	return result;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFEssenceData::Skip (aafUInt32  count)
+{
+  AAFRESULT result = AAFRESULT_SUCCESS;
+  aafUInt32 newCurrent = _current + count;
+  aafUInt32 siz = 0;
+
+  result = _contentStorage->GetNumEssenceData(&siz);
+  if (AAFRESULT_SUCCESS == result)
+  {
+    if (newCurrent < siz)
+      _current = newCurrent;
+    else
+      result = AAFRESULT_NO_MORE_OBJECTS;
+  }
+
+  return result;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFEssenceData::Reset ()
+{
+  _current = 0;
+  return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFEssenceData::Clone (ImplEnumAAFEssenceData ** ppEnum)
+{
+	if (NULL == ppEnum)
+		return AAFRESULT_NULL_PARAM;
+	*ppEnum = 0;
+	
+	ImplEnumAAFEssenceData *theEnum = (ImplEnumAAFEssenceData *)CreateImpl (CLSID_EnumAAFEssenceData);
+	
+	XPROTECT()
+	{
+		CHECK(theEnum->SetContentStorage(_contentStorage));
+		CHECK(theEnum->Reset());
+		CHECK(theEnum->Skip(_current));
+		*ppEnum = theEnum;
+	}
+	XEXCEPT
+	{
+		if (theEnum)
+			theEnum->ReleaseReference();
+		return(XCODE());
+	}
+	XEND;
+	
+	return(AAFRESULT_SUCCESS);
+}
+
+
+
+//Internal
+AAFRESULT
+    ImplEnumAAFEssenceData::SetContentStorage(ImplAAFContentStorage *pContentStorage)
+{
+  if (_contentStorage)
+    _contentStorage->ReleaseReference();
+
+  _contentStorage = pContentStorage;
+
+  if (pContentStorage)
+    pContentStorage->AcquireReference();
+
+  return AAFRESULT_SUCCESS;
+}
+
+
+
+
+
+
+
+
+
---
+++ ref-impl/src/impl/ImplEnumAAFEssenceData.h
@@ -0,0 +1,106 @@
+//@doc
+//@class    EnumAAFEssenceData | Implementation class for EnumAAFEssenceData
+#ifndef __ImplEnumAAFEssenceData_h__
+#define __ImplEnumAAFEssenceData_h__
+
+#include "OMStorable.h"
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+class ImplAAFEssenceData;
+class ImplAAFContentStorage;
+
+
+
+
+
+
+#ifndef __ImplAAFRoot_h__
+#include "ImplAAFRoot.h"
+#endif
+
+
+class ImplEnumAAFEssenceData : public ImplAAFRoot
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplEnumAAFEssenceData ();
+
+protected:
+  virtual ~ImplEnumAAFEssenceData ();
+
+public:
+
+  //****************
+  // NextOne()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    NextOne
+		// @parm [out,retval] The Next EssenceData
+        (ImplAAFEssenceData ** ppEssenceData);
+
+  //****************
+  // Next()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Next
+        (// @parm [in] number of essence data requested
+		 aafUInt32  count,
+
+		 // @parm [out, size_is(count), length_is(*pFetched)] array to receive EssenceData
+		 ImplAAFEssenceData ** ppEssenceData,
+
+		 // @parm [out,ref] number of actual EssenceData fetched into ppEssenceData array
+         aafUInt32 *  pFetched);
+
+  //****************
+  // Skip()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Skip
+		// @parm [in] Number of elements to skip
+        (aafUInt32  count);
+
+  //****************
+  // Reset()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Reset ();
+
+
+  //****************
+  // Clone()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Clone
+		// @parm [out,retval] new enumeration
+        (ImplEnumAAFEssenceData ** ppEnum);
+
+
+
+public:
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplEnumAAFEssenceDataTest.cpp.
+  static AAFRESULT test();
+
+public:
+// Internal to the toolkit
+AAFRESULT SetContentStorage(ImplAAFContentStorage *pContentStorage);
+
+private:
+	aafUInt32 _current;
+	ImplAAFContentStorage *_contentStorage;
+};
+
+#endif // ! __ImplEnumAAFEssenceData_h__
+
