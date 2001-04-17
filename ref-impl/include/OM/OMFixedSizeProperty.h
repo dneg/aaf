bc57116e2c4f5e9e2253e44283fe2daa1de3c414
tbingham <>
2001-04-17 07:11:12 +0000

    Do the shuffle - the header file shuffle.

--- ref-impl/include/OM/OMFixedSizeProperty.h
+++ ref-impl/include/OM/OMFixedSizeProperty.h
@@ -1,6 +1,6 @@
 /***********************************************************************
 *
-*              Copyright (c) 1998-1999 Avid Technology, Inc.
+*              Copyright (c) 1998-2000 Avid Technology, Inc.
 *
 * Permission to use, copy and modify this software and accompanying
 * documentation, and to distribute and sublicense application software
@@ -29,7 +29,7 @@
 #ifndef OMFIXEDSIZEPROPERTY_H
 #define OMFIXEDSIZEPROPERTY_H
 
-#include "OMPropertyBase.h"
+#include "OMProperty.h"
 
   // @class Fixed size simple (data) persistent
   //        properties supported by the Object Manager.
