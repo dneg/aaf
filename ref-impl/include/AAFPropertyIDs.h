0101314ae4a946815656b0ad4d9635aa146691e6
tbingham <>
2001-04-17 04:05:14 +0000

    Obtain property ids via the automatically generated AAFMetaDictionary.h. Property ids are now (mostly) unique within an AAF file. Increment the stored format version number from 0.2 to 0.3.

--- ref-impl/include/AAFPropertyIDs.h
+++ ref-impl/include/AAFPropertyIDs.h
@@ -1,107 +1,17 @@
 #ifndef _AAFPropertyIDs_H_
 #define _AAFPropertyIDs_H_
 
-const int PID_CDCI_DESC_COMPPONENTWIDTH               =  40;
-const int PID_CDCI_DESC_HORIZONTALSUBSAMPLING         =  41;
-const int PID_CDCI_DESC_COLORSITING                   =  42;
-const int PID_CDCI_DESC_BLACKREFERENCELEVEL           =  43;
-const int PID_CDCI_DESC_WHITEREFERENCELEVEL           =  44;
-const int PID_CDCI_DESC_COLORRANGE                    =  45;
-const int PID_CDCI_DESC_PADDINGBITS                   =  46;
+// Missing PIDs
+//
+const int PID_SOURCECLIP_FADEINPRESENT = 6;
+const int PID_SOURCECLIP_FADEOUTPRESENT = 9;
+const int PID_TIMELINEMOBSLOT_ORIGIN = 5;
+const int PID_TIMECODE_TC = 5;
+const int PID_ESSENCEDATA_MEDIADATA = 1;
 
-const int PID_COMPONENT_DATADEF                       =   0;
-const int PID_COMPONENT_LENGTH                        =   1;
-
-const int PID_COMPOSITIONMOB_DEFAULTFADELENGTH        =   6;
-const int PID_COMPOSITIONMOB_DEFAULTFADETYPE          =   7;
-const int PID_COMPOSITIONMOB_DEFAULTFADEEDITUNIT      =   8;
-
-const int PID_CONTENT_STORAGE_MOBS                    =   0;
-const int PID_CONTENT_STORAGE_ESSENCEDATA             =   1;
-
-const int PID_DIGITAL_IMAGE_DESC_COMPRESSION          =  20;
-const int PID_DIGITAL_IMAGE_DESC_STOREDHEIGHT         =  21;
-const int PID_DIGITAL_IMAGE_DESC_STOREDWIDTH          =  22;
-const int PID_DIGITAL_IMAGE_DESC_SAMPLEDHEIGHT        =  23;
-const int PID_DIGITAL_IMAGE_DESC_SAMPLEDWIDTH         =  24;
-const int PID_DIGITAL_IMAGE_DESC_SAMPLEDXOFFSET       =  25;
-const int PID_DIGITAL_IMAGE_DESC_SAMPLEDYOFFSET       =  26;
-const int PID_DIGITAL_IMAGE_DESC_DISPLAYHEIGHT        =  27;
-const int PID_DIGITAL_IMAGE_DESC_DISPLAYWIDTH         =  28;
-const int PID_DIGITAL_IMAGE_DESC_DISPLAYXOFFSET       =  29;
-const int PID_DIGITAL_IMAGE_DESC_DISPLAYYOFFSET       =  30;
-const int PID_DIGITAL_IMAGE_DESC_FRAMELAYOUT          =  31;
-const int PID_DIGITAL_IMAGE_DESC_VIDEOLINEMAP         =  32;
-const int PID_DIGITAL_IMAGE_DESC_IMAGEASPECTRATIO     =  33;
-const int PID_DIGITAL_IMAGE_DESC_ALPHATRANSPARENCY    =  34;
-const int PID_DIGITAL_IMAGE_DESC_GAMMA                =  35;
-const int PID_DIGITAL_IMAGE_DESC_IMAGEALIGNMENTFACTOR =  36;
-
-const int PID_ESSENCEDATA_FILEMOBID                   =   0;
-const int PID_ESSENCEDATA_MEDIADATA                   =   1;
-
-const int PID_ESSENCE_DESC_LOCATORS                   =   0;
-
-const int PID_FILE_DESC_SAMPLERATE                    =   1;
-const int PID_FILE_DESC_LENGTH                        =   2;
-const int PID_FILE_DESC_INCONTAINER                   =   3;
-const int PID_FILE_DESC_CONTAINERFMT                  =   4;
-
-const int PID_HEADER_BYTEORDER                        =   0;
-const int PID_HEADER_LASTMODIFIED                     =   1;
-const int PID_HEADER_IDENTIFICATIONLIST               =   2;
-const int PID_HEADER_CONTENTSTORAGE                   =   3;
-
-const int PID_IDENTIFICATION_COMPANYNAME              =   0;
-const int PID_IDENTIFICATION_PRODUCTNAME              =   1;
-const int PID_IDENTIFICATION_PRODUCTVERSIONSTRING     =   2;
-const int PID_IDENTIFICATION_DATE                     =   3;
-const int PID_IDENTIFICATION_PLATFORM                 =   4;
-const int PID_IDENTIFICATION_PRODUCTVERSION           =   5;
-const int PID_IDENTIFICATION_PRODUCTID                =   6;
-const int PID_IDENTIFICATION_TOOLKITVERSION           =   7;
-const int PID_IDENTIFICATION_GENERATION               =   8;
-
-const int PID_MOB_MOBID                               =   0;
-const int PID_MOB_NAME                                =   1;
-const int PID_MOB_CREATE_TIME                         =   2;
-const int PID_MOB_MOD_TIME                            =   3;
-const int PID_MOB_SLOTS                               =   4;
-
-const int PID_MOBSLOT_NAME                            =   0;
-const int PID_MOBSLOT_TRACKID                         =   1;
-const int PID_MOBSLOT_PHYSICAL_TRACK                  =   2;
-const int PID_MOBSLOT_SEGMENT                         =   3;
-
-const int PID_NETWORKLOCATOR_URLSTRING                =   0;
-
-const int PID_SEQUENCE_COMPONENTS                     =  30;
-
-const int PID_SOURCECLIP_FADEINLEN                    =   4;
-const int PID_SOURCECLIP_FADEINTYPE                   =   5;
-const int PID_SOURCECLIP_FADEINPRESENT                =   6;
-const int PID_SOURCECLIP_FADEOUTLEN                   =   7;
-const int PID_SOURCECLIP_FADEOUTTYPE                  =   8;
-const int PID_SOURCECLIP_FADEOUTPRESENT               =   9;
-const int PID_SOURCECLIP_STARTTIME                    =  10;
-
-const int PID_SOURCEMOB_EDESC                         =   5;
-
-const int PID_SOURCEREFERENCE_SOURCEID                =   2;
-const int PID_SOURCEREFERENCE_SOURCEMOBSLOTID         =   3;
-
-const int PID_TAPEDESCRIPTOR_FORMFACTOR               =   1;
-const int PID_TAPEDESCRIPTOR_VIDEOSIGNAL              =   2;
-const int PID_TAPEDESCRIPTOR_TAPEFORMAT               =   3;
-const int PID_TAPEDESCRIPTOR_LENGTH                   =   4;
-const int PID_TAPEDESCRIPTOR_MANUFACTURER             =   5;
-const int PID_TAPEDESCRIPTOR_MODEL                    =   6;
-
-const int PID_TIMELINEMOBSLOT_EDITRATE                =   4;
-const int PID_TIMELINEMOBSLOT_ORIGIN                  =   5;
-
-const int PID_TIMECODE_TC                             =   5;
-
-const int PID_WAVEDESCRIPTOR_SUMMARY                  =  30;
+#define AAF_PROPERTY(name, id, tag, type, mandatory, container) \
+const int PID_##container##_##name = tag;
+#include "AAFMetaDictionary.h"
+#undef AAF_PROPERTY
 
 #endif
