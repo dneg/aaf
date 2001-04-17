8c1b40a9aaa9fca1b498d31ac38d99d11d524055
tbingham <>
2001-04-17 03:31:34 +0000

    Adding missing example projects to the Mac build.

---
+++ AAFMacSDK/examples/com-api/ExportAudioExample/ExportAudioExample_prefix.h
@@ -0,0 +1,19 @@
+// >>> © 1998 Avid Technology, Inc.  All rights reserved. <<<
+#define __dest_os	__mac_os
+
+//#define CHECKMEMORYLEAKS // Uncomment this line to turn leak checking on
+
+#define _MAC
+#define NONAMELESSUNION
+
+#include "MacHeaders.h"
+
+
+
+#include <ole2.h>
+#include "dispatch.h"
+#include "wintypes.h"
+#include <olectl.h>
+
+
+
