f42a8e8692c02b830149113840a58317e4d96087
tbingham <>
2001-04-17 10:43:32 +0000

    Add OMFile::isReadable() and OMFile::isWritable().

--- ref-impl/src/OM/OMFile.h
+++ ref-impl/src/OM/OMFile.h
@@ -230,6 +230,12 @@ public:
     // @cmember The access mode of this <c OMFile>.
   OMAccessMode accessMode(void) const;
 
+    // @cmember Is it possible to read from this <c OMFile> ?
+  bool isReadable(void) const;
+
+    // @cmember Is it possible to write to this <c OMFile> ?
+  bool isWritable(void) const;
+
     // @cmember Is this file recognized by the Object Manager ?
   bool isOMFile(void) const;
 
