13066c2b178154b886043bad1be71ea4cfaf86d1
tbingham <>
2001-04-17 09:50:43 +0000

    Make Reference properties more like pointers, add a -> operator, change = operator to accept a value rather than a reference. OMStorable no longer inherits from OMReferenceCountable.

--- ref-impl/src/OM/OMFile.h
+++ ref-impl/src/OM/OMFile.h
@@ -41,12 +41,6 @@ public:
 
   virtual OMFile* file(void) const;
 
-  // OMReferenceCountable interface
-  //
-  virtual size_t AcquireReference(void);
-  virtual size_t ReleaseReference(void);
-  virtual size_t ReferenceCount(void);
-
 private:
 
   OMStorable* _root;
