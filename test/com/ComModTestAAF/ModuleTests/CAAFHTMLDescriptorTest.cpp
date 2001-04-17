cdec43d903035847ae1a90d14b7c848e8ff96c17
tbingham <>
2001-04-17 03:03:00 +0000

    Removing CoCreateInstance from the module tests.

--- test/com/ComModTestAAF/ModuleTests/CAAFHTMLDescriptorTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFHTMLDescriptorTest.cpp
@@ -1,13 +1,13 @@
 // @doc INTERNAL
-// @com This file implements the module test for CAAFDefinitionObject
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+// @com This file implements the module test for CAAFHTMLDescriptor
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
 
 
 
@@ -21,56 +21,13 @@
 #error - improperly defined include guard
 #endif
 
-
-#include <iostream.h>
-
-// Temporarily necessary global declarations.
-extern "C" const CLSID CLSID_AAFHTMLDescriptor; // generated
+#include "AAFResult.h"
 
 
 HRESULT CAAFHTMLDescriptor::test()
 {
-  HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
-  IAAFHTMLDescriptor *pObject = NULL;
-
-  try
-    {
-      // Attempt to create an AAFHTMLDescriptor.
-      hr =  CoCreateInstance(
-                             CLSID_AAFHTMLDescriptor,
-                             NULL, 
-                             CLSCTX_INPROC_SERVER, 
-                             IID_IAAFHTMLDescriptor, (void **)&pObject);
-      if (FAILED(hr))
-        {
-          cerr << "CAAFHTMLDescriptor::test...FAILED!";
-          cerr << hr;
-          cerr << "\tCoCreateInstance(&CLSID_AAFHTMLDescriptor, NULL,"
-            " CLSCTX_INPROC_SERVER, &IID_IAAFHTMLDescriptor, ...);" <<
-              endl;
-          return hr;
-        }
-
-      // module-specific tests go here
-
-      if (pObject)
-        pObject->Release();
-      return AAFRESULT_NOT_IMPLEMENTED;
-
-    }
-  catch (...)
-    {
-      cerr << "CAAFHTMLDescriptor::test...Caught general C++"
-        " exception!" << endl; 
-    }
-
-  // Cleanup our object if it exists.
-  if (pObject)
-    pObject->Release();
-
-  return hr;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
-
 
 
 
