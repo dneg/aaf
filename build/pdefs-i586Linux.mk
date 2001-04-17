62f7019fca04170aec99f5b7e1e1ebd35ad65249
tbingham <>
2001-04-17 11:55:29 +0000

    Makefile definition for the Pentium Linux platform.

---
+++ build/pdefs-i586Linux.mk
@@ -0,0 +1,87 @@
+#------------------------------------------------------------------------------
+#
+# Advanced Authoring Format
+#
+# Copyright (c) 2000 Avid Technology, Inc.
+#
+#
+#	This file contains makefile definitions for the Linux platform.
+#
+#
+# Uses:
+#	COMPILER, XL, RPATH, CC
+#	
+# Requires:
+#	XL, RPATH, CC
+#
+# Sets:
+#	MIPS_ABI, COMPILER, PLATFORM_CFLAGS, RPATH_OPT, 
+#	LD, LD_STAT_LIB, LD_DYN_LIB, U_OPTS, OBJ, EXE, LIB, DLL, BYTE_ORDER
+#
+#------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------
+# Include common Unix definitions.
+#------------------------------------------------------------------------------
+include $(AAFBASE)/build/pdefs-Unix.mk
+
+
+#------------------------------------------------------------------------------
+# Compiler-specific definitions
+#------------------------------------------------------------------------------
+COMPILER ?= g++
+include $(AAFBASE)/build/cdefs-$(COMPILER).mk
+
+
+#------------------------------------------------------------------------------
+# Platform specific compiler options
+#------------------------------------------------------------------------------
+PLATFORM_CFLAGS = 
+
+
+#------------------------------------------------------------------------------
+# Linker command and options
+#------------------------------------------------------------------------------
+RPATH_OPT = $(XL)-rpath $(XL)$(RPATH)
+
+# Command to link executable.
+LD = $(CC) -ldl -rdynamic
+
+# Command to link static library
+ifndef LD_STAT_LIB
+    # Note: CC is invoked here to use IRIX specific 
+    # compiler option -ar.
+    LD_STAT_LIB = ar -cr 
+endif
+
+# Command to link dynamic library
+ifndef LD_DYN_LIB
+    LD_DYN_LIB = $(CC) -shared
+endif
+
+
+#------------------------------------------------------------------------------
+# Select UNICODE or ansi API's:
+#   U_OPTS=use_unicode
+#   U_OPTS=no_unicode
+#------------------------------------------------------------------------------
+U_OPTS=no_unicode
+
+
+#------------------------------------------------------------------------------
+# Binary file extensions
+#------------------------------------------------------------------------------
+OBJ ?= .o
+EXE ?= 
+LIB ?= .a
+DLL ?= .so
+
+
+#------------------------------------------------------------------------------
+# Intel machines are Little Endian (lower byte first)
+# Mac, HP, SUN, etc. are Big Endian (higher byte first)
+# BYTE_ORDER = -DBIGENDIAN=1
+#------------------------------------------------------------------------------
+BYTE_ORDER = -DLITTLEENDIAN=1
+
