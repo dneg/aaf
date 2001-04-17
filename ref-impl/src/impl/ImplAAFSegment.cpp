20ba08a87a7d48a4dab65fa5c0e1bc07f8632c80
tbingham <>
2001-04-17 06:06:07 +0000

    Use and persist GUIDs instead of small integers for class ids

--- ref-impl/src/impl/ImplAAFSegment.cpp
+++ ref-impl/src/impl/ImplAAFSegment.cpp
@@ -151,3 +151,7 @@ AAFRESULT ImplAAFSegment::GenerateSequence(ImplAAFSequence **seq)
 
 	return(AAFRESULT_SUCCESS);
 }
+
+extern "C" const aafClassID_t CLSID_AAFSegment;
+
+OMDEFINE_STORABLE(AAFSegment, CLSID_AAFSegment);
