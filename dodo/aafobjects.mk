222734284bfc2a085660064aed51c0a1a1a9cdb0
tbingham <>
2001-04-17 08:10:17 +0000

    This file contains the list of all objects that are "ready" to build in the com-api (and possibly in the cpp-api.

---
+++ dodo/aafobjects.mk
@@ -0,0 +1,14 @@
+#
+#
+# Advanced Authoring Format
+#
+# Copyright (c) 1998 Avid Technology, Inc.
+#
+#
+# Place the object root name is this list if it is ready
+# to build:
+AFFOBJECTS = \
+	AAFObject \
+	AAFDefinitionObject
+
+
