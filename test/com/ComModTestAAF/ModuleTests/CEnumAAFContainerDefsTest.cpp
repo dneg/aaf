243f14545d26b5935fd1bcf1a69bafe243fe1af2
tbingham <>
2001-04-17 05:56:21 +0000

    Fix enum Next call

--- test/com/ComModTestAAF/ModuleTests/CEnumAAFContainerDefsTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CEnumAAFContainerDefsTest.cpp
@@ -1,33 +1,13 @@
 // @doc INTERNAL
-// @com This file implements the module test for CAAFDefinitionObject
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
-
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
-
-
-/***********************************************\
-*	Stub only.   Implementation not yet added	*
-\***********************************************/
-
-
-
-
-
-
+// @com This file implements the module test for CEnumAAFContainerDefs object
+/************************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
+\************************************************/
 
 #include "CEnumAAFContainerDefs.h"
 #include "CEnumAAFContainerDefs.h"
@@ -35,67 +15,346 @@
 #error - improperly defined include guard
 #endif
 
-
 #include <iostream.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
 
-// Temporarily necessary global declarations.
-extern "C" const CLSID CLSID_EnumAAFContainerDefs; // generated
+#include "AAFStoredObjectIDs.h"
+#include "AAFResult.h"
+#include "AAFDefUIDs.h"
 
-
-HRESULT CEnumAAFContainerDefs::test()
+// Cross-platform utility to delete a file.
+static void RemoveTestFile(const wchar_t* pFileName)
 {
-  HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
-  IEnumAAFContainerDefs *pObject = NULL;
+  const size_t kMaxFileName = 512;
+  char cFileName[kMaxFileName];
+
+  size_t status = wcstombs(cFileName, pFileName, kMaxFileName);
+  if (status != (size_t)-1)
+  { // delete the file.
+    remove(cFileName);
+  }
+}
+
+// convenient error handlers.
+inline void checkResult(HRESULT r)
+{
+  if (FAILED(r))
+    throw r;
+}
+inline void checkExpression(bool expression, HRESULT r)
+{
+  if (!expression)
+    throw r;
+}
+
+static wchar_t *sName1 = L"Test Descriptor Name1";
+static wchar_t *sDescription1 = L"Test Descriptor Description1";
+static wchar_t *sName2 = L"Test Descriptor Name2";
+static wchar_t *sDescription2 = L"Test Descriptor Description2";
+
+
+static HRESULT OpenAAFFile(aafWChar*			pFileName,
+						   aafMediaOpenMode_t	mode,
+						   IAAFFile**			ppFile,
+						   IAAFHeader**			ppHeader)
+{
+	aafProductIdentification_t	ProductInfo;
+	HRESULT						hr = AAFRESULT_SUCCESS;
+
+	ProductInfo.companyName = L"AAF Developers Desk";
+	ProductInfo.productName = L"AAFMasterMob Test";
+	ProductInfo.productVersion.major = 1;
+	ProductInfo.productVersion.minor = 0;
+	ProductInfo.productVersion.tertiary = 0;
+	ProductInfo.productVersion.patchLevel = 0;
+	ProductInfo.productVersion.type = kVersionUnknown;
+	ProductInfo.productVersionString = NULL;
+	ProductInfo.productID = -1;
+	ProductInfo.platform = NULL;
+
+	if(mode == kMediaOpenAppend)
+		hr = AAFFileOpenNewModify(pFileName, 0, &ProductInfo, ppFile);
+	else
+		hr = AAFFileOpenExistingRead(pFileName, 0, ppFile);
+
+	if (FAILED(hr))
+	{
+		(*ppFile)->Release();
+		*ppFile = NULL;
+		return hr;
+	}
+  
+  	hr = (*ppFile)->GetHeader(ppHeader);
+	if (FAILED(hr))
+	{
+		(*ppFile)->Release();
+		*ppFile = NULL;
+		return hr;
+	}
+ 	
+	return hr;
+}
+
+static HRESULT CreateAAFFile(aafWChar * pFileName)
+{
+	IAAFFile*			pFile = NULL;
+	IAAFHeader *		pHeader = NULL;
+	IAAFDictionary*		pDictionary = NULL;
+	IAAFDefObject*		pDef = NULL;
+	IAAFContainerDef*	pContainerDef = NULL;
+	bool				bFileOpen = false;
+	HRESULT				hr = S_OK;
+/*	long				test;
+*/
 
   try
-    {
-      // Attempt to create an EnumAAFContainerDefs.
-      hr =  CoCreateInstance(
-                             CLSID_EnumAAFContainerDefs,
-                             NULL, 
-                             CLSCTX_INPROC_SERVER, 
-                             IID_IEnumAAFContainerDefs, (void **)&pObject);
-      if (FAILED(hr))
-        {
-          cerr << "CEnumAAFContainerDefs::test...FAILED!";
-          cerr << hr;
-          cerr << "\tCoCreateInstance(&CLSID_EnumAAFContainerDefs, NULL,"
-            " CLSCTX_INPROC_SERVER, &IID_IEnumAAFContainerDefs, ...);" <<
-              endl;
-          return hr;
-        }
+  {
+    // Remove the previous test file if any.
+    RemoveTestFile(pFileName);
 
-      // module-specific tests go here
 
-      if (pObject)
-        pObject->Release();
-      return AAFRESULT_NOT_IMPLEMENTED;
+	// Create the AAF file
+	checkResult(OpenAAFFile(pFileName, kMediaOpenAppend, /*&pSession,*/ &pFile, &pHeader));
+    bFileOpen = true;
 
-    }
-  catch (...)
-    {
-      cerr << "CEnumAAFContainerDefs::test...Caught general C++"
-        " exception!" << endl; 
-    }
+    // Get the AAF Dictionary so that we can create valid AAF objects.
+    checkResult(pHeader->GetDictionary(&pDictionary));
+    
+	checkResult(pDictionary->CreateInstance(&AUID_AAFContainerDef,
+							  IID_IAAFContainerDef, 
+							  (IUnknown **)&pContainerDef));
+    
+	checkResult(pContainerDef->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
 
-  // Cleanup our object if it exists.
-  if (pObject)
-    pObject->Release();
+	checkResult(pDef->SetName(sName1));
+	checkResult(pDef->SetDescription(sDescription1));
+	checkResult(pDictionary->RegisterContainerDefinition(pContainerDef));
+	pDef->Release();
+	pDef = NULL;
+	pContainerDef->Release();
+	pContainerDef = NULL;
+	checkResult(pDictionary->CreateInstance(&AUID_AAFContainerDef,
+							  IID_IAAFContainerDef, 
+							  (IUnknown **)&pContainerDef));
+    
+	checkResult(pContainerDef->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
+
+	checkResult(pDef->SetName(sName2));
+	checkResult(pDef->SetDescription(sDescription2));
+
+	checkResult(pDictionary->RegisterContainerDefinition(pContainerDef));
+  }
+  catch (HRESULT& rResult)
+  {
+    hr = rResult;
+  }
+
+
+  // Cleanup and return
+  if (pDef)
+    pDef->Release();
+
+  if (pContainerDef)
+    pContainerDef->Release();
+
+  if (pDictionary)
+    pDictionary->Release();
+
+  if (pHeader)
+    pHeader->Release();
+      
+  if (pFile)
+  {  // Close file
+    if (bFileOpen)
+	{
+		pFile->Save();
+		pFile->Close();
+	}
+     pFile->Release();
+  }
 
   return hr;
 }
 
+static HRESULT ReadAAFFile(aafWChar* pFileName)
+{
+	IAAFFile*			pFile = NULL;
+	IAAFHeader*			pHeader = NULL;
+	IAAFDictionary*		pDictionary = NULL;
+	IEnumAAFContainerDefs *pPlug = NULL;
+	IEnumAAFContainerDefs *pClonePlug = NULL;
+	IAAFDefObject*		pDef = NULL;
+	IAAFContainerDef*	pContainerDef = NULL;
+	IAAFContainerDef*	pArray[2] = { NULL, NULL };
+	IAAFContainerDef**	pArrayDef = pArray;
+	bool				bFileOpen = false;
+	HRESULT				hr = S_OK;
+	wchar_t				testString[256];
+	aafUInt32			resultCount;
 
+	try
+	{
+		// Open the AAF file
+		checkResult(OpenAAFFile(pFileName, kMediaOpenReadOnly, &pFile, &pHeader));
+		bFileOpen = true;
 
+		checkResult(pHeader->GetDictionary(&pDictionary));
+	
+		checkResult(pDictionary->GetContainerDefinitions(&pPlug));
+		/* Read and check the first element */
+		checkResult(pPlug->NextOne(&pContainerDef));
+		checkResult(pContainerDef->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
 
+		checkResult(pDef->GetName (testString, sizeof(testString)));
+		checkExpression (wcscmp(testString, sName1) == 0, AAFRESULT_TEST_FAILED);
+		pContainerDef->Release();
+		pContainerDef = NULL;
+		pDef->Release();
+		pDef = NULL;
 
+		/* Read and check the second element */
+		checkResult(pPlug->NextOne(&pContainerDef));
+		checkResult(pContainerDef->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
 
+		checkResult(pDef->GetName (testString, sizeof(testString)));
+		checkExpression (wcscmp(testString, sName2) == 0, AAFRESULT_TEST_FAILED);
+		pContainerDef->Release();
+		pContainerDef = NULL;
+		pDef->Release();
+		pDef = NULL;
+		/* Reset, and check the first element again*/
+		checkResult(pPlug->Reset());
+		checkResult(pPlug->NextOne(&pContainerDef));
+		checkResult(pContainerDef->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
 
+		checkResult(pDef->GetName (testString, sizeof(testString)));
+		checkExpression (wcscmp(testString, sName1) == 0, AAFRESULT_TEST_FAILED);
+		pContainerDef->Release();
+		pContainerDef = NULL;
+		pDef->Release();
+		pDef = NULL;
+		/* Reset, Skip, and check the second element again*/
+		checkResult(pPlug->Reset());
+		checkResult(pPlug->Skip(1));
+		checkResult(pPlug->NextOne(&pContainerDef));
+		checkResult(pContainerDef->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
 
+		checkResult(pDef->GetName (testString, sizeof(testString)));
+		checkExpression (wcscmp(testString, sName2) == 0, AAFRESULT_TEST_FAILED);
+		pContainerDef->Release();
+		pContainerDef = NULL;
+		pDef->Release();
+		pDef = NULL;
+		/* Reset, and read both elements */
+		checkResult(pPlug->Reset());
+		checkResult(pPlug->Next (2, (IAAFContainerDef **)&pArray, &resultCount));
+		checkExpression (resultCount == 2, AAFRESULT_TEST_FAILED);
+		checkResult(pArrayDef[0]->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
 
+		checkResult(pDef->GetName (testString, sizeof(testString)));
+		checkExpression (wcscmp(testString, sName1) == 0, AAFRESULT_TEST_FAILED);
+		pDef->Release();
+		pDef = NULL;
+		checkResult(pArrayDef[1]->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
 
+		checkResult(pDef->GetName (testString, sizeof(testString)));
+		checkExpression (wcscmp(testString, sName2) == 0, AAFRESULT_TEST_FAILED);
+		pDef->Release();
+		pDef = NULL;
+		/* Read one past to make sure that it fails */
+		checkExpression(pPlug->NextOne(&pContainerDef) != AAFRESULT_SUCCESS, AAFRESULT_TEST_FAILED);
+		/* Clone the enumerator, and read one element */
+		checkResult(pPlug->Clone(&pClonePlug));
+		checkResult(pClonePlug->Reset());
+		checkResult(pClonePlug->NextOne(&pContainerDef));
+		checkResult(pContainerDef->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
 
+		checkResult(pDef->GetName (testString, sizeof(testString)));
+		checkExpression (wcscmp(testString, sName1) == 0, AAFRESULT_TEST_FAILED);
+		pContainerDef->Release();
+		pContainerDef = NULL;
+		pDef->Release();
+		pDef = NULL;
+	}
+	catch (HRESULT& rResult)
+	{
+		hr = rResult;
+	}
 
+	// Cleanup and return
+	if (pHeader)
+		pHeader->Release();
+      
+	if (pPlug)
+		pPlug->Release();
 
+	if (pClonePlug)
+		pClonePlug->Release();
 
+	if (pDef)
+		pDef->Release();
+      
+	if (pContainerDef)
+		pContainerDef->Release();
+            
+	if (pArray[0])
+		pArray[0]->Release();
+	if (pArray[1])
+		pArray[1]->Release();
+            
+	if (pFile)
+	{  // Close file
+		if (bFileOpen)
+			pFile->Close();
+		pFile->Release();
+	}
 
+	return hr;
+}
+ 
+
+HRESULT CEnumAAFContainerDefs::test()
+{
+	HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
+	aafWChar * pFileName = L"EnumAAFContainerDefsTest.aaf";
+
+	try
+	{
+		hr = CreateAAFFile(pFileName);
+		if (SUCCEEDED(hr))
+			hr = ReadAAFFile(pFileName);
+	}
+	catch (...)
+	{
+		cerr << "CEnumAAFContainerDefs::test...Caught general C++ exception!" << endl; 
+	}
+
+	// When all of the functionality of this class is tested, we can return success.
+	// When a method and its unit test have been implemented, remove it from the list.
+	if (SUCCEEDED(hr))
+	{
+		cout << "The following CEnumAAFContainerDefs methods have not been tested:" << endl; 
+		cout << "     Clone" << endl; 
+		hr = AAFRESULT_TEST_PARTIAL_SUCCESS;
+	}
+
+	return hr;
+}
+
+//    CEnumAAFContainerDefs::NextOne (IAAFContainerDef ** ppPluggableDef)
+//        aafUInt32 *  pFetched)
+//    CEnumAAFContainerDefs::Skip (aafUInt32  count)
+//    CEnumAAFContainerDefs::Reset ()
+//   CEnumAAFContainerDefs::Clone (IEnumAAFContainerDefs ** ppEnum)
