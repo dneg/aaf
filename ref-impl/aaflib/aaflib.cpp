d726d1c5a5e88cc3d3a049e0286bf912e01b247d
tbingham <>
2001-04-17 10:27:06 +0000

    Moved platform specific code into separate files (see aaflibmac.cpp and aaflibwin32.cpp.

--- ref-impl/aaflib/aaflib.cpp
+++ ref-impl/aaflib/aaflib.cpp
@@ -24,23 +24,12 @@
  *
  ************************************************************************/
 
-// Declare all of the public functions.
-#include "AAF.h"
+
+
+// Declare the public interface that must be implemented.
+#include "aaflib.h"
+
 #include "AAFResult.h"
-
-
-
-// 
-#ifdef __cplusplus
-extern "C"{
-#endif 
-
-STDAPI AAFLoad(wchar_t * dllname);
-STDAPI AAFUnload();
-
-#ifdef __cplusplus
-}
-#endif
 
 
 
@@ -53,7 +42,7 @@ STDAPI AAFUnload();
 
 #define FAILURE -1
 
-static void reportAssertionFailure(char* kind,
+void reportAssertionFailure(char* kind,
                             char* name,
                             char* expressionString,
                             char* routine,
@@ -69,162 +58,16 @@ static void reportAssertionFailure(char* kind,
 }
 
 
-#define ASSERT(name, expression) \
-  (expression) \
-    ? (void)0  \
-    : reportAssertionFailure("Assertion",    name, #expression, \
-                             currentRoutineName, __FILE__, __LINE__)
-
-
-#define TRACE(routine) char* currentRoutineName = routine;
-
-#else
-
-#define ASSERT(name, expression)
-
-#define TRACE(routine)
-
 #endif
 
 
 
-//***********************************************************
-// Define function prototypes in a manner consistent with the 
-// ActiveX and OLE SDK's.
-
-#if !defined(_MSC_VER)
-
-typedef STDAPICALLTYPE HRESULT (* LPFNAAFFILEOPENEXISTINGREAD)(
-    wchar_t *  pFileName,
-    aafUInt32  modeFlags,
-    IAAFFile ** ppFile);
-
-typedef STDAPICALLTYPE HRESULT (* LPFNAAFFILEOPENEXISTINGMODIFY)(
-    wchar_t *  pFileName,
-    aafUInt32  modeFlags,
-    aafProductIdentification_t *  pIdent,
-    IAAFFile ** ppFile);
-
-typedef STDAPICALLTYPE HRESULT (* LPFNAAFFILEOPENNEWMODIFY)(
-    wchar_t *  pFileName,
-    aafUInt32  modeFlags,
-    aafProductIdentification_t *  pIdent,
-    IAAFFile ** ppFile);
-
-typedef STDAPICALLTYPE HRESULT (* LPFNAAFFILEOPENTRANSIENT)(
-    aafProductIdentification_t *  pIdent,
-    IAAFFile ** ppFile);
-
-typedef STDAPICALLTYPE HRESULT (* LPFNAAFGETPLUGINMANAGER)(
-    IAAFPluginManager ** ppPluginManager);
-
-#else
-
-typedef HRESULT (STDAPICALLTYPE * LPFNAAFFILEOPENEXISTINGREAD)(
-    wchar_t *  pFileName,
-    aafUInt32  modeFlags,
-    IAAFFile ** ppFile);
-
-typedef HRESULT (STDAPICALLTYPE * LPFNAAFFILEOPENEXISTINGMODIFY)(
-    wchar_t *  pFileName,
-    aafUInt32  modeFlags,
-    aafProductIdentification_t *  pIdent,
-    IAAFFile ** ppFile);
-
-typedef HRESULT (STDAPICALLTYPE * LPFNAAFFILEOPENNEWMODIFY)(
-    wchar_t *  pFileName,
-    aafUInt32  modeFlags,
-    aafProductIdentification_t *  pIdent,
-    IAAFFile ** ppFile);
-
-typedef HRESULT (STDAPICALLTYPE * LPFNAAFFILEOPENTRANSIENT)(
-    aafProductIdentification_t *  pIdent,
-    IAAFFile ** ppFile);
-
-typedef HRESULT (STDAPICALLTYPE * LPFNAAFGETPLUGINMANAGER)(
-    IAAFPluginManager ** ppPluginManager);
-
-#endif
-
-//***********************************************************
-// Abstract helper class to hide the platform dependent
-// details for handling dll entrypoints.
-//
-class AAFDLL;
-
-class AAFDLL
-{
-public:
-  // Constructor and destructor for base class.
-  AAFDLL();
-  virtual ~AAFDLL();
-
-  // Return the singleton instance.
-  static AAFDLL * GetAAFDLL();
-
-
-  // Abstract method that must be overridden to implement
-  // platform specific initialization of dll and entry points.
-  virtual HRESULT Load(wchar_t *dllname) = 0;
-  
-  // Abstract method that must be overridden to implement
-  // platform specific cleanup of dll and entry points.
-  virtual HRESULT Unload() = 0;
-
-
-  // Wrapper functions for calling member entry points.
-  //
-  HRESULT OpenExistingRead (
-    wchar_t *  pFileName,
-    aafUInt32  modeFlags,
-    IAAFFile ** ppFile);
-  
-  HRESULT OpenExistingModify (
-    wchar_t *  pFileName,
-    aafUInt32  modeFlags,
-    aafProductIdentification_t *  pIdent,
-    IAAFFile ** ppFile);
-
-  HRESULT OpenNewModify (
-    wchar_t *  pFileName,
-    aafUInt32  modeFlags,
-    aafProductIdentification_t *  pIdent,
-    IAAFFile ** ppFile);
-
-  HRESULT OpenTransient (
-    aafProductIdentification_t *  pIdent,
-    IAAFFile ** ppFile);
-
-  HRESULT GetPluginManager (
-    IAAFPluginManager ** ppPluginManager);
-  
-  // Make the global factory a friend.
-  friend AAFDLL * MakeAAFDLL();
-
-protected:
-  // The single instance of the dll wrapper.
-  static AAFDLL *_singleton;
-
-  // Callback function member data loaded by overridden versions
-  // of the Load() method:
-  LPFNAAFFILEOPENEXISTINGREAD _pfnOpenExistingRead;
-  LPFNAAFFILEOPENEXISTINGMODIFY _pfnOpenExistingModify;
-  LPFNAAFFILEOPENNEWMODIFY _pfnOpenNewModify;
-  LPFNAAFFILEOPENTRANSIENT _pfnOpenTransient;
-  LPFNAAFGETPLUGINMANAGER _pfnGetPluginManager;
-};
 
 //
-// Initialize static data.
+// Initialize static singleton data.
 //
 AAFDLL * AAFDLL::_singleton = NULL;
 
-
-//
-// Forward declaration for factory function just returns an instance 
-// of the currect platform dll wrapper object.
-//
-AAFDLL * MakeAAFDLL();
 
 
 
@@ -251,7 +94,7 @@ static AAFUnloadWrapper g_GuarenteeAAFUnload;
 // initialize the following functions to call the corresponding
 // entry point exported from the DLL.
 // 
-STDAPI AAFLoad(wchar_t * dllname)
+STDAPI AAFLoad(const char * dllname)
 {
   AAFDLL *pAAFDLL = AAFDLL::GetAAFDLL();
   if (NULL != pAAFDLL)
@@ -262,13 +105,8 @@ STDAPI AAFLoad(wchar_t * dllname)
   if (NULL == pAAFDLL)
     return AAFRESULT_NOMEMORY;
 
-  if (NULL == dllname)
-  { // use a realistic default name.
-    dllname = L"AAFCOAPI.dll";
-  }
-
   // Attempt to load the dll and initialize the entry points.
-  // If the load fails the cleanup immediately.
+  // If the load fails cleanup immediately.
   HRESULT hr = pAAFDLL->Load(dllname);
   if (S_OK != hr)
     AAFUnload();
@@ -457,11 +295,56 @@ STDAPI AAFGetPluginManager (
 
 
 
+
+
+//***********************************************************
+//
 // Constructor for the base class
 AAFDLL::AAFDLL()
 {
   TRACE("AAFDLL::AAFDLL");
   ASSERT("There Can Be Only One!", NULL == _singleton);
+  _singleton = this;
+  
+  // Initialize all funtions pointers
+  ClearEntrypoints();
+}
+
+
+
+//***********************************************************
+//
+// Destructor for the base class
+AAFDLL::~AAFDLL()
+{
+  TRACE("AAFDLL::~AAFDLL");
+  ASSERT("There Can Be Only One!", this == _singleton);
+
+  // Reset the entry point function pointers to NULL.
+  ClearEntrypoints();
+
+  // Clear the reference to the singleton
+  _singleton = NULL;
+}
+
+
+
+//***********************************************************
+//
+// Static accessor method for the singleton.
+//
+AAFDLL * AAFDLL::GetAAFDLL()
+{
+  return _singleton;
+}
+
+
+
+//***********************************************************
+//
+// Resets all entry point function pointers to NULL.
+void AAFDLL::ClearEntrypoints()
+{
   _pfnOpenExistingRead = NULL;
   _pfnOpenExistingModify = NULL;
   _pfnOpenNewModify = NULL;
@@ -469,19 +352,12 @@ AAFDLL::AAFDLL()
   _pfnGetPluginManager = NULL;
 }
 
-// Destructor for the base class
-AAFDLL::~AAFDLL()
-{
-  TRACE("AAFDLL::~AAFDLL");
-  ASSERT("There Can Be Only One!", this == _singleton);
 
-  _singleton = NULL;
-}
 
-AAFDLL * AAFDLL::GetAAFDLL()
-{
-  return _singleton;
-}
+//***********************************************************
+//
+// Wrapper functions for calling member entry points.
+//
 
 HRESULT AAFDLL::OpenExistingRead (
     wchar_t *  pFileName,
@@ -490,6 +366,7 @@ HRESULT AAFDLL::OpenExistingRead (
 {
   TRACE("AAFDLL::OpenExistingRead");
   ASSERT("Valid dll callback function", _pfnOpenExistingRead);
+  
   return _pfnOpenExistingRead(pFileName, modeFlags, ppFile);  
 }
   
@@ -537,157 +414,20 @@ HRESULT AAFDLL::GetPluginManager (
 
 
 
-#if defined(macintosh) || defined(_MAC)
-class MacAAFDLL : public AAFDLL
-{
-public:
-  // Constructor and destructor.
-  MacAAFDLL();
-  ~MacAAFDLL();
 
-  // Implements Mac specific initialization of dll and entry points.
-  virtual HRESULT Load(wchar_t *);
-  
-  // Implements Mac specific cleanup of dll and entry points.
-  virtual HRESULT Unload();
+// Define UNKNOWNAAFDLL to satisfy the compiler and linker for platforms
+// where there is not a working implementation.
 
-private:
-  // The handle to the dll's module.
-  long _libref;
-};
-
-// Factory function just returns an instance of the currect platform
-// dll wrapper object.
-AAFDLL * MakeAAFDLL()
-{
-  AAFDLL *pAAFDLL =  new MacAAFDLL;
-  AAFDLL::_singleton = pAAFDLL;
-  return pAAFDLL;
-}
+#if defined(UNKNOWNAAFDLL)
 
 
-HRESULT MacAAFDLL::Load(wchar_t *)
-{
-  return AAFRESULT_NOT_IMPLEMENTED;
-}
-
-
-HRESULT MacAAFDLL::Unload()
-{
-  return AAFRESULT_NOT_IMPLEMENTED;
-}
- 
-
-#elif defined(WIN32) || defined(_WIN32)
-
-class Win32AAFDLL : public AAFDLL
-{
-public:
-  // Constructor and destructor.
-  Win32AAFDLL();
-  ~Win32AAFDLL();
-
-  // Implements Win32 specific initialization of dll and entry points.
-  virtual HRESULT Load(wchar_t *);
-  
-  // Implements Win32 specific cleanup of dll and entry points.
-  virtual HRESULT Unload();
-
-private:
-  // The handle to the dll's module instance.
-  HINSTANCE _hInstance;
-};
-
-
-// Factory function just returns an instance of the currect platform
-// dll wrapper object.
-AAFDLL * MakeAAFDLL()
-{
-  TRACE("MakeAAFDLL for Win32AAFDLL");
-  ASSERT("There Can Be Only One!", NULL == AAFDLL::_singleton);
-  AAFDLL *pAAFDLL =  new Win32AAFDLL;
-  AAFDLL::_singleton = pAAFDLL;
-  return pAAFDLL;
-}
-
-
-Win32AAFDLL::Win32AAFDLL()
-{
-  _hInstance = NULL;
-}
-
-Win32AAFDLL::~Win32AAFDLL()
-{
-}
-
-
-HRESULT Win32AAFDLL::Load(wchar_t *dllname)
-{
-  // Attempt to load the library.
-  _hInstance = ::LoadLibraryW(dllname);
-
-  if (NULL == _hInstance)
-  {
-    return HRESULT_FROM_WIN32(GetLastError());
-  }
-
-  //
-  // Attempt to initialize the entry points...
-  //
-  _pfnOpenExistingRead = (LPFNAAFFILEOPENEXISTINGREAD)
-                         GetProcAddress(_hInstance, "AAFFileOpenExistingRead");
-  if (NULL == _pfnOpenExistingRead)
-    return HRESULT_FROM_WIN32(GetLastError());
-
-  _pfnOpenExistingModify = (LPFNAAFFILEOPENEXISTINGMODIFY)
-                           GetProcAddress(_hInstance, "AAFFileOpenExistingModify");
-  if (NULL == _pfnOpenExistingModify)
-    return HRESULT_FROM_WIN32(GetLastError());
-
-  _pfnOpenNewModify = (LPFNAAFFILEOPENNEWMODIFY)
-                      GetProcAddress(_hInstance, "AAFFileOpenNewModify");
-  if (NULL == _pfnOpenNewModify)
-    return HRESULT_FROM_WIN32(GetLastError());
-
-  _pfnOpenTransient = (LPFNAAFFILEOPENTRANSIENT)
-                         GetProcAddress(_hInstance, "AAFFileOpenTransient");
-  if (NULL == _pfnOpenTransient)
-    return HRESULT_FROM_WIN32(GetLastError());
-
-  _pfnGetPluginManager = (LPFNAAFGETPLUGINMANAGER)
-                         GetProcAddress(_hInstance, "AAFGetPluginManager");
-  if (NULL == _pfnGetPluginManager)
-    return HRESULT_FROM_WIN32(GetLastError());
-
-  return S_OK;
-}
-
-
-HRESULT Win32AAFDLL::Unload()
-{
-  if (_hInstance)
-  {
-    _pfnOpenExistingRead = NULL;
-    _pfnOpenExistingModify = NULL;
-    _pfnOpenNewModify = NULL;
-    _pfnOpenTransient = NULL;
-
-    if (!::FreeLibrary(_hInstance))
-      return HRESULT_FROM_WIN32(GetLastError());
-
-    _hInstance = NULL;
-  }
-  
-  return S_OK;
-}
-
-#else
-
+//***********************************************************
+//
 class UnknownAAFDLL : public AAFDLL
 {
 public:
   // Implements Unknown platform specific initialization of dll and entry points.
-  virtual HRESULT Load(wchar_t *aafdll);
+  virtual HRESULT Load(const char *dllname);
   
   // Implements Unknown platform specific cleanup of dll and entry points.
   virtual HRESULT Unload();
@@ -696,27 +436,35 @@ private:
   // The handle to the dll's module instance.
 };
 
+
+
+//***********************************************************
+//
 // Factory function just returns an instance of the currect platform
 // dll wrapper object.
 AAFDLL * MakeAAFDLL()
 {
-  TRACE("MakeAAFDLL for UnknownAAFDLL");
-  ASSERT("There Can Be Only One!", NULL == AAFDLL::_singleton);
   AAFDLL *pAAFDLL =  new UnknownAAFDLL;
-  AAFDLL::_singleton = pAAFDLL;
   return pAAFDLL;
 }
 
 
-HRESULT UnknownAAFDLL::Load(wchar_t *)
+
+//***********************************************************
+//
+HRESULT UnknownAAFDLL::Load(const char *)
 {
   return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
+
+//***********************************************************
+//
 HRESULT UnknownAAFDLL::Unload()
 {
   return AAFRESULT_NOT_IMPLEMENTED;
 }
  
-#endif
+
+#endif /* #if defined(UNKNOWNAAFDLL) */
