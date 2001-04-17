f6e37b07dad5d2db533190598aaf1a831750567c
tbingham <>
2001-04-17 07:03:17 +0000

    Fixed bug in Mac-only code: Init method now only takes a single argument.

--- examples/com-api/ComExtension/ComPersonnelExtension/AAFPersonnelExtension.cpp
+++ examples/com-api/ComExtension/ComPersonnelExtension/AAFPersonnelExtension.cpp
@@ -310,7 +310,7 @@ DllInitializationRoutine(CFragInitBlockPtr initBlkPtr)
 	__initialize(initBlkPtr);
 
 	// Initialize the inproc server object with a copy of the contents of the initBlkPtr.
-	g_AAFPluginServer.Init(AAFPluginObjectMap, (HINSTANCE)&DllData.InitBlock);
+	g_AAFPluginServer.Init((HINSTANCE)&DllData.InitBlock);
 
 	if (!DllData.Inited && DllData.InitBlock.fragLocator.where == kDataForkCFragLocator)
 	{
