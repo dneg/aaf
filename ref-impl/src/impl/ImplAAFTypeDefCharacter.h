f2097b751203de12571d8fcface64dd2a7b6fe19
tbingham <>
2001-04-17 04:25:32 +0000

    Added support for aafCharacter (runtime type), TypeDefCharacter (type and class definition), TypeDefString (uses aafCharacter and TypeDefCharacter instead of UInt16. Strings and characters should now work on unix systems that have sizeof(wchar_t) == 4 (this is untested).

---
+++ ref-impl/src/impl/ImplAAFTypeDefCharacter.h
@@ -0,0 +1,148 @@
+//@doc
+//@class    AAFTypeDefCharacter | Implementation class for AAFTypeDefCharacter
+#ifndef __ImplAAFTypeDefCharacter_h__
+#define __ImplAAFTypeDefCharacter_h__
+
+
+/***********************************************************************
+*
+*              Copyright (c) 1998-1999 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted,
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+*  prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
+
+
+
+class ImplAAFPropertyValue;
+
+
+
+
+
+
+#ifndef __ImplAAFTypeDef_h__
+#include "ImplAAFTypeDef.h"
+#endif
+
+
+class ImplAAFTypeDefCharacter : public ImplAAFTypeDef
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplAAFTypeDefCharacter ();
+
+protected:
+  virtual ~ImplAAFTypeDefCharacter ();
+
+public:
+
+
+  //****************
+  // Initialize()
+  //
+  AAFRESULT pvtInitialize
+        (// @parm [in, ref] auid to be used to identify this type
+         aafUID_constref  id,
+
+         // @parm [in] friendly name of this type definition
+         aafCharacter_constptr  pTypeName);
+
+  //****************
+  // CreateValueFromCharacter()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    CreateValueFromCharacter
+        (// @parm [in] character data to write the the new value
+         aafCharacter  character,
+
+         // @parm [out] character property value with the given character
+         ImplAAFPropertyValue ** ppCharacterValue);
+
+  //****************
+  // GetCharacter()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetCharacter
+        (// @parm [in] character property value to read
+         ImplAAFPropertyValue * pCharacterValue,
+
+         // @parm [out] character that is read
+         aafCharacter *  pCharacter);
+
+  // Override from AAFTypeDef
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetTypeCategory (/*[out]*/ eAAFTypeCategory_t *  pTid);
+
+public:
+  //
+  // OMType methods
+  //
+  virtual void reorder(OMByte* externalBytes,
+                       size_t externalBytesSize) const;
+
+  virtual size_t externalSize(OMByte* internalBytes,
+                              size_t internalBytesSize) const;
+
+  virtual void externalize(OMByte* internalBytes,
+                           size_t internalBytesSize,
+                           OMByte* externalBytes,
+                           size_t externalBytesSize,
+                           OMByteOrder byteOrder) const;
+
+  virtual size_t internalSize(OMByte* externalBytes,
+                              size_t externalSize) const;
+
+  virtual void internalize(OMByte* externalBytes,
+                           size_t externalBytesSize,
+                           OMByte* internalBytes,
+                           size_t internalBytesSize,
+                           OMByteOrder byteOrder) const;
+
+  
+  
+  //
+  // ImplAAFTypeDef methods
+  //
+  virtual aafBool IsFixedSize (void) const;
+  virtual size_t PropValSize (void) const;
+  virtual aafBool IsRegistered (void) const;
+  virtual size_t NativeSize (void) const;
+
+  virtual OMProperty * 
+    pvtCreateOMPropertyMBS (OMPropertyId pid,
+							const char * name) const;
+
+  virtual bool IsAggregatable () const;
+  virtual bool IsStreamable () const;
+  virtual bool IsFixedArrayable () const;
+  virtual bool IsVariableArrayable () const;
+  virtual bool IsStringable () const;
+
+};
+
+#endif // ! __ImplAAFTypeDefCharacter_h__
+
+
