12ea752cf6bda63fbd54dac46400b3a0d59d2895
tbingham <>
2001-04-17 07:45:51 +0000

    Do the shuffle - the header file shuffle.

--- ref-impl/include/OM/OMContainerProperty.h
+++ ref-impl/include/OM/OMContainerProperty.h
@@ -1,6 +1,6 @@
 /***********************************************************************
 *
-*              Copyright (c) 1998-1999 Avid Technology, Inc.
+*              Copyright (c) 1998-2000 Avid Technology, Inc.
 *
 * Permission to use, copy and modify this software and accompanying
 * documentation, and to distribute and sublicense application software
@@ -29,7 +29,7 @@
 #ifndef OMCONTAINERPROPERTY_H
 #define OMCONTAINERPROPERTY_H
 
-#include "OMPropertyBase.h"
+#include "OMProperty.h"
 
   // @class Abstract base class for persistent container properties
   //        supported by the Object Manager.
