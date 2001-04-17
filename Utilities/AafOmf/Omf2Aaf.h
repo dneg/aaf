e9a8da043c1733c8ce6f7a6e43bda4845b0352fb
tbingham <>
2001-04-17 06:41:30 +0000

    added prototype GetAAFOperationDef.

--- Utilities/AafOmf/Omf2Aaf.h
+++ Utilities/AafOmf/Omf2Aaf.h
@@ -77,7 +77,8 @@ public:
 	HRESULT ConvertOMFLocator(OMF2::omfObject_t locator, IAAFEssenceDescriptor* pEssenceDesc);
 	HRESULT ConvertOMFCDCIDescriptorLocator(OMF2::omfObject_t mediaDescriptor, IAAFCDCIDescriptor* pAAFDescriptor);
 	HRESULT ConvertOMFEffects(OMF2::omfEffObj_t effect, IAAFOperationGroup* pEffect);
-	HRESULT ConvertOMFEffectDefinition(OMF2::omfDDefObj_t effectDef, aafUID_t* pEffectAUID, IAAFOperationDef** ppEffectDef);
+	HRESULT ConvertOMFEffectDefinition(OMF2::omfDDefObj_t effectDef, IAAFOperationDef** ppEffectDef);
+	HRESULT GetAAFOperationDefinition(OMF2::omfUniqueName_t datakindName, char* defName, char* defDescription, aafUInt32 bypassOverride, aafBool isTimeWarp, aafInt32 numberInputs, aafUID_t defDataDef, IAAFOperationDef** ppEffectDef);
 	HRESULT GetParameterDefinition(aafUID_t* pDefUID, IAAFTypeDef* pTypeDef,	aafWChar* pwName, aafWChar* pwDesc, aafWChar* pwDisplayUnits, IAAFParameterDef** ppParameterDef);
 	HRESULT	ConvertOMFConstValue(OMF2::omfSegObj_t segment, IAAFConstantValue* pConstValue);
 	HRESULT	ConvertOMFVaryingValue(OMF2::omfSegObj_t segment, IAAFVaryingValue* pVaryingValue);
