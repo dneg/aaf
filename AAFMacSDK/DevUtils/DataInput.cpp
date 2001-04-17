03b19c2da66713fc7c7a45459320470e33e9d17d
tbingham <>
2001-04-17 11:58:07 +0000

    shared source file that gets input data for the examples and test apps

---
+++ AAFMacSDK/DevUtils/DataInput.cpp
@@ -0,0 +1,75 @@
+#include <console.h> /* Mac command line window */
+#include <fstream.h>
+#include "DataInput.h"
+
+void getInputData(int *pargc, char **argv, char *inputFile)
+{		
+	char *newstring = NULL;
+	
+	/* try to open inputFile for read */
+	ifstream ifsCommandLine(inputFile);
+		
+	/* Test to make sure it is open.  If not it doesn't exist */
+	if (ifsCommandLine.is_open()) 
+	{
+		char inputstr[100];
+		char appname[] = "Dummy App Name";
+		int  count = 0;
+
+		/* Determine the number of lines */
+		while (	ifsCommandLine >> inputstr )
+			++count;
+ 
+		/* *pargc = count + 1 extra for appName */
+		*pargc = count + 1;  
+		
+		/* Create the array of char pointers */
+		*argv = new char [*pargc];
+
+		/* Reset file pointer to beginning of file */
+		ifsCommandLine.clear();
+		ifsCommandLine.seekg(0);
+
+		count = 0;
+		newstring = new char [strlen(appname)+1];
+		strcpy(newstring, appname);
+		argv[0] = newstring;
+
+		/* Get Data */
+		while (	ifsCommandLine >> inputstr )	
+		{
+			newstring = new char [strlen(inputstr)+1];
+			strcpy(newstring, inputstr);
+			argv[++count] = newstring;
+		}
+		ifsCommandLine.close();
+	}
+	else	
+	{	
+		char **tempargv = NULL;
+		int tempargc = 0;
+
+		/* console window for mac */
+		tempargc = ccommand(&tempargv);
+
+		*pargc = tempargc;
+		
+		/* Create the array of char pointers */
+		*argv = new char [*pargc];
+
+		/* Fill it in */
+		for (int i = 0; i<*pargc; ++i)
+		{
+			newstring = new char [strlen(tempargv[i])+1];
+			strcpy(newstring, tempargv[i]);
+			argv[i] = newstring;
+		}
+	}
+}
+
+
+void cleanUpInputData(int pargc, char **argv)
+{
+	for ( int i=0; i<pargc; ++i)
+		delete [] argv[i];
+}
