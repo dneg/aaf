915976ea0cb9fab2e96102498b677a2f526eca8f
tbingham <>
2001-04-17 02:43:31 +0000

    New sources for vc pre-compiled headers.

---
+++ AAFWinSDK/ref-impl/Impl/stdafx.h
@@ -0,0 +1,19 @@
+// stdafx.h : include file for standard system include files,
+//      or project specific include files that are used frequently,
+//      but are changed infrequently
+
+#if !defined(AFX_STDAFX_H__180166C2_2418_11D3_BFCC_00104BC9156D__INCLUDED_)
+#define AFX_STDAFX_H__180166C2_2418_11D3_BFCC_00104BC9156D__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "AAFTypes.h"
+#include "AAFResult.h"
+
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__180166C2_2418_11D3_BFCC_00104BC9156D__INCLUDED_)
