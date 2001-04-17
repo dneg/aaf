77e7675cbbb6436da782e49126a93365eb181d54
tbingham <>
2001-04-17 10:47:18 +0000

    Adding support support for Weak references in the DM, singltons, variable arrays and sets.

--- test/com/ComModTestAAF/ModuleTests/CAAFTypeDefWeakObjRefTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFTypeDefWeakObjRefTest.cpp
@@ -49,6 +49,12 @@ typedef IAAFSmartPointer<IAAFPropertyDef>           IAAFPropertyDefSP;
 typedef IAAFSmartPointer<IAAFPropertyValue>         IAAFPropertyValueSP;
 typedef IAAFSmartPointer<IAAFTypeDef>               IAAFTypeDefSP;
 typedef IAAFSmartPointer<IAAFTypeDefWeakObjRef>     IAAFTypeDefWeakObjRefSP;
+typedef IAAFSmartPointer<IAAFMob>                   IAAFMobSP;
+typedef IAAFSmartPointer<IAAFSequence>              IAAFSequenceSP;
+typedef IAAFSmartPointer<IAAFFiller>                IAAFFillerSP;
+typedef IAAFSmartPointer<IAAFComponent>             IAAFComponentSP;
+typedef IAAFSmartPointer<IAAFSegment>               IAAFSegmentSP;
+typedef IAAFSmartPointer<IAAFTimelineMobSlot>       IAAFTimelineMobSlotSP;
 
 
 #include <iostream.h>
@@ -59,7 +65,7 @@ typedef IAAFSmartPointer<IAAFTypeDefWeakObjRef>     IAAFTypeDefWeakObjRefSP;
 
 // Weak references may not be in v1.0...
 #ifndef ENABLE_WEAK_REFERENCES
-#define ENABLE_WEAK_REFERENCES 0
+#define ENABLE_WEAK_REFERENCES 1
 #endif
 
 
@@ -78,7 +84,7 @@ extern "C"
 
 
 extern "C" HRESULT CAAFTypeDefWeakObjRef_test(testMode_t);
-extern "C" HRESULT CAAFTypeDefWeakObjRef_test(testMode_t /*mode*/)
+extern "C" HRESULT CAAFTypeDefWeakObjRef_test(testMode_t mode)
 {
 #if ENABLE_WEAK_REFERENCES
 
@@ -102,7 +108,7 @@ extern "C" HRESULT CAAFTypeDefWeakObjRef_test(testMode_t /*mode*/)
   if (SUCCEEDED (result))
   {
     cout << "The following IAAFTypeWeakObjRef methods have not been tested yet:" << endl;
-    cout << "     Initialize" << endl;
+//    cout << "     Initialize" << endl;
     cout << "     GetObjectType" << endl;
     cout << "     GetObject" << endl;
     cout << "     SetObject" << endl;
@@ -124,9 +130,21 @@ extern "C" HRESULT CAAFTypeDefWeakObjRef_test(testMode_t /*mode*/)
 extern "C" const aafUID_t kAAFTypeID_TestWeakReferenceToType = 
 { 0x012345678, 0x0123, 0x0123, {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77}};
 
-const aafCharacter kMyWeakReferenceToTypeDefinitionName[] = L"My Weak Reference to Type Definitions";
+// Constant for "new" weak reference to a type definition.
+extern "C" const aafUID_t kAAFPropID_TestWeakReferenceToType = 
+{ 0x012345679, 0x0123, 0x0123, {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77}};
 
-#ifndef _DEBUG
+const aafCharacter kMyWeakReferenceToTypeDefinitionName[] = L"My Weak Reference to Type Definitions";
+const aafCharacter kMyWeakReferenceToTypeDefinitionPropertyName[] = L"My Weak Reference to Type Definition Property";
+
+
+static const 	aafMobID_t	TEST_MobID =
+{{0x06, 0x0c, 0x2b, 0x34, 0x02, 0x05, 0x11, 0x01, 0x01, 0x00, 0x10, 0x00},
+0x13, 0x00, 0x00, 0x00,
+{0xc68dee89, 0x0405, 0x11d4, 0x8e, 0x3d, 0x00, 0x90, 0x27, 0xdf, 0xca, 0x7c}};
+
+
+#if 1 //#ifndef _DEBUG
 // convenient error handlers.
 inline void checkResult(HRESULT r)
 {
@@ -142,8 +160,9 @@ inline void checkExpression(bool expression, HRESULT r)
 #else // #ifndef _DEBUG
 
 // convenient error handlers.
-#define checkResult(r)\
+#define checkResult(x)\
 do {\
+  HRESULT r = (x);\
   if (FAILED(r))\
   {\
     cerr << "FILE:" << __FILE__ << " LINE:" << __LINE__ << " Error code = " << hex << r << dec << endl;\
@@ -183,23 +202,11 @@ static void RemoveTestFile(const wchar_t* pFileName)
 // Create the test file.
 void CAAFTypeDefWeakObjRef_create (aafCharacter_constptr pFileName) // throw HRESULT
 {
-  aafProductIdentification_t	ProductInfo;
-  IAAFFileSP pFile;
-  IAAFHeaderSP pHeader;
-  IAAFDictionarySP pDictionary;
-  IAAFTypeDefWeakObjRefSP pWeakObjRef;
-  IAAFClassDefSP pClassDef;
-  IAAFTypeDefSP pTypeDef;
-  eAAFTypeCategory_t category;
-  IAAFPropertyValueSP pPropertyValue;
-
-	aafUID_t targetSet[1];
-
-
   // Remove the previous test file is one exists
   RemoveTestFile (pFileName);
 
   // Create the file.
+  aafProductIdentification_t	ProductInfo;
   aafProductVersion_t v;
   v.major = 1;
   v.minor = 0;
@@ -215,48 +222,131 @@ void CAAFTypeDefWeakObjRef_create (aafCharacter_constptr pFileName) // throw HRE
   ProductInfo.platform = NULL;
 
 
+  IAAFFileSP pFile;
   checkResult (AAFFileOpenNewModify(pFileName, 0, &ProductInfo, &pFile));
-  checkResult (pFile->GetHeader (&pHeader));
-  checkResult (pHeader->GetDictionary (&pDictionary));
   
-  CAAFBuiltinDefs defs (pDictionary);
- 
-  // Create a Mob
-  checkResult(pDictionary->CreateMetaInstance(AUID_AAFTypeDefWeakObjRef,
-                                              IID_IAAFTypeDefWeakObjRef,
-                                              (IUnknown **)&pWeakObjRef));
+  try
+  {
+    IAAFHeaderSP pHeader;
+    checkResult (pFile->GetHeader (&pHeader));
+    
+    IAAFDictionarySP pDictionary;
+    checkResult (pHeader->GetDictionary (&pDictionary));
+    
+    CAAFBuiltinDefs defs (pDictionary);
+   
+    // Create a Mob
+    IAAFTypeDefWeakObjRefSP pWeakObjRef;
+    checkResult(pDictionary->CreateMetaInstance(AUID_AAFTypeDefWeakObjRef,
+                                                IID_IAAFTypeDefWeakObjRef,
+                                                (IUnknown **)&pWeakObjRef));
+    
+    // Find the class definition for all type definitions.
+    IAAFClassDefSP pClassDef;
+    checkResult(pDictionary->LookupClassDef(AUID_AAFTypeDef, &pClassDef));
+    
+
+	  aafUID_t targetSet[2];
+    targetSet[0] = kAAFPropID_Root_MetaDictionary;
+    targetSet[1] = kAAFPropID_MetaDictionary_TypeDefinitions;
+    checkResult(pWeakObjRef->Initialize(kAAFTypeID_TestWeakReferenceToType,
+                                        pClassDef,
+                                        kMyWeakReferenceToTypeDefinitionName,
+                                        sizeof(targetSet)/sizeof(aafUID_t),
+                                        targetSet));
+
+    // Validate that we make the correct "type" by inspecting the type category.
+    IAAFTypeDefSP pTypeDef;
+    checkResult(pWeakObjRef->QueryInterface(IID_IAAFTypeDef, (void **)&pTypeDef));
+
+    eAAFTypeCategory_t category;
+    checkResult(pTypeDef->GetTypeCategory(&category));
+    checkExpression(kAAFTypeCatWeakObjRef == category, AAFRESULT_TEST_FAILED);
+
+
+  	// Add the new type to the dictionary.
+  	checkResult(pDictionary->RegisterTypeDef(pTypeDef));
+  	
+  	
+  	// Now add a new optional weak reference property to an existing class.
+  	IAAFClassDefSP pFillerClass;
+  	IAAFPropertyDefSP pTypePropertyDef;  	
+    checkResult(pDictionary->LookupClassDef(AUID_AAFComponent, &pFillerClass));   
+    checkResult(pFillerClass->RegisterOptionalPropertyDef(
+      kAAFPropID_TestWeakReferenceToType,
+      kMyWeakReferenceToTypeDefinitionPropertyName,
+      pTypeDef,
+      &pTypePropertyDef));
   
-  // Find the class definition for all type definitions.
-  checkResult(pDictionary->LookupClassDef(AUID_AAFTypeDef, &pClassDef));
+
+
   
+    //
+	  // Create a Composition Mob
+	  //
+	  IAAFMobSP pMob;
+	  checkResult(defs.cdCompositionMob()->CreateInstance(IID_IAAFMob, (IUnknown **)&pMob));
+	  checkResult(pMob->SetMobID(TEST_MobID));
+	  checkResult(pMob->SetName(L"TestCompMob"));
+	  
+	  //
+	  // Create a sequence to hold our test fillers components.
+	  //
+	  IAAFSequenceSP pSequence;
+	  checkResult (defs.cdSequence()->CreateInstance(IID_IAAFSequence, (IUnknown **)&pSequence));
+	  checkResult (pSequence->Initialize (defs.ddPicture()));
 
-  targetSet[0] = kAAFPropID_MetaDictionary_TypeDefinitions;
-#if 0
-  checkResult(pWeakObjRef->Initialize(kAAFTypeID_TestWeakReferenceToType,
-                                      pClassDef,
-                                      kMyWeakReferenceToTypeDefinitionName,
-                                      1,
-                                      targetSet);
-#else
-  HRESULT hr = pWeakObjRef->Initialize(kAAFTypeID_TestWeakReferenceToType,
-                                      pClassDef,
-                                      kMyWeakReferenceToTypeDefinitionName,
-                                      1,
-                                      targetSet);
- checkResult(hr);
-#endif
-
-  // Validate that we make the correct "type" by inspecting the type category.
-  checkResult(pWeakObjRef->QueryInterface(IID_IAAFTypeDef, (void **)&pTypeDef));
-  checkResult(pTypeDef->GetTypeCategory(&category));
-  checkExpression(kAAFTypeCatWeakObjRef == category, AAFRESULT_TEST_FAILED);
+	  
+	  IAAFFillerSP pFiller1;
+	  checkResult (defs.cdFiller()->CreateInstance(IID_IAAFFiller, (IUnknown **)&pFiller1));
+	  checkResult (pFiller1->Initialize (defs.ddPicture(), 16));
+	
+		IAAFFillerSP pFiller2;
+	  checkResult (defs.cdFiller()->CreateInstance(IID_IAAFFiller, (IUnknown **)&pFiller2));
+	  checkResult (pFiller2->Initialize (defs.ddPicture(), 32));
 
 
-	// Add the new type to the dictionary.
-	checkResult(pDictionary->RegisterTypeDef(pTypeDef));
+    //
+    // Add the initialized fillers to the sequence.
+    //
+    IAAFComponentSP pComp1;
+  	checkResult(pFiller1->QueryInterface(IID_IAAFComponent, (void **)&pComp1));
+  	checkResult(pSequence->AppendComponent(pComp1));
 
-  checkResult(pFile->Save());
-  checkResult(pFile->Close());
+    IAAFComponentSP pComp2;
+  	checkResult(pFiller2->QueryInterface(IID_IAAFComponent, (void **)&pComp2));	
+  	checkResult(pSequence->AppendComponent(pComp2));
+  	
+  	//
+  	// Append the sequence as the segment in a new timeline mob slot.
+  	//
+  	IAAFSegmentSP pSegment;
+  	checkResult(pSequence->QueryInterface(IID_IAAFSegment, (void **)&pSegment));
+   	aafRational_t  editRate = {30000, 1001};
+   	aafCharacter_constptr pSlotName = L"Slot 1";
+   	aafPosition_t  origin = 0;
+   	IAAFTimelineMobSlotSP pNewSlot;
+   	checkResult(pMob->AppendNewTimelineSlot(editRate, pSegment, 1, pSlotName, origin, &pNewSlot));
+
+    //
+    // Add to the set of mobs in the file.
+    //
+    checkResult(pHeader->AddMob(pMob));
+
+    checkResult(pFile->Save());
+    checkResult(pFile->Close());
+  }
+  catch (HRESULT& rhr)
+  {
+     pFile->Save();  // This may not be safe???
+     pFile->Close();
+     throw rhr;
+  }
+  catch (...)
+  {
+     pFile->Close();  
+     throw;
+  }
 }
 
 
