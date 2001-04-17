7a85055a278be96a5e8d82b49b7170b4a3c9cd82
tbingham <>
2001-04-17 04:42:03 +0000

    Initial implementation - persistent properties

---
+++ ref-impl/src/impl/ImplAAFNestedScope.h
@@ -0,0 +1,103 @@
+//@doc
+//@class    AAFNestedScope | Implementation class for AAFNestedScope
+#ifndef __ImplAAFNestedScope_h__
+#define __ImplAAFNestedScope_h__
+
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+#ifndef __AAFTypes_h__
+#include "AAFTypes.h"
+#endif
+
+/*************************************************************************
+ * 
+ * @class AAFNestedScope | an AAFNestedScope object defines a scope that 
+ *			contains an ordered set of AAFSegments and produces the value
+ *			specified by the last AAFSegement in the ordered set.
+ *
+ * @base public | AAFSegment
+ *
+ *************************************************************************/
+
+#ifndef __ImplAAFSegment_h__
+#include "ImplAAFSegment.h"
+#endif
+
+class ImplEnumAAFSegments;
+
+
+
+
+
+
+#ifndef __ImplAAFSegment_h__
+#include "ImplAAFSegment.h"
+#endif
+
+
+class ImplAAFNestedScope : public ImplAAFSegment
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplAAFNestedScope ();
+
+protected:
+  virtual ~ImplAAFNestedScope ();
+
+public:
+
+
+  //****************
+  // AppendSegment()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    AppendSegment
+        // @parm [in] Pointer to segment to be added
+        (ImplAAFSegment * pSegment);
+
+  //****************
+  // RemoveSegment()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    RemoveSegment
+        // @parm [in] Pointer to segment to be added
+        (ImplAAFSegment * pSegment);
+
+  //****************
+  // GetSlots()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetSlots
+        // @parm [retval][out] Slots - segment list  enumeration
+        (ImplEnumAAFSegments ** ppEnum);
+
+
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFNestedScope)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFNestedScopeTest.cpp.
+  static AAFRESULT test();
+
+	// Persistent Properties	
+  OMStrongReferenceVectorProperty<ImplAAFSegment>		_slots;
+};
+
+#endif // ! __ImplAAFNestedScope_h__
+
+
