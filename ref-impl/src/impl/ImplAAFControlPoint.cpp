cd2cbafc3d05b60fae1d838b15a7d74b7d30701e
tbingham <>
2001-04-17 05:24:50 +0000

    Start implementing effects classes

--- ref-impl/src/impl/ImplAAFControlPoint.cpp
+++ ref-impl/src/impl/ImplAAFControlPoint.cpp
@@ -27,17 +27,33 @@
 
 
 #include "AAFStoredObjectIDs.h"
+#include "AAFPropertyIDs.h"
 
 #ifndef __ImplAAFControlPoint_h__
 #include "ImplAAFControlPoint.h"
 #endif
 
+#include "ImplAAFDictionary.h"
+#include "ImplAAFPluggableDef.h"
+#include "ImplAAFTypeDef.h"
+#include "ImplAAFHeader.h"
+
 #include <assert.h>
 #include <string.h>
+#include "aafErr.h"
 
 
 ImplAAFControlPoint::ImplAAFControlPoint ()
-{}
+: _type(			PID_ControlPoint_Type,			"Type"),
+  _time(			PID_ControlPoint_Time,			"Time"),
+  _value(			PID_ControlPoint_Value,			"Value"),
+  _hint(			PID_ControlPoint_EditHint,		"EditHint")
+{
+	_persistentProperties.put(_type.address());
+	_persistentProperties.put(_time.address());
+	_persistentProperties.put(_value.address());
+	_persistentProperties.put(_hint.address());
+}
 
 
 ImplAAFControlPoint::~ImplAAFControlPoint ()
@@ -46,79 +62,145 @@ ImplAAFControlPoint::~ImplAAFControlPoint ()
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFControlPoint::GetTime (
-      aafRational_t *  /*pTime*/)
+      aafRational_t *pTime)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+	if(pTime == NULL)
+		return AAFRESULT_NULL_PARAM;
+	*pTime = _time;
+	return AAFRESULT_SUCCESS;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFControlPoint::GetEditHint (
-      aafEditHint_t *  /*pEditHint*/)
+      aafEditHint_t *pEditHint)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
-}
-
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplAAFControlPoint::GetDataDefID (
-      aafUID_t *  /*pDatadefID*/)
-{
-  return AAFRESULT_NOT_IMPLEMENTED;
+	aafUInt16	intHint;
+	
+	if(pEditHint == NULL)
+		return AAFRESULT_NULL_PARAM;
+	intHint = _hint;
+	*pEditHint = (aafEditHint_t)intHint;
+	return AAFRESULT_SUCCESS;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFControlPoint::GetValueBufLen (
-      aafUInt32 *  /*pLen*/)
+      aafUInt32 * pLen)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+	if(pLen == NULL)
+		return(AAFRESULT_NULL_PARAM);
+
+	*pLen = _value.size();
+
+	return(AAFRESULT_SUCCESS); 
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFControlPoint::GetValue (
-      aafUInt32  /*valueSize*/,
-      aafDataBuffer_t  /*pValue*/,
-      aafUInt32*  /*bytesRead*/)
+      aafUInt32  valueSize,
+      aafDataBuffer_t  pValue,
+      aafUInt32*  bytesRead)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+	if(pValue == NULL || bytesRead == NULL)
+		return(AAFRESULT_NULL_PARAM);
+
+	if (_value.size() > valueSize)
+	  return AAFRESULT_SMALLBUF;
+
+	_value.copyToBuffer(pValue, valueSize);
+	*bytesRead  = _value.size();
+
+	return(AAFRESULT_SUCCESS); 
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFControlPoint::SetTime (
-      aafRational_t  /*pTime*/)
+      aafRational_t pTime)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+	_time = pTime;
+	return AAFRESULT_SUCCESS;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFControlPoint::SetEditHint (
-      aafEditHint_t  /*editHint*/)
+      aafEditHint_t  editHint)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
-}
-
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplAAFControlPoint::SetDataDefID (
-       aafUID_t * /*pDatadefID*/)
-{
-  return AAFRESULT_NOT_IMPLEMENTED;
+	_hint = (aafUInt16)editHint;
+	return AAFRESULT_SUCCESS;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFControlPoint::SetValue (
-      aafUInt32  /*valueSize*/,
-      aafDataBuffer_t  /*pValue*/)
+      aafUInt32  valueSize,
+      aafDataBuffer_t  pValue)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+	if(pValue == NULL)
+		return(AAFRESULT_NULL_PARAM);
+
+	_value.setValue(pValue, valueSize);
+
+	return(AAFRESULT_SUCCESS); 
 }
 
 
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFControlPoint::SetTypeDefinition (
+      ImplAAFTypeDef*  pTypeDef)
+{
+	aafUID_t			newUID;
+	ImplAAFHeader		*head;
+	ImplAAFDictionary	*dict;
+	ImplAAFPluggableDef	*def;
+
+	if(pTypeDef == NULL)
+		return AAFRESULT_NULL_PARAM;
+
+	XPROTECT()
+	{
+		CHECK(pTypeDef->GetAUID(&newUID));
+		CHECK(pTypeDef->MyHeadObject(&head));
+		CHECK(head->GetDictionary(&dict));
+		if(dict->LookupPluggableDef(&newUID, &def) == AAFRESULT_SUCCESS)
+			def->ReleaseReference();
+
+		_type = newUID;
+		pTypeDef->AcquireReference();
+	}
+	XEXCEPT
+	XEND;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFControlPoint::GetTypeDefinition (
+      ImplAAFTypeDef **ppTypeDef)
+{
+	ImplAAFHeader		*head;
+	ImplAAFDictionary	*dict;
+
+	if(ppTypeDef == NULL)
+		return AAFRESULT_NULL_PARAM;
+
+	XPROTECT()
+	{
+		CHECK(MyHeadObject(&head));
+		CHECK(head->GetDictionary(&dict));
+		CHECK(dict->LookupPluggableDef(&_type, (ImplAAFPluggableDef **)ppTypeDef) == AAFRESULT_SUCCESS);
+		(*ppTypeDef)->AcquireReference();
+	}
+	XEXCEPT
+	XEND;
+
+	return AAFRESULT_SUCCESS;
+}
 
 OMDEFINE_STORABLE(ImplAAFControlPoint, AUID_AAFControlPoint);
 
