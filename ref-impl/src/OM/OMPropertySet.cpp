de39321b3aaaa2d80cf8d9a3c90d9b67d4c156d6
tbingham <>
2001-04-17 08:29:17 +0000

    Implement OMPropertySet in terms of OMSet (Actually OMRedBlackTree)

--- ref-impl/src/OM/OMPropertySet.cpp
+++ ref-impl/src/OM/OMPropertySet.cpp
@@ -29,31 +29,22 @@
 #include "OMPropertySet.h"
 #include "OMProperty.h"
 #include "OMUtilities.h"
+#include "OMRedBlackTreeIterator.h"
 
 #include "OMAssertions.h"
 
 #define OMPROPERTYSET_CHUNKSIZE (20)
 
 OMPropertySet::OMPropertySet(void)
-: _propertySet(0), _capacity(OMPROPERTYSET_CHUNKSIZE), _count(0), _container(0)
+: _set(),
+  _container(0)
 {
   TRACE("OMPropertySet::OMPropertySet");
-
-  _propertySet = new OMPropertySetElement[_capacity];
-  ASSERT("Valid heap pointer", _propertySet != 0);
-
-  for (size_t i = 0; i < _capacity; i++) {
-    _propertySet[i]._valid = false;
-  }
-  POSTCONDITION("Valid count", _count <= _capacity);
 }
 
 OMPropertySet::~OMPropertySet(void)
 {
   TRACE("OMPropertySet::~OMPropertySet");
-
-  delete [] _propertySet;
-  _propertySet = 0;
 }
 
   // @mfunc Get the <c OMProperty> associated with the property
@@ -64,16 +55,14 @@ OMPropertySet::~OMPropertySet(void)
 OMProperty* OMPropertySet::get(const OMPropertyId propertyId) const
 {
   TRACE("OMPropertySet::get");
-  OMProperty* result;
+  OMProperty* result = 0;
   PRECONDITION("Valid property id", propertyId != 0 );
   PRECONDITION("Property is allowed", isAllowed(propertyId));
   PRECONDITION("Property is present", isPresent(propertyId));
-  OMPropertySetElement* element = find(propertyId);
-  ASSERT("Element found", element != 0);
-  ASSERT("Valid element", element->_valid);
-  result = element->_property;
+
+  bool status = _set.find(propertyId, result);
+  ASSERT("Property found", status);
   POSTCONDITION("Valid result", result != 0);
-  POSTCONDITION("Valid count", _count <= _capacity);
   return result;
 }
 
@@ -88,10 +77,7 @@ OMProperty* OMPropertySet::get(const wchar_t* propertyName) const
   PRECONDITION("Valid property name", validWideString(propertyName));
   PRECONDITION("Property is present", isPresent(propertyName));
 
-  OMPropertySetElement* element = find(propertyName);
-  ASSERT("Element found", element != 0);
-  ASSERT("Valid element", element->_valid);
-  OMProperty* result = element->_property;
+  OMProperty* result = find(propertyName);
 
   POSTCONDITION("Valid result", result != 0);
   return result;
@@ -103,63 +89,20 @@ OMProperty* OMPropertySet::get(const wchar_t* propertyName) const
 void OMPropertySet::put(OMProperty* property)
 {
   TRACE("OMPropertySet::put");
-  // SAVE(_count);
+
   PRECONDITION("Valid property", property != 0);
   PRECONDITION("Valid property id", property->propertyId() != 0);
   PRECONDITION("Property is not present", !isPresent(property->propertyId()));
 
   OMPropertyId propertyId = property->propertyId();
-  OMPropertySetElement* element = find();
-  if (element == 0) {
-    grow(OMPROPERTYSET_CHUNKSIZE);
-    element = find();
-  }
-  ASSERT("Found space for new element", element != 0);
 
-  element->_propertyId = propertyId;
-  element->_property = property;
-  element->_valid = true;
-  element->_property->setPropertySet(this);
-  _count++;
+  property->setPropertySet(this);
+  _set.insert(propertyId, property);
 
   POSTCONDITION("Property installed", isPresent(property->propertyId()));
   POSTCONDITION("Consistent property set",
-                property == find(property->propertyId())->_property);
-  // POSTCONDITION("Count increased by one", _count == OLD(_count) + 1);
-  POSTCONDITION("Valid count", _count <= _capacity);
+                                      property == get(property->propertyId()));
 }
-
-  // @mfunc Iterate over the <c OMProperty> objects in this
-  //        <c OMPropertySet>. Call this function <mf OMPropertySet::count>
-  //        times to iterate over all the <c OMProperty> objects in this
-  //        <c OMPropertySet>.
-  //   @parm Iteration context. Set this to 0 to start with the
-  //         "first" <c OMProperty>.
-  //   @parm The "current" <c OMProperty>.
-  //   @this const
-void OMPropertySet::iterate(size_t& context, OMProperty*& property) const
-{
-  TRACE("OMPropertySet::iterate");
-
-  OMPropertySetElement* element = 0;
-  size_t start = context;
-  size_t found = 0;
-
-  for (size_t i = start; i < _capacity; i++) {
-    if (_propertySet[i]._valid) {
-      element = &_propertySet[i];
-      found = i;
-      break;
-    }
-  }
-  if (element != 0) {
-    property = element->_property;
-    context = ++found;
-  } else {
-    context = 0;
-  }
-}
-
 
   // @mfunc Is an <c OMProperty> with property id <p propertyId>
   //        present in this <c OMPropertySet> ?
@@ -171,12 +114,9 @@ bool OMPropertySet::isPresent(const OMPropertyId propertyId) const
 {
   TRACE("OMPropertySet::isPresent");
 
-  OMPropertySetElement* element = find(propertyId);
-  if (element != 0) {
-    return true;
-  } else {
-    return false;
-  }
+  OMProperty* p;
+  bool result = _set.find(propertyId, p);
+  return result;
 }
 
   // @mfunc Is an <c OMProperty> with name <p propertyName>
@@ -189,8 +129,8 @@ bool OMPropertySet::isPresent(const wchar_t* propertyName) const
 {
   TRACE("OMPropertySet::isPresent");
 
-  OMPropertySetElement* element = find(propertyName);
-  if (element != 0) {
+  OMProperty* p = find(propertyName);
+  if (p != 0) {
     return true;
   } else {
     return false;
@@ -231,20 +171,20 @@ bool OMPropertySet::isRequired(const OMPropertyId propertyId) const
 size_t OMPropertySet::count(void) const
 {
   TRACE("OMPropertySet::count");
-  POSTCONDITION("Valid count", _count <= _capacity);
-  return _count;
+
+  return _set.count();
 }
 
 size_t OMPropertySet::countPresent(void) const
 {
   TRACE("OMPropertySet::countPresent");
+
   size_t result = 0;
-  for (size_t i = 0; i < _capacity; i++) {
-    if (_propertySet[i]._valid) {
-      OMProperty*p = _propertySet[i]._property;
-      if (!p->isOptional() || p->isPresent()) {
-        result = result + 1;
-      }
+  SetIterator iterator(_set, OMBefore);
+  while (++iterator) {
+    OMProperty* p = iterator.value();
+    if (!p->isOptional() || p->isPresent()) {
+      result = result + 1;
     }
   }
   return result;
@@ -264,7 +204,6 @@ void OMPropertySet::setContainer(const OMStorable* container)
   PRECONDITION("No valid old container", _container == 0);
   PRECONDITION("Valid new container", container != 0);
   _container = container;
-  POSTCONDITION("Valid count", _count <= _capacity);
 }
 
   // @mfunc The <c OMStorable> object that contains this
@@ -278,101 +217,18 @@ OMStorable* OMPropertySet::container(void) const
   return const_cast<OMStorable*>(_container);
 }
 
-bool OMPropertySet::equal(const OMPropertyId& propertyIda,
-                          const OMPropertyId& propertyIdb)
-{
-  TRACE("OMPropertySet::equal");
-  return propertyIda == propertyIdb;
-}
-
-OMPropertySet::OMPropertySetElement* OMPropertySet::find(
-                                           const OMPropertyId propertyId) const
+OMProperty* OMPropertySet::find(const wchar_t* propertyName) const
 {
   TRACE("OMPropertySet::find");
 
-  OMPropertySetElement* result = 0;
-
-  for (size_t i = 0; i < _capacity; i++) {
-    if (_propertySet[i]._valid) {
-      if (equal(_propertySet[i]._propertyId, propertyId)) {
-        result = &_propertySet[i];
-        break;
-      }
-    }
-  }
-  return result;
-}
-
-OMPropertySet::OMPropertySetElement* OMPropertySet::find(
-                                             const wchar_t* propertyName) const
-{
-  TRACE("OMPropertySet::find");
-
-  OMPropertySetElement* result = 0;
-
-  for (size_t i = 0; i < _capacity; i++) {
-    if (_propertySet[i]._valid) {
-      if (compareWideString(_propertySet[i]._property->name(),
-                            propertyName) == 0) {
-        result = &_propertySet[i];
-        break;
-      }
-    }
-  }
-  return result;
-}
-
-OMPropertySet::OMPropertySetElement* OMPropertySet::find(void) const
-{
-  TRACE("OMPropertySet::find");
-
-  OMPropertySetElement* result = 0;
-
-  for (size_t i = 0; i < _capacity; i++) {
-    if (!_propertySet[i]._valid) {
-      result = &_propertySet[i];
+  OMProperty* result = 0;
+  SetIterator iterator(_set, OMBefore);
+  while (++iterator) {
+    OMProperty* p = iterator.value();
+    if (compareWideString(p->name(), propertyName) == 0) {
+      result = p;
       break;
     }
   }
   return result;
-
-}
-
-void OMPropertySet::grow(const size_t additionalElements)
-{
-  TRACE("OMPropertySet::grow");
-
-  // Save old size and old property set element array
-  //
-  size_t oldCapacity = _capacity;
-  OMPropertySetElement* oldPropertySet = _propertySet;
-
-  // New capacity
-  //
-  _capacity = _capacity + additionalElements;
-
-  // Allocate new property set element array
-  //
-  _propertySet = new OMPropertySetElement[_capacity];
-  ASSERT("Valid heap pointer", _propertySet != 0);
-
-  // Copy over all elements from the old array
-  //
-  for (size_t i = 0; i < oldCapacity; i++) {
-    _propertySet[i] = oldPropertySet[i];
-  }
-
-  // Initialize new elements to be invalid
-  //
-  for (size_t j = oldCapacity; j < _capacity; j++) {
-    _propertySet[j]._valid = false;
-  }
-
-  // Delete the old array
-  //
-  delete [] oldPropertySet;
-
-  POSTCONDITION("Size properly increased",
-                _capacity == oldCapacity + additionalElements);
-
 }
