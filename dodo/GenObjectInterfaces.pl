ccabf4b1ecd874e19ea8ca63ee5d3259d173bdd2
tbingham <>
2001-04-17 11:49:09 +0000

    Formatting changes...

--- dodo/GenObjectInterfaces.pl
+++ dodo/GenObjectInterfaces.pl
@@ -1,19 +1,23 @@
 ##################################################################### 
-# FILE:    GenerateObjectInterfaces.pl
+# FILE:    GenObjectInterfaces.pl
 # AUTHOR:  Tom Ransdell, Avid Technology, Inc.
 # CREATED: 2000-OCT-29
 # PURPOSE: 
-#   
+#   For a given AAF object or (list of objects) produce a list of
+# interfaces that it supports.
 #
 # perl GenerateObjectInterfaces.pl AAFPulldown[.dod]
 # perl GenerateObjectInterfaces.pl *.dod
 # perl GenerateObjectInterfaces.pl AAFHeader AAFObject
 #
 ##################################################################### 
+# The script implements a simple state machine to parse dodo
+# source files and produce a formatted list of interfaces that
+# a given dodo object supports.
+##################################################################### 
 
 
-require 5.002;
-use FileHandle;
+require 5.002; # Needed for Class::Struct
 
 use Class::Struct;
 
@@ -21,7 +25,7 @@ use Class::Struct;
 #
 # Define a structure to represent an enum.
 #
-struct(ObjectStruct => [name => '$', num => '$', parent => '$', ex => '$']);
+struct(ObjectStruct => [name => '$', parent => '$', ex => '$']);
 
 
 
@@ -37,8 +41,8 @@ foreach $arg (@ARGV) {
   $name = $arg;
   $name =~ s/\..*//;
 
-	&processObjectName ($name);
-	&printSupportedInterfaces ($name);
+  &processObjectName ($name);
+  &printSupportedInterfaces ($name);
 }
 
 
@@ -54,149 +58,158 @@ exit;
 #
 sub processObjectName 
 {
-	my ($name) = @_;
-	my $file = ''; # the dodo file to open and parse
-	my $obj;	
-	# State variable that holds a reference to the current line
-	# matching subroutine.
-	my $processLine;
+  my ($name) = @_;
+  my $file = ''; # the dodo file to open and parse
+  my $obj;  
+  # State variable that holds a reference to the current line
+  # matching subroutine.
+  my $processLine;
 
-	local (*DODOFILE);
-	
-	# Terminate processing it we reach the root object.
-	if (($name ne 'AAFRoot' && $name ne 'AAFMetaDictionary') && (!defined($objects{$name}))) {
+  local (*DODOFILE);
+  
+  # Terminate processing it we reach the root object.
+  if (($name ne 'AAFRoot' && $name ne 'AAFMetaDictionary') && (!defined($objects{$name}))) {
 
-		# make sure the file name ends in .dod
-		$file = $name . '.dod';
+    # make sure the file name ends in .dod
+    $file = $name . '.dod';
 
-		open (DODOFILE , "$file") or die "Could not open \"$file\".\n";
-	
-	
-		# Initially we want to start out by searching for the first line 
-		# that contains an enum declaration.
-		$processLine = \&find_start_of_parent_module;
-		
-LINE: while (<DODOFILE>)
-		{
-			# Skip any lines that contain a dodo comments.
-			next LINE if (/\#c/);
+    open (DODOFILE , "$file") or die "Could not open \"$file\".\n";
+  
+    # Initially we want to start out by searching for the first line 
+    # that contains a parent module macro.
+    $processLine = \&find_start_of_parent_module;
+    
+    LINE: while (<DODOFILE>)
+    {
+      # Skip any lines that contain a dodo comments.
+      next LINE if (/\#c/ || /^\s*$/);
 
-			$processLine = &$processLine ($name, $processLine, $_);
-			
-			# Stop if we have finished processing the file...
-			last if ($processLine eq \&finished_processing)
-		}
-		close (DODOFILE);
-	}
+      # Parse the current line using the current $processLine subroutine.
+      $processLine = &$processLine ($name, $processLine, $_);
+      
+      # Stop if we have finished processing the file...
+      last if ($processLine eq \&finished_processing)
+    }
+    close (DODOFILE);
+  }
 }
 
 
 # Subroutine to process characters until the we match the
 # starting line of the parent module.
 sub find_start_of_parent_module {
-	my ($name, $processLine, $line) = @_;
-	my $obj;	
+  my ($name, $processLine, $line) = @_;
+  my $obj;  
 
-	#print STDERR "# find_start_of_parent_module: ", $line, "\n";
-	
-	if (/\.parent-module/) {
-		# Create an new object structure
-		$obj = new ObjectStruct;
-		$obj->name($name);
-			
-		# Save the new object in the hash table.
-		$objects{$obj->name} = $obj;
+  #print STDERR "# find_start_of_parent_module: ", $line, "\n";
+  
+  if (/\.parent-module/) {
+    # Create an new object structure
+    $obj = new ObjectStruct;
+    $obj->name($name);
+      
+    # Save the new object in the hash table.
+    $objects{$obj->name} = $obj;
  
     # Extract the parent module name from the next line.
     \&get_parent_module;
   }
-	else
-	{
-		$processLine;
-	}
+  else
+  {
+    $processLine;
+  }
 }
 
 # Subroutine to process characters to extract parent
 # module name.
 sub get_parent_module {
-	my ($name, $processLine, $line) = @_;
-	my $obj = $objects{$name};	
+  my ($name, $processLine, $line) = @_;
+  my $obj = $objects{$name};  
 
-	$line =~ s/\s+//g; # strip all of the white space from the current line ($_)
-	$line =~ s/\\+//g; # strip dodo escape characters from the current line ($_)
-	
-	#print STDERR "# get_parent_module: ", $line, "\n";
+  $line =~ s/\s+//g; # strip all of the white space from the current line ($_)
+  $line =~ s/\\+//g; # strip dodo escape characters from the current line ($_)
+  
+  #print STDERR "# get_parent_module: ", $line, "\n";
 
-	# What's left should be the name of the parent object.
-	$obj->parent($line);
+  # What's left should be the name of the parent object.
+  $obj->parent($line);
 
-	# Recursively process the parent class
-	&processObjectName ($obj->parent);
-	
-	# Find the start of the class/com extention interface.
-	\&find_start_of_class;
+
+  # Recursively process the parent class
+  &processObjectName ($obj->parent);
+  
+  # Find the start of the class/com extention interface.
+  \&find_start_of_class;
 }
 
 
 # Subroutine to process characters until the we match the
-# starting line of the parent module.
+# starting line of one of the CLASS_BEGIN macros. If
+# It is a com extension class (interface) then extra
+# the name and save it with the corresponding object
+# from the hash table.
 sub find_start_of_class {
-	my ($name, $processLine, $line) = @_;
-	my ($macro, $access, $classEx);
-	my ($obj);	
+  my ($name, $processLine, $line) = @_;
+  my ($macro, $access, $classEx);
+  my ($obj);  
 
-	#print STDERR "# find_start_of_class: ", $line, "\n";
-	
-	if (/CLASS_BEGIN/) {
-	
-		if (/AD_COM_EXT_CLASS_BEGIN/) {
-			($macro, $access, $classEx) = split(/[() ]/);
-			
-			$obj = $objects{$name};
-			$obj->ex($classEx);
-			
-			#print STDERR "# find_start_of_class: ", $classEx, "\n";
-		}
-	
-		\&finished_processing;
-	}
-	else
-	{
-		$processLine;
-	}
+  #print STDERR "# find_start_of_class: ", $line, "\n";
+  
+  if (/CLASS_BEGIN/) {
+  
+    if (/AD_COM_EXT_CLASS_BEGIN/) {
+      ($macro, $access, $classEx) = split(/[() ]/);
+      
+      $obj = $objects{$name};
+      $obj->ex($classEx);
+      
+      #print STDERR "# find_start_of_class: ", $classEx, "\n";
+    }
+  
+    # We are done. There are no more lines to process...
+    \&finished_processing;
+  }
+  else
+  {
+    $processLine;
+  }
 }
 
 # stub to indicate processing of the file has been finished.
 sub finished_processing {
-	my ($name, $processLine, $line) = @_;
+  my ($name, $processLine, $line) = @_;
 
-	print STDERR "# WARNING: finished_processing should not be called!\n";
-	
+  print STDERR "# WARNING: finished_processing should not be called!\n";
 
-	\&finished_processing;
+  \&finished_processing;
 }
 
 
 # Subroutine to process the given object name: AAFObject, AAFClassDef, ...etc.
 #
 sub printSupportedInterfaces {
-	my ($name) = @_;
-	
-	if (($name ne 'AAFRoot' && $name ne 'AAFMetaDictionary') ) {
-		$obj = $objects{$name};
-		if (defined($obj) && ($obj->parent ne 'AAFRoot' || defined($obj->ex))) {
-			print "// Objects that implement I", $obj->name, " also implement the following interfaces:\n";
-			
-			if (defined($obj->ex)) {
-				print "//  - ", $obj->ex, " (extended interface)\n";
-			}
-			
-			while ($obj->parent ne 'AAFRoot') {
-				$obj = $objects{$obj->parent};
-				print "//  - I", $obj->name, "\n";
-			}
-			
-			print "\n";
-		}
-	}
+  my ($name) = @_;
+  
+  if (($name ne 'AAFRoot' && $name ne 'AAFMetaDictionary') ) {
+    $obj = $objects{$name};
+    if (defined($obj) && ($obj->parent ne 'AAFRoot' || defined($obj->ex))) {
+      print "//\n";	
+      print "// Objects that implement I", $obj->name, " also implement the following interfaces:\n";
+      
+      if (defined($obj->ex)) {
+        print "//  - ", $obj->ex, "\n";
+      }
+      
+      while ($obj->parent ne 'AAFRoot') {
+        $obj = $objects{$obj->parent};
+        print "//  - I", $obj->name, "\n";
+
+        if (defined($obj->ex)) {
+          print "//  - ", $obj->ex, "\n";
+        }
+      }
+      
+      print "\n";
+    }
+  }
 }
