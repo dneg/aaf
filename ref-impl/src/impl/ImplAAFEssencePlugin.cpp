08bf82bf6c3cb236ebacaf39b52b0877f073be07
tbingham <>
2001-04-17 06:03:54 +0000

    Add AAFEssencePlugin

---
+++ ref-impl/src/impl/ImplAAFEssencePlugin.cpp
@@ -0,0 +1,49 @@
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+#ifndef __ImplAAFEssencePlugin_h__
+#include "ImplAAFEssencePlugin.h"
+#endif
+
+#include <string.h>
+#include "ImplAAFSession.h"
+
+const CLSID CLSID_AAFDefaultCodec = { 0xDC089C31, 0x9527, 0x11d2, { 0x80, 0x89, 0x00, 0x60, 0x08, 0x14, 0x3e, 0x6f } };
+
+ImplAAFEssencePlugin::ImplAAFEssencePlugin ()
+{
+}
+
+
+ImplAAFEssencePlugin::~ImplAAFEssencePlugin ()
+{}
+
+AAFRESULT STDMETHODCALLTYPE
+	ImplAAFEssencePlugin::NumCodecsMatching(
+		aafUID_t		dataDefinition,	//Looking for audio or video codecs
+		aafFileRev_t	revision,		//for which variety of file,
+		aafInt32		*result)		// Returns the number of matches,
+{
+	*result = 1;
+	return AAFRESULT_SUCCESS;
+}
+
+AAFRESULT STDMETHODCALLTYPE
+	ImplAAFEssencePlugin::GetIndexedCodecInfo(
+		aafUID_t		dataDefinition,	//Looking for audio or video codecs,
+		aafFileRev_t	revision,		//for which variety of file,
+		aafInt32		index,			// Which codec. 1 to number of codecs,
+		wchar_t			*pName,			// Codec Name,
+		aafInt32		bufSize,		//length of the buffer to hold Codec Name,
+		aafUID_t		*codecID)		//ID used to access the codec,
+{
+	memcpy(pName, L"Test Codec", 28);
+	return AAFRESULT_SUCCESS;
+}
