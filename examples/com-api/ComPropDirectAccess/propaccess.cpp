fd14061a989078295c71188c38baa32f29475216
tbingham <>
2001-04-17 07:47:08 +0000

    Don't attempt to create an abstract class.

--- examples/com-api/ComPropDirectAccess/propaccess.cpp
+++ examples/com-api/ComPropDirectAccess/propaccess.cpp
@@ -120,7 +120,7 @@ typedef IAAFSmartPointer<IAAFClassDef>          IAAFClassDefSP;
 typedef IAAFSmartPointer<IAAFDictionary>        IAAFDictionarySP;
 typedef IAAFSmartPointer<IAAFEssenceDescriptor> IAAFEssenceDescriptorSP;
 typedef IAAFSmartPointer<IAAFFile>              IAAFFileSP;
-typedef IAAFSmartPointer<IAAFFileDescriptor>    IAAFFileDescriptorSP;
+typedef IAAFSmartPointer<IAAFHTMLDescriptor>    IAAFHTMLDescriptorSP;
 typedef IAAFSmartPointer<IAAFFiller>            IAAFFillerSP;
 typedef IAAFSmartPointer<IAAFHeader>            IAAFHeaderSP;
 typedef IAAFSmartPointer<IAAFMob>               IAAFMobSP;
@@ -746,15 +746,14 @@ static void CreateAAFFile(aafWChar * pFileName,
   check (spMob->SetMobID(newMobID));
   check (spMob->SetName(L"a Source Mob"));
 
-  IAAFFileDescriptorSP  spFileDesc;
-  check (defs.cdFileDescriptor()->
-		 CreateInstance(IID_IAAFFileDescriptor, 
-						(IUnknown **) &spFileDesc));
+  IAAFHTMLDescriptorSP  spHTMLDesc;
+  check (defs.cdHTMLDescriptor()->
+		 CreateInstance(IID_IAAFHTMLDescriptor, 
+						(IUnknown **) &spHTMLDesc));
   aafRational_t  audioRate = { 44100, 1 };
-  check (spFileDesc->SetSampleRate(audioRate));
 
   IAAFEssenceDescriptorSP spEssenceDesc;
-  check (spFileDesc->QueryInterface (IID_IAAFEssenceDescriptor,
+  check (spHTMLDesc->QueryInterface (IID_IAAFEssenceDescriptor,
 								   (void **)&spEssenceDesc));
   check (smob->SetEssenceDescriptor (spEssenceDesc));
 
