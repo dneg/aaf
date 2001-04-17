fc411d11e0861982d4bdc2d62b5e28ee0936eab3
tbingham <>
2001-04-17 04:27:26 +0000

    New debug version that uses pre-compiled AAFHeaders.

---
+++ AAFMacSDK/test/com/ComModTestAAF/ComModTestAAFDbg_prefix.h
@@ -0,0 +1,16 @@
+// >>> © 1998 Avid Technology, Inc.  All rights reserved. <<<
+
+// Include the pre-compiled header that includes AAF.h
+#include "AAFHeaders.h"
+
+
+// Conditional debugging and ansi assertions enabled
+#ifndef _DEBUG
+#define _DEBUG
+#endif
+
+
+// Define to enable the module testing of the shared library.
+#define AAF_MODULE_TEST
+
+
