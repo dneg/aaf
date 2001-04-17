26a0481a6fa5dff33b915290c2e44d07eeb5ac9e
tbingham <>
2001-04-17 09:05:38 +0000

    First simplistic implementation. The 'optional strong reference' annotation is currently implmented as a 'strong reference'. There is no module test yet.

--- ref-impl/src/impl/ImplAAFCommentMarker.cpp
+++ ref-impl/src/impl/ImplAAFCommentMarker.cpp
@@ -7,48 +7,72 @@
 *                                          *
 \******************************************/
 
-
-
+#ifndef __ImplAAFCommentMarker_h__
+#include "ImplAAFCommentMarker.h"
+#endif
 
 #ifndef __ImplAAFSourceReference_h__
 #include "ImplAAFSourceReference.h"
 #endif
 
-
-
-
-
 #include "AAFStoredObjectIDs.h"
+#include "AAFPropertyIDs.h"
+#include "AAFResult.h"
+#include "AAFClassIDs.h"
 
-#ifndef __ImplAAFCommentMarker_h__
-#include "ImplAAFCommentMarker.h"
-#endif
 
 #include <assert.h>
 #include <string.h>
 
 
-ImplAAFCommentMarker::ImplAAFCommentMarker ()
-{}
+ImplAAFCommentMarker::ImplAAFCommentMarker () :
+  _annotation(PID_CommentMarker_Annotation, "Annotation")
+{
+  _persistentProperties.put(_annotation.address());
+}
 
 
 ImplAAFCommentMarker::~ImplAAFCommentMarker ()
-{}
+{
+  // Cleanup references to contained objects.
+  ImplAAFSourceReference *annotation = _annotation.setValue(0);
+  if (annotation)
+    annotation->ReleaseReference();
+}
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFCommentMarker::GetAnnotation (
-      ImplAAFSourceReference ** /*ppResult*/)
+      ImplAAFSourceReference ** ppAnnotation)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+  if (NULL == ppAnnotation)
+    return (AAFRESULT_NULL_PARAM);
+
+  *ppAnnotation = _annotation;
+
+  if (*ppAnnotation)
+    (*ppAnnotation)->AcquireReference();
+  else
+    return (AAFRESULT_NULLOBJECT);
+
+  return AAFRESULT_SUCCESS;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFCommentMarker::SetAnnotation (
-      ImplAAFSourceReference * /*pAnnotation*/)
+      ImplAAFSourceReference * pAnnotation)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+  ImplAAFSourceReference *oldValue = _annotation.setValue(0);
+  if (oldValue)
+    oldValue->ReleaseReference();
+
+  _annotation = pAnnotation;
+
+  if (pAnnotation)
+    pAnnotation->AcquireReference();
+
+  return AAFRESULT_SUCCESS;
 }
 
 
