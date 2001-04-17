439c85d22074b663ebdbc461834c6942f8ff64e1
tbingham <>
2001-04-17 08:01:09 +0000

    Update AAFMetaDictionary.h to remove PluggableDef. Reassign PIDs. Bump format version.

--- ref-impl/src/OM/OMStoredObject.cpp
+++ ref-impl/src/OM/OMStoredObject.cpp
@@ -18,7 +18,7 @@
 #include <objbase.h>
 #endif
 
-const int currentVersion = 7;
+const OMUInt32 currentVersion = 8;
 
 const size_t indexHeaderSize = sizeof(OMByteOrder) +  // Byte order flag
                                sizeof(OMUInt32) +     // Version number
