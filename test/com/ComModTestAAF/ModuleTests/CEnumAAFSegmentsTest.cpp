3a417ac1af8aa9d8cda3ee13a6aefaaf2ccf5a3f
tbingham <>
2001-04-17 07:37:37 +0000

    Fix semantic errors

--- test/com/ComModTestAAF/ModuleTests/CEnumAAFSegmentsTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CEnumAAFSegmentsTest.cpp
@@ -152,10 +152,6 @@ static HRESULT CreateAAFFile(aafWChar * pFileName)
 	IAAFSelector*		pSelector = NULL;
 	IAAFCompositionMob*	pCompMob = NULL;
 	IAAFComponent*		pComponent = NULL;
-	aafInt32			fadeInLen  = 1000;
-	aafInt32			fadeOutLen = 2000;
-	aafFadeType_t		fadeInType = kAAFFadeLinearAmp;
-	aafFadeType_t		fadeOutType = kAAFFadeLinearPower;
 	aafSourceRef_t		sourceRef; 
 	aafLength_t			fillerLength = 3200;
 
@@ -202,7 +198,6 @@ static HRESULT CreateAAFFile(aafWChar * pFileName)
 		pComponent = NULL;
 
 		// Set the properties for the SourceClip
-		checkResult(pSourceClip->SetFade( fadeInLen, fadeInType, fadeOutLen, fadeOutType));
 		sourceRef.sourceID = TEST_referencedMobID;
 		sourceRef.sourceSlotID = 0;
 		sourceRef.startTime = 0;
