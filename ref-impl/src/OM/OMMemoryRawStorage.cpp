8a73bedd925c62f4cae96ba834f64607622cf3d4
tbingham <>
2001-04-17 09:35:06 +0000

    Add OMRawStorage::readAt() (stubs).

--- ref-impl/src/OM/OMMemoryRawStorage.cpp
+++ ref-impl/src/OM/OMMemoryRawStorage.cpp
@@ -155,6 +155,31 @@ void OMMemoryRawStorage::read(OMByte* bytes,
   bytesRead = count;
 }
 
+  // @mfunc Attempt to read the number of bytes given by <p byteCount>
+  //        from the current position in this <c OMMemoryRawStorage>
+  //        into the buffer at address <p bytes>.
+  //        The actual number of bytes read is returned in <p bytesRead>.
+  //        Reading from positions greater than
+  //        <mf OMMemoryRawStorage::size> causes <p bytesRead> to be less
+  //        than <p byteCount>. Reading bytes that have never been written
+  //        returns undefined data in <p bytes>.
+  //   @parm TBS
+  //   @parm The buffer into which the bytes are to be read.
+  //   @parm The number of bytes to read.
+  //   @parm The number of bytes actually read.
+  //   @this const
+void OMMemoryRawStorage::readAt(OMUInt64 /* position */,
+                                OMByte* /* bytes */,
+                                OMUInt32 /* byteCount */,
+                                OMUInt32& /* bytesRead */) const
+{
+  TRACE("OMMemoryRawStorage::readAt");
+  PRECONDITION("Readable", isReadable());
+  PRECONDITION("Readable", isPositionable());
+
+  ASSERT("Unimplemented code not reached", false); // tjb TBS
+}
+
   // @mfunc Is it possible to write to this <c OMMemoryRawStorage> ?
   //  @rdesc True if this <c OMMemoryRawStorage> is writable, false otherwise.
   //  @this const
