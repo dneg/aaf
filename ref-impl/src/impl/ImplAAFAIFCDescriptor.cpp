f17d0ccb4758efa944374fa7e8fe79d5eb618a59
tbingham <>
2001-04-17 04:18:15 +0000

    Initial implementation, persistence and Get/Set methods

---
+++ ref-impl/src/impl/ImplAAFAIFCDescriptor.cpp
@@ -0,0 +1,77 @@
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+#ifndef __ImplAAFAIFCDescriptor_h__
+#include "ImplAAFAIFCDescriptor.h"
+#endif
+
+#include "AAFStoredObjectIDs.h"
+#include "AAFPropertyIDs.h"
+
+#include <assert.h>
+#include <string.h>
+
+
+ImplAAFAIFCDescriptor::ImplAAFAIFCDescriptor ()
+:   _summary(PID_AIFCDescriptor_Summary,	"Summary")
+{
+	_persistentProperties.put(_summary.address());
+}
+
+
+ImplAAFAIFCDescriptor::~ImplAAFAIFCDescriptor ()
+{}
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFAIFCDescriptor::Initialize ()
+{
+	return AAFRESULT_SUCCESS;
+}
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFAIFCDescriptor::GetSummary (aafUInt32	size, aafDataValue_t  pSummary)
+{
+	if(pSummary == NULL)
+		return(AAFRESULT_NULL_PARAM);
+
+	if (_summary.size() > size)
+	  return AAFRESULT_SMALLBUF;
+
+	_summary.copyToBuffer(pSummary, size);
+
+	return(AAFRESULT_SUCCESS); 
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFAIFCDescriptor::GetSummaryBufferSize (aafUInt32 *pSize)
+{
+	if(pSize == NULL)
+		return(AAFRESULT_NULL_PARAM);
+
+	*pSize = _summary.size();
+
+	return(AAFRESULT_SUCCESS); 
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFAIFCDescriptor::SetSummary (aafUInt32 size, aafDataValue_t pSummary)
+{
+	if(pSummary == NULL)
+		return(AAFRESULT_NULL_PARAM);
+
+	_summary.setValue(pSummary, size);
+
+	return(AAFRESULT_SUCCESS); 
+}
+
+OMDEFINE_STORABLE(ImplAAFAIFCDescriptor, AUID_AAFAIFCDescriptor);
+
+
