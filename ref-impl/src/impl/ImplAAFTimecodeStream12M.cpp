9c6c25069578d8e7e50e456aeb7e8bf2ce4da01b
tbingham <>
2001-04-17 09:49:56 +0000

    Change DataDef internals & split of dataDef defines

--- ref-impl/src/impl/ImplAAFTimecodeStream12M.cpp
+++ ref-impl/src/impl/ImplAAFTimecodeStream12M.cpp
@@ -74,7 +74,8 @@ typedef struct
 
 #include <assert.h>
 #include <string.h>
-
+#include "aafErr.h"
+#include "aafUtils.h"
 
 ImplAAFTimecodeStream12M::ImplAAFTimecodeStream12M ()
 {}
@@ -83,74 +84,6 @@ ImplAAFTimecodeStream12M::ImplAAFTimecodeStream12M ()
 ImplAAFTimecodeStream12M::~ImplAAFTimecodeStream12M ()
 {}
 
-
-#if 0
-/****/
-  // Override from AAFTimecodeStream
-  AAFRESULT STDMETHODCALLTYPE
-    ImplAAFTimecodeStream12M::GetPositionTimecode (/*[in]*/ aafPosition_t  /*position*/,
-      /*[out]*/ aafTimecode_t *  /*timecode*/)
-  {
-    return AAFRESULT_NOT_IMPLEMENTED;
-  }
-
-  // Override from AAFTimecodeStream
-  AAFRESULT STDMETHODCALLTYPE
-    ImplAAFTimecodeStream12M::SetPositionTimecode (/*[in]*/ aafPosition_t  /*position*/,
-      /*[in]*/ aafTimecode_t  /*timecode*/)
-  {
-    return AAFRESULT_NOT_IMPLEMENTED;
-  }
-
-
-  // Override from AAFTimecodeStream
-AAFRESULT STDMETHODCALLTYPE
-    ImplAAFTimecodeStream12M::GetUserDataLength (/*[out]*/ aafInt32 *length)
-{
-    if(length == NULL)
-		return AAFRESULT_NULL_PARAM;
-	
-	*length = 4;		// It's always 8 groups of 4 bits
-	return AAFRESULT_SUCCESS;
-}
-
-  // Override from AAFTimecodeStream
-  AAFRESULT STDMETHODCALLTYPE
-    ImplAAFTimecodeStream12M::GetUserDataAtPosition (/*[in]*/ aafPosition_t  position,
-      /*[in]*/ aafInt32  buflen,
-      /*[out]*/ aafDataBuffer_t  buffer)
-{
-    return AAFRESULT_NOT_IMPLEMENTED;
-}
-
-  // Override from AAFTimecodeStream
-  AAFRESULT STDMETHODCALLTYPE
-    ImplAAFTimecodeStream12M::SetUserDataAtPosition (/*[in]*/ aafPosition_t  position,
-      /*[in]*/ aafInt32  /*buflen*/,
-      /*[in]*/ aafDataBuffer_t  /*buffer*/)
-  {
-    return AAFRESULT_NOT_IMPLEMENTED;
-  }
-
-
-/****/
-  // Override from AAFSegment
-  AAFRESULT STDMETHODCALLTYPE
-    ImplAAFTimecodeStream12M::SegmentOffsetToTC (/*[in]*/ aafPosition_t *  /*pOffset*/,
-      /*[out]*/ aafTimecode_t *  /*pTimecode*/)
-  {
-    return AAFRESULT_NOT_IMPLEMENTED;
-  }
-
-  // Override from AAFSegment
-  AAFRESULT STDMETHODCALLTYPE
-    ImplAAFTimecodeStream12M::SegmentTCToOffset (/*[in]*/ aafTimecode_t *  /*pTimecode*/,
-      /*[in]*/ aafRational_t *  /*pEditRate*/,
-      /*[out]*/ aafFrameOffset_t *  /*pOffset*/)
-  {
-    return AAFRESULT_NOT_IMPLEMENTED;
-  }
-#endif
 
 /****/
   // Override from AAFTimecodeStream
@@ -165,70 +98,63 @@ AAFRESULT STDMETHODCALLTYPE
 }
 
   AAFRESULT STDMETHODCALLTYPE
-	ImplAAFTimecodeStream12M::UnpackTimecode(aafUInt8 *buffer, aafUInt32 buflen, aafTimecode_t *tc)
+	ImplAAFTimecodeStream12M::UnpackTimecode(aafUInt8 *buffer, aafUInt32 buflen, aafUInt32 fps, aafTimecode_t *tc)
 {
-#if 0
-	  smpte_12MFormat_t	fmt;
+	smpte_12MFormat_t	fmt;
 	aafInt16			hours, minutes, seconds, frames;
 
-	if(unpacked == NULL || packed == NULL)
+	if(buffer == NULL || tc == NULL)
 		return AAFRESULT_NULL_PARAM;
-    if(unpackedBuflen < sizeof(smpte_12MFormat_t))
-		return AAFRESULT_SMALLBUF;
-    if(packedBuflen < 4)
+    if(buflen < sizeof(smpte_12MFormat_t))
 		return AAFRESULT_SMALLBUF;
     XPROTECT()
 	{
-		memcpy((aafUInt8 *)&fmt, packed, sizeof(smpte_12MFormat_t));
-		hours = (((foo.bits56_63 >> 6L) & 0x03) * 10) + ((foo.bits48_55 >> 4L) & 0x0F);
-		minutes = (((foo.bits40_47 >> 5L) & 0x07) * 10) + ((foo.bits32_39 >> 4L) & 0x0F);
-		seconds = (((foo.bits24_31 >> 5L) & 0x07) * 10) + ((foo.bits16_23 >> 4L) & 0x0F);
-		frames = (((foo.bits8_15 >> 6L) & 0x03) * 10) + ((foo.bits0_7 >> 4L) & 0x0F);
-		tc->drop = ((foo.bits8_15 >> 5L) & 0x01 ? kTcDrop : kTcNonDrop);
-		CHECK(PvtTimecodeToOffset(fps, hours, minutes, seconds, frames, tc->drop, tc->startFrame));
+		memcpy((aafUInt8 *)&fmt, buffer, sizeof(smpte_12MFormat_t));
+		hours = (((fmt.bits56_63 >> 6L) & 0x03) * 10) + ((fmt.bits48_55 >> 4L) & 0x0F);
+		minutes = (((fmt.bits40_47 >> 5L) & 0x07) * 10) + ((fmt.bits32_39 >> 4L) & 0x0F);
+		seconds = (((fmt.bits24_31 >> 5L) & 0x07) * 10) + ((fmt.bits16_23 >> 4L) & 0x0F);
+		frames = (((fmt.bits8_15 >> 6L) & 0x03) * 10) + ((fmt.bits0_7 >> 4L) & 0x0F);
+		tc->drop = ((fmt.bits8_15 >> 5L) & 0x01 ? kTcDrop : kTcNonDrop);
+		CHECK(PvtTimecodeToOffset((aafInt16)fps, hours, minutes, seconds, frames, tc->drop, &tc->startFrame));
 	}
 	XEXCEPT
 	{
 	}
 	XEND;
     return AAFRESULT_SUCCESS;
-#else
-    return AAFRESULT_NOT_IMPLEMENTED;
-#endif
 }
 
 AAFRESULT STDMETHODCALLTYPE
 	ImplAAFTimecodeStream12M::PackTimecode(aafTimecode_t *tc, aafUInt8 *buffer, aafUInt32 buflen)
 {
-#if 0
 	smpte_12MFormat_t	fmt;
 	aafInt16			hours, minutes, seconds, frames;
 
-    if(unpacked == NULL || packed == NULL)
+    if(tc == NULL || buffer == NULL)
 		return AAFRESULT_NULL_PARAM;
-    if(unpackedBuflen < sizeof(smpte_12MFormat_t))
-		return AAFRESULT_SMALLBUF;
-    if(packedBuflen < 4)
+    if(buflen < sizeof(fmt))
 		return AAFRESULT_SMALLBUF;
 
-	CHECK(PvtOffsetToTimecode(tc->startFrame, tc->fps. tc->drop, &hours, &seconds, &seconds, &frames));
-
-	memcpy((aafUInt8 *)&fmt, packed, sizeof(smpte_12MFormat_t));
-	fmt.bits0_7 = (fmt.bits0_7 & ~0xF0)		| (((frames % 10) << 4L) & 0xF0);
-	fmt.bits8_15 = (fmt.bits8_15 & ~0xC0	| (((frames / 10) << 4L) & 0xC0);
-	fmt.bits16_23 = (fmt.bits16_23 & ~0xF0) | (((seconds % 10) << 4L) & 0xF0);
-	fmt.bits24_31 = (fmt.bits24_31 & ~0xE0) | (((seconds / 10) << 4L) & 0xE0);
-	fmt.bits32_39 = (fmt.bits32_39 & ~0xF0) | (((minutes % 10) << 4L) & 0xF0);
-	fmt.bits40_47 = (fmt.bits40_47 & ~0xE0) | (((minutes / 10) << 4L) & 0xE0);
-	fmt.bits48_55 = (fmt.bits48_55 & ~0xF0) | (((hours % 10) << 4L) & 0xF0);
-	fmt.bits56_63 = (fmt.bits56_63 & ~0xC0) | (((hours / 10) << 4L) & 0xC0);
-	fmt.bits8_15 = (fmt.bits8_15 & ~0x20	| (drop == kTcDrop ? 0x20 : 0x00);
-    memcpy(packed, (aafUInt8 *)&fmt, sizeof(smpte_12MFormat_t));
+	XPROTECT()
+	{
+		CHECK(PvtOffsetToTimecode(tc->startFrame, tc->fps, tc->drop, &hours, &minutes, &seconds, &frames));
+		
+		memcpy((aafUInt8 *)&fmt, buffer, sizeof(smpte_12MFormat_t));
+		fmt.bits0_7 = (fmt.bits0_7 & ~0xF0)		| (((frames % 10) << 4L) & 0xF0);
+		fmt.bits8_15 = (fmt.bits8_15 & ~0xC0)	| (((frames / 10) << 6L) & 0xC0);
+		fmt.bits16_23 = (fmt.bits16_23 & ~0xF0) | (((seconds % 10) << 4L) & 0xF0);
+		fmt.bits24_31 = (fmt.bits24_31 & ~0xE0) | (((seconds / 10) << 5L) & 0xE0);
+		fmt.bits32_39 = (fmt.bits32_39 & ~0xF0) | (((minutes % 10) << 4L) & 0xF0);
+		fmt.bits40_47 = (fmt.bits40_47 & ~0xE0) | (((minutes / 10) << 5L) & 0xE0);
+		fmt.bits48_55 = (fmt.bits48_55 & ~0xF0) | (((hours % 10) << 4L) & 0xF0);
+		fmt.bits56_63 = (fmt.bits56_63 & ~0xC0) | (((hours / 10) << 6L) & 0xC0);
+		fmt.bits8_15 = (fmt.bits8_15 & ~0x20)	| (tc->drop == kTcDrop ? 0x20 : 0x00);
+		memcpy(buffer, (aafUInt8 *)&fmt, sizeof(smpte_12MFormat_t));
+	}
+	XEXCEPT
+	XEND;
 	
 	return AAFRESULT_SUCCESS;
-#else
-    return AAFRESULT_NOT_IMPLEMENTED;
-#endif
 }
 
 
@@ -241,9 +167,9 @@ AAFRESULT STDMETHODCALLTYPE
 
 	if(unpacked == NULL || packed == NULL)
 		return AAFRESULT_NULL_PARAM;
-    if(unpackedBuflen < sizeof(smpte_12MFormat_t))
+    if(packedBuflen < sizeof(smpte_12MFormat_t))
 		return AAFRESULT_SMALLBUF;
-    if(packedBuflen < 4)
+    if(unpackedBuflen < 4)
 		return AAFRESULT_SMALLBUF;
     memcpy((aafUInt8 *)&fmt, packed, sizeof(smpte_12MFormat_t));
 	unpacked[0] = ((fmt.bits0_7 & 0x0F) << 4L) | (fmt.bits8_15 & 0x0F);
@@ -261,9 +187,9 @@ AAFRESULT STDMETHODCALLTYPE
 
     if(unpacked == NULL || packed == NULL)
 		return AAFRESULT_NULL_PARAM;
-    if(unpackedBuflen < sizeof(smpte_12MFormat_t))
+    if(unpackedBuflen < 4)
 		return AAFRESULT_SMALLBUF;
-    if(packedBuflen < 4)
+    if(packedBuflen < sizeof(smpte_12MFormat_t))
 		return AAFRESULT_SMALLBUF;
     memcpy((aafUInt8 *)&fmt, packed, sizeof(smpte_12MFormat_t));
 	fmt.bits0_7 = (fmt.bits0_7 & ~0x0F)		| ((unpacked[0] >> 4L) & 0x0F);
