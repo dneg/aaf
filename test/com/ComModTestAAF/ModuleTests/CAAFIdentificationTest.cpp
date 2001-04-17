6002285697c86018215708c3665b800e1e5a38e8
tbingham <>
2001-04-17 07:00:27 +0000

    Removing CoCreateInstance from the module tests.

--- test/com/ComModTestAAF/ModuleTests/CAAFIdentificationTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFIdentificationTest.cpp
@@ -1,13 +1,14 @@
 // @doc INTERNAL
-// @com This file implements the module test for CAAFDefinitionObject
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+// @com This file implements the module test for CAAFIdentification
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
+
 
 
 
@@ -18,54 +19,20 @@
 #error - improperly defined include guard
 #endif
 
-#include <iostream.h>
-
-// Temporarily necessary global declarations.
-extern "C" const CLSID CLSID_AAFIdentification;
+#include "AAFResult.h"
 
 
 HRESULT CAAFIdentification::test()
 {
-  HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
-  IAAFIdentification *pObject = NULL;
-
-  try
-	{
-	  // Attempt to create an AAFIdentification.
-	  hr =  CoCreateInstance(
-							 CLSID_AAFIdentification,
-							 NULL, 
-							 CLSCTX_INPROC_SERVER, 
-							 IID_IAAFIdentification, (void **)&pObject);
-	  if (FAILED(hr))
-		{
-		  cerr << "CAAFIdentification::test...FAILED!";
-		  cerr << hr;
-		  cerr << "\tCoCreateInstance(&CLSID_AAFIdentification, NULL,"
-			" CLSCTX_INPROC_SERVER, &IID_IAAFIdentification, ...);" <<
-			  endl;
-		  return hr;
-		}
-
-	  // module-specific tests go here
-
-      if (pObject)
-        pObject->Release();
-	  return AAFRESULT_NOT_IMPLEMENTED;
-
-	}
-  catch (...)
-	{
-	  cerr << "CAAFIdentification::test...Caught general C++"
-		" exception!" << endl; 
-	}
-
-  // Cleanup our object if it exists.
-  if (pObject)
-	pObject->Release();
-
-  return hr;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
+
+
+
+
+
+
+
 
 
 
--- test/com/ComModTestAAF/ModuleTests/CAAFTypeDefStringTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFTypeDefStringTest.cpp
@@ -1,13 +1,14 @@
 // @doc INTERNAL
-// @com This file implements the module test for CAAFDefinitionObject
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+// @com This file implements the module test for CAAFTypeDefString
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
+
 
 
 
@@ -19,55 +20,24 @@
 #error - improperly defined include guard
 #endif
 
-
-#include <iostream.h>
-
-// Temporarily necessary global declarations.
-extern "C" const CLSID CLSID_AAFTypeDefString; // generated
+#include "AAFResult.h"
 
 
 HRESULT CAAFTypeDefString::test()
 {
-  HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
-  IAAFTypeDefString *pObject = NULL;
-
-  try
-    {
-      // Attempt to create an AAFTypeDefString.
-      hr =  CoCreateInstance(
-                             CLSID_AAFTypeDefString,
-                             NULL, 
-                             CLSCTX_INPROC_SERVER, 
-                             IID_IAAFTypeDefString, (void **)&pObject);
-      if (FAILED(hr))
-        {
-          cerr << "CAAFTypeDefString::test...FAILED!";
-          cerr << hr;
-          cerr << "\tCoCreateInstance(&CLSID_AAFTypeDefString, NULL,"
-            " CLSCTX_INPROC_SERVER, &IID_IAAFTypeDefString, ...);" <<
-              endl;
-          return hr;
-        }
-
-      // module-specific tests go here
-
-      if (pObject)
-        pObject->Release();
-      return AAFRESULT_NOT_IMPLEMENTED;
-
-    }
-  catch (...)
-    {
-      cerr << "CAAFTypeDefString::test...Caught general C++"
-        " exception!" << endl; 
-    }
-
-  // Cleanup our object if it exists.
-  if (pObject)
-    pObject->Release();
-
-  return hr;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
