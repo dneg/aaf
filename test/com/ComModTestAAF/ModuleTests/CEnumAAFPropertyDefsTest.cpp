de0c58631073384f8d679c89a962ce2657b40cfa
tbingham <>
2001-04-17 06:01:49 +0000

    Newly checked-in test.

---
+++ test/com/ComModTestAAF/ModuleTests/CEnumAAFPropertyDefsTest.cpp
@@ -0,0 +1,35 @@
+// @doc INTERNAL
+// @com This file implements the module test for CEnumAAFPropertyDefs
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
+
+#include "AAFTypes.h" //Use #include "AAF.h" for functional module test.
+#include "AAFResult.h"
+
+// Required function prototype.
+extern "C" HRESULT CEnumAAFPropertyDefs_test(void);
+
+HRESULT CEnumAAFPropertyDefs_test()
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
