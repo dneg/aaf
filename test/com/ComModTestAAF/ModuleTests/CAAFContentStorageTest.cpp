b7256f8e337f7dcee73fa4116c5c2b45f12b69ad
tbingham <>
2001-04-17 07:40:25 +0000

    Found and fixed latent memory bug. In each of these tests initialized a pointer to aafProductVersion_t v that was incorrectly allocated in the wrong "stack frame".

--- test/com/ComModTestAAF/ModuleTests/CAAFContentStorageTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFContentStorageTest.cpp
@@ -77,7 +77,8 @@ struct ContentStorageTest
 	
 	// Shared member data:
 	HRESULT _hr;
-	aafProductIdentification_t _productInfo;
+	aafProductVersion_t _productVersion;
+  aafProductIdentification_t _productInfo;
 	IAAFFile *_pFile;
 	bool _bFileOpen;
 	IAAFHeader *_pHeader;
@@ -183,15 +184,14 @@ _pEssenceData(NULL),
 _buffer(NULL),
 _bufferSize(0)
 {
-  aafProductVersion_t v;
-  v.major = 1;
-  v.minor = 0;
-  v.tertiary = 0;
-  v.patchLevel = 0;
-  v.type = kAAFVersionUnknown;
+  _productVersion.major = 1;
+  _productVersion.minor = 0;
+  _productVersion.tertiary = 0;
+  _productVersion.patchLevel = 0;
+  _productVersion.type = kAAFVersionUnknown;
   _productInfo.companyName = L"AAF Developers Desk";
   _productInfo.productName = L"AAFContentStorage Test";
-  _productInfo.productVersion = &v;
+  _productInfo.productVersion = &_productVersion;
   _productInfo.productVersionString = NULL;
   _productInfo.productID = UnitTestProductID;
   _productInfo.platform = NULL;
