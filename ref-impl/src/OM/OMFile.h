71967f029a311cf510f8118b1677a0c3fe113901
tbingham <>
2001-04-17 09:50:30 +0000

    Initial creation.

---
+++ ref-impl/src/OM/OMFile.h
@@ -0,0 +1,41 @@
+#ifndef OMFILE_H
+#define OMFILE_H
+
+#include "OMStorable.h"
+#include "OMStoredObject.h"
+
+class OMClassFactory;
+class OMObjectDirectory;
+
+class OMFile : public OMStorable {
+public:
+  OMFile(const char* name);
+  ~OMFile(void);
+  void create(void);
+  void open(void);
+  void close(void);
+  OMStoredObject& root(void);
+  OMStoredObject& openStoragePath(const char* storagePathName);
+
+  OMFile& operator << (const OMStorable& o);
+
+  static OMClassFactory* classFactory(void);
+  static OMObjectDirectory* OMFile::objectDirectory(void);
+  
+  virtual int classId(void) const;
+
+  virtual OMFile* file(void) const;
+
+private:
+
+  OMStoredObject& createFile(const char* fileName);
+  OMStoredObject& openFile(const char* fileName);
+
+  const char* _name;
+  OMStoredObject _root;
+  
+  static OMClassFactory* _classFactory;
+  static OMObjectDirectory* _objectDirectory;
+};
+
+#endif
