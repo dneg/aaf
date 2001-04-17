b03d338dfa94c06c3ffc7cc7043f15e1eac345fc
tbingham <>
2001-04-17 09:26:27 +0000

    Task: Fixed toolkit so that new optional properties can be added to existing instances (only tested for dictionary). Now a stub file like all of the old enumerators that use the common enumerator template class.

--- ref-impl/src/impl/ImplEnumAAFProperties.cpp
+++ ref-impl/src/impl/ImplEnumAAFProperties.cpp
@@ -1,7 +1,7 @@
 
 /***********************************************************************
  *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *              Copyright (c) 1998-2001 Avid Technology, Inc.
  *
  * Permission to use, copy and modify this software and accompanying 
  * documentation, and to distribute and sublicense application software
@@ -26,112 +26,5 @@
  *
  ************************************************************************/
 
-#include "ImplEnumAAFProperties.h"
-
-#include "ImplAAFProperty.h"
-#include "ImplAAFObjectCreation.h"
-
-#include <assert.h>
-#include <string.h>
-
-extern "C" const aafClassID_t CLSID_EnumAAFProperties;
-
-
-ImplEnumAAFProperties::ImplEnumAAFProperties ()
-  : _rep (0)
-{}
-
-
-ImplEnumAAFProperties::~ImplEnumAAFProperties ()
-{
-  if (_rep)
-	delete _rep;
-}
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplEnumAAFProperties::NextOne (
-      ImplAAFProperty ** ppProperty)
-{
-  if (! _rep) return AAFRESULT_NOT_INITIALIZED;
-  return _rep->NextOne (ppProperty);
-}
-
-
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplEnumAAFProperties::Next (
-      aafUInt32  count,
-      ImplAAFProperty ** ppProperties,
-      aafUInt32 *  pNumFetched)
-{
-  if (! _rep) return AAFRESULT_NOT_INITIALIZED;
-  return _rep->Next (count, ppProperties, pNumFetched);
-}
-
-
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplEnumAAFProperties::Skip (
-      aafUInt32  count)
-{
-  if (! _rep) return AAFRESULT_NOT_INITIALIZED;
-  return _rep->Skip (count);
-}
-
-
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplEnumAAFProperties::Reset ()
-{
-  if (! _rep) return AAFRESULT_NOT_INITIALIZED;
-  return _rep->Reset ();
-}
-
-
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplEnumAAFProperties::Clone (
-      ImplEnumAAFProperties ** ppEnum)
-{
-  ImplEnumAAFProperties * theEnum;
-
-  if (! ppEnum) return AAFRESULT_NULL_PARAM;
-  if (! _rep) return AAFRESULT_NOT_INITIALIZED;
-
-  theEnum = (ImplEnumAAFProperties *)CreateImpl(CLSID_EnumAAFProperties);
-  if (theEnum == NULL)
-	return E_FAIL;
-		
-  // copy this enumerator
-  assert (_rep);
-  theEnum->_rep = new OldImplAAFEnumerator<ImplAAFProperty*>(*_rep);
-  if (! theEnum->_rep)
-	{
-	  theEnum->ReleaseReference();
-	  theEnum = 0;
-	  return AAFRESULT_NOMEMORY;
-	}
-
-  assert (ppEnum);
-  *ppEnum = theEnum;
-  return AAFRESULT_SUCCESS;
-}
-
-
-
-AAFRESULT
-    ImplEnumAAFProperties::Initialize (
-      ImplAAFCollection<ImplAAFProperty*> * pProperties)
-{
-  if (! pProperties)
-	return AAFRESULT_NULL_PARAM;
-
-  // make sure it hasn't been init'd before
-  assert (! _rep);
-  _rep = new OldImplAAFEnumerator<ImplAAFProperty*>(pProperties);
-  if (! _rep)
-	return AAFRESULT_NOMEMORY;
-  assert (_rep);
-
-  return AAFRESULT_SUCCESS;
-}
+// This module has been made obsolete by the implementation of a common enumerator
+// template class.
