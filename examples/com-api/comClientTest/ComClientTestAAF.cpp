2f7851eba0e8b2fd10e70a75a75f04be7d34868a
tbingham <>
2001-04-17 07:22:15 +0000

    transdel updating for mac support

--- examples/com-api/comClientTest/ComClientTestAAF.cpp
+++ examples/com-api/comClientTest/ComClientTestAAF.cpp
@@ -154,6 +154,43 @@ static void formatMobID(char *cBuffer, size_t length, aafUID_t *pMobID)
   }
 }
 
+#if defined(_MAC) || defined(macintosh)
+// For some reason the CoCreateGuid() function is not implemented in the 
+// Microsoft Component Library...so we define something that should be
+// fairly unique on the mac.
+
+#include <Events.h>
+#include <time.h>
+
+STDAPI CoCreateGuid(GUID  *pguid)
+{
+  // {1994bd00-69de-11d2-b6bc-fcab70ff7331}
+  static GUID sTemplate = 
+    { 0x1994bd00, 0x69de, 0x11d2, { 0xb6, 0xbc, 0xfc, 0xab, 0x70, 0xff, 0x73, 0x31 } };
+
+  static bool sInitializedTemplate = false;
+  
+  if (NULL == pguid)
+    return E_INVALIDARG;
+    
+  if (!sInitializedTemplate)
+  {
+    time_t timer = time(NULL);
+    UInt32 ticks = TickCount();
+   
+    sTemplate.Data1 += timer + ticks;
+    
+    sInitializedTemplate = true;
+  }
+  
+  // Just bump the first member of the guid to emulate GUIDGEN behavior.
+  ++sTemplate.Data1;
+  
+  *pguid = sTemplate;
+}
+
+#endif
+
 static void printIdentification(IAAFIdentification* pIdent)
 {
     aafWChar wchName[500];
