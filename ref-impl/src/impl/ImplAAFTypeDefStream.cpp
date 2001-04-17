7e4d7ad186980b3f854416a3a7907dd979c8cd19
tbingham <>
2001-04-17 06:30:02 +0000

    Initial dodo-generated checkin

---
+++ ref-impl/src/impl/ImplAAFTypeDefStream.cpp
@@ -0,0 +1,106 @@
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+
+
+
+#include "AAFStoredObjectIDs.h"
+
+#ifndef __ImplAAFTypeDefStream_h__
+#include "ImplAAFTypeDefStream.h"
+#endif
+
+#include <assert.h>
+#include <string.h>
+
+
+ImplAAFTypeDefStream::ImplAAFTypeDefStream ()
+{}
+
+
+ImplAAFTypeDefStream::~ImplAAFTypeDefStream ()
+{}
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefStream::Initialize (
+      aafUID_t *  /*pID*/,
+      ImplAAFTypeDef * /*pTypeDef*/,
+      wchar_t *  /*pTypeName*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefStream::GetType (
+      ImplAAFTypeDef ** /*ppTypeDef*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefStream::GetCount (
+      ImplAAFPropertyValue * /*pPropVal*/,
+      aafInt64 *  /*pCount*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefStream::AppendElements (
+      ImplAAFPropertyValue * /*pInPropVal*/,
+      ImplAAFPropertyValue * /*pMemberPropVals*/,
+      aafUInt32  /*numElements*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefStream::CreateEmpty (
+      ImplAAFPropertyValue ** /*ppPropVal*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefStream::GetElementValues (
+      ImplAAFPropertyValue * /*pInPropVal*/,
+      aafInt64  /*startElement*/,
+      ImplAAFPropertyValue ** /*pOutPropVals*/,
+      aafUInt32  /*numElements*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+
+ AAFRESULT STDMETHODCALLTYPE
+   ImplAAFTypeDefStream::GetElements (
+      ImplAAFPropertyValue * /*pPropVal*/,
+      aafInt64  /*startElement*/,
+      aafMemPtr_t  /*pData*/,
+      aafUInt32  /*numElements*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+OMDEFINE_STORABLE(ImplAAFTypeDefStream, AUID_AAFTypeDefStream);
+
+
