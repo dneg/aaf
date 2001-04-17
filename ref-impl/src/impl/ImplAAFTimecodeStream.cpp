91b25753cf6c8c02a9f23528f63c495f93a7a6ad
tbingham <>
2001-04-17 06:07:25 +0000

    Fix more FULL_TOOLKIT and !!! places

--- ref-impl/src/impl/ImplAAFTimecodeStream.cpp
+++ ref-impl/src/impl/ImplAAFTimecodeStream.cpp
@@ -512,7 +512,7 @@ AAFRESULT STDMETHODCALLTYPE
   {
     _source.write(buffer, bytes, *bytesWritten);
     if (0 < bytes && 0 == *bytesWritten)
-      result = AAFRESULT_NOT_IMPLEMENTED;
+      result = AAFRESULT_CONTAINERWRITE;
   }
   //catch (OMException& ome)
   //{
@@ -543,7 +543,7 @@ AAFRESULT STDMETHODCALLTYPE
   {
     _source.read(buffer, bytes, *bytesRead);
     if (0 < bytes && 0 == *bytesRead)
-      result = AAFRESULT_NOT_IMPLEMENTED;
+      result = AAFRESULT_END_OF_DATA;
   }
   //catch (OMException& ome)
   //{
