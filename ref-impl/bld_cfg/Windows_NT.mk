4e7274dde9f91ed0f8d9d2dab56ed78f09401bb3
tbingham <>
2001-04-17 03:37:28 +0000

    AAF/ref-impl/bld_cfg/Windows_NT.mk

---
+++ ref-impl/bld_cfg/Windows_NT.mk
@@ -0,0 +1,34 @@
+############################################
+#                                          #
+# Advanced Authoring Format                #
+#                                          #
+# Copyright (c) 1998 Avid Technology, Inc. #
+#                                          #
+############################################
+
+#
+# Contains elements for Windows NT/i386 builds.
+#
+
+#
+# Hack! Windows NT doesn't return the proper uname, so when common.mk
+# includes $(UNAME).mk, on WinNT it will only include ".mk", not
+# "Windows_NT.mk".  That's why a file called ".mk" exists in this
+# directory, which includes this file.
+#
+
+PTFM_SUFX = nt
+OBJ = obj
+LIB = lib
+EXE = exe
+CC = cl
+LD = cl
+LFLAGS = 
+CFLAGS = /nologo /Gi-
+OBJFILE = /Fo$*.${OBJ}
+EXEFLAG = /Fo
+
+SH_PREFIX = sh -c "
+SH_SUFFIX = "
+
+IDL = midl
