d2ef181c3b4b136087ccb968b68aa4234cc3a344
tbingham <>
2001-04-17 09:13:08 +0000

    Implemented persistence and Create method.

--- ref-impl/src/impl/ImplAAFOperationGroup.cpp
+++ ref-impl/src/impl/ImplAAFOperationGroup.cpp
@@ -34,39 +34,50 @@
 #include "ImplAAFSegment.h"
 #endif
 
-
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
-
- 
-/***********************************************\
-*	Stub only.   Implementation not yet added	*
-\***********************************************/
-
-
-
-
+#ifndef __ImplAAFSourceReference_h__
+#include "ImplAAFSourceReference.h"
+#endif
 
 
 
 #include "AAFStoredObjectIDs.h"
+#include "AAFPropertyIds.h"
 
 #ifndef __ImplAAFGroup_h__
 #include "ImplAAFGroup.h"
 #endif
 
+#include "ImplAAFObjectCreation.h"
+
 #include <assert.h>
 #include <string.h>
 
+#include "AAFTypes.h"
+#include "AAFResult.h"
+#include "aafErr.h"
+#include "aafCvt.h"
+#include "aafUtils.h"
+#include "AAFDefUIDs.h"
 
-ImplAAFGroup::ImplAAFGroup ()
-{}
+extern "C" const aafClassID_t CLSID_AAFSourceReference;
+extern "C" const aafClassID_t CLSID_AAFParameter;
+extern "C" const aafClassID_t CLSID_AAFSegment;
+
+const aafUID_t kNullID = {0};
+
+ImplAAFGroup::ImplAAFGroup ():
+_effectDefinition( PID_Group_EffectDefinition, "Effect Definition"),
+_inputSegments( PID_Group_InputSegments, "Input Segments"),
+_parameters( PID_Group_Parameters, "Parameters"),
+_bypassOverride( PID_Group_BypassOverride, "Bypass Override"),
+_rendering( PID_Group_Rendering, "Rendering")
+{
+	_persistentProperties.put(_effectDefinition.address());
+	_persistentProperties.put(_inputSegments.address());
+	_persistentProperties.put(_parameters.address());
+	_persistentProperties.put(_bypassOverride.address());
+	_persistentProperties.put(_rendering.address());
+}
 
 
 ImplAAFGroup::~ImplAAFGroup ()
@@ -74,11 +85,54 @@ ImplAAFGroup::~ImplAAFGroup ()
 
 
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFGroup::Initialize (ImplAAFDataDef * /*datadef*/,
-                           aafLength_t  /*length*/,
-                           ImplAAFEffectDef * /*effectDef*/)
+    ImplAAFGroup::Initialize(aafUID_t*		pDatadef,
+							 aafLength_t    length,
+                             ImplAAFEffectDef* pEffectDef)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+	HRESULT		rc = AAFRESULT_SUCCESS;
+
+	if (pDatadef == NULL || pEffectDef == NULL)
+		return AAFRESULT_NULL_PARAM;
+
+	XPROTECT()
+	{
+		CHECK(SetNewProps(length, pDatadef));
+		// ***********************************************************************
+		// ************************************************************************
+		// Optional arguments are set here only as a way to NOT 
+		// fail inside the OM during initial development.
+		// Once optional properties are properly supported
+		// this setting instructions MUST BE REMOVED !!!
+		ImplAAFSourceReference*		pSourceRef = NULL;
+		ImplAAFParameter*			pParms = NULL;
+		ImplAAFSegment*				pSeg = NULL;
+														  
+		pSourceRef = (ImplAAFSourceReference *)CreateImpl(CLSID_AAFSourceReference);
+		if (pSourceRef == NULL)
+			return (E_FAIL);
+		pParms = (ImplAAFParameter *)CreateImpl(CLSID_AAFParameter);
+		if (pParms == NULL)
+			return (E_FAIL);
+		pSeg = (ImplAAFSegment *)CreateImpl(CLSID_AAFSegment);
+		if (pSeg == NULL)
+			return (E_FAIL);
+		_inputSegments.appendValue(pSeg);
+		_parameters.appendValue(pParms);
+		pSourceRef->SetSourceID(*pDatadef);				
+		pSourceRef->SetSourceMobSlotID((aafSlotID_t)1); 
+		_bypassOverride = (aafUInt32)1;							
+		_rendering = pSourceRef;
+		// Here we have to find the effect definition auid from the 
+		// Effect definition object pointer we got as an input 
+		// EffectDef is NOT implemented yet - therefore:
+		_effectDefinition = kNullID;
+		// ************************************************************************
+		// ************************************************************************
+	}
+	XEXCEPT
+	XEND;
+	
+	return rc;
 }
 
 	//@comm  This function takes an already created effect definition object as an argument.
