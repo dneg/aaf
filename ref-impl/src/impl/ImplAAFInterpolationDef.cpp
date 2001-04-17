bc71756f6557a2b996dc2d0c9d26941232def3c1
tbingham <>
2001-04-17 05:31:03 +0000

    Added VaryingValue, Pulldown, and Interpolation plugins

---
+++ ref-impl/src/impl/ImplAAFInterpolationDef.cpp
@@ -0,0 +1,50 @@
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+
+#ifndef __ImplAAFClassDef_h__
+#include "ImplAAFClassDef.h"
+#endif
+
+
+/***********************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
+\***********************************************/
+
+
+
+
+#include "AAFStoredObjectIDs.h"
+
+#ifndef __ImplAAFInterpolationDef_h__
+#include "ImplAAFInterpolationDef.h"
+#endif
+
+#include <assert.h>
+#include <string.h>
+
+
+ImplAAFInterpolationDef::ImplAAFInterpolationDef ()
+{}
+
+
+ImplAAFInterpolationDef::~ImplAAFInterpolationDef ()
+{}
+
+  
+
+OMDEFINE_STORABLE(ImplAAFInterpolationDef, AUID_AAFInterpolationDefinition);
+
+
