35642c119fa2eb62bf12e9a17767e47a21be1c41
tbingham <>
2001-04-17 03:28:01 +0000

    Added more unit tests

---
+++ ref-impl/src/impl/ImplAAFLocator.cpp
@@ -0,0 +1,59 @@
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+
+
+
+#include "AAFStoredObjectIDs.h"
+
+#ifndef __ImplAAFLocator_h__
+#include "ImplAAFLocator.h"
+#endif
+
+#include <assert.h>
+#include <string.h>
+
+
+ImplAAFLocator::ImplAAFLocator ()
+{}
+
+
+ImplAAFLocator::~ImplAAFLocator ()
+{}
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFLocator::GetPath (
+      aafCharacter *  /*pPathBuf*/,
+      aafInt32  /*bufSize*/)
+{
+  return AAFRESULT_ABSTRACT_CLASS;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFLocator::GetPathBufLen (
+      aafInt32 *  /*pLen*/)
+{
+  return AAFRESULT_ABSTRACT_CLASS;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFLocator::SetPath (
+      aafCharacter *  /*pPathBuf*/)
+{
+  return AAFRESULT_ABSTRACT_CLASS;
+}
+
+OMDEFINE_STORABLE(ImplAAFLocator, AUID_AAFLocator);
+
+
