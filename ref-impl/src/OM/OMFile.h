50985215fa6b217c913fd931a120d1c0e68667a7
tbingham <>
2001-04-17 09:50:40 +0000

    More wide character support. The OM now uses wide character strings throughout. Some additiona header file clean up.

--- ref-impl/src/OM/OMFile.h
+++ ref-impl/src/OM/OMFile.h
@@ -5,19 +5,22 @@
 #include "OMStoredObject.h"
 #include "OMTypes.h"
 
+#include <stddef.h>
+
 class OMClassFactory;
 class OMObjectDirectory;
 
 class OMFile : public OMStorable {
 public:
 
-  OMFile(const char* name, const OMAccessMode mode);
+  OMFile(const OMAccessMode mode, OMStoredObject* store);
+  OMFile(const OMAccessMode mode, OMStoredObject* store, OMStorable* root);
   ~OMFile(void);
 
-  static OMFile* openRead(const char* fileName);
-  static OMFile* openModify(const char* fileName);
-  static OMFile* createWrite(const char* fileName, const OMStorable* root);
-  static OMFile* createModify(const char* fileName, const OMStorable* root);
+  static OMFile* openRead(const wchar_t* fileName);
+  static OMFile* openModify(const wchar_t* fileName);
+  static OMFile* createWrite(const wchar_t* fileName, OMStorable* root);
+  static OMFile* createModify(const wchar_t* fileName, OMStorable* root);
 
   void save(void);
 
@@ -40,11 +43,6 @@ public:
 
 private:
 
-  void openRead(void);
-  void openModify(void);
-  void createModify(const OMStorable* root);
-
-  char* _name;
   OMStorable* _root;
   OMStoredObject* _rootStoredObject;
   
