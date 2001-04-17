13dc73fb1498079b84887b3fc499cb92627e5e34
tbingham <>
2001-04-17 09:19:47 +0000

    Now human-typed.

---
+++ ref-impl/src/impl/ImplAAFTypeDefSet.h
@@ -0,0 +1,183 @@
+//@doc
+//@class    AAFTypeDefSet | Implementation class for AAFTypeDefSet
+#ifndef __ImplAAFTypeDefSet_h__
+#define __ImplAAFTypeDefSet_h__
+
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+class ImplAAFPropertyValue;
+
+class ImplEnumAAFPropertyValues;
+
+
+
+
+
+#ifndef __ImplAAFTypeDef_h__
+#include "ImplAAFTypeDef.h"
+#endif
+
+
+class ImplAAFTypeDefSet : public ImplAAFTypeDef
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplAAFTypeDefSet ();
+
+protected:
+  virtual ~ImplAAFTypeDefSet ();
+
+public:
+
+  //****************
+  // Initialize()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Initialize
+        (// @parm [in] auid to be used to identify this type
+         aafUID_t *  pID,
+
+         // @parm [in] type of each element to be contained in this set
+         ImplAAFTypeDef * pTypeDef,
+
+         // @parm [in] friendly name of this type definition
+         aafCharacter *  pTypeName);
+
+
+  //****************
+  // GetType()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetType
+        // @parm [out] type of elements in this array
+        (ImplAAFTypeDef ** ppTypeDef);
+
+
+  //****************
+  // GetCount()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetCount
+        (// @parm [in] property value of array
+         ImplAAFPropertyValue * pPropVal,
+
+         // @parm [out] count of elements in the specified set property value
+         aafUInt32 *  pCount);
+
+
+  //****************
+  // AddElement()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    AddElement
+        (// @parm [in] property value corresponding to set to which element is added
+         ImplAAFPropertyValue * pInPropVal,
+
+         // @parm [in] value to be added to this set
+         ImplAAFPropertyValue * pMemberPropVal);
+
+
+  //****************
+  // CreateValueFromValues()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    CreateValueFromValues
+        (// @parm [in, size_is(numElements)] array of property values for elements of set value which
+    // is to be created.
+         ImplAAFPropertyValue ** pElementValues,
+
+         // @parm [in] size of pElementValues array.
+         aafUInt32  numElements,
+
+         // @parm [out] newly-created property value
+         ImplAAFPropertyValue ** ppPropVal);
+
+
+  //****************
+  // CreateValueFromCArray()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    CreateValueFromCArray
+        (// @parm [in, size_is(initDataSize)] pointer to compile-time C array containing data to use
+         aafMemPtr_t  pInitData,
+
+         // @parm [in] size of data in pInitData, in bytes
+         aafUInt32  initDataSize,
+
+         // @parm [out] newly created property value
+         ImplAAFPropertyValue ** ppPropVal);
+
+
+  //****************
+  // GetElements()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetElements
+        (// @parm [in] property value to read
+         ImplAAFPropertyValue * pInPropVal,
+
+         // @parm [out] enumerator across property values
+         ImplEnumAAFPropertyValues ** ppEnum);
+
+
+
+  //*************************************************************
+  //
+  // Overrides from OMType, via inheritace through ImplAAFTypeDef
+  //
+  //*************************************************************
+
+  /*
+  virtual void reorder(OMByte* bytes,
+                       size_t bytesSize) const;
+
+  virtual size_t externalSize(void) const;
+
+  virtual void externalize(OMByte* internalBytes,
+                           size_t internalBytesSize,
+                           OMByte* externalBytes,
+                           size_t externalBytesSize,
+                           OMByteOrder byteOrder) const;
+
+  virtual size_t internalSize(void) const;
+
+  virtual void internalize(OMByte* externalBytes,
+                           size_t externalBytesSize,
+                           OMByte* internalBytes,
+                           size_t internalBytesSize,
+                           OMByteOrder byteOrder) const;
+						   */
+
+  // overrides from ImplAAFTypeDef
+  //
+  virtual aafBool IsFixedSize (void) const;
+  virtual size_t PropValSize (void) const;
+  virtual aafBool IsRegistered (void) const;
+  virtual size_t NativeSize (void) const;
+
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFTypeDefSet)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFTypeDefSetTest.cpp.
+  static AAFRESULT test();
+};
+
+#endif // ! __ImplAAFTypeDefSet_h__
+
+
---
+++ test/com/ComModTestAAF/ModuleTests/CAAFTypeDefSetTest.cpp
@@ -0,0 +1,47 @@
+// @doc INTERNAL
+// @com This file implements the module test for CAAFTypeDefSet
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
+
+
+
+
+
+
+
+#include "AAFTypes.h" //Use #include "AAF.h" for functional module test.
+#include "AAFResult.h"
+
+
+extern "C" HRESULT CAAFTypeDefSet_test()
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
