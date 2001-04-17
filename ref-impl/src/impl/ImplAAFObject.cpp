22693715bf78a1c28f01e2ec26befd8bc2e67a0e
tbingham <>
2001-04-17 09:28:07 +0000

    Initial creation.

---
+++ ref-impl/src/impl/ImplAAFObject.cpp
@@ -0,0 +1,77 @@
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+
+
+
+#ifndef __ImplAAFObject_h__
+#include "ImplAAFObject.h"
+#endif
+
+#include <assert.h>
+
+
+ImplAAFObject::ImplAAFObject ()
+: _pContainer (0)
+{}
+
+
+ImplAAFObject::~ImplAAFObject ()
+{}
+
+
+void ImplAAFObject::InitContainer (void * pContainer)
+{
+  assert (! _pContainer);
+  _pContainer = pContainer;
+  assert (_pContainer);
+}
+
+
+// AxD_XMETHOD1(itsFile,
+// 			[out],objOut,AAFFile,aFile, ,
+// 			Returns the AAFFile containing the object in persistent
+//   // store\, or NULL for transient objects.)
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFObject::isForeignByteOrder (aafBool*  /*isForeign*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+  //@comm This routine is optimized in the common case (objects byte order == file byte order).	
+  //@comm Replaces ompvtIsForeignByteOrder
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFObject::Delete ()
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+  //@comm Replaces omfsObjectDelete
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFObject::IsTypeOf (aafUID_t      /*aClass*/,
+                           aafBool*  /*Result*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+ 
+
+
+class IAAFObject;
+
+void * ImplAAFObject::GetContainer ()
+{
+  assert (_pContainer);
+  return _pContainer;
+}
+
