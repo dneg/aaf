91c2721d765cf0e84beaefdd2f2398f3c488511a
tbingham <>
2001-04-17 06:32:11 +0000

    Removing CoCreateInstance from the module tests.

--- test/com/ComModTestAAF/ModuleTests/CAAFTimecodeStream12MTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFTimecodeStream12MTest.cpp
@@ -1,13 +1,13 @@
 // @doc INTERNAL
-// @com This file implements the module test for CAAFDefinitionObject
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+// @com This file implements the module test for CAAFTimecodeStream12M
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
 
 /******************************************\
 *                                          *
@@ -35,56 +35,13 @@
 #error - improperly defined include guard
 #endif
 
-
-#include <iostream.h>
-
-// Temporarily necessary global declarations.
-extern "C" const CLSID CLSID_AAFTimecodeStream12M; // generated
+#include "AAFResult.h"
 
 
 HRESULT CAAFTimecodeStream12M::test()
 {
-  HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
-  IAAFTimecodeStream12M *pObject = NULL;
-
-  try
-    {
-      // Attempt to create an AAFTimecodeStream12M.
-      hr =  CoCreateInstance(
-                             CLSID_AAFTimecodeStream12M,
-                             NULL, 
-                             CLSCTX_INPROC_SERVER, 
-                             IID_IAAFTimecodeStream12M, (void **)&pObject);
-      if (FAILED(hr))
-        {
-          cerr << "CAAFTimecodeStream12M::test...FAILED!";
-          cerr << hr;
-          cerr << "\tCoCreateInstance(&CLSID_AAFTimecodeStream12M, NULL,"
-            " CLSCTX_INPROC_SERVER, &IID_IAAFTimecodeStream12M, ...);" <<
-              endl;
-          return hr;
-        }
-
-      // module-specific tests go here
-
-      if (pObject)
-        pObject->Release();
-      return AAFRESULT_NOT_IMPLEMENTED;
-
-    }
-  catch (...)
-    {
-      cerr << "CAAFTimecodeStream12M::test...Caught general C++"
-        " exception!" << endl; 
-    }
-
-  // Cleanup our object if it exists.
-  if (pObject)
-    pObject->Release();
-
-  return hr;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
-
 
 
 /****/
