7adddc11a06adf811f0ec8c52d187ae0d47ae96d
tbingham <>
2001-04-17 11:55:24 +0000

    AAF/build/pdefs-Unix.mk

---
+++ build/pdefs-Unix.mk
@@ -0,0 +1,21 @@
+############################################
+#                                          #
+# Advanced Authoring Format                #
+#                                          #
+# Copyright (c) 2000 Avid Technology, Inc. #
+#                                          #
+############################################
+
+#
+# This file contains makefile definitions common to Unix platforms.
+# Platform definition files (pdefs-*.mk) for Unix include this file.
+#
+
+ECHO = echo
+MV = mv
+CP = cp
+RM = rm -f
+RMDIR = rm -rf
+CAT = cat
+MKDIR = mkdir -p
+
