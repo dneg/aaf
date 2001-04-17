1a7cddd753d13afe97a016eb7a3abd4925a77297
tbingham <>
2001-04-17 05:05:41 +0000

    Added missing "HUMAN_TYPED_IMPL" to ClearCase. The 12:30 watchdog failed today because these files were not checked into clearcase.

---
+++ ref-impl/src/impl/ImplEnumAAFPropertyValues.h
@@ -0,0 +1,120 @@
+//@doc
+//@class    EnumAAFPropertyValues | Implementation class for EnumAAFPropertyValues
+#ifndef __ImplEnumAAFPropertyValues_h__
+#define __ImplEnumAAFPropertyValues_h__
+
+
+/***********************************************************************
+*
+*              Copyright (c) 1998-1999 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted,
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+*  prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
+
+
+
+class ImplAAFPropertyValue;
+
+
+
+
+
+#ifndef __ImplAAFRoot_h__
+#include "ImplAAFRoot.h"
+#endif
+
+
+class ImplEnumAAFPropertyValues : public ImplAAFRoot
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplEnumAAFPropertyValues ();
+
+protected:
+  virtual ~ImplEnumAAFPropertyValues ();
+
+public:
+
+  //****************
+  // NextOne()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    NextOne
+        // @parm [out,retval] The Next Property Value
+        (ImplAAFPropertyValue ** ppPropertyValue);
+
+
+  //****************
+  // Next()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Next
+        (// @parm [in] number of objects requested
+         aafUInt32  count,
+
+         // @parm [out, size_is(count), length_is(*pFetched)] array to receive Property Values
+         ImplAAFPropertyValue ** ppPropertyValues,
+
+         // @parm [out,ref] number of actual Property Values fetched into ppPropertyValues
+	// array
+         aafUInt32 *  pFetched);
+
+
+  //****************
+  // Skip()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Skip
+        // @parm [in] Number of elements to skip
+        (aafUInt32  count);
+
+
+  //****************
+  // Reset()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Reset ();
+
+
+
+  //****************
+  // Clone()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Clone
+        // @parm [out,retval] new enumeration
+        (ImplEnumAAFPropertyValues ** ppEnum);
+
+
+public:
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplEnumAAFPropertyValuesTest.cpp.
+  static AAFRESULT test();
+};
+
+#endif // ! __ImplEnumAAFPropertyValues_h__
+
+
