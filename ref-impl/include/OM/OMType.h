a490b62848a5934a00c47c2b48c5dafb37d4999b
tbingham <>
2001-04-17 07:15:14 +0000

    New Object Manager abstract base class to parameterize type specific persistence behaviour.

---
+++ ref-impl/include/OM/OMType.h
@@ -0,0 +1,135 @@
+// @doc OMEXTERNAL
+#ifndef OMTYPE_H
+#define OMTYPE_H
+
+#include "OMDataTypes.h"
+
+#include <stddef.h>
+
+  // @class Abstract base class describing the data types that may be
+  //        assumed by persistent properties supported by the Object Manager.
+class OMType {
+public:
+  // @access Public members.
+
+    // @cmember Reorder (swap) the given <p bytes> according to the
+    //          data type described by this <c OMType>. The number of
+    //          bytes is given by <p bytesSize>. Object Manager clients
+    //          must provide a suitable implementation of this virtual
+    //          function for the data type being described. This class
+    //          provides static functions to aid in such an implementation.
+    // @this const
+  virtual void reorder(OMByte* bytes,
+                       size_t bytesSize) const = 0;
+
+    // @cmember The size, in bytes, of an entity with data type
+    //          described by this <c OMType> when persisted.
+    //          Object Manager clients must provide a suitable
+    //          implementation of this virtual function for the data
+    //          type being described. This class provides static
+    //          functions to aid in such an implementation.
+    // @this const
+  virtual size_t externalSize(void) const = 0;
+
+    // @cmember Convert the given <p internalBytes> from internal
+    //          (in memory) representation to external (persisted)
+    //          representation, according to the data type described
+    //          by this <c OMType>. The number of bytes is given by
+    //          <p internalBytesSize>. Together <p externalBytes>
+    //          and <p externalBytesSize> define a buffer that must
+    //          be of appropriate size to receive the converted
+    //          value. Note that the resulting external value may be
+    //          smaller than, larger than, or the same size as the
+    //          original internal value depending on the relationship
+    //          between <mf OMType::internalSize> and
+    //          <mf OMType::externalSize>.
+    //          The value is in the <p byteOrder> specified.
+    //          Object Manager clients must provide a suitable
+    //          implementation of this virtual function for the data
+    //          type being described. This class provides static
+    //          functions to aid in such an implementation.
+    // @this const
+  virtual void externalize(OMByte* internalBytes,
+                           size_t internalBytesSize,
+                           OMByte* externalBytes,
+                           size_t externalBytesSize,
+                           OMByteOrder byteOrder) const = 0;
+
+
+    // @cmember The size, in bytes, of an entity with data type
+    //          described by this <c OMType> when in memory.
+    //          Object Manager clients must provide a suitable
+    //          implementation of this virtual function for the data
+    //          type being described. This class provides static
+    //          functions to aid in such an implementation.
+    // @this const
+  virtual size_t internalSize(void) const = 0;
+
+    // @cmember Convert the given <p externalBytes> from external
+    //          (persisted) representation to internal (in memory)
+    //          representation, according to the data type described
+    //          by this <c OMType>. The number of bytes is given by
+    //          <p externalBytesSize>. Together <p internalBytes>
+    //          and <p internalBytesSize> define a buffer that must
+    //          be of appropriate size to receive the converted
+    //          value. Note that the resulting internal value may be
+    //          smaller than, larger than, or the same size as the
+    //          original external value depending on the relationship
+    //          between <mf OMType::externalSize> and
+    //          <mf OMType::internalSize>.
+    //          The value is in the <p byteOrder> specified.
+    //          Object Manager clients must provide a suitable
+    //          implementation of this virtual function for the data
+    //          type being described. This class provides static
+    //          functions to aid in such an implementation.
+    // @this const
+  virtual void internalize(OMByte* externalBytes,
+                           size_t externalBytesSize,
+                           OMByte* internalBytes,
+                           size_t internalBytesSize,
+                           OMByteOrder byteOrder) const = 0;
+
+  // @access Static members.
+
+    // @cmember Reorder (swap) the integer described by <p bytes> and
+    //          <p bytesSize>. This static function is provided to aid
+    //          Object Manager clients in providing suitable
+    //          implementations of the virtual functions in this class.
+    //          In particular, Object Manager clients may wish to use
+    //          this function when implementing <mf OMType::reorder>.
+  static void reorderInteger(OMByte* bytes,
+                             size_t bytesSize);
+
+    // @cmember Expand, by padding with leading zeros, the value decribed
+    //          by <p inputBytes> and <p inputBytesSize> into the buffer
+    //          described by <p outputBytes> and <p outputBytesSize>. The
+    //          value is in the <p byteOrder> specified. This static function
+    //          is provided to aid Object Manager clients in providing
+    //          suitable implementations of the virtual functions in this
+    //          class. In particular, Object Manager clients may wish to use
+    //          this function when implementing <mf OMType::internalize> and
+    //          <mf OMType::externalize>.
+  static void expand(OMByte* inputBytes,
+                     size_t inputBytesSize,
+                     OMByte* outputBytes,
+                     size_t outputBytesSize,
+                     OMByteOrder byteOrder);
+
+    // @cmember Contract, by discarding leading bytes, the value decribed
+    //          by <p inputBytes> and <p inputBytesSize> into the buffer
+    //          described by <p outputBytes> and <p outputBytesSize>. The
+    //          value is in the <p byteOrder> specified. This static function
+    //          is provided to aid Object Manager clients in providing
+    //          suitable implementations of the virtual functions in this
+    //          class. In particular, Object Manager clients may wish to use
+    //          this function when implementing <mf OMType::internalize> and
+    //          <mf OMType::externalize>.
+  static void contract(OMByte* inputBytes,
+                       size_t inputBytesSize,
+                       OMByte* outputBytes,
+                       size_t outputBytesSize,
+                       OMByteOrder byteOrder);
+
+};
+
+#endif
---
+++ ref-impl/src/OM/OMType.cpp
@@ -0,0 +1,119 @@
+// @doc OMEXTERNAL
+#include "OMType.h"
+
+#include "OMAssertions.h"
+
+#include <string.h>
+
+  // @mfunc Reorder (swap) the integer described by <p bytes> and
+  //        <p bytesSize>. This static function is provided to aid
+  //        Object Manager clients in providing suitable
+  //        implementations of the virtual functions in this class.
+  //        In particular, Object Manager clients may wish to use
+  //        this function when implementing <mf OMType::reorder>.
+  //   @parm The buffer containing the bytes to reorder.
+  //   @parm The size of the buffer.
+void OMType::reorderInteger(OMByte* bytes,
+                            size_t bytesSize)
+{
+  TRACE("OMType::reorderInteger");
+  PRECONDITION("Valid size", (bytesSize == 2) ||
+                             (bytesSize == 4) ||
+                             (bytesSize == 8));
+
+
+  size_t bytesToSwap = bytesSize / 2;
+  for (size_t i = 0; i < bytesToSwap; i++) {
+    OMByte temp = bytes[i];
+    bytes[i] = bytes[bytesSize - i - 1];
+    bytes[bytesSize - i - 1] = temp;
+  }
+
+}
+
+  // @mfunc Expand, by padding with leading zeros, the value decribed
+  //        by <p inputBytes> and <p inputBytesSize> into the buffer
+  //        described by <p outputBytes> and <p outputBytesSize>. The
+  //        value is in the <p byteOrder> specified. This static function
+  //        is provided to aid Object Manager clients in providing
+  //        suitable implementations of the virtual functions in this
+  //        class. In particular, Object Manager clients may wish to use
+  //        this function when implementing <mf OMType::internalize> and
+  //        <mf OMType::externalize>.
+  //   @parm The buffer containing the input bytes.
+  //   @parm The size of the input buffer.
+  //   @parm The buffer to receive the output bytes.
+  //   @parm The size of the output buffer.
+  //   @parm The byte order.
+void OMType::expand(OMByte* inputBytes,
+                    size_t inputBytesSize,
+                    OMByte* outputBytes,
+                    size_t outputBytesSize,
+                    OMByteOrder byteOrder)
+{
+  TRACE("OMType::expand");
+  PRECONDITION("Valid input bytes", inputBytes != 0);
+  PRECONDITION("Valid output bytes", outputBytes != 0);
+  PRECONDITION("Valid output size", outputBytesSize > 0);
+  PRECONDITION("Valid sizes", outputBytesSize > inputBytesSize);
+
+  OMByte* ip = inputBytes;
+  OMByte* op = outputBytes;
+  size_t padSize = outputBytesSize - inputBytesSize;
+
+  if (byteOrder == littleEndian) {
+    // copy valid bytes
+    memcpy(op, ip, inputBytesSize);
+    op = op + inputBytesSize;
+    // zero fill pad bytes (assumes sign extension not wanted)
+    memset(op, 0, padSize); 
+  } else { // byteOrder == bigEndian
+    // zero fill pad bytes (assumes sign extension not wanted)
+    memset(op, 0, padSize); 
+    op = op + padSize;
+    // copy valid bytes
+    memcpy(op, ip, inputBytesSize);
+  }
+}
+
+  // @mfunc Contract, by discarding leading bytes, the value decribed
+  //        by <p inputBytes> and <p inputBytesSize> into the buffer
+  //        described by <p outputBytes> and <p outputBytesSize>. The
+  //        value is in the <p byteOrder> specified. This static function
+  //        is provided to aid Object Manager clients in providing
+  //        suitable implementations of the virtual functions in this
+  //        class. In particular, Object Manager clients may wish to use
+  //        this function when implementing <mf OMType::internalize> and
+  //        <mf OMType::externalize>.
+  //   @parm The buffer containing the input bytes.
+  //   @parm The size of the input buffer.
+  //   @parm The buffer to receive the output bytes.
+  //   @parm The size of the output buffer.
+  //   @parm The byte order.
+void OMType::contract(OMByte* inputBytes,
+                      size_t inputBytesSize,
+                      OMByte* outputBytes,
+                      size_t outputBytesSize,
+                      OMByteOrder byteOrder)
+{
+  TRACE("OMType::contract");
+  PRECONDITION("Valid input bytes", inputBytes != 0);
+  PRECONDITION("Valid output bytes", outputBytes != 0);
+  PRECONDITION("Valid output size", outputBytesSize > 0);
+  PRECONDITION("Valid sizes", outputBytesSize < inputBytesSize);
+
+  OMByte* ip = inputBytes;
+  OMByte* op = outputBytes;
+  size_t trimSize = inputBytesSize - outputBytesSize;
+
+  if (byteOrder == littleEndian) {
+    // copy valid bytes
+    memcpy(op, ip, outputBytesSize);
+    // ignore trimmed bytes (nop)
+  } else { // byteOrder == bigEndian
+    // ignore trimmed bytes
+    ip = ip + trimSize;
+    // copy valid bytes
+    memcpy(op, ip, outputBytesSize);
+  }
+}
