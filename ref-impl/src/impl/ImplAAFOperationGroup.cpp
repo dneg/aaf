78679be276f4ef24bd189a466db980e7db164687
tbingham <>
2001-04-17 09:24:15 +0000

    Make common effect translation code

--- ref-impl/src/impl/ImplAAFOperationGroup.cpp
+++ ref-impl/src/impl/ImplAAFOperationGroup.cpp
@@ -232,8 +232,8 @@ AAFRESULT STDMETHODCALLTYPE
 	if(sourceRef == NULL)
 		return AAFRESULT_NULL_PARAM;
 
-//	if (_rendering.isPresent())
-//	{
+	if (_rendering.isPresent())
+	{
 		if (_rendering)
 		{
 			*sourceRef = _rendering;
@@ -241,9 +241,9 @@ AAFRESULT STDMETHODCALLTYPE
 		}
 		else
 			return AAFRESULT_PROP_NOT_PRESENT;
-//	}
-//	else
-//		return AAFRESULT_PROP_NOT_PRESENT;
+	}
+	else
+		return AAFRESULT_PROP_NOT_PRESENT;
 
 	return AAFRESULT_SUCCESS;
 }
