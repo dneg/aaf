5fc0cf3855ca0fd23c267d25f65936f9b443aee2
tbingham <>
2001-04-17 07:49:11 +0000

    DR4_CPR: Updated test to call CreateEmptyValue and CreateValueFromValues methods for arrays of strong references.

--- test/com/ComModTestAAF/ModuleTests/CAAFTypeDefVariableArryTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFTypeDefVariableArryTest.cpp
@@ -65,6 +65,11 @@ typedef IAAFSmartPointer<IAAFSegment>           IAAFSegmentSP;
 typedef IAAFSmartPointer<IAAFTimelineMobSlot>   IAAFTimelineMobSlotSP;
 typedef IAAFSmartPointer<IEnumAAFMobSlots>      IEnumAAFMobSlotsSP;
 typedef IAAFSmartPointer<IAAFMobSlot>           IAAFMobSlotSP;
+typedef IAAFSmartPointer<IAAFTypeDefObjectRef>  IAAFTypeDefObjectRefSP;
+typedef IAAFSmartPointer<IAAFTypeDefStrongObjRef>   IAAFTypeDefStrongObjRefSP;
+typedef IAAFSmartPointer<IAAFTaggedValue>       IAAFTaggedValueSP;
+typedef IAAFSmartPointer<IAAFMetaDefinition>    IAAFMetaDefinitionSP;
+typedef IAAFSmartPointer<IEnumAAFPropertyValues> IEnumAAFPropertyValuesSP;
 
 //// Our TEST values ...
 	
@@ -95,6 +100,48 @@ static  aafMobID_t						TEST_MobID =
 static const aafSlotID_t				TEST_SLOT_ID = 7;
 #define									TEST_SLOT_NAME	L"Slot containing our VA Segment"
 
+
+
+static aafCharacter_constptr kTaggedValueStrongReferenceName = L"TaggedValueStrongReference type";
+// {da60bb00-ba41-11d4-a812-8e541b972ea3}
+static const aafUID_t kTaggedValueStrongReferenceTypeID =
+{ 0xda60bb00, 0xba41, 0x11d4, { 0xa8, 0x12, 0x8e, 0x54, 0x1b, 0x97, 0x2e, 0xa3 } };
+
+static aafCharacter_constptr kTaggedValueVariableArrayName = L"TaggedValueVariableArray type";
+// {c9fb1100-ba3e-11d4-a812-8e541b972ea3}
+static const aafUID_t kTaggedValueVariableArrayTypeID =
+{ 0xc9fb1100, 0xba3e, 0x11d4, { 0xa8, 0x12, 0x8e, 0x54, 0x1b, 0x97, 0x2e, 0xa3 } };
+
+static aafCharacter_constptr kComponentAttributePropertyName1 = L"Component Attributes property1";
+// {198e0c80-ba40-11d4-a812-8e541b972ea3}
+static const aafUID_t kComponentAttributesProperty1ID =
+{ 0x198e0c80, 0xba40, 0x11d4, { 0xa8, 0x12, 0x8e, 0x54, 0x1b, 0x97, 0x2e, 0xa3 } };
+
+static aafCharacter_constptr kComponentAttributePropertyName2 = L"Component Attributes property2";
+// {198e0c81-ba40-11d4-a812-8e541b972ea3}
+static const aafUID_t kComponentAttributesProperty2ID =
+{ 0x198e0c81, 0xba40, 0x11d4, { 0xa8, 0x12, 0x8e, 0x54, 0x1b, 0x97, 0x2e, 0xa3 } };
+
+static const aafUInt32 kMaxComponentAttributes = 5;
+
+static aafCharacter_constptr kTaggedValueTag[kMaxComponentAttributes] =
+{
+  L"TAG 1",
+  L"TAG 2",
+  L"TAG 3",
+  L"TAG 4",
+  L"TAG 5"  
+};
+
+static aafCharacter_constptr kTaggedValueValue[kMaxComponentAttributes] =
+{
+  L"VALUE 1",
+  L"VALUE 2",
+  L"VALUE 3",
+  L"VALUE 4",
+  L"VALUE 5"  
+};
+ 
 
 // i find this convenient to compare the IUNK's of two interfaces :
 template <class T1, class T2>
@@ -193,6 +240,270 @@ static HRESULT addVATypeToComponent(IAAFDictionary* const pDict)
 }//addVATypeToComponent()
 
 
+static HRESULT  createTaggedValueStrongReferenceType (IAAFDictionary* const pDict)
+{
+	IAAFClassDefSP spTargetClass;
+	checkResult(pDict->LookupClassDef (AUID_AAFTaggedValue, &spTargetClass));
+	
+	IAAFTypeDefStrongObjRefSP spTaggedValueStrongReference;
+	checkResult(pDict->CreateMetaInstance (AUID_AAFTypeDefStrongObjRef, IID_IAAFTypeDefStrongObjRef, (IUnknown **) &spTaggedValueStrongReference));
+	checkResult(spTaggedValueStrongReference->Initialize(kTaggedValueStrongReferenceTypeID, spTargetClass,  kTaggedValueStrongReferenceName));
+	
+	IAAFTypeDefSP spTD_fa;
+	checkResult(spTaggedValueStrongReference->QueryInterface(IID_IAAFTypeDef, (void**)&spTD_fa));
+	checkResult(pDict->RegisterTypeDef(spTD_fa));
+	
+	return S_OK;
+}//createVAType()
+
+
+static HRESULT  createTaggedValueVAType (IAAFDictionary* const pDict)
+{
+	IAAFTypeDefSP spTD_elem;
+	checkResult(pDict->LookupTypeDef (kTaggedValueStrongReferenceTypeID, &spTD_elem));
+	
+	//Create a Variable Array
+	IAAFTypeDefVariableArraySP spVA;
+	checkResult(pDict->CreateMetaInstance (AUID_AAFTypeDefVariableArray, IID_IAAFTypeDefVariableArray, (IUnknown **) &spVA));
+	
+	//IAAFTypeDefVariableArray::Initialize
+	checkResult(spVA->Initialize(kTaggedValueVariableArrayTypeID, spTD_elem,  kTaggedValueVariableArrayName));
+	
+	//  Register our new VA type def :
+	IAAFTypeDefSP spTD_fa;
+	checkResult(spVA->QueryInterface(IID_IAAFTypeDef, (void**)&spTD_fa));
+	checkResult(pDict->RegisterTypeDef(spTD_fa));
+	
+	return S_OK;
+}//createVAType()
+
+
+static HRESULT addTaggedValueVATypeToComponent(IAAFDictionary* const pDict)
+{
+	CAAFBuiltinDefs defs(pDict);
+	
+	///////////////////////////////////////////////////////////////////////
+	//Look up a Class based on a component? or some meaningful class
+	IAAFClassDefSP spSomeClass;
+	checkResult(pDict->LookupClassDef (AUID_AAFComponent, &spSomeClass));	
+	//OK, lets register (and get) a property def, give it a name + typedef, and
+	
+	//Lookup the TEST_VA_TYPE_ID we just created!
+	IAAFTypeDefSP spTD;
+	checkResult(pDict->LookupTypeDef (kTaggedValueVariableArrayTypeID, &spTD));
+	
+		
+	//	Register the Properties
+	IAAFPropertyDefSP spPropDef1;
+	checkResult(spSomeClass->RegisterOptionalPropertyDef (kComponentAttributesProperty1ID,
+		kComponentAttributePropertyName1,
+		spTD,
+		&spPropDef1));
+
+	IAAFPropertyDefSP spPropDef2;
+	checkResult(spSomeClass->RegisterOptionalPropertyDef (kComponentAttributesProperty2ID,
+		kComponentAttributePropertyName2,
+		spTD,
+		&spPropDef2));
+	
+	return S_OK;
+}//addTaggedValueVATypeToComponent()
+
+
+static HRESULT createStringTaggedValue(
+  IAAFDictionary* const pDict,
+  aafCharacter_constptr tag, 
+  aafCharacter_constptr value, 
+  IAAFPropertyValue **ppPropertyValue)
+{
+  assert (pDict && tag && value && ppPropertyValue);
+	CAAFBuiltinDefs defs(pDict);
+	
+	/// first, create  tagged value 
+	IAAFTaggedValueSP  spTaggedValue;
+	checkResult(defs.cdTaggedValue()->CreateInstance(IID_IAAFTaggedValue, (IUnknown**)&spTaggedValue));
+	checkResult(spTaggedValue->Initialize(tag, defs.tdString(), (wcslen(value) + 1) * sizeof(aafCharacter), (aafDataBuffer_t)value));
+	
+	// Now create the strong reference to tagged value
+	IAAFTypeDefSP spType;
+	checkResult(pDict->LookupTypeDef(kTaggedValueStrongReferenceTypeID, &spType));
+	
+	IAAFTypeDefObjectRefSP spObjectReferenceType;
+	checkResult(spType->QueryInterface(IID_IAAFTypeDefObjectRef, (void **)&spObjectReferenceType));
+	
+	checkResult(spObjectReferenceType->CreateValue(spTaggedValue, ppPropertyValue));
+	
+	return S_OK;
+}
+
+static HRESULT createTaggedValueVAFiller(IAAFDictionary* const pDict, aafUID_constref propertyID, IAAFFillerSP&  spFill)
+{
+	
+	//handy - QI filler for  Object intf.		
+	IAAFObjectSP spObj;
+	checkResult(spFill->QueryInterface(IID_IAAFObject, (void**)&spObj));
+	
+	//Get the  property def for Component::VA
+	IAAFClassDefSP spCD_comp;
+	checkResult(pDict->LookupClassDef(AUID_AAFComponent, &spCD_comp));
+	
+	//From Class Def, get the Property Def
+	IAAFPropertyDefSP spPD_comp;
+	checkResult(spCD_comp->LookupPropertyDef(propertyID, &spPD_comp));
+	
+	aafBoolean_t  bIsPresent = kAAFTrue;
+	
+	//Verify that optional property is NOT yet present in object
+	checkResult(spObj->IsPropertyPresent(spPD_comp, &bIsPresent));
+	checkExpression(bIsPresent == kAAFFalse, AAFRESULT_TEST_FAILED);
+	
+	
+	//Now, create a property value .......
+	
+	//first, get the type def
+	//Lookup the VA type
+	IAAFTypeDefSP spTypeDef;
+	checkResult(pDict->LookupTypeDef(kTaggedValueVariableArrayTypeID, &spTypeDef));
+	
+	//Get the VA typedef
+	IAAFTypeDefVariableArraySP  spVA;
+	checkResult(spTypeDef->QueryInterface(IID_IAAFTypeDefVariableArray, (void**)&spVA));
+	
+	
+	//Set the array up: Create an array of strong references to tagged values...
+	IAAFPropertyValueSP spElementPropertyValueArray[kMaxComponentAttributes];
+	IAAFPropertyValue * pElementPropertyValueArray[kMaxComponentAttributes]; // copies of pointers "owned by" the smartptr array.
+  aafUInt32 i;
+  
+	for (i = 0; i < kMaxComponentAttributes; i++)
+	{
+	  IAAFPropertyValueSP spElementPropertyValue;
+	  checkResult(createStringTaggedValue(pDict, kTaggedValueTag[i], kTaggedValueValue[i], &spElementPropertyValue));
+	  
+	  spElementPropertyValueArray[i] = spElementPropertyValue;
+	  pElementPropertyValueArray[i] = spElementPropertyValueArray[i];
+	}
+
+  
+	IAAFPropertyValueSP spArrayPropertyValue;
+	
+	if (0 == memcmp(&propertyID, &kComponentAttributesProperty1ID, sizeof(aafUID_t)))
+	{
+	  // Create an empty array and then fill it by appending elements...
+	  //
+  	checkResult( spVA->CreateEmptyValue (&spArrayPropertyValue) );
+  	for (i = 0; i < kMaxComponentAttributes; i++)
+  	{	  
+  	  checkResult(spVA->AppendElement(spArrayPropertyValue, pElementPropertyValueArray[i]));
+  	}
+	}
+	else if (0 == memcmp(&propertyID, &kComponentAttributesProperty2ID, sizeof(aafUID_t)))
+	{
+	  // Create a new array from and array of values.
+	  //
+	  checkResult(spVA->CreateValueFromValues(pElementPropertyValueArray, kMaxComponentAttributes, &spArrayPropertyValue));
+	}
+	else
+	{
+	  checkResult(AAFRESULT_INVALID_PARAM);
+	}
+	
+	//Set the value VA to the Object *****************************************
+	checkResult(spObj->SetPropertyValue(spPD_comp, spArrayPropertyValue));
+	
+	//Verify that the optional property is now present in the object
+	checkResult(spObj->IsPropertyPresent(spPD_comp, &bIsPresent));
+	checkExpression(bIsPresent == kAAFTrue,  AAFRESULT_TEST_FAILED);
+	
+	return S_OK;
+}//createTaggedValueVAFiller()
+
+static HRESULT verityTaggedValueAttributes(
+  IAAFDictionary* const pDict, 
+  IAAFObject * pObject, 
+  aafUID_constref propertyID, 
+  testMode_t testMode)
+{
+  if (kAAFUnitTestReadWrite == testMode)
+  { 
+    // Write this test so that it will still work for DR3 & DR4 files.  
+    IAAFClassDefSP spComponentClass;
+    checkResult(pDict->LookupClassDef(AUID_AAFComponent, &spComponentClass));
+
+    IAAFPropertyDefSP spPropertyDef;
+  	checkResult(spComponentClass->LookupPropertyDef(propertyID, &spPropertyDef));
+
+  	// Verify that optional property is present in object
+  	aafBoolean_t  bIsPresent = kAAFFalse;
+  	checkResult(pObject->IsPropertyPresent(spPropertyDef, &bIsPresent));
+  	checkExpression(bIsPresent == kAAFTrue, AAFRESULT_TEST_FAILED);
+  	
+  	// Get the value...
+  	IAAFPropertyValueSP spArrayPropertyValue;
+  	checkResult(pObject->GetPropertyValue(spPropertyDef, &spArrayPropertyValue));
+  	IAAFTypeDefSP spPropertyValueType;
+  	checkResult(spArrayPropertyValue->GetType(&spPropertyValueType));
+  	
+  	// Make sure that the type is a variable array.
+  	IAAFTypeDefVariableArraySP spVA;
+  	checkResult(spPropertyValueType->QueryInterface(IID_IAAFTypeDefVariableArray, (void **)&spVA));
+  	
+  	IAAFTypeDefSP spElementType;
+  	checkResult(spVA->GetType(&spElementType));
+  	
+  	IAAFTypeDefStrongObjRefSP spStrongObjectReference;
+  	checkResult(spElementType->QueryInterface(IID_IAAFTypeDefStrongObjRef, (void **)&spStrongObjectReference));
+  	
+  	IAAFTypeDefObjectRefSP spObjectReference;
+  	checkResult(spStrongObjectReference->QueryInterface(IID_IAAFTypeDefObjectRef, (void **)&spObjectReference));
+  	
+  	IAAFClassDefSP spObjectClass;
+  	checkResult(spObjectReference->GetObjectType(&spObjectClass));
+  	
+  	IAAFMetaDefinitionSP spMetaDefinition;
+  	checkResult(spObjectClass->QueryInterface(IID_IAAFMetaDefinition, (void **)&spMetaDefinition));
+  	
+  	aafUID_t objectClassID;
+  	checkResult(spMetaDefinition->GetAUID(&objectClassID));
+    checkExpression(0 == memcmp(&objectClassID, &AUID_AAFTaggedValue, sizeof(aafUID_t)), AAFRESULT_TEST_FAILED);
+    
+    // Check the count.
+    aafUInt32 elementCount;
+    checkResult(spVA->GetCount(spArrayPropertyValue, &elementCount));
+    checkExpression(kMaxComponentAttributes == elementCount, AAFRESULT_TEST_FAILED);
+    
+    // Check the contents
+    IEnumAAFPropertyValuesSP spPropertyValues;
+    checkResult(spVA->GetElements(spArrayPropertyValue, &spPropertyValues));
+    
+    aafCharacter buffer[32]; // must be big enought for sample tags and values.
+    aafUInt32 index, bufferSize;;
+    for (index = 0; index < elementCount; index++)
+    {
+      IAAFPropertyValueSP spElementValue;
+      checkResult(spPropertyValues->NextOne(&spElementValue));
+      
+      IAAFTaggedValueSP spTaggedValue;
+      checkResult(spObjectReference->GetObject(spElementValue, IID_IAAFTaggedValue, (IUnknown **)&spTaggedValue));
+      
+      // Validate the tag...
+      checkResult(spTaggedValue->GetNameBufLen(&bufferSize));
+      checkExpression(sizeof(buffer) >= bufferSize, AAFRESULT_TEST_FAILED);  
+      checkResult(spTaggedValue->GetName(buffer, bufferSize));
+      checkExpression(0 == wcscmp(buffer, kTaggedValueTag[index]), AAFRESULT_TEST_FAILED);
+      
+      // Validate the value...
+      aafUInt32 bytesRead = 0;
+      checkResult(spTaggedValue->GetValueBufLen(&bufferSize));
+      checkExpression(sizeof(buffer) >= bufferSize, AAFRESULT_TEST_FAILED);  
+      checkResult(spTaggedValue->GetValue(bufferSize, (aafDataBuffer_t)buffer, &bytesRead));
+      checkExpression(0 == wcscmp(buffer, kTaggedValueValue[index]), AAFRESULT_TEST_FAILED);
+    }
+  }
+  	
+	return S_OK;
+}
+
 static HRESULT createVAFiller(IAAFDictionary* const pDict, IAAFFillerSP&  spFill)
 {
 	
@@ -247,8 +558,7 @@ static HRESULT createVAFiller(IAAFDictionary* const pDict, IAAFFillerSP&  spFill
 
 
 
-
-static HRESULT verifyContents (IAAFHeader* const pHeader, IAAFDictionary* const pDict,
+static HRESULT verifyContents (testMode_t testMode, IAAFHeader* const pHeader, IAAFDictionary* const pDict,
 							   const aafBoolean_t bMinimalTesting)
 {
 
@@ -341,7 +651,17 @@ static HRESULT verifyContents (IAAFHeader* const pHeader, IAAFDictionary* const 
 	checkResult(spVA->GetCArray(spPropVal, (aafMemPtr_t) check_va, sizeof(check_va)));
 	//VERIFY values:
 	for (i=0; i<TEST_VA_COUNT; i++)
-		checkExpression( check_va[i] == TEST_VA_VALUES[i], AAFRESULT_TEST_FAILED );	
+		checkExpression( check_va[i] == TEST_VA_VALUES[i], AAFRESULT_TEST_FAILED );
+	
+
+  aafProductVersion_t			testRev;
+  checkResult(pHeader->GetRefImplVersion(&testRev));
+  if(testRev.major >= 1 && (testRev.minor > 0 || testRev.patchLevel > 3))
+  { 	
+  	//Test see if the variable arrays of tagged values are present.
+  	checkResult(verityTaggedValueAttributes(pDict, spObj, kComponentAttributesProperty1ID, testMode));
+  	checkResult(verityTaggedValueAttributes(pDict, spObj, kComponentAttributesProperty2ID, testMode));
+	}
 
 	//At this point, the test is succesful for both the CREATE and READ (unscrambling of .aaf file) routines
 	if (bMinimalTesting)
@@ -544,7 +864,7 @@ static HRESULT verifyContents (IAAFHeader* const pHeader, IAAFDictionary* const 
 }//verifyContents()
 
 
-static HRESULT CreateAAFFile(aafWChar *  pFileName )
+static HRESULT CreateAAFFile(aafWChar *  pFileName)
 {
 	HRESULT hr = AAFRESULT_SUCCESS;
 	
@@ -575,6 +895,10 @@ static HRESULT CreateAAFFile(aafWChar *  pFileName )
 		assert (pFile);
 		checkResult (pFile->GetHeader (&pHeader));
 		assert (pHeader);
+
+    aafProductVersion_t			testRev;
+    checkResult(pHeader->GetRefImplVersion(&testRev));
+
 		checkResult (pHeader->GetDictionary (&pDict));
 		assert (pDict);
 		CAAFBuiltinDefs defs (pDict);
@@ -584,6 +908,15 @@ static HRESULT CreateAAFFile(aafWChar *  pFileName )
 		checkResult(createVAType (pDict));
 		
 		checkResult(addVATypeToComponent (pDict));
+		///////////
+
+    if(testRev.major >= 1 && (testRev.minor > 0 || testRev.patchLevel > 3))
+    { 	
+  		checkResult(createTaggedValueStrongReferenceType (pDict));
+  		checkResult(createTaggedValueVAType (pDict));
+
+      checkResult(addTaggedValueVATypeToComponent (pDict));
+    }
 		///////////
 		
 		//Create a concrete subclass of mob 
@@ -602,6 +935,13 @@ static HRESULT CreateAAFFile(aafWChar *  pFileName )
 		checkResult(spFill->Initialize(defs.ddSound(), 10));
 		
 		checkResult(createVAFiller(pDict, spFill));
+
+    if(testRev.major >= 1 && (testRev.minor > 0 || testRev.patchLevel > 3))
+    { 	
+      //Add tagged value attributes...
+  		checkResult(createTaggedValueVAFiller(pDict, kComponentAttributesProperty1ID, spFill));
+  		checkResult(createTaggedValueVAFiller(pDict, kComponentAttributesProperty2ID, spFill));
+		}
 		
 		//seg
 		//QI the filler for its segment
@@ -628,7 +968,7 @@ static HRESULT CreateAAFFile(aafWChar *  pFileName )
 		//////////////////// done /!!!!!!!!!!!!!!!!!!!!!!
 		
 		//Verify results right away (during this creation process) ....
-		checkResult(verifyContents (pHeader, pDict, kAAFTrue));  //True => minimal testing 
+		checkResult(verifyContents (kAAFUnitTestReadWrite, pHeader, pDict, kAAFTrue));  //True => minimal testing 
 		
 	}
 	catch (HRESULT & rResult)
@@ -649,7 +989,7 @@ static HRESULT CreateAAFFile(aafWChar *  pFileName )
 }//CreateAAFFile()
 
 
-static HRESULT  ReadAAFFile(aafWChar *  pFileName )
+static HRESULT  ReadAAFFile(aafWChar *  pFileName, testMode_t testMode)
 {
 	HRESULT hr = AAFRESULT_SUCCESS;
 	
@@ -670,7 +1010,7 @@ static HRESULT  ReadAAFFile(aafWChar *  pFileName )
 		assert (pDict);
 		
 		// Read the mob, slots, etc  to verify the contents ...
-		checkResult(verifyContents (pHeader, pDict, kAAFFalse));  //False => NOT minimal testing; i.e. test everything 
+		checkResult(verifyContents (testMode, pHeader, pDict, kAAFFalse));  //False => NOT minimal testing; i.e. test everything 
 		
 		
 	}//try
@@ -705,7 +1045,7 @@ extern "C" HRESULT CAAFTypeDefVariableArray_test(testMode_t mode)
 		else
 			hr = AAFRESULT_SUCCESS;
 		if(hr == AAFRESULT_SUCCESS)
-			hr = ReadAAFFile( pFileName );		
+			hr = ReadAAFFile( pFileName, mode);		
 		
 		if(hr == AAFRESULT_SUCCESS)
 			hr = AAFRESULT_NOT_IN_CURRENT_VERSION;
