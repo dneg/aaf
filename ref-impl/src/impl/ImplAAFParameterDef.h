c8b0b3334879c243352735f16a5161389c4dcfeb
tbingham <>
2001-04-17 04:16:11 +0000

    First round of effects checkins

---
+++ ref-impl/src/impl/ImplAAFParameterDef.h
@@ -0,0 +1,131 @@
+//@doc
+//@class    AAFParameterDef | Implementation class for AAFParameterDef
+#ifndef __ImplAAFParameterDef_h__
+#define __ImplAAFParameterDef_h__
+
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+
+class ImplAAFReferenceValue;
+
+class ImplEnumAAFReferenceValues;
+
+
+/***********************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
+\***********************************************/
+
+ 
+
+
+
+#ifndef __ImplAAFPluggableDef_h__
+#include "ImplAAFPluggableDef.h"
+#endif
+
+
+class ImplAAFParameterDef : public ImplAAFPluggableDef
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplAAFParameterDef ();
+
+protected:
+  virtual ~ImplAAFParameterDef ();
+
+public:
+
+
+  //****************
+  // GetParameterDataDefID()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetParameterDataDefID
+        // @parm [retval][out] Pointer to an AUID reference
+        (aafUID_t *  pParameterDataDefID);
+
+  //****************
+  // SetParameterDataDefID()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetParameterDataDefID
+        // @parm [in] an AUID reference
+        (aafUID_t  ParameterDataDefID);
+
+  //****************
+  // GetDisplayUnits()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetDisplayUnits
+        (// @parm [in,string] DisplayUnits
+         wchar_t *  pDisplayUnits,
+
+         // @parm [in] length of the buffer to hold DisplayUnits
+         aafInt32  bufSize);
+
+  //****************
+  // GetDisplayUnitsBufLen()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetDisplayUnitsBufLen
+        // @parm [out] DisplayUnits
+        (aafInt32 *  pLen);
+
+
+
+  //****************
+  // SetDisplayUnits()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetDisplayUnits
+        // @parm [in, string] DisplayUnits
+        (wchar_t *  pDisplayUnits);
+
+  //****************
+  // AddReferenceValue()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    AddReferenceValue
+        // @parm [in] Ref value to be added
+        (ImplAAFReferenceValue * pReferenceValue);
+
+  //****************
+  // GetRefValues()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetRefValues
+        // @parm [out,retval] Reference value Enumeration
+        (ImplEnumAAFReferenceValues ** ppEnum);
+
+
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFParameterDef)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFParameterDefTest.cpp.
+  static AAFRESULT test();
+};
+
+#endif // ! __ImplAAFParameterDef_h__
+
+
