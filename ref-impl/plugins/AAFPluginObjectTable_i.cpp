7252531f9cde649491c9c4494e96e33a676608e1
tbingham <>
2001-04-17 08:26:08 +0000

    Remove hard coded interface ptrs from plugins

--- ref-impl/plugins/AAFPluginObjectTable_i.cpp
+++ ref-impl/plugins/AAFPluginObjectTable_i.cpp
@@ -18,7 +18,6 @@
 // Include all objects in the following table:
 //
 AAF_BEGIN_OBJECT_MAP(AAFPluginObjectMap)
-	AAF_OBJECT_ENTRY(AAFDefaultCodec)
 	AAF_OBJECT_ENTRY(AAFEssenceDataStream)
 	AAF_OBJECT_ENTRY(AAFEssenceFileContainer)
 	AAF_OBJECT_ENTRY(AAFWaveCodec)
