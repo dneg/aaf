35a296ea5c671b613457bc9fb118706506dfe9ed
tbingham <>
2001-04-17 10:38:49 +0000

    Update TypeDefSet interface: 1) Fix incorrect argument type. 2) Use consistent argument and method names. 3) Use correct(?) midl attributes.

--- ref-impl/src/impl/ImplAAFTypeDefSet.h
+++ ref-impl/src/impl/ImplAAFTypeDefSet.h
@@ -6,7 +6,7 @@
 
 /***********************************************************************
  *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *              Copyright (c) 1998-2000 Avid Technology, Inc.
  *
  * Permission to use, copy and modify this software and accompanying 
  * documentation, and to distribute and sublicense application software
@@ -66,45 +66,13 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     Initialize
         (// @parm [in] auid to be used to identify this type
-         const aafUID_t & id,
+         aafUID_constref  id,
 
          // @parm [in] type of each element to be contained in this set
          ImplAAFTypeDef * pTypeDef,
 
-         // @parm [in] friendly name of this type definition
-         const aafCharacter *  pTypeName);
-
-  //****************
-  // AddElement()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    AddElement
-        (// @parm [in] property value corresponding to set to which element is added
-         ImplAAFPropertyValue * pInPropVal,
-
-         // @parm [in] value to be added to this set
-         ImplAAFPropertyValue * pMemberPropVal);
-
-    //****************
-  // RemoveElement()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    RemoveElement
-        (// @parm [in] property value corresponding to set to which element is added
-         ImplAAFPropertyValue * pInPropVal,
-
-         ImplAAFPropertyValue * pMemberPropVal);
-
-      //****************
-  // ContainsElement()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    ContainsElement
-        (// @parm [in] property value corresponding to set to which element is added
-         ImplAAFPropertyValue * pInPropVal,
-
-         ImplAAFPropertyValue * pMemberPropVal,
-         aafBool * pContains);
+         // @parm [in,string] friendly name of this type definition
+         aafCharacter_constptr  pTypeName);
 
   //****************
   // GetElementType()
@@ -114,6 +82,43 @@ public:
         // @parm [out] type of elements in this array
         (ImplAAFTypeDef ** ppTypeDef);
 
+  //****************
+  // AddElement()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    AddElement
+        (// @parm [in] property value corresponding to set to which element is added
+         ImplAAFPropertyValue * pSetPropertyValue,
+
+         // @parm [in] value to be added to this set
+         ImplAAFPropertyValue * pElementPropertyValue);
+
+  //****************
+  // RemoveElement()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    RemoveElement
+        (// @parm [in] property value corresponding to set from which element is removed
+         ImplAAFPropertyValue * pSetPropertyValue,
+
+         // @parm [in] value to be removed from this set
+         ImplAAFPropertyValue * pElementPropertyValue);
+
+  //****************
+  // ContainsElement()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    ContainsElement
+        (// @parm [in] property value corresponding to set to which element is added
+         ImplAAFPropertyValue * pSetPropertyValue,
+
+         // @parm [in] value whose presence is being tested in this set
+         ImplAAFPropertyValue * pElementPropertyValue,
+
+         // @parm [out] value to be added to this set
+         aafBoolean_t*  pContainsElement);
+
+
 
   //****************
   // GetCount()
@@ -121,64 +126,67 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     GetCount
         (// @parm [in] property value of array
-         ImplAAFPropertyValue * pPropVal,
+         ImplAAFPropertyValue * pSetPropertyValue,
 
          // @parm [out] count of elements in the specified set property value
          aafUInt32 *  pCount);
 
 
-
-
-  //***********************************************************
+  //****************
   // CreateKey()
   //
-  AAFRESULT CreateKey (
-    // Pointer to the key value bytes
-    aafDataBuffer_t  pKeyPtr,
+  virtual AAFRESULT STDMETHODCALLTYPE
+    CreateKey
+        (// @parm [in,size_is(length)] Pointer to the key value bytes
+         aafDataBuffer_t  pKeyPtr,
 
-    // The size of the key in bytes
-    aafUInt32 pLength,
+         // @parm [in] The size of the key in bytes
+         aafUInt32  length,
 
-    // An interface which may be passed to LookupKey() or ContainsKey()
-    ImplAAFPropertyValue ** ppKey);
+         // @parm [out] An interface which may be passed to LookupElement() or ContainsKey()
+         ImplAAFPropertyValue ** ppKey);
 
 
-  //***********************************************************
-  // LookupKey()
+  //****************
+  // LookupElement()
   //
-  AAFRESULT LookupKey (
-    // property value of set
-    ImplAAFPropertyValue * pInPropVal,
+  virtual AAFRESULT STDMETHODCALLTYPE
+    LookupElement
+        (// @parm [in] property value of set
+         ImplAAFPropertyValue * pSetPropertyValue,
 
-    // A key returned from CreateKey()
-    ImplAAFPropertyValue * pKey,
+         // @parm [in] A key returned from CreateKey()
+         ImplAAFPropertyValue * pKey,
 
-    // The returned property value
-    ImplAAFPropertyValue ** pOutPropVal);
+         // @parm [out] The returned property value
+         ImplAAFPropertyValue ** ppElementPropertyValue);
 
-  //***********************************************************
+  //****************
   // ContainsKey()
   //
-  AAFRESULT ContainsKey (
-    // property value of set
-    ImplAAFPropertyValue * pInPropVal,
+  virtual AAFRESULT STDMETHODCALLTYPE
+    ContainsKey
+        (// @parm [in] property value of set
+         ImplAAFPropertyValue * pSetPropertyValue,
 
-    // A key returned from CreateKey()
-    ImplAAFPropertyValue * pKey,
+         // @parm [in] A key returned from CreateKey()
+         ImplAAFPropertyValue * pKey,
 
-    // Value returned is AAFTrue if an entry with the correct key is present
-    ImplAAFPropertyValue ** pOutPropVal);
+         // @parm [out] Value returned is AAFTrue if an entry with the correct key is present
+         aafBoolean_t*  pContainsKey);
 
 
-  //***********************************************************
+  //****************
   // GetElements()
   //
-  AAFRESULT GetElements (
-    // property value to read
-    ImplAAFPropertyValue * pInPropVal,
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetElements
+        (// @parm [in] property value to read
+         ImplAAFPropertyValue * pSetPropertyValue,
 
-    // enumerator across property values
-    ImplEnumAAFPropertyValues ** ppEnum);
+         // @parm [out] enumerator across property values
+         ImplEnumAAFPropertyValues ** ppEnum);
+
 
 public:
   //****************
@@ -194,42 +202,6 @@ public:
 
          // @parm [in] friendly name of this type definition
          const aafCharacter *  pTypeName);
-
-
-
-  //*************************************************************
-  //
-  // Overrides from OMType, via inheritace through ImplAAFTypeDef
-  //
-  //*************************************************************
-
-  /*
-  virtual void reorder(OMByte* bytes,
-                       size_t bytesSize) const;
-
-  virtual size_t externalSize(void) const;
-
-  virtual void externalize(OMByte* internalBytes,
-                           size_t internalBytesSize,
-                           OMByte* externalBytes,
-                           size_t externalBytesSize,
-                           OMByteOrder byteOrder) const;
-
-  virtual size_t internalSize(void) const;
-
-  virtual void internalize(OMByte* externalBytes,
-                           size_t externalBytesSize,
-                           OMByte* internalBytes,
-                           size_t internalBytesSize,
-                           OMByteOrder byteOrder) const;
-						   */
-
-  // overrides from ImplAAFTypeDef
-  //
-  virtual aafBool IsFixedSize (void) const;
-  virtual size_t PropValSize (void) const;
-  virtual aafBool IsRegistered (void) const;
-  virtual size_t NativeSize (void) const;
 
 public:
   // Overrides from ImplAAFTypeDef
