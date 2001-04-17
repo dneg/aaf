77195b95e2a2c281315f1040a847b0e9eb7b3168
tbingham <>
2001-04-17 03:03:44 +0000

    Interface cleanup from Drop27 review pass 1

---
+++ ref-impl/include/AAFOperationCategories.h
@@ -0,0 +1,11 @@
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
+\***********************************************/
+
+// Following are the strings of the well-known operation group categories in AAF.
+//
+const aafUID_t CATEGORY_Effect = L"Effect";
