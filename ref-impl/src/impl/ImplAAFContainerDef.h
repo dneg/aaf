154ce5bff31a910d2a96c0a96bd3427d4d8bcb39
tbingham <>
2001-04-17 07:26:40 +0000

    Remove Initialize() from IAAFDefObject() and put it on primary interfaces of concrete derived classes.

--- ref-impl/src/impl/ImplAAFContainerDef.h
+++ ref-impl/src/impl/ImplAAFContainerDef.h
@@ -15,7 +15,7 @@
  * notice appear in all copies of the software and related documentation,
  * and (ii) the name Avid Technology, Inc. may not be used in any
  * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
+ * prior written permission of Avid Technology, Inc.
  *
  * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
@@ -45,6 +45,17 @@ public:
   //
   //********
   ImplAAFContainerDef ();
+
+
+  //****************
+  // Initialize()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Initialize
+        // @parm [in] Pointer to an AUID reference
+        (const aafUID_t & id,
+		 const aafCharacter *name,
+		 const aafCharacter *description);
 
 protected:
   virtual ~ImplAAFContainerDef ();
