d8bfcd227b5b1595ab554f21a2d6ba6f12bc0ca0
tbingham <>
2001-04-17 03:40:05 +0000

    transdel initial checkin of all Mac project files for ASAP release

---
+++ AAFMacSDK/ref-impl/om/AAFOM_prefix.h
@@ -0,0 +1,19 @@
+
+// >>> © 1998 Avid Technology, Inc.  All rights reserved. <<<
+#define __dest_os	__mac_os
+
+#define _MSL_USING_NAMESPACE
+
+//#define CHECKMEMORYLEAKS // Uncomment this line to turn leak checking on
+
+#define _MAC
+#define NONAMELESSUNION
+
+#include "MacHeaders.h"
+
+
+// Define to enable the module testing of the shared library.
+#define AAF_MODULE_TEST
+//#ifndef _DEBUG
+//#define _DEBUG
+//#endif
