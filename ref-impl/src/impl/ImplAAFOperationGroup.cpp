36dfeaeb9fec5b99e3d497a3a96bd54cd6cbf730
tbingham <>
2001-04-17 09:13:09 +0000

    Added some persistence to class.

--- ref-impl/src/impl/ImplAAFOperationGroup.cpp
+++ ref-impl/src/impl/ImplAAFOperationGroup.cpp
@@ -66,13 +66,13 @@ extern "C" const aafClassID_t CLSID_AAFSegment;
 const aafUID_t kNullID = {0};
 
 ImplAAFGroup::ImplAAFGroup ():
-_effectDefinition( PID_Group_EffectDefinition, "Effect Definition"),
+//_effectDefinition( PID_Group_EffectDefinition, "Effect Definition"),
 _inputSegments( PID_Group_InputSegments, "Input Segments"),
 _parameters( PID_Group_Parameters, "Parameters"),
 _bypassOverride( PID_Group_BypassOverride, "Bypass Override"),
 _rendering( PID_Group_Rendering, "Rendering")
 {
-	_persistentProperties.put(_effectDefinition.address());
+//	_persistentProperties.put(_effectDefinition.address());
 	_persistentProperties.put(_inputSegments.address());
 	_persistentProperties.put(_parameters.address());
 	_persistentProperties.put(_bypassOverride.address());
@@ -91,7 +91,10 @@ AAFRESULT STDMETHODCALLTYPE
 {
 	HRESULT		rc = AAFRESULT_SUCCESS;
 
-	if (pDatadef == NULL || pEffectDef == NULL)
+//	if (pDatadef == NULL || pEffectDef == NULL)
+//		return AAFRESULT_NULL_PARAM;
+	// Only validate data def for now
+	if (pDatadef == NULL )
 		return AAFRESULT_NULL_PARAM;
 
 	XPROTECT()
@@ -125,7 +128,7 @@ AAFRESULT STDMETHODCALLTYPE
 		// Here we have to find the effect definition auid from the 
 		// Effect definition object pointer we got as an input 
 		// EffectDef is NOT implemented yet - therefore:
-		_effectDefinition = kNullID;
+//		_effectDefinition = kNullID;
 		// ************************************************************************
 		// ************************************************************************
 	}
