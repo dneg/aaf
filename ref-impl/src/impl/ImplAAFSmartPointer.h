0a71089af46526d2cf4afb0e02e40087ff02c1b1
tbingham <>
2001-04-17 03:28:40 +0000

    Smart pointers for Impl objects.

---
+++ ref-impl/src/impl/ImplAAFSmartPointer.h
@@ -0,0 +1,130 @@
+#ifndef __ImplAAFSmartPointer_h__
+#define __ImplAAFSmartPointer_h__
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
+
+#ifndef __AAFSmartPointerBase_h__
+#include "AAFSmartPointerBase.h"
+#endif
+
+//
+// Smart pointer class for use with AAF Impl objects.  See
+// AAFSmartPointerBase.h for details.  The basics:
+//
+// Usage example:
+//
+// // Handy typedef:
+// typedef ImplAAFSmartPointer<ImplAAFSequence> ImplAAFSequenceSP;
+//
+// // Instantiate a smart pointer to sequences:
+// ImplAAFSequenceSP seqSP;
+//
+// // assume GetSequence(ImplAAFSequence**) already declared
+// // Get a sequence pointer, just as you would if it was declared as
+// // ImplAAFSequence**:
+// GetSequence (&seqSP);
+//
+// // use the smart pointer as if it was declared as ImplAAFSequence**:
+// AAFRESULT hr = seqSP->GetNumComponents(&count);
+//
+// // No need to call seqSP->ReleaseReference(); when seqSP goes out
+// // of scope the referenced sequence interface is automatically
+// // released.
+//
+// One detail repeated from AAFSmartPointerBase.h: clients may define
+// the macro AAF_SMART_POINTER_ASSERT(condition) which is used by the
+// smart pointer implementation to test pointer values.  If not
+// client-defined, it will be set to assert(condition) by default.
+//
+
+//
+// And now, the guts.
+//
+
+#ifndef AAF_SMART_POINTER_ASSERT
+#error - AAF_SMART_POINTER_ASSERT should have been defined
+// (at least in AAFSmartPointerBase.h)
+#endif
+
+//
+// Implementation of the AAFCountedReference abstract base class for
+// use in servicing reference counts to AAF Impl objects.
+//
+class ImplAAFRoot;
+//
+struct AAFCountedImplReference : public AAFCountedReference<ImplAAFRoot>
+{
+protected:
+  void acquire (ImplAAFRoot * pObj)
+  {
+	AAF_SMART_POINTER_ASSERT (pObj);
+	pObj->AcquireReference ();
+  }
+
+  void release (ImplAAFRoot * pObj)
+  {
+	AAF_SMART_POINTER_ASSERT (pObj);
+	pObj->ReleaseReference ();
+  }
+};
+
+
+//
+// The smart pointer class.
+//
+template <typename ReferencedType>
+struct ImplAAFSmartPointer
+  : public AAFSmartPointerBase <ReferencedType, AAFCountedImplReference>
+{
+  // Occasionally it might be necessary to set the value of this ptr;
+  // however we'd prefer that clients would find other means.  The
+  // only case I've seen its utility is when it is necessary to
+  // dynamically cast an Impl object to another Impl object, e.g.:
+  //
+  // (where DerivedClass inherits from BaseClass):
+  // ImplAAFSmartPointer<BaseClass> baseSP = [...something];
+  // ImplAAFSmartPointer<DerivedClass> derivedSP;
+  // derivedSP.SetPtr (dynamic_cast<DerivedClass>(baseSP));
+  //
+  // In other words, the dynamic_cast has to be done on the referenced
+  // type, not on the smart pointer type.
+  //
+  void SetPtr (ReferencedType * ptr);
+
+  // operator =
+  ImplAAFSmartPointer<ReferencedType> & operator =
+  (ReferencedType * src) { SetPtr (src); return *this; }
+};
+
+
+template <class ReferencedType>
+void ImplAAFSmartPointer<ReferencedType>::SetPtr (ReferencedType * ptr)
+{
+  // in case something changed since last time
+  updateRefCounts ();
+
+  // Hack! get a pointer to the rep of this object
+  ReferencedType ** ppRep = this->operator&();
+  AAF_SMART_POINTER_ASSERT (ppRep);
+  
+  // set the pointer
+  *ppRep = ptr;
+
+  // BobT 1999-07-07: Bug! See comment in
+  // AAFSmartPointerBase::updateRefCounts() to see why we need to
+  // addref this one explicitly.
+  if (*ppRep)
+	acquire(*ppRep);
+
+  // put us back in sync
+  updateRefCounts ();
+}
+
+
+#endif // ! __ImplAAFSmartPointer_h__
