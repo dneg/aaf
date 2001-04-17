562984c8a2d5e15b6776a49fc5d41e64455ad079
tbingham <>
2001-04-17 08:53:39 +0000

    Adding support support for Weak references in the DM, singltons, variable arrays and sets.

--- ref-impl/src/impl/ImplAAFWeakRefValue.cpp
+++ ref-impl/src/impl/ImplAAFWeakRefValue.cpp
@@ -59,8 +59,23 @@ ImplAAFWeakRefValue::~ImplAAFWeakRefValue ()
 
 
 
-
-
+AAFRESULT ImplAAFWeakRefValue::Initialize (
+  const ImplAAFTypeDefWeakObjRef *referenceType)
+{
+  AAFRESULT result = AAFRESULT_SUCCESS;
+  
+  assert (!isInitialized());
+  if (isInitialized())
+    return AAFRESULT_ALREADY_INITIALIZED;
+    
+  result = ImplAAFRefValue::Initialize(referenceType);
+  if (AAFRESULT_SUCCEEDED(result))
+  {
+    // This instance is now fully initialized.
+    setInitialized();
+  }
+  return result;
+}
 
 
 AAFRESULT ImplAAFWeakRefValue::Initialize (
@@ -83,21 +98,99 @@ AAFRESULT ImplAAFWeakRefValue::Initialize (
 
 AAFRESULT STDMETHODCALLTYPE ImplAAFWeakRefValue::GetObject(ImplAAFStorable **ppObject) const
 {
-  AAFRESULT result = AAFRESULT_NOT_IMPLEMENTED;
+  AAFRESULT result = AAFRESULT_SUCCESS;
+  ImplAAFStorable *pObject = NULL;
   
   if (NULL == ppObject)
     return AAFRESULT_NULL_PARAM;
   
+  *ppObject = NULL;
+
+  assert (isInitialized());
+  if (!isInitialized())
+    return AAFRESULT_NOT_INITIALIZED;
+
+  OMReferenceProperty* pProperty =  referenceProperty();
+  if (NULL != pProperty)
+  {
+    if (! pProperty->isOptional() || pProperty->isPresent ())
+    {
+      OMObject *object = pProperty->getObject();
+      if (NULL == object)
+        return AAFRESULT_NULLOBJECT;
+      pObject = ConvertOMObjectToRoot(object); 
+      assert (NULL != pObject);
+      if (NULL == pObject)
+        return AAFRESULT_INVALID_OBJ; // ???
+    }
+    else
+    {
+      return AAFRESULT_PROP_NOT_PRESENT;
+    }
+  }
+  else
+  {
+    // Use an "indirect access" method of retrieving a strong object reference.
+    //
+    // If there was not associated reference property then there MUST
+    // be a local object pointer assigned to this instance.
+    pObject = GetLocalObject();
+    assert (NULL != pObject);
+  }
+  
+  *ppObject = pObject;
+  (*ppObject)->AcquireReference();
+  
   return result;
 }
+
+static AAFRESULT SetNewObjectReference(
+  OMReferenceProperty* refProperty,
+  ImplAAFStorable* pNewObject)
+{   
+  assert (NULL != pNewObject);
+  if (NULL == pNewObject)
+    return AAFRESULT_INVALID_OBJ; // ???
+
+  // TODO: Validation
+  // The given storable must already be in the target strong reference set!
+
+  // Save the reference in the OMReferenceProperty
+  OMObject *object = refProperty->setObject(pNewObject);
+  
+  return AAFRESULT_SUCCESS;
+}
+
 
 
 AAFRESULT STDMETHODCALLTYPE ImplAAFWeakRefValue::SetObject(ImplAAFStorable *pObject)
 {
-  AAFRESULT result = AAFRESULT_NOT_IMPLEMENTED;
+  AAFRESULT result = AAFRESULT_SUCCESS;
   
   if (NULL == pObject)
     return AAFRESULT_NULL_PARAM;
+
+  assert (isInitialized());
+  if (!isInitialized())
+    return AAFRESULT_NOT_INITIALIZED;
+
+  if (NULL != referenceProperty())
+  {
+    // Save the reference in the OMReferenceProperty
+    result = SetNewObjectReference(referenceProperty(), pObject);
+  }
+  else
+  {
+    // Use an "indirect access" method of saving a weak object reference.
+    //
+    // If there was not associated reference property then we MUST
+    // set the local object pointer for this instance.
+    //
+    // This weak reference should be an element of a weak reference set or
+    // weak reference array.
+    SetLocalObject(pObject);
+  }
+  
   
   return result;
 }
@@ -108,5 +201,29 @@ AAFRESULT STDMETHODCALLTYPE ImplAAFWeakRefValue::SetObject(ImplAAFStorable *pObj
 AAFRESULT STDMETHODCALLTYPE ImplAAFWeakRefValue::WriteTo(
   OMProperty* pOmProp)
 {
-  return (ImplAAFRefValue::WriteTo(pOmProp));
+  assert (isInitialized());
+  if (!isInitialized())
+    return AAFRESULT_NOT_INITIALIZED;
+
+  AAFRESULT result = ImplAAFRefValue::WriteTo(pOmProp);
+  if (AAFRESULT_SUCCEEDED(result))
+  {
+    // If there is a referenceProperty then the SetObject method has already
+    // written the object reference to the property.
+  
+    if (NULL == referenceProperty())
+    {
+      OMReferenceProperty* refProperty = dynamic_cast<OMReferenceProperty *>(pOmProp);
+      assert(refProperty);
+      if (NULL == refProperty)
+        return AAFRESULT_INVALID_OBJ; // ???
+
+      // Use an "indirect access" method of saving a strong object reference.
+      // The local object reference must exist! NOTE: GetLocalObject() does NOT increment
+      // the reference count of the returned object!
+      result = SetNewObjectReference(refProperty, GetLocalObject());
+    }
+  }
+  
+  return result;
 }
