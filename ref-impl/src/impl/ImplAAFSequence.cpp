eefc14831e2d9b1a1679e13587390dd472161b62
tbingham <>
2001-04-17 09:27:10 +0000

    Get rid of memory leaks on remove

--- ref-impl/src/impl/ImplAAFSequence.cpp
+++ ref-impl/src/impl/ImplAAFSequence.cpp
@@ -337,7 +337,15 @@ AAFRESULT STDMETHODCALLTYPE
   if (index >= count)
 	return AAFRESULT_BADINDEX;
 
-	_components.removeAt(index);
+	ImplAAFComponent *pComp = NULL;
+	pComp = 	_components.removeAt(index);
+	if (pComp)
+	{
+		// We have removed an element from a "stong reference container" so we must
+		// decrement the objects reference count. This will not delete the object
+		// since the caller must have alread acquired a reference. (transdel 2000-MAR-10)
+		pComp->ReleaseReference ();
+	}
 	return AAFRESULT_SUCCESS;
 }
 
@@ -372,6 +380,8 @@ AAFRESULT STDMETHODCALLTYPE
 	  return AAFRESULT_BADINDEX;
 
 	_components.removeValue(pComponent);
+	pComponent->ReleaseReference();
+
 	return AAFRESULT_SUCCESS;
 }
 
