0cc2558edacc0e29c474eb4304cffa30dee00459
tbingham <>
2001-04-17 05:13:57 +0000

    First non-dodo generated EventMobSlot implementation and module test. TODO: We still need to decide what sort of semantic validation needs to be added to the override of SetSegment.

--- ref-impl/src/impl/ImplAAFEventMobSlot.cpp
+++ ref-impl/src/impl/ImplAAFEventMobSlot.cpp
@@ -7,24 +7,29 @@
 *                                          *
 \******************************************/
 
-
-
-
-
-
-
-#include "AAFStoredObjectIDs.h"
-
 #ifndef __ImplAAFEventMobSlot_h__
 #include "ImplAAFEventMobSlot.h"
 #endif
+
+#ifndef __ImplAAFSequence_h__
+#include "ImplAAFSequence.h"
+#endif
+
+
+#include "AAFStoredObjectIDs.h"
+#include "AAFPropertyIDs.h"
+#include <AAFResult.h>
+
 
 #include <assert.h>
 #include <string.h>
 
 
-ImplAAFEventMobSlot::ImplAAFEventMobSlot ()
-{}
+ImplAAFEventMobSlot::ImplAAFEventMobSlot ():
+  _editRate(PID_EventMobSlot_EditRate,	"EditRate")
+{
+  _persistentProperties.put(_editRate.address());
+}
 
 
 ImplAAFEventMobSlot::~ImplAAFEventMobSlot ()
@@ -33,17 +38,25 @@ ImplAAFEventMobSlot::~ImplAAFEventMobSlot ()
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFEventMobSlot::GetEditRate (
-      aafRational_t *  /*pEditRate*/)
+      aafRational_t *  pEditRate)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+  if (NULL == pEditRate)
+    return AAFRESULT_NULL_PARAM;
+
+  *pEditRate = _editRate;
+  return AAFRESULT_SUCCESS;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFEventMobSlot::SetEditRate (
-      aafRational_t *  /*pEditRate*/)
+      aafRational_t *  pEditRate)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+  if (NULL == pEditRate)
+    return AAFRESULT_NULL_PARAM;
+
+  _editRate = *pEditRate;
+  return AAFRESULT_SUCCESS;
 }
 
 
@@ -51,12 +64,32 @@ AAFRESULT STDMETHODCALLTYPE
 
 
 
-  // Override from AAFMobSlot
-  AAFRESULT STDMETHODCALLTYPE
-    ImplAAFEventMobSlot::SetSegment (/*[in]*/ ImplAAFSegment * /*pSegment*/)
+// Override from AAFMobSlot
+AAFRESULT STDMETHODCALLTYPE
+ImplAAFEventMobSlot::SetSegment (/*[in]*/ ImplAAFSegment * pSegment)
+{
+  if (NULL == pSegment)
+    return AAFRESULT_NULL_PARAM;
+
+  // Do even attempt to set the given segment if has already been
+  // attached to another persistent object.
+  if (pSegment->attached())
+    return AAFRESULT_OBJECT_ALREADY_ATTACHED;
+
+  // TODO:
+  // Validate the following constraints if the segment is a sequence:
+  // A) all events must have the same Event subclass and 
+  // B) all events must have the same data kind.
+  //
+  ImplAAFSequence *pSequence = dynamic_cast<ImplAAFSequence *>(pSegment);
+  if (NULL != pSequence)
   {
-    return AAFRESULT_NOT_IMPLEMENTED;
   }
+
+
+  // Call the parent class to set the segment...
+  return ImplAAFMobSlot::SetSegment(pSegment);
+}
 
 
 
