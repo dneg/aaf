fc75d7b79e6864097ebb985329752c3bdd2ea03c
tbingham <>
2001-04-17 08:53:35 +0000

    Adding stub implementation files for private subclasses of AAFPropertyValue to support object reference, both singletons and collections (sets and vectors).

---
+++ ref-impl/src/impl/ImplAAFWeakRefValue.cpp
@@ -0,0 +1,84 @@
+/***********************************************************************
+ *
+ *              Copyright (c) 2000 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
+
+#include "ImplAAFWeakRefValue.h"
+
+#ifndef __ImplAAFRoot_h__
+#include "ImplAAFRoot.h"
+#endif
+
+#ifndef __ImplAAFTypeDefWeakObjRef_h__
+#include "ImplAAFTypeDefWeakObjRef.h"
+#endif
+
+
+#include "OMProperty.h"
+//#include "OMRefProperty.h" // TBD: include header for class for singleton strong references.
+
+#include <assert.h>
+#include <string.h>
+
+
+ImplAAFWeakRefValue::ImplAAFWeakRefValue ()
+{}
+
+
+ImplAAFWeakRefValue::~ImplAAFWeakRefValue ()
+{
+}
+
+
+
+
+
+
+
+AAFRESULT ImplAAFWeakRefValue::Initialize (
+  const ImplAAFTypeDefWeakObjRef *referenceType,    
+  OMProperty *property)
+{
+  AAFRESULT result = AAFRESULT_SUCCESS;
+  
+  result = ImplAAFRefValue::Initialize(referenceType, property);
+  
+  if (AAFRESULT_SUCCEEDED(result))
+  {
+    // This instance is now fully initialized.
+    setInitialized();
+  }
+  
+  return result;
+}
+
+//
+// WriteTo
+//
+AAFRESULT STDMETHODCALLTYPE ImplAAFWeakRefValue::WriteTo(
+  OMProperty* pOmProp)
+{
+  return (ImplAAFRefValue::WriteTo(pOmProp));
+}
