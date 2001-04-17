564486698f9c8abe03f245dd9b3ae6973a39c7ed
tbingham <>
2001-04-17 04:06:58 +0000

    Interface cleanup from Drop27 review pass 1

---
+++ ref-impl/include/AAFCodecDefs.h
@@ -0,0 +1,17 @@
+/***********************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
+\***********************************************/
+
+// Following are the UIDs of the well-known codec definition objects in AAF, as well as well known
+// flavours of codecs.
+//
+const aafUID_t NoCodec = { 0x568FB761, 9458, 0x11d2, { 0x80, 0x89, 0x00, 0x60, 0x08, 0x14, 0x3E, 0x6F } };
+const aafUID_t CodecWave       = { 0x820F09B1, 0xEB9B, 0x11d2, { 0x80, 0x9F, 0x00, 0x60, 0x08, 0x14, 0x3e, 0x6f } };
+
+
+const aafUID_t NilCodecFlavour = { 0x1B31F3B1, 0x9450, 0x11d2, { 0x80, 0x89, 0x00, 0x60, 0x08, 0x14, 0x3E, 0x6F } };
