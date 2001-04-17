39eb188c43cbe464efb6579e4bd959ddf7c744f9
tbingham <>
2001-04-17 08:02:36 +0000

    common enumerators

--- ref-impl/src/impl/ImplAAFEnumerator.h
+++ ref-impl/src/impl/ImplAAFEnumerator.h
@@ -27,196 +27,205 @@
  *
  ************************************************************************/
 
+#include "ImplAAFObjectCreation.h"
+#include "ImplAAFRoot.h"
+#include "OMReferenceContainerIter.h"
 
-#ifndef __ImplAAFCollection_h__
-#include "ImplAAFCollection.h"
-#endif
-
-//
-// Standard enumerator implementation which works with elements of
-// type T, and works on an ImplAAFCollection<T>.  The class
-// ImplAAFCollection<T> must have the following methods defined:
-//
-// - AAFRESULT GetNumElements([out] aafInt32 * pCount);
-//
-// - AAFRESULT GetNthElement([in] aafInt32 index,
-//                           [out] T * pElem);
-// 
-
-
-template <typename T>
-class ImplAAFEnumerator
+template<class T>
+class ImplAAFEnumerator: public ImplAAFRoot
 {
 public:
-  //
-  // Constructor/destructor
-  //
-  //********
-  ImplAAFEnumerator (ImplAAFCollection<T> * pCollection);
+	ImplAAFEnumerator();
+	~ImplAAFEnumerator();
+	AAFRESULT STDMETHODCALLTYPE Initialize(const aafClassID_t* pClassID,
+		ImplAAFRoot *pObj,OMReferenceContainerIterator<T>* pIterator);
 
-  virtual ~ImplAAFEnumerator ();
+	virtual AAFRESULT STDMETHODCALLTYPE NextOne (T ** ppItem);
+	virtual AAFRESULT STDMETHODCALLTYPE Next(aafUInt32  count, T ** ppItems,
+		aafUInt32 *  pFetched);
+	virtual AAFRESULT STDMETHODCALLTYPE Skip(aafUInt32  count);
+	virtual AAFRESULT STDMETHODCALLTYPE Reset ();
+	virtual AAFRESULT STDMETHODCALLTYPE Clone(ImplAAFEnumerator<T> ** ppEnum);
 
-public:
-
-  //****************
-  // NextOne()
-  //
-  virtual AAFRESULT
-    NextOne
-        // @parm [out,retval] The Next element
-        (T * pElem);
-
-
-  //****************
-  // Next()
-  //
-  virtual AAFRESULT
-    Next
-        (// @parm [in] number of elements requested
-         aafUInt32  count,
-
-         // @parm [out, size_is(count), length_is(*pNumFetched)] array to receive elements
-         T * pElems,
-
-         // @parm [out,ref] number of actual elements fetched into pElems array
-         aafUInt32 *  pNumFetched);
-
-
-  //****************
-  // Skip()
-  //
-  virtual AAFRESULT
-    Skip
-        // @parm [in] Number of elements to skip
-        (aafUInt32  count);
-
-
-  //****************
-  // Reset()
-  //
-  virtual AAFRESULT
-    Reset ();
-
-private:
-  aafInt32            _current;
-  ImplAAFCollection<T> * _pCollection;
+protected:
+	const aafClassID_t* _pClassID; // needed for Clone() 
+	ImplAAFRoot* _pObj;
+	OMReferenceContainerIterator<T>* _pIterator;
 };
 
-
-//
-// Implementation stuff
-//
-
-template <typename T>
-ImplAAFEnumerator<T>::ImplAAFEnumerator
-(
- ImplAAFCollection<T> * pCollection
-)
-  : _pCollection (pCollection),
-	_current (0)
-{}
-
-
-template <typename T>
-ImplAAFEnumerator<T>::~ImplAAFEnumerator ()
-{}
-
-
-template <typename T>
-AAFRESULT ImplAAFEnumerator<T>::NextOne
-(
- T * pElem
-)
+template <class T>
+ImplAAFEnumerator<T>::ImplAAFEnumerator()
+: _pClassID(0), _pObj(0), _pIterator(0)
 {
-  AAFRESULT	result;
-  aafUInt32	cur = _current;
-  aafUInt32  siz;
-
-  if (pElem == NULL)
-	return AAFRESULT_NULL_PARAM;
-
-  if (!_pCollection) return AAFRESULT_NULL_PARAM;
-  _pCollection->GetNumElements(&siz);
-  if(cur < siz)
-	{
-	  result = _pCollection->GetNthElement(cur, pElem);
-	  _current = ++cur;
-	}
-  else
-	result = AAFRESULT_NO_MORE_OBJECTS;
-
-  return result;
 }
 
-
-template <typename T>
-AAFRESULT ImplAAFEnumerator<T>::Next
-( aafUInt32 count,
-  T * pElems,
-  aafUInt32 * pFetched)
+template <class T>
+ImplAAFEnumerator<T>::~ImplAAFEnumerator ()
 {
-  T * pElem;
-  aafUInt32 numElems;
-  HRESULT hr;
-
-  if (pFetched == NULL && count != 1)
-	return AAFRESULT_NULL_PARAM;
-
-  // Point at the first element in the array.
-  pElem = pElems;
-  for (numElems = 0; numElems < count; numElems++)
+	if(_pObj)
 	{
-	  hr = NextOne(pElem);
-	  if (FAILED(hr))
-		break;
+		_pObj->ReleaseReference();
+		_pObj=NULL;
+	}
 
-	  // Point at the next element in the array.  This
-	  // will increment off the end of the array when
-	  // numElems == count-1, but the for loop should
-	  // prevent access to this location.
-	  pElem++;
+	delete(_pIterator);
+	_pIterator=0;
+}
+
+template <class T>
+AAFRESULT STDMETHODCALLTYPE 
+	ImplAAFEnumerator<T>::Initialize(const aafClassID_t* pClassID,
+		ImplAAFRoot *pObj,OMReferenceContainerIterator<T>* pIterator)
+{
+	AAFRESULT ar=AAFRESULT_SUCCESS;
+	
+	if(_pObj)
+	{
+		_pObj->ReleaseReference();
+		_pObj = 0;
 	}
 	
-  if (pFetched)
-	*pFetched = numElems;
+	_pObj = pObj;
+	if(pObj)
+		pObj->AcquireReference();
+	
+	delete(_pIterator);
+	_pIterator = pIterator;
 
-  return hr;
+	_pClassID=pClassID;
+
+	return(ar);
 }
 
-
-template <typename T>
-AAFRESULT ImplAAFEnumerator<T>::Skip
-(
- aafUInt32  count
-)
+template <class T>
+AAFRESULT STDMETHODCALLTYPE	
+	ImplAAFEnumerator<T>::NextOne(
+		T **ppItem)
 {
-  AAFRESULT	hr;
-  aafUInt32	newCurrent;
-  aafUInt32 size;
+	if(ppItem==NULL)
+		return(AAFRESULT_NULL_PARAM);
 
-  newCurrent = _current + count;
+	if(_pIterator==NULL)
+		return(AAFRESULT_NOT_INITIALIZED);
 
-  _pCollection->GetNumElements(&size);
-  if(newCurrent < size)
+	AAFRESULT ar=AAFRESULT_NO_MORE_OBJECTS;
+	
+	if (_pIterator->before() || _pIterator->valid())
 	{
-	  _current = newCurrent;
-	  hr = AAFRESULT_SUCCESS;
+		if (++(*_pIterator))
+		{
+			*ppItem = _pIterator->value();
+			(*ppItem)->AcquireReference();
+			ar = AAFRESULT_SUCCESS;
+		}
 	}
-  else
-	{
-	  hr = AAFRESULT_NO_MORE_OBJECTS;
-	}
-
-  return hr;
+	return(ar);
 }
 
-
-template <typename T>
-AAFRESULT ImplAAFEnumerator<T>::Reset()
+template <class T>
+AAFRESULT STDMETHODCALLTYPE
+	ImplAAFEnumerator<T>::Next(
+		aafUInt32  count,
+		T **ppItems,
+		aafUInt32 *pFetched)
 {
-  _current = 0;
-  return AAFRESULT_SUCCESS;
+	aafUInt32			numItems;
+	AAFRESULT			ar=AAFRESULT_SUCCESS;
+
+	if(ppItems==NULL||pFetched==NULL)
+		return(AAFRESULT_NULL_PARAM);
+	
+	if(count==0)
+		return(AAFRESULT_INVALID_PARAM);
+
+	for (numItems = 0; numItems < count; numItems++)
+	{
+		ar = NextOne(&ppItems[numItems]);
+		if (FAILED(ar))
+			break;
+	}
+	
+	if (pFetched)
+		*pFetched=numItems;
+
+	return(ar);
 }
 
+template <class T>
+AAFRESULT STDMETHODCALLTYPE
+	ImplAAFEnumerator<T>::Skip (
+		aafUInt32  count)
+{
+	if(_pIterator==NULL)
+		return(AAFRESULT_NOT_INITIALIZED);
+
+	if(count==0)
+		return(AAFRESULT_INVALID_PARAM);
+
+	AAFRESULT ar=AAFRESULT_SUCCESS;
+	aafUInt32 n;
+	
+	for(n=1;n<=count;n++)
+	{
+		// Defined behavior of skip is to NOT advance at all if it would push 
+		// us off of the end
+		if(!++(*_pIterator))
+		{
+			// Off the end, decrement n and iterator back to the starting 
+			// position
+			while(n>=1)
+			{
+				--(*_pIterator);
+				n--;
+			}
+			ar=AAFRESULT_NO_MORE_OBJECTS;
+			break;
+		}
+	}
+
+	return(ar);
+}
+
+template <class T>
+AAFRESULT STDMETHODCALLTYPE
+	ImplAAFEnumerator<T>::Reset ()
+{
+	if(_pIterator==NULL)
+		return(AAFRESULT_NOT_INITIALIZED);
+
+	AAFRESULT ar=AAFRESULT_SUCCESS;
+	_pIterator->reset();
+	return(ar);
+}
+
+template <class T>
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFEnumerator<T>::Clone (
+      ImplAAFEnumerator<T> **ppEnum)
+{
+	if(_pIterator==NULL||_pClassID==NULL||_pObj==NULL)
+		return(AAFRESULT_NOT_INITIALIZED);
+
+	AAFRESULT ar=AAFRESULT_SUCCESS;
+	ImplAAFEnumerator<T> *pResult;
+
+	pResult=dynamic_cast<ImplAAFEnumerator<T>*>(CreateImpl(*_pClassID));
+	if(pResult==NULL)
+		return E_FAIL;
+
+    ar=pResult->Initialize(_pClassID,_pObj,_pIterator->copy());
+	if(SUCCEEDED(ar))
+	{
+		*ppEnum=pResult;
+	}
+	else
+	{
+	  pResult->ReleaseReference();
+	  pResult=0;
+	  *ppEnum=NULL;
+	}
+	
+	return(ar);
+}
 
 #endif // ! __ImplAAFEnumerator_h__
