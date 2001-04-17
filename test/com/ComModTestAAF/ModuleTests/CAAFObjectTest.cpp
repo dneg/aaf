b5cf612e6ce1d42546f6c7e6958a82b14db214ce
tbingham <>
2001-04-17 09:26:36 +0000

    implemented methods that had returned AAFRESULT_NOT_IN_CURRENT_VERSION

--- test/com/ComModTestAAF/ModuleTests/CAAFObjectTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFObjectTest.cpp
@@ -40,13 +40,22 @@
 #include "CAAFBuiltinDefs.h"
 
 #include "AAFSmartPointer.h"
-typedef IAAFSmartPointer<IAAFFile>           IAAFFileSP;
-typedef IAAFSmartPointer<IAAFHeader>         IAAFHeaderSP;
-typedef IAAFSmartPointer<IAAFIdentification> IAAFIdentificationSP;
+typedef IAAFSmartPointer<IAAFCompositionMob>	IAAFCompositionMobSP;
+typedef IAAFSmartPointer<IAAFFile>				IAAFFileSP;
+typedef IAAFSmartPointer<IAAFHeader>			IAAFHeaderSP;
+typedef IAAFSmartPointer<IAAFIdentification>	IAAFIdentificationSP;
+typedef IAAFSmartPointer<IAAFMob>				IAAFMobSP;
+typedef IAAFSmartPointer<IAAFMobSlot>			IAAFMobSlotSP;
+typedef IAAFSmartPointer<IAAFObject>			IAAFObjectSP;
+typedef IAAFSmartPointer<IAAFPropertyDef>		IAAFPropertyDefSP;
+typedef IAAFSmartPointer<IAAFPropertyValue>		IAAFPropertyValueSP;
+typedef IAAFSmartPointer<IAAFSegment>			IAAFSegmentSP;
+typedef IAAFSmartPointer<IAAFSequence>			IAAFSequenceSP;
+typedef IAAFSmartPointer<IAAFTimelineMobSlot>	IAAFTimelineMobSlotSP;
+typedef IAAFSmartPointer<IAAFTypeDefInt>		IAAFTypeDefIntSP;
 
 static aafUID_t    fillerUID = DDEF_Timecode;
 static aafLength_t  fillerLength = 3200;
-
 
 // convenient error handlers.
 inline void checkResult(HRESULT r)
@@ -54,14 +63,13 @@ inline void checkResult(HRESULT r)
   if (FAILED(r))
     throw r;
 }
-inline void checkExpression(bool expression, HRESULT r)
+inline void checkExpression(bool expression, HRESULT r=AAFRESULT_TEST_FAILED)
 {
   if (!expression)
     throw r;
 }
 
 static wchar_t testFileName[] = L"AAFObjectTest.aaf";
-
 
 // Cross-platform utility to delete a file.
 static void RemoveTestFile(const wchar_t* pFileName)
@@ -76,6 +84,23 @@ static void RemoveTestFile(const wchar_t* pFileName)
   }
 }
 
+// AUIDs of optional properties we will create
+static const aafUID_t AUID_OptionalProperty = 
+{ 0xacf15840, 0x58d6, 0x11d4, { 0x92, 0x2a, 0x0, 0x50, 0x4, 0x9c, 0x3b, 0x9d } };
+
+static const aafUID_t AUID_OptionalProperty2 = 
+{ 0xecde5700, 0x58e8, 0x11d4, { 0x92, 0x2a, 0x0, 0x50, 0x4, 0x9c, 0x3b, 0x9d } };
+
+// IDs of Mobs we will create
+static const aafMobID_t	kTestMobID =
+	{{0x07, 0x0c, 0x2b, 0x34, 0x02, 0x05, 0x11, 0x01, 0x01, 0x00, 0x10, 0x00},
+	0x13, 0x00, 0x00, 0x00,
+	{0x49394200, 0x58ed, 0x11d4,0x92, 0x2a, 0x0, 0x50, 0x4, 0x9c, 0x3b, 0x9d}};
+
+static const aafMobID_t	kTestMobID2 =
+	{{0x07, 0x0c, 0x2b, 0x34, 0x02, 0x05, 0x11, 0x01, 0x01, 0x00, 0x10, 0x00},
+	0x13, 0x00, 0x00, 0x00,
+	{0x4d540120, 0x5900, 0x11d4,0x92, 0x2a, 0x0, 0x50, 0x4, 0x9c, 0x3b, 0x9d}};
 
 static HRESULT ObjectWriteTest ()
 {
@@ -117,14 +142,39 @@ static HRESULT ObjectWriteTest ()
 	  assert (pDict);
 	  CAAFBuiltinDefs defs (pDict);
 
-	  checkResult (defs.cdCompositionMob()->
-				   CreateInstance (IID_IAAFCompositionMob,
-								   (IUnknown **) &pCMob));
+	  // Add an optional property to the class definition for AAFCompositionMob
+	  // (pOptionalPropertyDef)
+	  IAAFClassDefSP pClassDef;
+	  checkResult(pDict->LookupClassDef(AUID_AAFCompositionMob,&pClassDef));
+      IAAFPropertyDefSP pOptionalPropertyDef;
+      checkResult(pClassDef->RegisterOptionalPropertyDef(AUID_OptionalProperty,
+			L"New property",
+			defs.tdUInt16(),
+			&pOptionalPropertyDef));
+      checkResult(pDict->RegisterClassDef(pClassDef));
+
+      // Add an optional property to the class definition for AAFSequence
+	  // (pOptionalPropertyDef2)
+	  checkResult(pDict->LookupClassDef(AUID_AAFSequence,&pClassDef));
+      IAAFPropertyDefSP pOptionalPropertyDef2;
+      checkResult(pClassDef->RegisterOptionalPropertyDef(AUID_OptionalProperty2,
+			L"New property",
+			defs.tdUInt16(),
+			&pOptionalPropertyDef2));
+      checkResult(pDict->RegisterClassDef(pClassDef));
+
+	  // Create an instance of AAFCompositionMob using the newly registered
+	  // class definition.
+	  checkResult(pDict->LookupClassDef(AUID_AAFCompositionMob,&pClassDef));
+	  checkResult (pClassDef->CreateInstance (IID_IAAFCompositionMob,
+		  (IUnknown **) &pCMob));
 	  assert (pCMob);
+
 	  checkResult (pCMob->QueryInterface (IID_IAAFMob,
 										  (void **) &pMob));
 	  assert (pMob);
-
+      
+	  checkResult(pMob->SetMobID(kTestMobID));
 	  checkResult (pCMob->Initialize (L"TestMob"));
 	  checkResult (pHeader->AddMob (pMob));
 
@@ -132,19 +182,114 @@ static HRESULT ObjectWriteTest ()
 										  (void **) &pObj));
 	  assert (pObj);
 
-	  aafUInt32 propCount = 0;
-	  checkResult (pObj->CountProperties (&propCount));
-	  // checkExpression(9 == propCount, AAFRESULT_TEST_FAILED);
+      // Before the optional property is set, calls to RemoveOptionalProperty()
+	  // should return AAFRESULT_PROP_NOT_PRESENT
+      checkExpression(pObj->RemoveOptionalProperty(pOptionalPropertyDef)
+		  ==AAFRESULT_PROP_NOT_PRESENT);
+
+      // Set the optional property we just created
+	  IAAFTypeDefIntSP pTypeDefInt;
+	  checkResult(defs.tdUInt16()->QueryInterface(IID_IAAFTypeDefInt,
+		  (void**)&pTypeDefInt));
+	  IAAFPropertyValueSP pOptionalPropertyValue;
+	  aafUInt16 five=5;
+	  checkResult(pTypeDefInt->CreateValue((aafMemPtr_t)&five,sizeof(aafUInt16),
+		  &pOptionalPropertyValue));
+      checkResult(pObj->SetPropertyValue(pOptionalPropertyDef,
+		  pOptionalPropertyValue));
+
+	  // Now try getting the optional property value from the object
+      IAAFPropertyValueSP pReturnedPropertyValue;
+	  checkResult(pObj->GetPropertyValue(pOptionalPropertyDef,
+		  &pReturnedPropertyValue));
+
+      // Perform some negative tests on RemoveOptionalProperty()
+
+	  // RemoveOptionalProperty() with NULL property definition should return
+	  // AAFRESULT_NULL_PARAM
+	  checkExpression(pObj->RemoveOptionalProperty(0)==AAFRESULT_NULL_PARAM);
+
+      // RemoveOptionalProperty() with property def. for a property that is not
+	  // in the class should return AAFRESULT_BAD_PROP
+      checkExpression(pObj->RemoveOptionalProperty(pOptionalPropertyDef2)
+		  ==AAFRESULT_BAD_PROP);
+
+      // Count number of properties before removing optional property value
+      aafUInt32 oldNumProperties=0;
+      checkResult(pObj->CountProperties(&oldNumProperties));
+	  checkExpression(oldNumProperties>=1);
+
+	  // Remove the optional property value
+	  checkResult(pObj->RemoveOptionalProperty(pOptionalPropertyDef));
+
+      // Verify that property count is reduced by 1.
+	  aafUInt32 newNumProperties=(aafUInt32)(-1);
+	  checkResult(pObj->CountProperties(&newNumProperties));
+	  checkExpression(newNumProperties==oldNumProperties-1);
+
+	  // Now we should not be able to get the optional property value from the
+	  // object
+	  checkExpression(pObj->GetPropertyValue(pOptionalPropertyDef,
+		  &pReturnedPropertyValue)==AAFRESULT_PROP_NOT_PRESENT);
 
 	  checkResult (pObj->GetProperties (&pEnum));
 	  checkExpression (pEnum != 0, AAFRESULT_TEST_FAILED);
-	  while (propCount--)
-		{
+	  while (newNumProperties--)
+	  {
 		  IAAFProperty * pProp = NULL;
 		  checkResult (pEnum->NextOne (&pProp));
 		  checkExpression (pProp != 0, AAFRESULT_TEST_FAILED);
 		  pProp->Release();
-		}
+	  }
+
+      // Now use CreateOptionalPropertyValue() to add an optional property to
+	  // an instance of AAFSequence
+
+	  // Create an instance of AAFSequence & initialize it
+	  checkResult(pDict->LookupClassDef(AUID_AAFSequence,&pClassDef));
+	  IAAFSequenceSP pSequence;
+	  checkResult (pClassDef->CreateInstance (IID_IAAFSequence,
+		  (IUnknown **)&pSequence));
+	  checkResult(pSequence->Initialize(defs.ddSound()));
+
+	  // Add sequence to composition Mob
+      IAAFSegmentSP pSegment;
+      checkResult(pSequence->QueryInterface(IID_IAAFSegment,(void **)&pSegment));
+      aafRational_t editRate = { 0, 1};
+      IAAFTimelineMobSlotSP pTimelineMobSlot;
+      checkResult(pMob->AppendNewTimelineSlot(editRate,pSegment,1,
+        L"AAF Test Sequence",0,&pTimelineMobSlot));
+
+  	  IAAFObjectSP pObj2;
+	  checkResult(pSequence->QueryInterface(IID_IAAFObject,(void**)&pObj2));
+
+      // Test CreateOptionalPropertyValue()
+
+      // First perform some negative tests on CreateOptionalPropertyValue()
+	  IAAFPropertyValueSP pCreatedPropVal;
+	  checkExpression(pObj2->CreateOptionalPropertyValue(0,&pCreatedPropVal)
+		  ==AAFRESULT_NULL_PARAM);
+	  checkExpression(pObj2->CreateOptionalPropertyValue(pOptionalPropertyDef2,0)
+		  ==AAFRESULT_NULL_PARAM);
+	  checkExpression(pObj2->CreateOptionalPropertyValue(pOptionalPropertyDef,
+		  &pCreatedPropVal)==AAFRESULT_BAD_PROP);
+	  checkExpression(pCreatedPropVal==0);
+      
+      // Now use CreateOptionalPropertyValue() to create a property value and
+	  // set it as an optional property of an instance of AAFSequence.
+	  checkResult(pObj2->CreateOptionalPropertyValue(pOptionalPropertyDef2,
+		  &pCreatedPropVal));
+
+	  IAAFTypeDefSP pTypeDef;
+	  checkResult(pCreatedPropVal->GetType(&pTypeDef));
+	  checkResult(pTypeDef->QueryInterface(IID_IAAFTypeDefInt,
+		  (void**)&pTypeDefInt));
+	  aafUInt16 ten=10;
+	  checkResult(pTypeDefInt->SetInteger(pCreatedPropVal,(aafMemPtr_t)&ten,
+		  sizeof(aafUInt16)));
+
+      checkResult(pObj2->SetPropertyValue(pOptionalPropertyDef2,
+		  pCreatedPropVal));
 
 	  //
 	  // Check Generation functionality.
@@ -312,12 +457,72 @@ static HRESULT ObjectReadTest ()
 	  checkResult (pFile->GetHeader (&pHeader));
 	  assert (pHeader);
 
+      IAAFDictionarySP pDict;
+	  checkResult(pHeader->GetDictionary(&pDict));
+
 	  IAAFIdentificationSP pReadIdent;
 	  checkResult (pHeader->GetLastIdentification(&pReadIdent));
 
 	  // ident should be initialized, so this should work 
 	  aafUID_t junkAuid;
 	  checkResult (pReadIdent->GetProductID(&junkAuid));
+
+      // Look up the (extended) class definition for AAFCompositionMob, and get
+	  // the definition for the optional property we added.
+	  IAAFClassDefSP pClassDef;
+	  checkResult(pDict->LookupClassDef(AUID_AAFCompositionMob,&pClassDef));
+	  IAAFPropertyDefSP pOptionalPropertyDef;
+	  checkResult(pClassDef->LookupPropertyDef(AUID_OptionalProperty,
+		  &pOptionalPropertyDef));
+
+      // Also look up the optional property we added to AAFSequence
+	  checkResult(pDict->LookupClassDef(AUID_AAFSequence,&pClassDef));
+	  IAAFPropertyDefSP pOptionalPropertyDef2;
+	  checkResult(pClassDef->LookupPropertyDef(AUID_OptionalProperty2,
+		  &pOptionalPropertyDef2));
+
+	  // Look up the Mob we created
+	  IAAFMobSP pMob;
+	  checkResult(pHeader->LookupMob(kTestMobID,&pMob));
+
+	  // Verify that this is a composition Mob
+	  IAAFCompositionMobSP pCompositionMob;
+	  checkResult(pMob->QueryInterface(IID_IAAFCompositionMob,
+		  (void**)&pCompositionMob));
+
+      IAAFObjectSP pObject;
+	  checkResult(pMob->QueryInterface(IID_IAAFObject,(void**)&pObject));
+
+	  // Property should *not* be present in the object, since we removed it via
+	  // RemoveOptionalProperty()
+	  IAAFPropertyValueSP pOptionalPropertyValue;
+	  checkExpression(pObject->GetPropertyValue(pOptionalPropertyDef,
+		  &pOptionalPropertyValue)==AAFRESULT_PROP_NOT_PRESENT);
+
+	  // RemoveOptionalProperty() should fail since the property is not present
+	  checkExpression(pObject->RemoveOptionalProperty(pOptionalPropertyDef)
+		  ==AAFRESULT_PROP_NOT_PRESENT);
+
+      // Look up the sequence we created & get the property value we added
+      IAAFMobSlotSP pSlot;
+      checkResult(pMob->GetSlotAt(0,&pSlot));
+      IAAFObjectSP pObj2;
+	  IAAFSegmentSP pSegment;
+	  checkResult(pSlot->GetSegment(&pSegment));
+      checkResult(pSegment->QueryInterface(IID_IAAFObject,(void**)&pObj2));
+      checkResult(pObj2->GetPropertyValue(pOptionalPropertyDef2,
+		  &pOptionalPropertyValue));
+
+      // Verify optional property value created via CreateOptionalPropertyValue()
+	  IAAFTypeDefSP pTypeDef;
+	  checkResult(pOptionalPropertyValue->GetType(&pTypeDef));
+	  IAAFTypeDefIntSP pTypeDefInt;
+	  checkResult(pTypeDef->QueryInterface(IID_IAAFTypeDefInt,
+		  (void**)&pTypeDefInt));
+	  aafUInt16 storedValue;
+	  checkResult(pTypeDefInt->GetInteger(pOptionalPropertyValue,
+		  (aafMemPtr_t)&storedValue,sizeof(aafUInt16)));
+      checkExpression(storedValue==10);
 
 	  hr = AAFRESULT_SUCCESS;
 	}
@@ -359,10 +564,6 @@ extern "C" HRESULT CAAFObject_test()
 		  cerr << "CAAFObject_test...FAILED!" << endl;
 		  return hr;
 		}
-
-		
-		if(hr == AAFRESULT_SUCCESS)
-			hr = AAFRESULT_NOT_IN_CURRENT_VERSION;
 	}
   catch (...)
 	{
