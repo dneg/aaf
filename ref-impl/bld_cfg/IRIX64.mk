be1670627c9cd611cd5f4fc9d1bae54103c25f1a
tbingham <>
2001-04-17 02:42:18 +0000

    Changed from sym link to IRIX.mk, to a text file which includes IRIX.mk .

---
+++ ref-impl/bld_cfg/IRIX64.mk
@@ -0,0 +1,14 @@
+############################################
+#                                          #
+# Advanced Authoring Format                #
+#                                          #
+# Copyright (c) 1998 Avid Technology, Inc. #
+#                                          #
+############################################
+
+#
+# Contains elements for SGI/IRIX64 builds.
+#
+
+# Same as for SGI/IRIX.
+include $(BLD_CFG_DIR)/IRIX.mk
