948e0001fa5758a5717a10347623b932deb6c83c
tbingham <>
2001-04-17 05:13:34 +0000

    Enhance AAFEssenceAccess interface

---
+++ ref-impl/src/impl/ImplAAFEssenceFormat.cpp
@@ -0,0 +1,171 @@
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+
+
+
+#ifndef __ImplAAFEssenceFormat_h__
+#include "ImplAAFEssenceFormat.h"
+#endif
+
+#include <assert.h>
+#include <string.h>
+
+#include "AAFUtils.h"
+#define CHUNK_SIZE	10
+
+ImplAAFEssenceFormat::ImplAAFEssenceFormat ()
+{
+	_elements = NULL;
+	_elemUsed = 0;
+	_elemAllocated = 0;
+}
+
+
+ImplAAFEssenceFormat::~ImplAAFEssenceFormat ()
+{
+	aafUInt32	n;
+
+	for(n = 0; n < _elemUsed; n++)
+	{
+		delete _elements[n].parmValue;
+	}
+	delete _elements;
+}
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFEssenceFormat::AddFormatSpecifier (aafUID_t  essenceFormatCode,
+                           aafInt32  valueSize,
+                           aafDataBuffer_t  value)
+{
+	oneParm_t		*parm, *tempParm;
+	aafDataBuffer_t	temp;
+	aafUInt32		n;
+
+	parm = Lookup(essenceFormatCode);
+	if(parm == NULL)	// New format spec
+	{
+		if(_elemUsed >= _elemAllocated)	// Allocate more memory
+		{
+			tempParm = _elements;
+			_elements = new oneParm_t[_elemAllocated + CHUNK_SIZE];
+			_elemAllocated += CHUNK_SIZE;
+			for(n = 0; n < _elemUsed; n++)
+				_elements[n] = tempParm[n];
+			if(tempParm != NULL)
+				delete tempParm;
+		}
+		
+		parm = _elements + _elemUsed;
+		_elemUsed++;
+		if(valueSize != 0)
+		{
+			parm->parmValue = new unsigned char[valueSize];
+			memcpy(parm->parmValue, value, valueSize);
+		}
+		parm->valueSize = valueSize;
+		parm->allocSize = valueSize;
+		parm->parmName = essenceFormatCode;
+	}
+	else
+	{
+		if(parm->allocSize < (aafUInt32)valueSize)
+		{
+			temp = parm->parmValue;
+			if(valueSize != 0)
+			{
+				parm->parmValue = new unsigned char[valueSize];
+				memcpy(parm->parmValue, temp, valueSize);
+			}
+			parm->allocSize = valueSize;
+			
+			if(temp != NULL)
+				delete temp;
+		}
+		memcpy(parm->parmValue, value, valueSize);
+		parm->valueSize = valueSize;
+	}
+
+	return(AAFRESULT_SUCCESS);
+}
+
+			//@comm The value data is passed in as a void * through the "value"
+			// argument.  The size of the value must be passed through the
+			// valueSize argument.	
+
+
+/****/
+ AAFRESULT STDMETHODCALLTYPE
+   ImplAAFEssenceFormat::GetFormatSpecifier (aafUID_t  essenceFormatCode,
+                           aafInt32  bufSize,
+                           aafDataBuffer_t  value,
+                           aafInt32*  bytesRead)
+{
+	oneParm_t	*parm;
+	
+	parm = Lookup(essenceFormatCode);
+	if(parm == NULL)
+		return(AAFRESULT_FORMAT_NOT_FOUND);
+	if((aafUInt32)bufSize < parm->valueSize)
+		return(AAFRESULT_SMALLBUF);
+
+	memcpy(value, parm->parmValue, parm->valueSize);
+	*bytesRead = parm->valueSize;
+
+	return AAFRESULT_SUCCESS;
+}
+
+			//@comm The actual number of bytes read is returned in
+			// bytesRead.  If the buffer is not big enough to return the entire
+			// value, an error is returned.
+/****/
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFEssenceFormat::NumFormatSpecifiers (aafInt32* numSpecifiers)
+{
+	*numSpecifiers = _elemUsed;
+	return AAFRESULT_SUCCESS;
+}
+
+/****/
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFEssenceFormat::GetIndexedFormatSpecifier (aafInt32  index,
+                           aafUID_t		*essenceFormatCode,
+                           aafInt32  bufSize,
+                           aafDataBuffer_t  value,
+                           aafInt32*  bytesRead)
+{	
+	if((aafUInt32)index >= _elemUsed)
+	if((aafUInt32)bufSize < _elements[index].valueSize)
+		return(AAFRESULT_SMALLBUF);
+
+	*essenceFormatCode = _elements[index].parmName;
+	memcpy(value, _elements[index].parmValue, _elements[index].valueSize);
+	*bytesRead = _elements[index].valueSize;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+
+//****** Internal
+oneParm_t	*ImplAAFEssenceFormat::Lookup(aafUID_t lookup)
+{
+	aafUInt32	n;
+
+	for(n = 0; n < _elemUsed; n++)
+	{
+		if(EqualAUID(&_elements[n].parmName, &lookup))
+			return(_elements+n);
+	}
+
+	return(NULL);
+}
+
+
