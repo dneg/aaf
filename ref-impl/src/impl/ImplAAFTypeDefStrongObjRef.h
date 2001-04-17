0eaf1767fb35dde64814a1b6ac944590495bbf05
tbingham <>
2001-04-17 05:32:06 +0000

    Implement OMType virtual functions.

--- ref-impl/src/impl/ImplAAFTypeDefStrongObjRef.h
+++ ref-impl/src/impl/ImplAAFTypeDefStrongObjRef.h
@@ -68,34 +68,6 @@ public:
     GetTypeCategory (/*[out]*/ eAAFTypeCategory_t *  pTid);
 
 
-  //*************************************************************
-  //
-  // Overrides from OMType, via inheritace through ImplAAFTypeDef
-  //
-  //*************************************************************
-
-  /*
-  virtual void reorder(OMByte* bytes,
-                       size_t bytesSize) const;
-
-  virtual size_t externalSize(void) const;
-
-  virtual void externalize(OMByte* internalBytes,
-                           size_t internalBytesSize,
-                           OMByte* externalBytes,
-                           size_t externalBytesSize,
-                           OMByteOrder byteOrder) const;
-
-  virtual size_t internalSize(void) const;
-
-  virtual void internalize(OMByte* externalBytes,
-                           size_t externalBytesSize,
-                           OMByte* internalBytes,
-                           size_t internalBytesSize,
-                           OMByteOrder byteOrder) const;
-						   */
-
-
 public:
   // Declare this class to be storable.
   //
@@ -109,6 +81,7 @@ public:
   aafBool IsFixedSize (void) const;
   size_t PropValSize (void) const;
 
+private:
   // OMWeakReferenceProperty<ImplAAFClassDef> _referencedType;
   OMFixedSizeProperty<aafUID_t>           _referencedType;
 };
