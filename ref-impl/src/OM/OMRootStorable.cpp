15015cda8892f147e5992c509071a5b6c30d7cb1
tbingham <>
2001-04-17 08:04:42 +0000

    New Object Manager files.

---
+++ ref-impl/src/OM/OMRootStorable.cpp
@@ -0,0 +1,75 @@
+/***********************************************************************
+*
+*              Copyright (c) 1998-1999 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted,
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+* prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
+
+// @doc OMEXTERNAL
+#include "OMRootStorable.h"
+
+OMRootStorable::OMRootStorable(void)
+: _clientRoot(0xFFFF, "Header"),
+  _dictionary(0xFFFE, "MetaDictionary")
+{
+  _persistentProperties.put(_clientRoot.address());
+  _persistentProperties.put(_dictionary.address());
+
+  _clientRoot = 0;
+  _dictionary = 0;
+}
+
+OMRootStorable::OMRootStorable(OMStorable* clientRoot,
+                               OMDictionary* dictionary)
+: _clientRoot(0xFFFF, "Header"),
+  _dictionary(0xFFFE, "MetaDictionary")
+{
+  _persistentProperties.put(_clientRoot.address());
+  _persistentProperties.put(_dictionary.address());
+
+  _clientRoot = clientRoot;
+  _dictionary = dictionary;
+}
+
+OMRootStorable::~OMRootStorable(void)
+{
+}
+
+// {B3B398A5-1C90-11d4-8053-080036210804}
+const OMClassId OMRootStorable::_rootClassId = 
+{ 0xb3b398a5, 0x1c90, 0x11d4, { 0x80, 0x53, 0x8, 0x0, 0x36, 0x21, 0x8, 0x4 } };
+
+const OMClassId& OMRootStorable::classId(void) const
+{
+  return _rootClassId;
+}
+
+OMStorable* OMRootStorable::clientRoot(void) const
+{
+  return _clientRoot;
+}
+
+OMDictionary* OMRootStorable::dictionary(void) const
+{
+  return _dictionary;
+}
---
+++ ref-impl/src/OM/OMRootStorable.h
@@ -0,0 +1,61 @@
+/***********************************************************************
+*
+*              Copyright (c) 1998-2000 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted,
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+* prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
+
+// @doc OMEXTERNAL
+#ifndef OMROOTSTORABLE_H
+#define OMROOTSTORABLE_H
+
+#include "OMStorable.h"
+#include "OMProperty.h"
+#include "OMDictionary.h"
+
+class OMRootStorable : public OMStorable {
+public:
+
+  OMRootStorable(void);
+
+  OMRootStorable(OMStorable* clientRoot,
+                 OMDictionary* dictionary);
+
+  ~OMRootStorable(void);
+
+  virtual const OMClassId& classId(void) const;
+
+  OMStorable* clientRoot(void) const;
+
+  OMDictionary* dictionary(void) const;
+
+  static const OMClassId _rootClassId;
+
+private:
+
+  OMStrongReferenceProperty<OMStorable> _clientRoot;
+  OMStrongReferenceProperty<OMDictionary> _dictionary;
+
+};
+
+#endif
