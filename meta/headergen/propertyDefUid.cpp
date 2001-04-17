4f6c358ac3b19b0c320dfd6cac9783efc2b2c643
tbingham <>
2001-04-17 09:47:12 +0000

    Task: Updated to include definitions for special kAAFPropID_Root_MetaDictionary and kAAFPropID_Root_Header identifiers.

--- meta/headergen/propertyDefUid.cpp
+++ meta/headergen/propertyDefUid.cpp
@@ -23,6 +23,21 @@ static void doFile (const char * moduleName)
   cout << endl;
 #include "AAFMetaDictionary.h"
   cout << endl;
+  cout << endl;
+  cout << "//" << endl;
+  cout << "// Special property ids used for specifying the starting strong reference in the" << endl;
+  cout << "// targe list for weak references." << endl;
+  cout << "//" << endl;
+  cout << endl;
+  cout << "// SMPTE Label: 06-0E-2B-34-01-01-01-02-0D-01-03-01-01-01-01-00" << endl;
+  cout << "const aafUID_t kAAFPropID_Root_MetaDictionary =" << endl;
+  cout << "{0x0D010301, 0x0101, 0x0100, {0x06, 0x0E, 0x2B, 0x34, 0x01, 0x01, 0x01, 0x02}};" << endl;
+  cout << endl;
+  cout << "// SMPTE Label: 06-0E-2B-34-01-01-01-02-0D-01-03-01-01-02-01-00" << endl;
+  cout << "const aafUID_t kAAFPropID_Root_Header =" << endl;
+  cout << "{0x0D010301, 0x0102, 0x0100, {0x06, 0x0E, 0x2B, 0x34, 0x01, 0x01, 0x01, 0x02}};" << endl;
+  cout << endl;
+  cout << endl;
   cout << "#endif // ! __" << moduleName << "_h__" << endl;
 }
 
