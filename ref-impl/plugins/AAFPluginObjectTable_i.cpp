e645ae73c7977c6bc8ee9a9c2e274f1112f87cd8
tbingham <>
2001-04-17 08:26:00 +0000

    Make codecs into a DLL

---
+++ ref-impl/plugins/AAFPluginObjectTable_i.cpp
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////
+//
+// Advanced Authoring Format
+//
+// Copyright (c) 1998 Avid Technology, Inc.
+// Copyright (c) 1998 Microsoft Corporation
+//
+////////////////////////////////////////////////
+
+//
+// Include the appropriate header files:
+//
+#include "CAAFObjectStream.h"
+#include "CAAFWaveCodec.h"
+//
+// Include all objects in the following table:
+//
+AAF_BEGIN_OBJECT_MAP(AAFPluginObjectMap)
+	AAF_OBJECT_ENTRY(AAFDefaultCodec)
+	AAF_OBJECT_ENTRY(AAFDefaultStream)
+	AAF_OBJECT_ENTRY(AAFObjectStream)
+	AAF_OBJECT_ENTRY(AAFWaveCodec)
+AAF_END_OBJECT_MAP()
