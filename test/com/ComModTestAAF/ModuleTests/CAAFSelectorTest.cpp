a34e52853ad001295495859436b6163814852df2
tbingham <>
2001-04-17 06:52:31 +0000

    Updated all of the checked in module tests to use the new AAFFile factory functions: AAFFileOpenNewModify() and AAFFileOpenExistingRead() instead of CoCreateInstance(CLSID_AAFFile,...), IAAFFile::Initilize() and then IAAFFile::OpenNewModify() or IAAFFile::OpenExistingRead().

--- test/com/ComModTestAAF/ModuleTests/CAAFSelectorTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFSelectorTest.cpp
@@ -73,38 +73,15 @@ static HRESULT OpenAAFFile(aafWChar*			pFileName,
 	ProductInfo.productID = -1;
 	ProductInfo.platform = NULL;
 
-	/*
-	hr = CoCreateInstance(CLSID_AAFSession,
-						   NULL, 
-						   CLSCTX_INPROC_SERVER, 
-						   IID_IAAFSession, 
-						   (void **)ppSession);
-	*/
-	hr = CoCreateInstance(CLSID_AAFFile,
-						   NULL, 
-						   CLSCTX_INPROC_SERVER, 
-						   IID_IAAFFile, 
-						   (void **)ppFile);
-	if (AAFRESULT_SUCCESS != hr)
-		return hr;
-    hr = (*ppFile)->Initialize();
-	if (AAFRESULT_SUCCESS != hr)
-		return hr;
-
-	// hr = (*ppSession)->SetDefaultIdentification(&ProductInfo);
-	// if (AAFRESULT_SUCCESS != hr)
-	// 	return hr;
 
 	switch (mode)
 	{
 	case kMediaOpenReadOnly:
-		// hr = (*ppSession)->OpenReadFile(pFileName, ppFile);
-		hr = (*ppFile)->OpenExistingRead(pFileName, 0);
+		hr = AAFFileOpenExistingRead(pFileName, 0, ppFile);
 		break;
 
 	case kMediaOpenAppend:
-		// hr = (*ppSession)->CreateFile(pFileName, kAAFRev1, ppFile);
-		hr = (*ppFile)->OpenNewModify(pFileName, 0, &ProductInfo);
+		hr = AAFFileOpenNewModify(pFileName, 0, &ProductInfo, ppFile);
 		break;
 
 	default:
@@ -114,8 +91,6 @@ static HRESULT OpenAAFFile(aafWChar*			pFileName,
 
 	if (FAILED(hr))
 	{
-		// (*ppSession)->Release();
-		// *ppSession = NULL;
 		(*ppFile)->Release();
 		*ppFile = NULL;
 		return hr;
@@ -124,8 +99,6 @@ static HRESULT OpenAAFFile(aafWChar*			pFileName,
   	hr = (*ppFile)->GetHeader(ppHeader);
 	if (FAILED(hr))
 	{
-		// (*ppSession)->Release();
-		// *ppSession = NULL;
 		(*ppFile)->Release();
 		*ppFile = NULL;
 		return hr;
