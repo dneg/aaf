223344f59ef483f2214455a9323e875a978637d6
tbingham <>
2001-04-17 06:41:05 +0000

    Fix builds - incomplete Group -> Effect change.

--- Utilities/AafOmf/Omf2Aaf.cpp
+++ Utilities/AafOmf/Omf2Aaf.cpp
@@ -1413,7 +1413,7 @@ HRESULT Omf2Aaf::ProcessOMFComponent(OMF2::omfObject_t OMFSegment, IAAFComponent
 											  IID_IAAFTransition,
 											  (IUnknown **) &pTransition);
 
-			rc = pDictionary->CreateInstance(&AUID_AAFEffect,
+			rc = pDictionary->CreateInstance(&AUID_AAFGroup,
 											  IID_IAAFEffect,
 											  (IUnknown **) &pEffect);
 
