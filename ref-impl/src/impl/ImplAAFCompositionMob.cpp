09a5dee555efcfce3521e08782902318ee176115
tbingham <>
2001-04-17 04:18:27 +0000

    Added persistence and implemented public methods

--- ref-impl/src/impl/ImplAAFCompositionMob.cpp
+++ ref-impl/src/impl/ImplAAFCompositionMob.cpp
@@ -25,8 +25,18 @@
 #include <assert.h>
 #include "AAFResult.h"
 
-ImplAAFCompositionMob::ImplAAFCompositionMob ()
-{}
+ImplAAFCompositionMob::ImplAAFCompositionMob ():
+	_defaultFadeLen(	PID_COMPOSITIONMOB_DEFAULTFADELENGTH,		"defaultFadeLength"),
+	_defaultFadeType(	PID_COMPOSITIONMOB_DEFAULTFADETYPE,			"defaultFadeType"),
+	_defaultFadeEditUnit(PID_COMPOSITIONMOB_DEFAULTFADEEDITUNIT,	"defaultFadeEditUnit")
+{
+	_persistentProperties.put(		_defaultFadeLen.address());
+	_persistentProperties.put(		_defaultFadeType.address());
+	_persistentProperties.put(		_defaultFadeEditUnit.address());
+	
+}
+
+
 
 
 ImplAAFCompositionMob::~ImplAAFCompositionMob ()
@@ -35,33 +45,69 @@ ImplAAFCompositionMob::~ImplAAFCompositionMob ()
 
   //@access Public Members
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFCompositionMob::SetInitialValues (aafString_t *  /*name*/,
-                           aafBool  /*isPrimary*/)
+    ImplAAFCompositionMob::SetInitialValues (aafWChar	*pName,
+											 aafBool	isPrimary
+											 )
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+    AAFRESULT aafError = AAFRESULT_SUCCESS;
+	if (pName == NULL)
+	{
+		aafError = AAFRESULT_NULL_PARAM;
+	}
+	else
+	{
+		SetName( pName );
+		SetPrimary( isPrimary );
+	}
+
+	return aafError;
 }
 
   //@rdesc Error code [see below].
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplAAFCompositionMob::GetDefaultFade (aafDefaultFade_t *  /*result*/)
-{
-  return AAFRESULT_NOT_IMPLEMENTED;
-}
 
   //@rdesc Error code [see below].
   //@comm If there is no default fade, this function returns with no error,
   // but the VALID field of the structure is false.  This allows you to
-  // pass this struct to omfiSourceClipGetFade() in all cases.
-  //@comm Maps to omfiMobGetDefaultFade
-	
+  // pass this struct to SourceClip::GetFade() in all cases.
+  //@comm Maps to AAFGetDefaultFade
 
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFCompositionMob::SetDefaultFade (aafLength_t    /*fadeLength  */,
-                           aafFadeType_t  /*fadeType    */,
-                           aafRational_t  /*fadeEditUnit*/)
+    ImplAAFCompositionMob::GetDefaultFade (aafDefaultFade_t *pResult)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+    AAFRESULT aafError = AAFRESULT_SUCCESS;
+	if (pResult == NULL)
+	{
+		aafError = AAFRESULT_NULL_PARAM;
+	}
+	else
+	{
+		if (_defaultFadeType == kFadeNone)
+			pResult->valid = AAFFalse;
+		else
+		{
+			pResult->fadeLength = _defaultFadeLen;
+			pResult->fadeType = _defaultFadeType;
+			pResult->fadeEditUnit = _defaultFadeEditUnit;
+			pResult->valid = AAFTrue;
+		}
+	}
+	return aafError;
+}
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFCompositionMob::SetDefaultFade (
+											aafLength_t		fadeLength,
+											aafFadeType_t	fadeType,
+											aafRational_t	fadeEditUnit
+											)
+{
+    AAFRESULT aafError = AAFRESULT_SUCCESS;
+
+	_defaultFadeLen = fadeLength;
+	_defaultFadeType = fadeType;
+	_defaultFadeEditUnit = fadeEditUnit;
+
+	return aafError;
 }
 
   //@rdesc Error code [see below].
