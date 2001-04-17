76d662f3bac239473241c2dca106a591474a4498
tbingham <>
2001-04-17 04:54:43 +0000

    Enumerator for type defs

---
+++ ref-impl/src/impl/ImplEnumAAFTypeDefs.cpp
@@ -0,0 +1,305 @@
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+#ifndef __ImplAAFTypeDef_h__
+#include "ImplAAFTypeDef.h"
+#endif
+
+#ifndef __ImplEnumAAFTypeDefs_h__
+#include "ImplEnumAAFTypeDefs.h"
+#endif
+
+#include <assert.h>
+#include <string.h>
+#include "aafErr.h"
+#include "AAFResult.h"
+#include "ImplAAFObjectCreation.h"
+#include "ImplAAFHeader.h"
+#include "ImplAAFDictionary.h"
+
+extern "C" const aafClassID_t CLSID_EnumAAFTypeDefs;
+
+ImplEnumAAFTypeDefs::ImplEnumAAFTypeDefs ()
+{
+	_current = 0;
+	_enumObj = NULL;
+	_enumProp = NULL;
+	_enumStrongProp = NULL;
+}
+
+
+ImplEnumAAFTypeDefs::~ImplEnumAAFTypeDefs ()
+{
+	if (_enumObj)
+	{
+		_enumObj->ReleaseReference();
+		_enumObj = NULL;
+	}
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFTypeDefs::NextOne (
+      ImplAAFTypeDef **ppTypeDef)
+{
+	aafUInt32			numElem;
+	aafUID_t			value;
+	ImplAAFHeader		*head = NULL;
+	ImplAAFDictionary	*dict = NULL;
+
+	if(_enumProp != NULL)
+		numElem = _enumProp->size() / sizeof(aafUID_t);
+	else if(_enumStrongProp != NULL)
+	{
+		size_t	siz;
+		
+		_enumStrongProp->getSize(siz);
+		numElem = siz;
+	}
+	//!!!Else assert
+
+	if(ppTypeDef == NULL)
+		return(AAFRESULT_NULL_PARAM);
+	if(_current >= numElem)
+		return AAFRESULT_NO_MORE_OBJECTS;
+	XPROTECT()
+	{
+		if(_enumProp != NULL)
+		{
+			_enumProp->getValueAt(&value, _current);
+			CHECK(_enumObj->MyHeadObject(&head));
+			CHECK(head->GetDictionary (&dict));
+			CHECK(dict->LookupType(&value, ppTypeDef));
+		}
+		else if(_enumStrongProp != NULL)
+			_enumStrongProp->getValueAt(*ppTypeDef, _current);
+		//!!!Else assert
+		(*ppTypeDef)->AcquireReference();
+		_current++;
+		if (head) {
+			head->ReleaseReference();
+			head = NULL;
+		}
+		if (dict) {
+			dict->ReleaseReference();
+			dict = NULL;
+		}
+	}
+	XEXCEPT
+	{
+		if(head)
+			head->ReleaseReference();
+		if(dict)
+			dict->ReleaseReference();
+	}
+	XEND;
+
+	return(AAFRESULT_SUCCESS); 
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFTypeDefs::Next (
+      aafUInt32  count,
+      ImplAAFTypeDef **ppTypeDefs,
+      aafUInt32 *pFetched)
+{
+	ImplAAFTypeDef**	ppDef;
+	aafUInt32			numDefs;
+	HRESULT				hr;
+
+	if ((pFetched == NULL && count != 1) || (pFetched != NULL && count == 1))
+		return E_INVALIDARG;
+
+	// Point at the first component in the array.
+	ppDef = ppTypeDefs;
+	for (numDefs = 0; numDefs < count; numDefs++)
+	{
+		hr = NextOne(ppDef);
+		if (FAILED(hr))
+			break;
+
+		// Point at the next component in the array.  This
+		// will increment off the end of the array when
+		// numComps == count-1, but the for loop should
+		// prevent access to this location.
+		ppDef++;
+	}
+	
+	if (pFetched)
+		*pFetched = numDefs;
+
+	return hr;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFTypeDefs::Skip (
+      aafUInt32  count)
+{
+	AAFRESULT	hr;
+	aafUInt32	newCurrent;
+	aafUInt32	numElem;
+	if(_enumProp != NULL)
+		numElem = _enumProp->size() / sizeof(aafUID_t);
+	else if(_enumStrongProp != NULL)
+	{
+		size_t	siz;
+		
+		_enumStrongProp->getSize(siz);
+		numElem = siz;
+	}
+	//!!!Else assert
+
+	newCurrent = _current + count;
+
+	if(newCurrent < numElem)
+	{
+		_current = newCurrent;
+		hr = AAFRESULT_SUCCESS;
+	}
+	else
+	{
+		hr = E_FAIL;
+	}
+
+	return hr;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFTypeDefs::Reset ()
+{
+	_current = 0;
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFTypeDefs::Clone (
+      ImplEnumAAFTypeDefs **ppEnum)
+{
+	ImplEnumAAFTypeDefs	*result;
+	AAFRESULT				hr;
+
+	result = (ImplEnumAAFTypeDefs *)CreateImpl(CLSID_EnumAAFTypeDefs);
+	if (result == NULL)
+		return E_FAIL;
+
+	if(_enumProp != NULL)
+		hr = result->SetEnumProperty(_enumObj, _enumProp);
+	else if(_enumStrongProp != NULL)
+		hr = result->SetEnumStrongProperty(_enumObj, _enumStrongProp);
+	// !!!Else assert
+	if (SUCCEEDED(hr))
+	{
+		result->_current = _current;
+		*ppEnum = result;
+	}
+	else
+	{
+		result->ReleaseReference();
+		*ppEnum = NULL;
+	}
+	
+	return hr;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFTypeDefs::SetEnumProperty( ImplAAFObject *pObj, typeDefWeakRefArrayProp_t *pProp)
+{
+	if (_enumObj)
+		_enumObj->ReleaseReference();
+	_enumObj = pObj;
+
+	if (pObj)
+		pObj->AcquireReference();
+	/**/
+	_enumProp = pProp;				// Don't refcount, same lifetime as the object.
+	_enumStrongProp = NULL;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFTypeDefs::SetEnumStrongProperty( ImplAAFObject *pObj, typeDefStrongRefArrayProp_t *pProp)
+{
+	if (_enumObj)
+		_enumObj->ReleaseReference();
+	_enumObj = pObj;
+	if (pObj)
+		pObj->AcquireReference();
+	/**/
+	_enumStrongProp = pProp;				// Don't refcount, same lifetime as the object.
+	_enumProp = NULL;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+
+
+
+
+
+#if 0
+
+ImplEnumAAFTypeDefs::ImplEnumAAFTypeDefs ()
+{}
+
+
+ImplEnumAAFTypeDefs::~ImplEnumAAFTypeDefs ()
+{}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFTypeDefs::NextOne (
+      ImplAAFTypeDef ** /*ppTypeDef*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFTypeDefs::Next (
+      aafUInt32  /*count*/,
+      ImplAAFTypeDef ** /*ppTypeDefs*/,
+      aafUInt32 *  /*pFetched*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFTypeDefs::Skip (
+      aafUInt32  /*count*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFTypeDefs::Reset ()
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFTypeDefs::Clone (
+      ImplEnumAAFTypeDefs ** /*ppEnum*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+#endif
---
+++ ref-impl/src/impl/ImplEnumAAFTypeDefs.h
@@ -0,0 +1,104 @@
+//@doc
+//@class    EnumAAFTypeDefs | Implementation class for EnumAAFTypeDefs
+#ifndef __ImplEnumAAFTypeDefs_h__
+#define __ImplEnumAAFTypeDefs_h__
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+class ImplAAFTypeDef;
+
+#ifndef __ImplAAFObject_h__
+#include "ImplAAFObject.h"
+#endif
+
+
+typedef OMVariableSizeProperty<aafUID_t> typeDefWeakRefArrayProp_t;
+typedef OMStrongReferenceVectorProperty<ImplAAFTypeDef> typeDefStrongRefArrayProp_t;
+
+
+class ImplEnumAAFTypeDefs : public ImplAAFRoot
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplEnumAAFTypeDefs ();
+
+protected:
+  virtual ~ImplEnumAAFTypeDefs ();
+
+public:
+
+
+  //****************
+  // NextOne()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    NextOne
+        // @parm [out,retval] The Next TypeDefinition
+        (ImplAAFTypeDef ** ppTypeDef);
+
+  //****************
+  // Next()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Next
+        (// @parm [in] number of type definition definitions requested
+         aafUInt32  count,
+
+         // @parm [out, size_is(count), length_is(*pFetched)] array to receive type definition definitions
+         ImplAAFTypeDef ** ppTypeDefs,
+
+         // @parm [out,ref] number of actual TypeDefs fetched into ppTypeDefs array
+         aafUInt32 *  pFetched);
+
+  //****************
+  // Skip()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Skip
+        // @parm [in] Number of elements to skip
+        (aafUInt32  count);
+
+  //****************
+  // Reset()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Reset ();
+
+
+  //****************
+  // Clone()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Clone
+        // @parm [out,retval] new enumeration
+        (ImplEnumAAFTypeDefs ** ppEnum);
+
+
+
+public:
+  // SDK Internal 
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetEnumProperty( ImplAAFObject *pObj, typeDefWeakRefArrayProp_t *pProp);
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetEnumStrongProperty( ImplAAFObject *pObj, typeDefStrongRefArrayProp_t *pProp);
+
+private:
+	aafUInt32						_current;
+	ImplAAFObject					*_enumObj;
+	typeDefWeakRefArrayProp_t		*_enumProp;
+	typeDefStrongRefArrayProp_t		*_enumStrongProp;
+};
+
+#endif // ! __ImplEnumAAFTypeDefs_h__
+
+
