cbee7a795a8915e342f54e7b49eeea47ce261024
tbingham <>
2001-04-17 07:43:14 +0000

    Clean up conditional compilation - forget about CC -o32 on Irix. Don't mention compilers that are well behaved.

--- ref-impl/src/OM/OMPortability.h
+++ ref-impl/src/OM/OMPortability.h
@@ -41,19 +41,6 @@ const bool true = 1;
 
 #endif
 
-#elif defined (__MWERKS__)  // CodeWarrior
-
-
-#elif defined (__GNUC__)    // GNU C++
-
-
-#elif defined (__sgi)       // SGI C++
-
-#define typename class
-typedef int bool;
-const bool false = 0;
-const bool true = 1;
-
 #endif
 
 #endif
