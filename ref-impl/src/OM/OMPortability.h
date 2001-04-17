7f8251b9ecadf0f1e65dd0cfed0a279e9bfd759a
tbingham <>
2001-04-17 07:21:24 +0000

    Port to Macintosh and Unix

--- ref-impl/src/OM/OMPortability.h
+++ ref-impl/src/OM/OMPortability.h
@@ -1,14 +1,30 @@
 #ifndef OMPORTABILITY_H
 #define OMPORTABILITY_H
 
-#if defined (_MSC_VER)
-#if _MSC_VER <= 1000
+#if defined (_MSC_VER) // Microsoft Visual C++
+
+#if _MSC_VER <= 1000   // <= 1000 means earlier than version 5.0
+
 #define typename class
 typedef int bool;
 const bool false = 0;
 const bool true = 1;
-#include <stdlib.h>
+#include <stdlib.h> // for size_t
+
 #endif
+
+#elif defined (__MWERKS__)  // CodeWarrior
+
+#include <stdlib.h> // for size_t
+
+#elif defined (__GNUC__)  // GNU C++
+
+#include <stdlib.h> // for size_t
+
+#elif defined (__sgi)  // SGI C++
+
+#include <stdlib.h> // for size_t
+
 #endif
 
 #endif
