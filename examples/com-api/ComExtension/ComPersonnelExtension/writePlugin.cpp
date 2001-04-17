9a03085c6afe31b0ef464b33a067fd4f4c5a56db
tbingham <>
2001-04-17 05:11:34 +0000

    Source file for console application that implements that same extension bahavior as the ComExtensionWrite project but uses the PersonnelExension.dll plugin interface.

---
+++ examples/com-api/ComExtension/ComPersonnelExtension/writePlugin.cpp
@@ -0,0 +1,109 @@
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
+
+#include "AAF.h"
+
+#include "extensionUtils.h"
+#include "extensionReadPlugin.h"
+
+//
+// This example code is intended to show how AAF may be extended to
+// store personnel information relating to the staff which worked on a
+// project.
+//
+// As a side benefit, it will also show how to define subclasses of
+// existing classes, and add collections of them to AAF files.
+//
+// Specifically, this is what we'll do:
+//
+// - The eRole enumeration will be an extensible enumeration describing
+//   that person's role in the project:
+//
+//  extensibleEnum eRole 
+//  { 
+//    Producer, 
+//  	Editor, 
+//  	FloorManager, 
+//  	...
+//  };
+//
+//
+// - Create a new object (called PersonnelResource) which is used to
+//   store the information corresponding to one person who's worked on
+//   this project.  In pseudo-C++, it will look like this:
+//
+//   PersonnelResource : subclass of InterchangeObject 
+//   { 
+//     // mandatory properties 
+//     String    name; 
+//     eRole     role; 
+//     aafUInt32 contractID;
+//   };
+//
+//
+// - We will extend Mob to contain a collection of these
+//   PersonnelResource objects:
+//
+//   PersonnelMob : subclass of Mob 
+//   { 
+//     // mandatory property 
+//     StrongRefVector<Person> personnel;
+//   };
+//
+
+#include "extensionWritePlugin.h"
+
+void main ()
+{
+  const aafWChar * filename = L"extensionPlugin.aaf";
+
+  HRESULT status = extensionWritePlugin (filename);
+  if (FAILED(status))
+	{
+	  cout << "Error creating "
+		   << filename
+		   << "." << endl;
+
+	  exit (1);
+	}
+  else
+	{
+    status = extensionReadPlugin (filename);
+    if (FAILED(status))
+	  {
+	    cout << "Error dumping "
+		     << filename
+		     << "." << endl;
+
+	    exit (2);
+	  }
+    else
+	  {
+	    cout << "Succeeded." << endl;
+	  }
+	}
+}
