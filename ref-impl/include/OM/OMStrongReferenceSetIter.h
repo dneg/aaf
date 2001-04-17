48ff1911a7f10ad8eab508af474d7331362caaf1
tbingham <>
2001-04-17 07:45:04 +0000

    Add StrongReferenceSetProperty and associated iterator.

---
+++ ref-impl/include/OM/OMStrongReferenceSetIter.h
@@ -0,0 +1,147 @@
+/***********************************************************************
+*
+*              Copyright (c) 1998-1999 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted,
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+* prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
+
+// @doc OMEXTERNAL
+#ifndef OMSTRONGREFERENCESETITER_H
+#define OMSTRONGREFERENCESETITER_H
+
+#include "OMSetIterator.h"
+
+template <typename ReferencedObject>
+class OMStrongReferenceSetProperty;
+
+// @class Iterators over <c OMStrongReferenceSetProperty>s.
+//   @tcarg class | ReferencedObject | The type of the contained objects.
+template <typename ReferencedObject>
+class OMStrongReferenceSetIterator {
+public:
+  // @access Public members.
+
+    // @cmember Create an <c OMStrongReferenceSetIterator> over the given
+    //          <c OMStrongReferenceSetProperty> <p set> and initialize it
+    //          to the given <p initialPosition>.
+    //          If <p initialPosition> is specified as
+    //          <e OMIteratorPosition.OMBefore> then this
+    //          <c OMStrongReferenceSetIterator> is made ready to traverse the
+    //          associated <c OMStrongReferenceSetProperty> in the forward
+    //          direction (increasing <p Key>s).
+    //          If <p initialPosition> is specified as
+    //          <e OMIteratorPosition.OMAfter> then this
+    //          <c OMStrongReferenceSetIterator> is made ready to traverse the
+    //          associated <c OMStrongReferenceSetProperty> in the reverse
+    //          direction (decreasing <p Key>s).
+  OMStrongReferenceSetIterator(
+                     const OMStrongReferenceSetProperty<ReferencedObject>& set,
+                     OMIteratorPosition initialPosition);
+
+    // @cmember Destroy this <c OMStrongReferenceSetIterator>.
+  virtual ~OMStrongReferenceSetIterator(void);
+
+    // @cmember Reset this <c OMStrongReferenceSetIterator> to the given
+    //          <p initialPosition>.
+    //          If <p initialPosition> is specified as
+    //          <e OMIteratorPosition.OMBefore> then this
+    //          <c OMStrongReferenceSetIterator> is made ready to traverse the
+    //          associated <c OMStrongReferenceSetProperty> in the forward
+    //          direction (increasing <p Key>s).
+    //          If <p initialPosition> is specified as
+    //          <e OMIteratorPosition.OMAfter> then this
+    //          <c OMStrongReferenceSetIterator> is made ready to traverse the
+    //          associated <c OMStrongReferenceSetProperty> in the reverse
+    //          direction (decreasing <p Key>s).
+   virtual void reset(OMIteratorPosition initialPosition);
+
+    // @cmember Is this <c OMStrongReferenceSetIterator> positioned before
+    //          the first <p ReferencedObject> ?
+   virtual bool before(void) const;
+
+    // @cmember Is this <c OMStrongReferenceSetIterator> positioned after
+    //          the last <p ReferencedObject> ?
+   virtual bool after(void) const;
+
+    // @cmember Advance this <c OMStrongReferenceSetIterator> to the next
+    //          <p ReferencedObject>, if any.
+    //          If the end of the associated
+    //          <c OMStrongReferenceSetProperty> is not reached then the
+    //          result is <e bool.true>,
+    //          <mf OMStrongReferenceSetIterator::valid> becomes <e bool.true>
+    //          and <mf OMStrongReferenceSetIterator::after> becomes
+    //          <e bool.false>.
+    //          If the end of the associated
+    //          <c OMStrongReferenceSetProperty> is reached then the result
+    //          is <e bool.false>, <mf OMStrongReferenceSetIterator::valid>
+    //          becomes <e bool.false> and
+    //          <mf OMStrongReferenceSetIterator::after> becomes
+    //          <e bool.true>. 
+   virtual bool operator++();
+
+    // @cmember Retreat this <c OMStrongReferenceSetIterator> to the previous
+    //          <p ReferencedObject>, if any.
+    //          If the beginning of the associated
+    //          <c OMStrongReferenceSetProperty> is not reached then the
+    //          result is <e bool.true>,
+    //          <mf OMStrongReferenceSetIterator::valid> becomes <e bool.true>
+    //          and <mf OMStrongReferenceSetIterator::before> becomes
+    //          <e bool.false>.
+    //          If the beginning of the associated
+    //          <c OMStrongReferenceSetProperty> is reached then the result
+    //          is <e bool.false>, <mf OMStrongReferenceSetIterator::valid>
+    //          becomes <e bool.false> and
+    //          <mf OMStrongReferenceSetIterator::before> becomes
+    //          <e bool.true>. 
+   virtual bool operator--();
+
+    // @cmember Return the <p ReferencedObject> in the associated
+    //          <c OMStrongReferenceSetProperty> at the position currently
+    //          designated by this <c OMStrongReferenceSetIterator>.
+   virtual ReferencedObject* value(void) const;
+
+    // @cmember Set the <p ReferencedObject> in the associated
+    //          <c OMStrongReferenceSetProperty> at the position currently
+    //          designated by this <c OMStrongReferenceSetIterator> to
+    //          <p newObject>. The previous <p ReferencedObject>, if any,
+    //          is returned. To preserve the ordering of <p Key>s, the
+    //          <p Key> of <p newObject> must be the same as that of the
+    //          existing <p ReferencedObject>.
+   virtual ReferencedObject* setValue(const ReferencedObject* newObject);
+
+    // @cmember Return the <p Key> of the <p ReferencedObject> in the
+    //          associated <c OMStrongReferenceSetProperty> at the position
+    //          currently designated by this <c OMStrongReferenceSetIterator>.
+   OMUniqueObjectIdentification identification(void) const;
+
+private:
+
+  OMSetIterator<OMUniqueObjectIdentification,
+	            OMSetElement<OMStrongObjectReference<ReferencedObject>,
+                             ReferencedObject> > _iterator;
+
+};
+
+#include "OMStrongReferenceSetIterT.h"
+
+#endif
---
+++ ref-impl/src/OM/OMStrongReferenceSetIterT.h
@@ -0,0 +1,217 @@
+/***********************************************************************
+*
+*              Copyright (c) 1998-1999 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted,
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+* prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
+
+// @doc OMEXTERNAL
+#include "OMAssertions.h"
+
+  // @mfunc Create an <c OMStrongReferenceSetIterator> over the given
+  //        <c OMStrongReferenceSetProperty> <p set> and initialize it
+  //        to the given <p initialPosition>.
+  //        If <p initialPosition> is specified as
+  //        <e OMIteratorPosition.OMBefore> then this
+  //        <c OMStrongReferenceSetIterator> is made ready to traverse the
+  //        associated <c OMStrongReferenceSetProperty> in the forward
+  //        direction (increasing <p Key>s).
+  //        If <p initialPosition> is specified as
+  //        <e OMIteratorPosition.OMAfter> then this
+  //        <c OMStrongReferenceSetIterator> is made ready to traverse the
+  //        associated <c OMStrongReferenceSetProperty> in the reverse
+  //        direction (decreasing <p Key>s).
+  //   @tcarg class | ReferencedObject | The type of the contained objects.
+  //   @parm The <c OMStrongReferenceSet> over which to iterate.
+  //   @parm The initial position for this <c OMStrongReferenceSetIterator>.
+template <typename ReferencedObject>
+OMStrongReferenceSetIterator<ReferencedObject>::OMStrongReferenceSetIterator(
+                     const OMStrongReferenceSetProperty<ReferencedObject>& set,
+                     OMIteratorPosition initialPosition)
+  : _iterator(set._set, initialPosition)
+{
+}
+
+  // @mfunc Destroy this <c OMStrongReferenceSetIterator>.
+  //   @tcarg class | ReferencedObject | The type of the contained objects.
+template <typename ReferencedObject>
+OMStrongReferenceSetIterator<ReferencedObject>::~OMStrongReferenceSetIterator(
+                                                                          void)
+{
+}
+
+  // @mfunc Reset this <c OMStrongReferenceSetIterator> to the given
+  //        <p initialPosition>.
+  //        If <p initialPosition> is specified as
+  //        <e OMIteratorPosition.OMBefore> then this
+  //        <c OMStrongReferenceSetIterator> is made ready to traverse the
+  //        associated <c OMStrongReferenceSetProperty> in the forward
+  //        direction (increasing <p Key>s).
+  //        If <p initialPosition> is specified as
+  //        <e OMIteratorPosition.OMAfter> then this
+  //        <c OMStrongReferenceSetIterator> is made ready to traverse the
+  //        associated <c OMStrongReferenceSetProperty> in the reverse
+  //        direction (decreasing <p Key>s).
+  //   @tcarg class | ReferencedObject | The type of the contained objects.
+  //   @parm The position to which this <c OMStrongReferenceSetIterator>
+  //         should be reset.
+template <typename ReferencedObject>
+void OMStrongReferenceSetIterator<ReferencedObject>::reset(
+                                            OMIteratorPosition initialPosition)
+{
+  _iterator.reset(initialPosition);
+}
+
+  // @mfunc Is this <c OMStrongReferenceSetIterator> positioned before
+  //        the first <p ReferencedObject> ?
+  //   @tcarg class | ReferencedObject | The type of the contained objects.
+  //   @rdesc <e bool.true> if this <c OMStrongReferenceSetIterator> is
+  //          positioned before the first <p ReferencedObject>, <e bool.false>
+  //          otherwise.
+  //   @this const
+template <typename ReferencedObject>
+bool OMStrongReferenceSetIterator<ReferencedObject>::before(void) const
+{
+  return _iterator.before();
+}
+
+  // @mfunc Is this <c OMStrongReferenceSetIterator> positioned after
+  //        the last <p ReferencedObject> ?
+  //   @tcarg class | ReferencedObject | The type of the contained objects.
+  //   @rdesc <e bool.true> if this <c OMStrongReferenceSetIterator> is
+  //          positioned after the last <p ReferencedObject>, <e bool.false>
+  //          otherwise.
+  //   @this const
+template <typename ReferencedObject>
+bool OMStrongReferenceSetIterator<ReferencedObject>::after(void) const
+{
+  return _iterator.after();
+}
+
+  // @mfunc Advance this <c OMStrongReferenceSetIterator> to the next
+  //        <p ReferencedObject>, if any.
+  //        If the end of the associated
+  //        <c OMStrongReferenceSetProperty> is not reached then the
+  //        result is <e bool.true>,
+  //        <mf OMStrongReferenceSetIterator::valid> becomes <e bool.true>
+  //        and <mf OMStrongReferenceSetIterator::after> becomes
+  //        <e bool.false>.
+  //        If the end of the associated
+  //        <c OMStrongReferenceSetProperty> is reached then the result
+  //        is <e bool.false>, <mf OMStrongReferenceSetIterator::valid>
+  //        becomes <e bool.false> and
+  //        <mf OMStrongReferenceSetIterator::after> becomes
+  //        <e bool.true>. 
+  //   @tcarg class | ReferencedObject | The type of the contained objects.
+  //   @rdesc <e bool.false> if this <c OMStrongReferenceSetIterator> has
+  //          passed the last <p ReferencedObject>, <e bool.true> otherwise.
+template <typename ReferencedObject>
+bool OMStrongReferenceSetIterator<ReferencedObject>::operator++()
+{
+  return ++_iterator;
+}
+
+  // @mfunc Retreat this <c OMStrongReferenceSetIterator> to the previous
+  //        <p ReferencedObject>, if any.
+  //        If the beginning of the associated
+  //        <c OMStrongReferenceSetProperty> is not reached then the
+  //        result is <e bool.true>,
+  //        <mf OMStrongReferenceSetIterator::valid> becomes <e bool.true>
+  //        and <mf OMStrongReferenceSetIterator::before> becomes
+  //        <e bool.false>.
+  //        If the beginning of the associated
+  //        <c OMStrongReferenceSetProperty> is reached then the result
+  //        is <e bool.false>, <mf OMStrongReferenceSetIterator::valid>
+  //        becomes <e bool.false> and
+  //        <mf OMStrongReferenceSetIterator::before> becomes
+  //        <e bool.true>. 
+  //   @tcarg class | ReferencedObject | The type of the contained objects.
+  //   @rdesc <e bool.false> if this <c OMStrongReferenceSetIterator> has
+  //          passed the first <p ReferencedObject>, <e bool.true> otherwise.
+template <typename ReferencedObject>
+bool OMStrongReferenceSetIterator<ReferencedObject>::operator--()
+{
+  return --_iterator;
+}
+
+  // @mfunc Return the <p ReferencedObject> in the associated
+  //        <c OMStrongReferenceSetProperty> at the position currently
+  //        designated by this <c OMStrongReferenceSetIterator>.
+  //   @tcarg class | ReferencedObject | The type of the contained objects.
+  //   @rdesc The <p ReferencedObject> at the current position.
+  //   @this const
+template <typename ReferencedObject>
+ReferencedObject*
+OMStrongReferenceSetIterator<ReferencedObject>::value(void) const
+{
+  TRACE("OMStrongReferenceSetIterator<ReferencedObject>::value");
+
+  const OMSetElement<OMStrongObjectReference<ReferencedObject>, ReferencedObject>& element = _iterator.value();
+
+  ReferencedObject* result = element.getValue();
+
+  POSTCONDITION("Valid result", result != 0);
+  return result;
+}
+
+  // @mfunc Set the <p ReferencedObject> in the associated
+  //        <c OMStrongReferenceSetProperty> at the position currently
+  //        designated by this <c OMStrongReferenceSetIterator> to
+  //        <p newObject>. The previous <p ReferencedObject>, if any,
+  //        is returned. To preserve the ordering of <p Key>s, the
+  //        <p Key> of <p newObject> must be the same as that of the
+  //        existing <p ReferencedObject>.
+  //   @tcarg class | ReferencedObject | The type of the contained objects.
+  //   @parm The new <p ReferencedObject>.
+  //   @rdesc The previous <p ReferencedObject> if any, otherwise 0.
+template <typename ReferencedObject>
+ReferencedObject*
+OMStrongReferenceSetIterator<ReferencedObject>::setValue(
+                                             const ReferencedObject* newObject)
+{
+  TRACE("OMStrongReferenceSetIterator<ReferencedObject>::setValue");
+
+  PRECONDITION("Matching keys",
+    IMPLIES(newObject != 0 , newObject->identification() == identification()));
+
+  OMSetElement<OMStrongObjectReference<ReferencedObject>, ReferencedObject>& element = _iterator.value();
+
+  ReferencedObject* result = element.setValue(newObject);
+
+  return result;
+}
+
+  // @mfunc Return the <p Key> of the <p ReferencedObject> in the
+  //        associated <c OMStrongReferenceSetProperty> at the position
+  //        currently designated by this <c OMStrongReferenceSetIterator>.
+  //   @tcarg class | ReferencedObject | The type of the contained objects.
+  //   @rdesc The <p Key> at the current position.
+  //   @this const
+template <typename ReferencedObject>
+OMUniqueObjectIdentification
+OMStrongReferenceSetIterator<ReferencedObject>::identification(void) const
+{
+  TRACE("OMStrongReferenceSetIterator<ReferencedObject>::identification");
+
+  return _iterator.key();
+}
