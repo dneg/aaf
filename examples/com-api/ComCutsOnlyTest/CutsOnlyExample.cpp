85a3102fd676160897cee4311419e2231d773a16
tbingham <>
2001-04-17 08:31:07 +0000

    Changes to accommodate changed aafProductIdentification_t.

--- examples/com-api/ComCutsOnlyTest/CutsOnlyExample.cpp
+++ examples/com-api/ComCutsOnlyTest/CutsOnlyExample.cpp
@@ -175,13 +175,16 @@ static HRESULT CreateAAFFile(aafWChar * pFileName)
 	convert(chFileName, sizeof(chFileName), pFileName);
 	remove(chFileName);
 
+	aafProductVersion_t v;
+	v.major = 1;
+	v.minor = 0;
+	v.tertiary = 0;
+	v.patchLevel = 0;
+	v.type = kAAFVersionUnknown;
+	
 	ProductInfo.companyName = L"Company Name";
 	ProductInfo.productName = L"Cuts Only Composition Example";
-	ProductInfo.productVersion.major = 1;
-	ProductInfo.productVersion.minor = 0;
-	ProductInfo.productVersion.tertiary = 0;
-	ProductInfo.productVersion.patchLevel = 0;
-	ProductInfo.productVersion.type = kAAFVersionUnknown;
+	ProductInfo.productVersion = &v;
 	ProductInfo.productVersionString = NULL;
 	ProductInfo.productID = NIL_UID;
 	ProductInfo.platform = NULL;
