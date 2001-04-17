c4e086d3cce178ee257008c0b8affd70fc995bdc
tbingham <>
2001-04-17 05:58:09 +0000

    Added VaryingValue, Pulldown, and Interpolation plugins

---
+++ test/com/ComModTestAAF/ModuleTests/CEnumAAFInterpolateDefsTest.cpp
@@ -0,0 +1,344 @@
+// @doc INTERNAL
+// @com This file implements the module test for CEnumAAFInterpolationDefs object
+/************************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
+\************************************************/
+
+#include "CEnumAAFInterpolationDefs.h"
+#include "CEnumAAFInterpolationDefs.h"
+#ifndef __CEnumAAFInterpolationDefs_h__
+#error - improperly defined include guard
+#endif
+
+#include <iostream.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
+#include "AAFStoredObjectIDs.h"
+#include "AAFResult.h"
+#include "AAFDefUIDs.h"
+
+// Cross-platform utility to delete a file.
+static void RemoveTestFile(const wchar_t* pFileName)
+{
+  const size_t kMaxFileName = 512;
+  char cFileName[kMaxFileName];
+
+  size_t status = wcstombs(cFileName, pFileName, kMaxFileName);
+  if (status != (size_t)-1)
+  { // delete the file.
+    remove(cFileName);
+  }
+}
+
+// convenient error handlers.
+inline void checkResult(HRESULT r)
+{
+  if (FAILED(r))
+    throw r;
+}
+inline void checkExpression(bool expression, HRESULT r)
+{
+  if (!expression)
+    throw r;
+}
+
+static wchar_t *sName1 = L"Test Descriptor Name1";
+static wchar_t *sDescription1 = L"Test Descriptor Description1";
+static wchar_t *sName2 = L"Test Descriptor Name2";
+static wchar_t *sDescription2 = L"Test Descriptor Description2";
+
+
+static HRESULT OpenAAFFile(aafWChar*			pFileName,
+						   aafMediaOpenMode_t	mode,
+						   IAAFFile**			ppFile,
+						   IAAFHeader**			ppHeader)
+{
+	aafProductIdentification_t	ProductInfo;
+	HRESULT						hr = AAFRESULT_SUCCESS;
+
+	ProductInfo.companyName = L"AAF Developers Desk";
+	ProductInfo.productName = L"AAFMasterMob Test";
+	ProductInfo.productVersion.major = 1;
+	ProductInfo.productVersion.minor = 0;
+	ProductInfo.productVersion.tertiary = 0;
+	ProductInfo.productVersion.patchLevel = 0;
+	ProductInfo.productVersion.type = kVersionUnknown;
+	ProductInfo.productVersionString = NULL;
+	ProductInfo.productID = -1;
+	ProductInfo.platform = NULL;
+
+	if(mode == kMediaOpenAppend)
+		hr = AAFFileOpenNewModify(pFileName, 0, &ProductInfo, ppFile);
+	else
+		hr = AAFFileOpenExistingRead(pFileName, 0, ppFile);
+
+	if (FAILED(hr))
+	{
+		(*ppFile)->Release();
+		*ppFile = NULL;
+		return hr;
+	}
+  
+  	hr = (*ppFile)->GetHeader(ppHeader);
+	if (FAILED(hr))
+	{
+		(*ppFile)->Release();
+		*ppFile = NULL;
+		return hr;
+	}
+ 	
+	return hr;
+}
+
+static HRESULT CreateAAFFile(aafWChar * pFileName)
+{
+	IAAFFile*			pFile = NULL;
+	IAAFHeader *		pHeader = NULL;
+	IAAFDictionary*		pDictionary = NULL;
+	IAAFDefObject*		pDef = NULL;
+	IAAFInterpolationDef*	pInterpolationDef = NULL;
+	bool				bFileOpen = false;
+	HRESULT				hr = S_OK;
+/*	long				test;
+*/
+
+  try
+  {
+    // Remove the previous test file if any.
+    RemoveTestFile(pFileName);
+
+
+	// Create the AAF file
+	checkResult(OpenAAFFile(pFileName, kMediaOpenAppend, /*&pSession,*/ &pFile, &pHeader));
+    bFileOpen = true;
+
+    // Get the AAF Dictionary so that we can create valid AAF objects.
+    checkResult(pHeader->GetDictionary(&pDictionary));
+    
+	checkResult(pDictionary->CreateInstance(&AUID_AAFInterpolationDefinition,
+							  IID_IAAFInterpolationDef, 
+							  (IUnknown **)&pInterpolationDef));
+    
+	checkResult(pInterpolationDef->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
+
+	checkResult(pDef->SetName(sName1));
+	checkResult(pDef->SetDescription(sDescription1));
+	checkResult(pDictionary->RegisterInterpolationDefinition(pInterpolationDef));
+	pDef->Release();
+	pDef = NULL;
+	pInterpolationDef->Release();
+	pInterpolationDef = NULL;
+	checkResult(pDictionary->CreateInstance(&AUID_AAFInterpolationDefinition,
+							  IID_IAAFInterpolationDef, 
+							  (IUnknown **)&pInterpolationDef));
+    
+	checkResult(pInterpolationDef->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
+
+	checkResult(pDef->SetName(sName2));
+	checkResult(pDef->SetDescription(sDescription2));
+
+	checkResult(pDictionary->RegisterInterpolationDefinition(pInterpolationDef));
+  }
+  catch (HRESULT& rResult)
+  {
+    hr = rResult;
+  }
+
+
+  // Cleanup and return
+  if (pDef)
+    pDef->Release();
+
+  if (pInterpolationDef)
+    pInterpolationDef->Release();
+
+  if (pDictionary)
+    pDictionary->Release();
+
+  if (pHeader)
+    pHeader->Release();
+      
+  if (pFile)
+  {  // Close file
+    if (bFileOpen)
+	{
+		pFile->Save();
+		pFile->Close();
+	}
+     pFile->Release();
+  }
+
+  return hr;
+}
+
+static HRESULT ReadAAFFile(aafWChar* pFileName)
+{
+	IAAFFile*			pFile = NULL;
+	IAAFHeader*			pHeader = NULL;
+	IAAFDictionary*		pDictionary = NULL;
+	IEnumAAFInterpolationDefs *pPlug = NULL;
+	IEnumAAFInterpolationDefs *pClonePlug = NULL;
+	IAAFDefObject*		pDef = NULL;
+	IAAFInterpolationDef*	pInterpolationDef = NULL;
+	IAAFInterpolationDef*	pArray[2] = { NULL, NULL };
+	IAAFInterpolationDef**	pArrayDef = pArray;
+	bool				bFileOpen = false;
+	HRESULT				hr = S_OK;
+	wchar_t				testString[256];
+	aafUInt32			resultCount;
+
+	try
+	{
+		// Open the AAF file
+		checkResult(OpenAAFFile(pFileName, kMediaOpenReadOnly, &pFile, &pHeader));
+		bFileOpen = true;
+
+		checkResult(pHeader->GetDictionary(&pDictionary));
+	
+		checkResult(pDictionary->GetInterpolationDefinitions(&pPlug));
+		/* Read and check the first element */
+		checkResult(pPlug->NextOne(&pInterpolationDef));
+		checkResult(pInterpolationDef->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
+
+		checkResult(pDef->GetName (testString, sizeof(testString)));
+		checkExpression (wcscmp(testString, sName1) == 0, AAFRESULT_TEST_FAILED);
+		pInterpolationDef->Release();
+		pInterpolationDef = NULL;
+		pDef->Release();
+		pDef = NULL;
+
+		/* Read and check the second element */
+		checkResult(pPlug->NextOne(&pInterpolationDef));
+		checkResult(pInterpolationDef->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
+
+		checkResult(pDef->GetName (testString, sizeof(testString)));
+		checkExpression (wcscmp(testString, sName2) == 0, AAFRESULT_TEST_FAILED);
+		pInterpolationDef->Release();
+		pInterpolationDef = NULL;
+		pDef->Release();
+		pDef = NULL;
+		/* Reset, and check the first element again*/
+		checkResult(pPlug->Reset());
+		checkResult(pPlug->NextOne(&pInterpolationDef));
+		checkResult(pInterpolationDef->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
+
+		checkResult(pDef->GetName (testString, sizeof(testString)));
+		checkExpression (wcscmp(testString, sName1) == 0, AAFRESULT_TEST_FAILED);
+		pInterpolationDef->Release();
+		pInterpolationDef = NULL;
+		pDef->Release();
+		pDef = NULL;
+		/* Reset, Skip, and check the second element again*/
+		checkResult(pPlug->Reset());
+		checkResult(pPlug->Skip(1));
+		checkResult(pPlug->NextOne(&pInterpolationDef));
+		checkResult(pInterpolationDef->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
+
+		checkResult(pDef->GetName (testString, sizeof(testString)));
+		checkExpression (wcscmp(testString, sName2) == 0, AAFRESULT_TEST_FAILED);
+		pInterpolationDef->Release();
+		pInterpolationDef = NULL;
+		pDef->Release();
+		pDef = NULL;
+		/* Reset, and read both elements */
+		checkResult(pPlug->Reset());
+		checkResult(pPlug->Next (2, (IAAFInterpolationDef **)&pArray, &resultCount));
+		checkExpression (resultCount == 2, AAFRESULT_TEST_FAILED);
+		checkResult(pArrayDef[0]->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
+
+		checkResult(pDef->GetName (testString, sizeof(testString)));
+		checkExpression (wcscmp(testString, sName1) == 0, AAFRESULT_TEST_FAILED);
+		pDef->Release();
+		pDef = NULL;
+		checkResult(pArrayDef[1]->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
+
+		checkResult(pDef->GetName (testString, sizeof(testString)));
+		checkExpression (wcscmp(testString, sName2) == 0, AAFRESULT_TEST_FAILED);
+		pDef->Release();
+		pDef = NULL;
+		/* Read one past to make sure that it fails */
+		checkExpression(pPlug->NextOne(&pInterpolationDef) != AAFRESULT_SUCCESS, AAFRESULT_TEST_FAILED);
+		/* Clone the enumerator, and read one element */
+		checkResult(pPlug->Clone(&pClonePlug));
+		checkResult(pClonePlug->Reset());
+		checkResult(pClonePlug->NextOne(&pInterpolationDef));
+		checkResult(pInterpolationDef->QueryInterface (IID_IAAFDefObject,
+                                          (void **)&pDef));
+
+		checkResult(pDef->GetName (testString, sizeof(testString)));
+		checkExpression (wcscmp(testString, sName1) == 0, AAFRESULT_TEST_FAILED);
+		pInterpolationDef->Release();
+		pInterpolationDef = NULL;
+		pDef->Release();
+		pDef = NULL;
+	}
+	catch (HRESULT& rResult)
+	{
+		hr = rResult;
+	}
+
+	// Cleanup and return
+	if (pHeader)
+		pHeader->Release();
+      
+	if (pPlug)
+		pPlug->Release();
+
+	if (pClonePlug)
+		pClonePlug->Release();
+
+	if (pDef)
+		pDef->Release();
+      
+	if (pInterpolationDef)
+		pInterpolationDef->Release();
+            
+	if (pArray[0])
+		pArray[0]->Release();
+	if (pArray[1])
+		pArray[1]->Release();
+            
+	if (pFile)
+	{  // Close file
+		if (bFileOpen)
+			pFile->Close();
+		pFile->Release();
+	}
+
+	return hr;
+}
+ 
+
+HRESULT CEnumAAFInterpolationDefs::test()
+{
+	HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
+	aafWChar * pFileName = L"EnumAAFInterpolationDefsTest.aaf";
+
+	try
+	{
+		hr = CreateAAFFile(pFileName);
+		if (SUCCEEDED(hr))
+			hr = ReadAAFFile(pFileName);
+	}
+	catch (...)
+	{
+		cerr << "CEnumAAFInterpolationDefs::test...Caught general C++ exception!" << endl; 
+	}
+	return hr;
+}
