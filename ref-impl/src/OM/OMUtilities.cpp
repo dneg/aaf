d7f4ae72df4e4f91ba14bf9898b6176aff80cfb4
tbingham <>
2001-04-17 07:23:03 +0000

    Use and persist GUIDs instead of small integers for class ids

--- ref-impl/src/OM/OMUtilities.cpp
+++ ref-impl/src/OM/OMUtilities.cpp
@@ -2,6 +2,8 @@
 
 #include "OMUtilities.h"
 
+#include "OMAssertions.h"
+
 const char* programName = "Unknown program";
 
 void setProgramName(const char* name)
@@ -13,3 +15,20 @@ const char* getProgramName(void)
 {
   return programName;
 }
+
+ByteOrder hostByteOrder(void)
+{
+  OMInt16 word = 0x1234;
+  OMInt8  byte = *((OMInt8*)&word);
+  ByteOrder result;
+
+  ASSERT("Valid byte order", ((byte == 0x12) || (byte == 0x34)));
+
+  if (byte == 0x12) {
+    result = bigEndian;
+  } else {
+    result = littleEndian;
+  }
+  return result;
+}
+
--- ref-impl/src/OM/OMUtilities.h
+++ ref-impl/src/OM/OMUtilities.h
@@ -1,8 +1,12 @@
 #ifndef OMUTILITIES_H
 #define OMUTILITIES_H
 
+#include "OMTypes.h"
+
 void setProgramName(const char* name);
 
 const char* getProgramName(void);
+
+ByteOrder hostByteOrder(void);
 
 #endif
