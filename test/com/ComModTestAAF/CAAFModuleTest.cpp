a7afc437a307c793d2b4db4a695b9d2a9e8558ad
tbingham <>
2001-04-17 09:36:06 +0000

    done

--- test/com/ComModTestAAF/CAAFModuleTest.cpp
+++ test/com/ComModTestAAF/CAAFModuleTest.cpp
@@ -18,6 +18,8 @@
 
 #include <iostream.h>
 #include <string.h>
+#include <time.h>
+
 
 //
 // Define the AAF_BEGIN_OBJECT_XXXX macros so that we can reuse the
@@ -107,51 +109,127 @@ STDMETHODIMP CAAFModuleTest::Test
 {
 	HRESULT hr = S_OK;
 
+	struct STAT_t
+	{
+		aafInt16 	num_run;	/* total number of tests run */
+		aafInt16	num_failed;	/* number of tests that failed */
+		aafInt32	result[1000];	/* and the result of the test */
+	} stats = { 0, 0, NULL };
+
+	/* Print Header */
+
+	cout << endl; \
+	cout<< "*******************" << endl; \
+	cout<< " AAFCOMTST Results" << endl; \
+	cout<< "*******************" << endl<< endl;	
+
+
+	/* Get and print start time */
+
+	time_t s_time;
+	time(&s_time);
+	cout<<"AAF COM Interface tests started: " <<ctime(&s_time) <<endl <<endl;
+
 
 	// Search the object table for the given class id.
-	long int index = 0;
 
 	if (NULL != pClassName)
 	{
 		cout << "Looking for " << pClassName << " module test...";
 
-		while (NULL != AAFObjectMap[index].pCLSID)
+		while (NULL != AAFObjectMap[stats.num_run].pCLSID)
 		{
-			if (0 == strcmp(reinterpret_cast<char *>(pClassName), AAFObjectMap[index].pClassName))
+			if (0 == strcmp(reinterpret_cast<char *>(pClassName), AAFObjectMap[stats.num_run].pClassName))
 			{
 				cout << "Found." << endl;
-				cout << "Running " << AAFObjectMap[index].pClassName << " module test...";
-				hr = AAFObjectMap[index].pfnTestProc();
+				cout << "Running " << AAFObjectMap[stats.num_run].pClassName << " module test...";
+				hr = AAFObjectMap[stats.num_run].pfnTestProc();
 				if (FAILED(hr))
+				{
 					cout << "FAILED!" << endl;
+					++stats.num_failed;
+				}
 				else if (AAFRESULT_NOT_IMPLEMENTED == hr)
+				{
 					cout << "NOT IMPLEMENTED!" << endl;
+					++stats.num_failed;
+				}
 				else
 					cout << "SUCCEEDED." << endl;
 
 				return S_OK;
 			}
-			++index;
+			stats.result[stats.num_run] = hr;
+			++stats.num_run;
 		}
 		
 		cout<< "NOT FOUND IN MODULE TABLE!" << endl;
 	}
 	else
 	{
-		while (NULL != AAFObjectMap[index].pCLSID)
+		while (NULL != AAFObjectMap[stats.num_run].pCLSID)
 		{
-			cout << "Running " << AAFObjectMap[index].pClassName << " module test...";
-			hr = AAFObjectMap[index].pfnTestProc();
+			cout << "Running " << AAFObjectMap[stats.num_run].pClassName << " module test...";
+			hr = AAFObjectMap[stats.num_run].pfnTestProc();
 			if (FAILED(hr))
+			{
 				cout << "FAILED!" << endl;
+				++stats.num_failed;
+			}
 			else if (AAFRESULT_NOT_IMPLEMENTED == hr)
+			{
 				cout << "NOT IMPLEMENTED!" << endl;
+				++stats.num_failed;
+			}
 			else
 				cout << "SUCCEEDED." << endl;
 
-			++index;
+			stats.result[stats.num_run] = hr;
+			++stats.num_run;
 		}
 	}
+
+
+	/* Get and Print finish time	*/
+	time_t e_time;
+	time(&e_time);
+	cout<< endl<< "AAF COM Interface tests finished: " <<ctime(&e_time) <<endl;
+
+
+	/* Determine and print elapsed time */
+	double elapsed_time = difftime( e_time, s_time );
+	cout<< "AAF COM Interface tests completed in ";
+	cout<< ((long)elapsed_time/3600) <<":"; /* hours */
+	cout<< (((long)elapsed_time%3600)/60) <<":"; /* minutes */
+	cout<< (((long)elapsed_time%3600)%60) <<endl <<endl; /* seconds */
+
+	cout<< stats.num_run<< " Test(s) Run"<< endl;
+	cout<< stats.num_failed<< " Failed"<< endl<< endl;
+
+	/*	This is being commented out because until implementation, all tests fail
+	/* list of all tests that failed */
+/*	if ( stats.num_failed > 0 )
+	{
+		cout << endl << "List of tests that failed" << endl;
+		for ( index = 0; index < stats.num_run; ++index )
+			switch (stats.result[index])
+		{
+			case 0:
+				break;
+			case -1:
+				cout<< AAFObjectMap[index].pClassName << "	"<< "FAILED!"<< endl;
+				break;
+			case 1:
+				cout<< AAFObjectMap[index].pClassName << "	"<< "NOT IMPLEMENTED"<< endl;
+				break;
+			case -2:
+				cout<< AAFObjectMap[index].pClassName << "	"<< "Not found in table"<< endl;
+				break;
+			default:
+				cout<< AAFObjectMap[index].pClassName << "	"<< "UNKOWN RESULT"<< endl;
+		}
+	}
+*/
 	return hr;
 }
 
