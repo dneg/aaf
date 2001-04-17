060613ba3d896b9b2a45a6b1005528c091ce2c90
tbingham <>
2001-04-17 09:03:22 +0000

    Removing CoCreateInstance from the module tests.

--- test/com/ComModTestAAF/ModuleTests/CAAFTypeDefObjectRefTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFTypeDefObjectRefTest.cpp
@@ -1,13 +1,15 @@
 // @doc INTERNAL
-// @com This file implements the module test for CAAFDefinitionObject
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+// @com This file implements the module test for CAAFTypeDefObjectRef
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
+
+
 
 
 
@@ -19,55 +21,15 @@
 #error - improperly defined include guard
 #endif
 
-
-#include <iostream.h>
-
-// Temporarily necessary global declarations.
-extern "C" const CLSID CLSID_AAFTypeDefObjectRef; // generated
+#include "AAFResult.h"
 
 
 HRESULT CAAFTypeDefObjectRef::test()
 {
-  HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
-  IAAFTypeDefObjectRef *pObject = NULL;
-
-  try
-    {
-      // Attempt to create an AAFTypeDefObjectRef.
-      hr =  CoCreateInstance(
-                             CLSID_AAFTypeDefObjectRef,
-                             NULL, 
-                             CLSCTX_INPROC_SERVER, 
-                             IID_IAAFTypeDefObjectRef, (void **)&pObject);
-      if (FAILED(hr))
-        {
-          cerr << "CAAFTypeDefObjectRef::test...FAILED!";
-          cerr << hr;
-          cerr << "\tCoCreateInstance(&CLSID_AAFTypeDefObjectRef, NULL,"
-            " CLSCTX_INPROC_SERVER, &IID_IAAFTypeDefObjectRef, ...);" <<
-              endl;
-          return hr;
-        }
-
-      // module-specific tests go here
-
-      if (pObject)
-        pObject->Release();
-      return AAFRESULT_NOT_IMPLEMENTED;
-
-    }
-  catch (...)
-    {
-      cerr << "CAAFTypeDefObjectRef::test...Caught general C++"
-        " exception!" << endl; 
-    }
-
-  // Cleanup our object if it exists.
-  if (pObject)
-    pObject->Release();
-
-  return hr;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
+
+
 
 
 
--- test/com/ComModTestAAF/ModuleTests/CAAFTypeDefRenameTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFTypeDefRenameTest.cpp
@@ -1,13 +1,14 @@
 // @doc INTERNAL
-// @com This file implements the module test for CAAFDefinitionObject
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+// @com This file implements the module test for CAAFTypeDefRename
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
+
 
 
 
@@ -19,55 +20,15 @@
 #error - improperly defined include guard
 #endif
 
-
-#include <iostream.h>
-
-// Temporarily necessary global declarations.
-extern "C" const CLSID CLSID_AAFTypeDefRename; // generated
+#include "AAFResult.h"
 
 
 HRESULT CAAFTypeDefRename::test()
 {
-  HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
-  IAAFTypeDefRename *pObject = NULL;
-
-  try
-    {
-      // Attempt to create an AAFTypeDefRename.
-      hr =  CoCreateInstance(
-                             CLSID_AAFTypeDefRename,
-                             NULL, 
-                             CLSCTX_INPROC_SERVER, 
-                             IID_IAAFTypeDefRename, (void **)&pObject);
-      if (FAILED(hr))
-        {
-          cerr << "CAAFTypeDefRename::test...FAILED!";
-          cerr << hr;
-          cerr << "\tCoCreateInstance(&CLSID_AAFTypeDefRename, NULL,"
-            " CLSCTX_INPROC_SERVER, &IID_IAAFTypeDefRename, ...);" <<
-              endl;
-          return hr;
-        }
-
-      // module-specific tests go here
-
-      if (pObject)
-        pObject->Release();
-      return AAFRESULT_NOT_IMPLEMENTED;
-
-    }
-  catch (...)
-    {
-      cerr << "CAAFTypeDefRename::test...Caught general C++"
-        " exception!" << endl; 
-    }
-
-  // Cleanup our object if it exists.
-  if (pObject)
-    pObject->Release();
-
-  return hr;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
+
+
 
 
 
