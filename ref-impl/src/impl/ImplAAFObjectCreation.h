4ae639a587e48ae1eb31fef391a7185e3f1f9097
tbingham <>
2001-04-17 03:13:01 +0000

    AAF/ref-impl/src/impl/ImplAAFObjectCreation.h

---
+++ ref-impl/src/impl/ImplAAFObjectCreation.h
@@ -0,0 +1,25 @@
+#ifndef IMPLAAFOBJECTCREATION_H
+#define IMPLAAFOBJECTCREATION_H
+
+// Creates and returns an Impl object based on the given class ID.
+// Will create the appropriate kind of API class and attach it.
+//
+// Implementation note:  This function does the following:
+// 1) Create the API class.  (The API class will create the Impl class
+//    and attach it.)
+// 2) Ask the newly created API class for its contained Impl class.
+// 3) Return that Impl class.
+//
+AAFImplRoot * CreateImpl (const CLSID & rClassID);
+
+// Deletes the given Impl object.  Will make sure the associated API
+// class is also deleted.
+//
+// Implementation note:  This function does the following:
+// 1) Ask the given Impl object for its containing API object.
+// 2) Delete that containing API object.  (The API class will
+//    automatically delete the impl class.)
+//
+void DeleteImpl (AAFImplRoot *& pObj);
+
+#endif
