a47367ff49f6a9e9365e54be8b53fdc369f669d5
tbingham <>
2001-04-17 07:41:50 +0000

    Use AAFCONSTINT64 when using 64-bit constatnts (fix for gcc).

--- examples/com-api/InfoDumper/InfoDumper.cpp
+++ examples/com-api/InfoDumper/InfoDumper.cpp
@@ -1801,7 +1801,7 @@ HRESULT dumpPropertyValue (IAAFPropertyValueSP pPVal,
 		aafInt64 val;
 		checkResult(pTDI->GetInteger(pPVal, (aafMemPtr_t) &val, sizeof (val)));
 		
-		aafInt32 hi = (aafUInt32) ((val & 0xffffffff00000000) >> 32);
+		aafInt32 hi = (aafUInt32) ((val & AAFCONSTINT64(0xffffffff00000000)) >> 32);
 		aafInt32 lo = (aafInt32) val & 0xffffffff;
 		if (hi && ((hi != ~0) || (lo >= 0)))
 		  {
