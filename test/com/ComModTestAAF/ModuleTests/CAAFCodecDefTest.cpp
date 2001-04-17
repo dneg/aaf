0f192ef218876451a72bd6ebd23029000b1de98f
tbingham <>
2001-04-17 06:50:50 +0000

    Added VaryingValue, Pulldown, and Interpolation plugins

--- test/com/ComModTestAAF/ModuleTests/CAAFCodecDefTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFCodecDefTest.cpp
@@ -1,35 +1,13 @@
 // @doc INTERNAL
 // @com This file implements the module test for CAAFDefinitionObject
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
-
-
-
-
-
-
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
-
- 
 /***********************************************\
-*	Stub only.   Implementation not yet added	*
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
 \***********************************************/
-
-
-
 
 #include "CAAFCodecDef.h"
 #include "CAAFCodecDef.h"
