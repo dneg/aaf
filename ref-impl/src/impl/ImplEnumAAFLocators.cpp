1f27324a95ea7e3b4b2e6c8d2dd0ac80eabab955
tbingham <>
2001-04-17 04:49:37 +0000

    Impl EnumAAFLocators

---
+++ ref-impl/src/impl/ImplEnumAAFLocators.cpp
@@ -0,0 +1,125 @@
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+#ifndef __ImplAAFLocator_h__
+#include "ImplAAFLocator.h"
+#endif
+
+
+
+
+
+
+#ifndef __ImplEnumAAFLocators_h__
+#include "ImplEnumAAFLocators.h"
+#endif
+
+#include <assert.h>
+#include "aafErr.h"
+#include "AAFResult.h"
+#include "ImplAAFObjectCreation.h"
+
+extern "C" const aafClassID_t CLSID_EnumAAFLocators;
+
+ImplEnumAAFLocators::ImplEnumAAFLocators ()
+{
+	_current = 0;
+	_cEssenceDesc = NULL;
+}
+
+
+ImplEnumAAFLocators::~ImplEnumAAFLocators ()
+{}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFLocators::NextOne (ImplAAFLocator **ppLocator)
+{
+	aafNumSlots_t	cur = _current, siz;
+
+    XPROTECT()
+	{
+		CHECK(_cEssenceDesc->GetNumLocators (&siz));
+		if(cur < siz)
+		{
+			CHECK(_cEssenceDesc->GetNthLocator (cur, ppLocator));
+			_current = ++cur;
+		}
+		else
+			RAISE(AAFRESULT_NO_MORE_OBJECTS);
+	}
+	XEXCEPT
+	XEND
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFLocators::Next (aafUInt32  /*count*/,
+                           ImplAAFLocator ** /*ppLocators*/,
+                           aafUInt32 *  /*pFetched*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFLocators::Skip (aafUInt32  /*count*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFLocators::Reset ()
+{
+	_current = 0;
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFLocators::Clone (ImplEnumAAFLocators **ppEnum)
+{
+	ImplEnumAAFLocators		*result;
+
+	result = (ImplEnumAAFLocators *)CreateImpl(CLSID_EnumAAFLocators);
+	if(result != NULL)
+	{
+		result->_current = _current;
+		result->_cEssenceDesc = _cEssenceDesc;
+		*ppEnum = result;
+		return AAFRESULT_SUCCESS;
+	}
+	else
+		return AAFRESULT_NOMEMORY;
+}
+
+AAFRESULT
+    ImplEnumAAFLocators::SetEssenceDesc(ImplAAFEssenceDescriptor *pEDesc)
+{
+	_cEssenceDesc = pEDesc;
+	return AAFRESULT_SUCCESS;
+}
+
+
+extern "C" const aafClassID_t CLSID_EnumAAFLocators;
+
+OMDEFINE_STORABLE(ImplEnumAAFLocators, CLSID_EnumAAFLocators);
+
---
+++ ref-impl/src/impl/ImplEnumAAFLocators.h
@@ -0,0 +1,120 @@
+//@doc
+//@class    EnumAAFLocators | Implementation class for EnumAAFLocators
+#ifndef __ImplEnumAAFLocators_h__
+#define __ImplEnumAAFLocators_h__
+
+#include "OMStorable.h"
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+class ImplAAFLocator;
+
+
+
+
+
+
+
+#ifndef __ImplAAFRoot_h__
+#include "ImplAAFRoot.h"
+#endif
+
+#include "ImplAAFEssenceDescriptor.h"
+
+class ImplEnumAAFLocators : public ImplAAFRoot
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplEnumAAFLocators ();
+  virtual ~ImplEnumAAFLocators ();
+
+
+
+  //****************
+  // NextOne()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    NextOne
+		// @parm [out,retval] The Next Locator
+        (ImplAAFLocator ** ppLocator);
+
+  //****************
+  // Next()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Next
+        (// @parm [in] number of locators requested
+		 aafUInt32  count,
+
+		 // @parm [out, size_is(count), length_is(*pFetched)] array to receive locators
+		 ImplAAFLocator ** ppLocators,
+
+		 // @parm [out,ref] number of actual Locators fetched into ppLocators array
+         aafUInt32 *  pFetched);
+
+  //****************
+  // Skip()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Skip
+		// @parm [in] Number of elements to skip
+        (aafUInt32  count);
+
+  //****************
+  // Reset()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Reset ();
+
+
+  //****************
+  // Clone()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Clone
+		// @parm [out,retval] new enumeration
+        (ImplEnumAAFLocators ** ppEnum);
+
+
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplEnumAAFLocators)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplEnumAAFLocatorsTest.cpp.
+  static AAFRESULT test();
+
+public:
+// Internal to the toolkit
+AAFRESULT
+    SetEssenceDesc(ImplAAFEssenceDescriptor *pEDesc);
+
+private:
+	aafInt32					_current;
+	ImplAAFEssenceDescriptor	*_cEssenceDesc;
+
+};
+
+#endif // ! __ImplEnumAAFLocators_h__
+
