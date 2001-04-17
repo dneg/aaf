5773962e0147ffcc00a7b4ce2807c622fa0bc74d
tbingham <>
2001-04-17 04:58:10 +0000

    Initial Implementation: persistence of properties and Get/Set Methods

---
+++ ref-impl/src/impl/ImplAAFSelector.h
@@ -0,0 +1,102 @@
+//@doc
+//@class    AAFSelector | Implementation class for AAFSelector
+#ifndef __ImplAAFSelector_h__
+#define __ImplAAFSelector_h__
+
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+class ImplAAFSegment;
+
+class ImplEnumAAFSegments;
+
+
+
+#ifndef __ImplAAFSegment_h__
+#include "ImplAAFSegment.h"
+#endif
+
+
+class ImplAAFSelector : public ImplAAFSegment
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplAAFSelector ();
+
+protected:
+  virtual ~ImplAAFSelector ();
+
+public:
+
+
+  //****************
+  // GetSelectedSegment()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetSelectedSegment
+        // @parm [out] Returned the selected Segment object
+        (ImplAAFSegment ** ppSelSegment);
+
+  //****************
+  // SetSelectedSegment()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetSelectedSegment
+        // @parm [in] A Segment object
+        (ImplAAFSegment * pSelSegment);
+
+  //****************
+  // AppendAlternateSegment()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    AppendAlternateSegment
+        // @parm [in] Segment to append to the Alternate list of segments
+        (ImplAAFSegment * pSegment);
+
+
+  //****************
+  // GetNumAlternateSegments()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetNumAlternateSegments
+        // @parm [out] Number of Alternate Segments
+        (aafInt32 *  pNumSegments);
+
+  //****************
+  // EnumAlternateSegments()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    EnumAlternateSegments
+        // @parm [out, retval] Segment Enumeration
+        (ImplEnumAAFSegments ** ppEnum);
+
+  AAFRESULT GetNthSegment(aafUInt32 index, ImplAAFSegment** ppSegment);
+
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFSelector)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFSelectorTest.cpp.
+  static AAFRESULT test();
+private:
+	OMStrongReferenceProperty<ImplAAFSegment>		_selected;
+	OMStrongReferenceVectorProperty<ImplAAFSegment>	_alternates;
+};
+
+#endif // ! __ImplAAFSelector_h__
+
+
