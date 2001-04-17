e6458cb962d10bff487eac1b0a6463ddb68feeb8
tbingham <>
2001-04-17 07:32:16 +0000

    More bootstrap changes and preparation for transistion to two-roots

--- ref-impl/src/impl/ImplAAFTypeDefRename.h
+++ ref-impl/src/impl/ImplAAFTypeDefRename.h
@@ -150,6 +150,15 @@ public:
   virtual bool IsVariableArrayable () const;
   virtual bool IsStringable () const;
 
+
+
+  // override from OMStorable.
+  virtual const OMClassId& classId(void) const;
+
+  // Override callbacks from OMStorable
+  virtual void onSave(void* clientContext) const;
+  virtual void onRestore(void* clientContext) const;
+
 private:
   ImplAAFTypeDefSP BaseType () const;
 
