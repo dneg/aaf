fdcc5b667c40cc0caad02bc3cf6190ea1006a90d
tbingham <>
2001-04-17 07:25:51 +0000

    Comments only. Added comments with autoduck tags to OMPropertySet, OMStoredObject and OMProperty.

--- ref-impl/include/OM/OMPropertySet.h
+++ ref-impl/include/OM/OMPropertySet.h
@@ -1,3 +1,5 @@
+// @doc
+
 #ifndef OMPROPERTYSET_H
 #define OMPROPERTYSET_H
 
@@ -6,19 +8,42 @@
 class OMProperty;
 class OMStorable;
 
+  // @class Container class for <c OMProperty> objects.
 class OMPropertySet {
 public:
+  // @access Public members
 
+    // @cmember Constructor.
   OMPropertySet(void);
 
+    // @cmember Destructor.
   ~OMPropertySet(void);
 
+    // @cmember Get the <c OMProperty> associated with the property id
+    //          <p pid>.
   OMProperty* get(const int pid) const;
+
+    // @cmember Insert the <c OMProperty> <p property> into this
+    //          <c OMPropertySet>.
   void put(OMProperty* property);
+
+    // @cmember Iterate over the <c OMProperty> objects in this
+    //          <c OMPropertySet>.
   void iterate(size_t& context, OMProperty*& property) const;
+
+    // @cmember Does this <c OMPropertySet> contain an <c OMProperty>
+    //          with property id <p pid> ?
   bool contains(const int pid) const;
+
+    // @cmember The number of <c OMProperty> objects in this
+    //          <c OMPropertySet>.
   size_t count(void) const;
+
+    // @cmember Inform this <c OMPropertySet> that it contains the
+    //          <c OMProperty> objects of the <c OMStorable>
+    //          <p containingObject>.
   void setContainingObject(const OMStorable* containingObject);
+
 protected:
   static bool equal(const int& pida, const int& pidb);
 
