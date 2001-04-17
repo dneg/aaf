3472c9a93ab5a9737b8b9ae3413225c74fbb68a2
tbingham <>
2001-04-17 04:40:03 +0000

    Added ContainerDef CodecDef DefObject

---
+++ ref-impl/src/impl/ImplAAFCodecDef.h
@@ -0,0 +1,114 @@
+//@doc
+//@class    AAFCodecDef | Implementation class for AAFCodecDef
+#ifndef __ImplAAFCodecDef_h__
+#define __ImplAAFCodecDef_h__
+
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+
+class ImplAAFClassDef;
+
+class ImplEnumAAFCodecFlavours;
+
+
+
+#ifndef __ImplAAFPluggableDef_h__
+#include "ImplAAFPluggableDef.h"
+#endif
+
+
+class ImplAAFCodecDef : public ImplAAFPluggableDef
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplAAFCodecDef ();
+
+protected:
+  virtual ~ImplAAFCodecDef ();
+
+public:
+
+
+  //****************
+  // IsEssenceKindSupported()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    IsEssenceKindSupported
+        (// @parm [in,ref] The essence kind
+         aafUID_t *  pEssenceKind,
+
+         // @parm [out,retval] Is this type supported
+         aafBool*  pIsSupported);
+ 
+   //****************
+  // AppendEssenceKind()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    AppendEssenceKind
+        // @parm [in] The essence kind
+        (aafUID_t *  pEssenceKind);
+
+
+
+  //****************
+  // AreThereFlavours()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    AreThereFlavours
+        // @parm [out,retval] True if there are flavours of this codec
+        (aafBool *  pResult);
+
+
+  //****************
+  // GetFileDescriptorClass()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetFileDescriptorClass
+        // @parm [out] Returned file descriptor class object
+        (ImplAAFClassDef ** ppClass);
+
+
+  //****************
+  // SetFileDescriptorClass()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetFileDescriptorClass
+        // @parm [in] File descriptor class object
+        (ImplAAFClassDef * pClass);
+
+
+  //****************
+  // EnumCodecFlavours()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    EnumCodecFlavours
+        // @parm [out,retval] Codec flavour Enumeration
+        (ImplEnumAAFCodecFlavours ** ppEnum);
+  
+
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFCodecDef)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFCodecDefTest.cpp.
+  static AAFRESULT test();
+};
+
+#endif // ! __ImplAAFCodecDef_h__
+
+
