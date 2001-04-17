071a646372977d63d334a2c0976e697c3cbf2036
tbingham <>
2001-04-17 09:10:06 +0000

    ,

---
+++ ref-impl/src/impl/ImplAAFComponent.cpp
@@ -0,0 +1,115 @@
+
+
+#ifndef __ImplAAFDataDef_h__
+#include "ImplAAFDataDef.h"
+#endif
+
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+
+#ifndef __ImplAAFComponent_h__
+#include "ImplAAFComponent.h"
+#endif
+
+#include <assert.h>
+#include <AAFResult.h>
+
+
+ImplAAFComponent::ImplAAFComponent ():
+	_dataDef(	PID_IDENTIFICATION_DATADEF,	"dataDef"),
+	_length(	PID_IDENTIFICATION_LENGTH,	"length")
+{
+	_persistentProperties.put(   _dataDef.address());
+	_persistentProperties.put(   _length.address());
+}
+
+
+ImplAAFComponent::~ImplAAFComponent ()
+{}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFComponent::SetLength (aafLength_t *  length)
+{
+    AAFRESULT aafError = AAFRESULT_SUCCESS;
+	_length = *length;
+
+	return aafError;
+}
+
+
+/*************************************************************************
+ * Function: GetLength()
+ *
+ * 		This function returns the length of a component.  
+ *
+ * ReturnValue:
+ *		Error code (see below).
+ *
+ * Possible Errors:
+ *		Standard errors (see top of file).
+ *************************************************************************/
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFComponent::GetLength (aafLength_t * length)
+{
+    AAFRESULT aafError = AAFRESULT_SUCCESS;
+
+	*length = _length;
+	return aafError;
+}
+
+	
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFComponent::SetDataDef (aafUID_t *  /*datadef*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFComponent::GetDataDef (aafUID_t *  /*datadef*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+/*************************************************************************
+ * Private Function: SetNewProps()
+ *
+ *      This is an internal function that is called by functions that
+ *      create new components.  It sets the length and datakind property
+ *      values on the input component object.
+ *
+ *      This function supports both 1.x and 2.x files.
+ *
+ * Argument Notes:
+ *
+ * ReturnValue:
+ *		Error code (see below).
+ *
+ * Possible Errors:
+ *		Standard errors (see top of file).
+ *************************************************************************/
+AAFRESULT ImplAAFComponent::SetNewProps(
+        aafLength_t length,			/* IN - Length property value */
+        aafUID_t *dataDef)			/* IN - DataDef property value */
+{
+//	aafAssertValidFHdl(_file);
+//	aafAssert((datakind != NULL), _file, OM_ERR_INVALID_DATAKIND);
+	assert(dataDef != NULL);
+	
+	_dataDef = *dataDef;
+	_length	= length;
+
+	return(AAFRESULT_SUCCESS);
+}
+
+
