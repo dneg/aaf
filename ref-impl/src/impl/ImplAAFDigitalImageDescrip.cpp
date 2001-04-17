b4cb08e011827870ef4003792ce12e1d54305daf
tbingham <>
2001-04-17 04:19:23 +0000

    add implementations for CDCI and DigitalImage descriptors

---
+++ ref-impl/src/impl/ImplAAFDigitalImageDescrip.cpp
@@ -0,0 +1,353 @@
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+#include "ImplAAFDigitalImageDescriptor.h"
+
+#include <assert.h>
+#include <string.h>
+
+ImplAAFDigitalImageDescriptor::ImplAAFDigitalImageDescriptor ()
+:	_compression(PID_DIGITAL_IMAGE_DESC_COMPRESSION,					"Compression"),
+	_storedHeight(PID_DIGITAL_IMAGE_DESC_STOREDHEIGHT,					"Stored Height"),
+	_storedWidth(PID_DIGITAL_IMAGE_DESC_STOREDWIDTH,					"Stored Width"),
+	_sampledHeight(PID_DIGITAL_IMAGE_DESC_SAMPLEDHEIGHT,				"Sampled Height"),
+	_sampledWidth(PID_DIGITAL_IMAGE_DESC_SAMPLEDWIDTH,					"Sampled Width"),
+	_sampledXOffset(PID_DIGITAL_IMAGE_DESC_SAMPLEDXOFFSET,				"Sampled XOffset"),
+	_sampledYOffset(PID_DIGITAL_IMAGE_DESC_SAMPLEDYOFFSET,				"Sampled YOffset"),
+	_displayHeight(PID_DIGITAL_IMAGE_DESC_DISPLAYHEIGHT,				"Display Height"),
+	_displayWidth(PID_DIGITAL_IMAGE_DESC_DISPLAYWIDTH,					"Display Width"),
+	_displayXOffset(PID_DIGITAL_IMAGE_DESC_DISPLAYXOFFSET,				"Display XOffset"),
+	_displayYOffset(PID_DIGITAL_IMAGE_DESC_DISPLAYYOFFSET,				"Display YOffset"),
+	_frameLayout(PID_DIGITAL_IMAGE_DESC_FRAMELAYOUT,					"Frame Layout"),
+	_videoLineMap(PID_DIGITAL_IMAGE_DESC_VIDEOLINEMAP,					"Video Line Map"),
+	_imageAspectRatio(PID_DIGITAL_IMAGE_DESC_IMAGEASPECTRATIO,			"Image Aspect Ratio"),
+	_alphaTransparency(PID_DIGITAL_IMAGE_DESC_ALPHATRANSPARENCY,		"Alpha Transparency"),
+	_gamma(PID_DIGITAL_IMAGE_DESC_GAMMA,								"Gamma"),
+	_imageAlignmentFactor(PID_DIGITAL_IMAGE_DESC_IMAGEALIGNMENTFACTOR,	"Image Alignment Factor")
+{
+	_persistentProperties.put(_compression.address());
+	_persistentProperties.put(_storedHeight.address());
+	_persistentProperties.put(_storedWidth.address());
+	_persistentProperties.put(_sampledHeight.address());
+	_persistentProperties.put(_sampledWidth.address());
+	_persistentProperties.put(_sampledXOffset.address());
+	_persistentProperties.put(_sampledYOffset.address());
+	_persistentProperties.put(_displayHeight.address());
+	_persistentProperties.put(_displayWidth.address());
+	_persistentProperties.put(_displayXOffset.address());
+	_persistentProperties.put(_displayYOffset.address());
+	_persistentProperties.put(_frameLayout.address());
+	_persistentProperties.put(_videoLineMap.address());
+	_persistentProperties.put(_imageAspectRatio.address());
+	_persistentProperties.put(_alphaTransparency.address());
+	_persistentProperties.put(_gamma.address());
+	_persistentProperties.put(_imageAlignmentFactor.address());
+
+	// Initialize Required properties
+	_storedHeight = 0;
+	_storedWidth = 0;
+	//_frameLayout = kFullFrame;
+	//_imageAspectRatio = ;
+	//_videoLineMap =
+
+	// Initialize Optional properties
+	//_compression = ;
+	_sampledHeight = 0;
+	_sampledWidth = 0;
+	_sampledXOffset = 0;
+	_sampledYOffset = 0;
+	_displayHeight = 0;
+	_displayWidth = 0;
+	_displayXOffset = 0;
+	_displayYOffset = 0;
+	_alphaTransparency = kMinValueTransparent;
+	//_gamma = 0;
+	_imageAlignmentFactor = 0;
+}
+
+
+ImplAAFDigitalImageDescriptor::~ImplAAFDigitalImageDescriptor ()
+{}
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::SetCompression (aafUID_t*  pCompression)
+{
+	if (pCompression == NULL)
+		return AAFRESULT_NULL_PARAM;
+
+	_compression = *pCompression;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::SetStoredView (aafUInt32	StoredHeight, aafUInt32 StoredWidth)
+{
+	_storedHeight = StoredHeight;
+	_storedWidth = StoredWidth;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+ AAFRESULT STDMETHODCALLTYPE
+   ImplAAFDigitalImageDescriptor::SetSampledView (aafUInt32 SampledHeight,
+												  aafUInt32 SampledWidth,
+												  aafInt32 SampledXOffset,
+												  aafInt32 SampledYOffset)
+{
+	_sampledHeight = SampledHeight;
+	_sampledWidth = SampledWidth;
+	_sampledXOffset = SampledXOffset;
+	_sampledYOffset = SampledYOffset;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+ AAFRESULT STDMETHODCALLTYPE
+   ImplAAFDigitalImageDescriptor::SetDisplayView (aafUInt32 DisplayHeight,
+												  aafUInt32 DisplayWidth,
+												  aafInt32 DisplayXOffset,
+												  aafInt32 DisplayYOffset)
+{
+	_displayHeight = DisplayHeight;
+	_displayWidth = DisplayWidth;
+	_displayXOffset = DisplayXOffset;
+	_displayYOffset = DisplayYOffset;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::SetFrameLayout (aafFrameLayout_t FrameLayout)
+{
+	_frameLayout = FrameLayout;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::SetVideoLineMap (aafUInt32 numberElements, aafInt32* pVideoLineMap)
+{
+	if(pVideoLineMap == NULL)
+		return(AAFRESULT_NULL_PARAM);
+
+	_videoLineMap.setValue(pVideoLineMap, numberElements * sizeof(aafInt32));
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::SetImageAspectRatio (aafRational_t ImageAspectRatio)
+{
+	_imageAspectRatio = ImageAspectRatio;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::SetAlphaTransparency (aafAlphaTransparency_t AlphaTransparency)
+{
+	_alphaTransparency = AlphaTransparency;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::SetGamma (aafRational_t Gamma)
+{
+	_gamma = Gamma;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::SetImageAlignmentFactor (aafInt32 ImageAlignmentFactor)
+{
+	_imageAlignmentFactor = ImageAlignmentFactor;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::GetCompression (aafUID_t* pCompression)
+{
+	if (pCompression == NULL)
+		return AAFRESULT_NULL_PARAM;
+
+	*pCompression = _compression;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::GetStoredView (aafUInt32* pStoredHeight,
+												  aafUInt32* pStoredWidth)
+{
+	if ((pStoredHeight == NULL)|| (pStoredWidth == NULL))
+		return AAFRESULT_NULL_PARAM;
+
+	*pStoredHeight = _storedHeight;
+	*pStoredWidth = _storedWidth;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+ AAFRESULT STDMETHODCALLTYPE
+   ImplAAFDigitalImageDescriptor::GetSampledView (aafUInt32* pSampledHeight,
+												  aafUInt32* pSampledWidth,
+												  aafInt32* pSampledXOffset,
+												  aafInt32* pSampledYOffset)
+{
+	if ((pSampledHeight == NULL) || (pSampledWidth == NULL) ||
+		(pSampledXOffset == NULL) || (pSampledYOffset == NULL))
+		return(AAFRESULT_NULL_PARAM);
+
+	*pSampledHeight = _sampledHeight;
+	*pSampledWidth = _sampledWidth;
+	*pSampledXOffset = _sampledXOffset;
+	*pSampledYOffset = _sampledYOffset;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+ AAFRESULT STDMETHODCALLTYPE
+   ImplAAFDigitalImageDescriptor::GetDisplayView (aafUInt32* pDisplayHeight,
+												  aafUInt32* pDisplayWidth,
+												  aafInt32*  pDisplayXOffset,
+												  aafInt32* pDisplayYOffset)
+{
+	if ((pDisplayHeight == NULL) || (pDisplayWidth == NULL) ||
+		(pDisplayXOffset == NULL) || (pDisplayYOffset == NULL))
+		return(AAFRESULT_NULL_PARAM);
+
+	*pDisplayHeight = _displayHeight;
+	*pDisplayWidth = _displayWidth;
+	*pDisplayXOffset = _displayXOffset;
+	*pDisplayYOffset = _displayYOffset;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::GetFrameLayout (aafFrameLayout_t* pFrameLayout)
+{
+	if (pFrameLayout == NULL)
+		return(AAFRESULT_NULL_PARAM);
+
+	*pFrameLayout = _frameLayout;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::GetVideoLineMapSize (aafUInt32* pNumberElements)
+{
+	if(pNumberElements == NULL)
+		return(AAFRESULT_NULL_PARAM);
+
+	*pNumberElements = _videoLineMap.size() / sizeof(aafInt32);
+
+	return(AAFRESULT_SUCCESS); 
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::GetVideoLineMap (aafUInt32 numberElements, aafInt32* pVideoLineMap)
+{
+	if(pVideoLineMap == NULL)
+		return(AAFRESULT_NULL_PARAM);
+
+	if (_videoLineMap.size() /  sizeof(aafInt32) > numberElements)
+	  return AAFRESULT_SMALLBUF;
+
+	_videoLineMap.copyToBuffer(pVideoLineMap, _videoLineMap.size());
+
+	return(AAFRESULT_SUCCESS); 
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::GetImageAspectRatio (aafRational_t* pImageAspectRatio)
+{
+	if (pImageAspectRatio == NULL)
+		return(AAFRESULT_NULL_PARAM);
+
+	*pImageAspectRatio = _imageAspectRatio;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::GetAlphaTransparency (aafAlphaTransparency_t* pAlphaTransparency)
+{
+	if (pAlphaTransparency == NULL)
+		return(AAFRESULT_NULL_PARAM);
+
+	*pAlphaTransparency = _alphaTransparency;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::GetGamma (aafRational_t* pGamma)
+{
+	if (pGamma == NULL)
+		return(AAFRESULT_NULL_PARAM);
+
+	*pGamma = _gamma;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFDigitalImageDescriptor::GetImageAlignmentFactor (aafInt32* pImageAlignmentFactor)
+{
+	if (pImageAlignmentFactor == NULL)
+		return(AAFRESULT_NULL_PARAM);
+
+	*pImageAlignmentFactor = _imageAlignmentFactor;
+
+	return AAFRESULT_SUCCESS;
+}
+
+
+extern "C" const aafClassID_t CLSID_AAFDigitalImageDescriptor;
+
+OMDEFINE_STORABLE(ImplAAFDigitalImageDescriptor, CLSID_AAFDigitalImageDescriptor);
+
+// Cheat!  We're using this object's CLSID instead of object class...
+AAFRESULT STDMETHODCALLTYPE
+ImplAAFDigitalImageDescriptor::GetObjectClass(aafUID_t * pClass)
+{
+  if (! pClass)
+	{
+	  return AAFRESULT_NULL_PARAM;
+	}
+  memcpy (pClass, &CLSID_AAFDigitalImageDescriptor, sizeof (aafClassID_t));
+  return AAFRESULT_SUCCESS;
+}
+
+
---
+++ ref-impl/src/impl/ImplAAFDigitalImageDescriptor.h
@@ -0,0 +1,298 @@
+#ifndef __ImplAAFDigitalImageDescriptor_h__
+#define __ImplAAFDigitalImageDescriptor_h__
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+#include "OMStorable.h"
+#include "ImplAAFFileDescriptor.h"
+
+const int PID_DIGITAL_IMAGE_DESC_COMPRESSION			= 20;
+const int PID_DIGITAL_IMAGE_DESC_STOREDHEIGHT			= 21;
+const int PID_DIGITAL_IMAGE_DESC_STOREDWIDTH			= 22;
+const int PID_DIGITAL_IMAGE_DESC_SAMPLEDHEIGHT			= 23;
+const int PID_DIGITAL_IMAGE_DESC_SAMPLEDWIDTH			= 24;
+const int PID_DIGITAL_IMAGE_DESC_SAMPLEDXOFFSET			= 25;
+const int PID_DIGITAL_IMAGE_DESC_SAMPLEDYOFFSET			= 26;
+const int PID_DIGITAL_IMAGE_DESC_DISPLAYHEIGHT			= 27;
+const int PID_DIGITAL_IMAGE_DESC_DISPLAYWIDTH			= 28;
+const int PID_DIGITAL_IMAGE_DESC_DISPLAYXOFFSET			= 29;
+const int PID_DIGITAL_IMAGE_DESC_DISPLAYYOFFSET			= 30;
+const int PID_DIGITAL_IMAGE_DESC_FRAMELAYOUT			= 31;
+const int PID_DIGITAL_IMAGE_DESC_VIDEOLINEMAP			= 32;
+const int PID_DIGITAL_IMAGE_DESC_IMAGEASPECTRATIO		= 33;
+const int PID_DIGITAL_IMAGE_DESC_ALPHATRANSPARENCY		= 34;
+const int PID_DIGITAL_IMAGE_DESC_GAMMA					= 35;
+const int PID_DIGITAL_IMAGE_DESC_IMAGEALIGNMENTFACTOR	= 36;
+
+class ImplAAFDigitalImageDescriptor : public ImplAAFFileDescriptor
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplAAFDigitalImageDescriptor ();
+
+protected:
+  virtual ~ImplAAFDigitalImageDescriptor ();
+
+public:
+  //****************
+  // SetCompression()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetCompression
+		// @parm [in] Identifies the compression and format of compression information
+        (aafUID_t *  pCompression);
+
+  //****************
+  // SetStoredView()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetStoredView
+	    (// @parm [in] Number of pixels in vertical dimension of stored view.
+         aafUInt32  StoredHeight,
+
+		 // @parm [in] Number of pixels in horizontal dimension of stored view.
+		 aafUInt32  StoredWidth);
+
+  //****************
+  // SetSampledView()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetSampledView
+        (// @parm [in] Number of pixels in vertical dimension of sampled view.
+		 aafUInt32  SampledHeight,
+
+		 // @parm [in] Number of pixels in horizontal dimension of sampled view.
+		 aafUInt32  SampledWidth,
+
+		 // @parm [in] Number of pixels from top left corner of sampled view. Optional.
+         aafInt32  SampledXOffset,
+
+		 // @parm [in] Number of pixels from top left corner of sampled view. Optional.
+		 aafInt32  SampledYOffset);
+
+  //****************
+  // SetDisplayView()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetDisplayView
+        (// @parm [in] Number of pixels in vertical dimension of display view. Optional.
+		 aafUInt32  DisplayHeight,
+
+		 // @parm [in] Number of pixels in horizontal dimension of display view. Optional.
+		 aafUInt32  DisplayWidth,
+
+		 // @parm [in] Number of pixels from the top-left corner of the display view. Optional.
+         aafInt32  DisplayXOffset,
+
+		 // @parm [in] Number pixels from the top-left corner of the display view. Optional.
+		 aafInt32  DisplayYOffset);
+
+  //****************
+  // SetFrameLayout()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetFrameLayout
+		// @parm [in] layout of the frame
+        (aafFrameLayout_t  FrameLayout);
+
+  //****************
+  // SetVideoLineMap()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetVideoLineMap
+	    (// @parm [in] The number of elements in the array
+         aafUInt32  numberElements,
+
+		 // @parm [in, size_is(size)] Array to hold the Video Line Map information
+		 aafInt32 *  pVideoLineMap);
+
+  //****************
+  // SetImageAspectRatio()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetImageAspectRatio
+		// @parm [in] Ratio between horizontal and vertical size
+        (aafRational_t  ImageAspectRatio);
+
+  //****************
+  // SetAlphaTransparency()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetAlphaTransparency
+		// @parm [in] Optional.
+        (aafAlphaTransparency_t  AlphaTransparency);
+
+  //****************
+  // SetGamma()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetGamma
+		// @parm [in] Optional
+        (aafRational_t  Gamma);
+
+  //****************
+  // SetImageAlignmentFactor()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetImageAlignmentFactor
+		// @parm [in] Optional.
+        (aafInt32  ImageAlignmentFactor);
+
+  //****************
+  // GetCompression()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetCompression
+		// @parm [out] Identifies the compression and format of compression information
+        (aafUID_t *  pCompression);
+
+  //****************
+  // GetStoredView()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetStoredView
+	    (// @parm [out] Number of pixels in vertical dimension of stored view.
+         aafUInt32 *  pStoredHeight,
+
+		 // @parm [out] Number of pixels in horizontal dimension of stored view.
+		 aafUInt32 *  pStoredWidth);
+
+  //****************
+  // GetSampledView()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetSampledView
+        (// @parm [out] Number of pixels in vertical dimension of sampled view.
+		 aafUInt32 *  pSampledHeight,
+
+		 // @parm [out] Number of pixels in horizontal dimension of sampled view.
+		 aafUInt32 *  pSampledWidth,
+
+		 // @parm [out] Number of pixels from top left corner of sampled view. Optional.
+         aafInt32 *  pSampledXOffset,
+
+		 // @parm [out] Number of pixels from top left corner of sampled view. Optional.
+		 aafInt32 *  pSampledYOffset);
+
+  //****************
+  // GetDisplayView()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetDisplayView
+        (// @parm [out] Number of pixels in vertical dimension of display view. Optional.
+		 aafUInt32 *  pDisplayHeight,
+
+		 // @parm [out] Number of pixels in horizontal dimension of display view. Optional.
+		 aafUInt32 *  pDisplayWidth,
+
+		 // @parm [out] Number of pixels from the top-left corner of the display view. Optional.
+         aafInt32 *  pDisplayXOffset,
+
+		 // @parm [out] Number pixels from the top-left corner of the display view. Optional.
+		 aafInt32 *  pDisplayYOffset);
+
+  //****************
+  // GetFrameLayout()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetFrameLayout
+		// @parm [out] layout of the frame
+        (aafFrameLayout_t *  pFrameLayout);
+
+  //****************
+  // GetVideoLineMap()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetVideoLineMapSize
+	    (// @parm [out] The number of elements in the array
+         aafUInt32 *  pNumberElements);
+
+  //****************
+  // GetVideoLineMap()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetVideoLineMap
+	    (// @parm [in] The number of elements in the array
+         aafUInt32  numberElements,
+
+		 // @parm [out, size_is(numberElements)] Array to hold the Video Line Map information
+		 aafInt32 *  pVideoLineMap);
+
+  //****************
+  // GetImageAspectRatio()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetImageAspectRatio
+		// @parm [out] Ratio between horizontal and vertical size
+        (aafRational_t *  pImageAspectRatio);
+
+  //****************
+  // GetAlphaTransparency()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetAlphaTransparency
+		// @parm [out] Optional.
+        (aafAlphaTransparency_t *  pAlphaTransparency);
+
+  //****************
+  // GetGamma()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetGamma
+		// @parm [out] Optional.
+        (aafRational_t *  pGamma);
+
+  //****************
+  // GetImageAlignmentFactor()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetImageAlignmentFactor
+		// @parm [out] Optional.
+        (aafInt32 *  pImageAlignmentFactor);
+
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFDigitalImageDescriptor)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFDigitalImageDescriptorTest.cpp.
+  static AAFRESULT test();
+
+  // Return this objects stored object class.
+  virtual AAFRESULT STDMETHODCALLTYPE
+	GetObjectClass(aafUID_t * pClass);
+
+protected:
+	OMFixedSizeProperty<aafUID_t>				_compression;
+	OMFixedSizeProperty<aafUInt32>				_storedHeight;
+	OMFixedSizeProperty<aafUInt32>				_storedWidth;
+	OMFixedSizeProperty<aafUInt32>				_sampledHeight;
+	OMFixedSizeProperty<aafUInt32>				_sampledWidth;
+	OMFixedSizeProperty<aafInt32>				_sampledXOffset;
+	OMFixedSizeProperty<aafInt32>				_sampledYOffset;
+	OMFixedSizeProperty<aafUInt32>				_displayHeight;
+	OMFixedSizeProperty<aafUInt32>				_displayWidth;
+	OMFixedSizeProperty<aafInt32>				_displayXOffset;
+	OMFixedSizeProperty<aafInt32>				_displayYOffset;
+	OMFixedSizeProperty<aafFrameLayout_t>		_frameLayout;
+	OMVariableSizeProperty<aafInt32>			_videoLineMap;
+	OMFixedSizeProperty<aafRational_t>			_imageAspectRatio;
+	OMFixedSizeProperty<aafAlphaTransparency_t>	_alphaTransparency;
+	OMFixedSizeProperty<aafRational_t>			_gamma;
+	OMFixedSizeProperty<aafInt32>				_imageAlignmentFactor;
+};
+
+#endif // ! __ImplAAFDigitalImageDescriptor_h__
+
