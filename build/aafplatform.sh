fdd2089777341cb0be8a9fd70250f5dce716b0ba
tbingham <>
2001-04-17 11:56:05 +0000

    AAF/build/aafplatform.sh

---
+++ build/aafplatform.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+MACHINE=`(uname -m) 2>/dev/null` || MACHINE=Unknown
+OS=`(uname -s) 2>/dev/null` || OS=Unknown
+
+case "${MACHINE}:${OS}" in
+    *:IRIX*)
+	echo MipsIrix
+	exit 0;;
+
+    *:Linux)
+	echo ${MACHINE}Linux
+	exit 0;;
+
+    sun*:SunOS)
+	echo SunSunOS
+	exit 0;;
+esac
+
+echo Unknown
+exit 1
+
