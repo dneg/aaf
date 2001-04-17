696a86fedf750dc1b188b96b15731bc32fa8ca52
tbingham <>
2001-04-17 04:24:11 +0000

    Interface cleanup from Drop27 review pass 1

---
+++ ref-impl/include/AAFContainerDefs.h
@@ -0,0 +1,14 @@
+/***********************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
+\***********************************************/
+
+// Following are the UIDs of the well-known essence container definition objects in AAF.
+//
+
+const aafUID_t ContainerAAF = { 0x4313B571, 0xD8BA, 0x11d2, { 0x80, 0x9B, 0x00, 0x60, 0x08, 0x14, 0x3E, 0x6F } };
+const aafUID_t ContainerFile = { 0x4313B572, 0xD8BA, 0x11d2, { 0x80, 0x9B, 0x00, 0x60, 0x08, 0x14, 0x3E, 0x6F } };
