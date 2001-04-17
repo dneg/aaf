0f8d1c30ec12c639567536ad1fd6a37eb0f005b5
tbingham <>
2001-04-17 05:05:45 +0000

    AAF/meta/headergen/extEnumUid.cpp

---
+++ meta/headergen/extEnumUid.cpp
@@ -0,0 +1,37 @@
+#include <iostream.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#include "headerGenUtils.h"
+
+#define AAF_TYPE_DEFINITION_EXTENDIBLE_ENUMERATION_MEMBER( \
+   name, guid, container) \
+cout << endl; \
+cout << "const aafUID_t " << #name << " =" << endl; \
+guid
+
+#include "literalAuidDef.h"
+
+static void doFile (const char * moduleName)
+{
+  assert (moduleName);
+  cout << "#ifndef __" << moduleName << "_h__" << endl;
+  cout << "#define __" << moduleName << "_h__" << endl;
+  printCopyright (cout);
+  cout << endl;
+#include "AAFMetaDictionary.h"
+  cout << endl;
+  cout << "#endif // ! __" << moduleName << "_h__" << endl;
+}
+
+
+void main (int argc, char ** argv)
+{
+  char * moduleName = 0;
+  validateArgs (argc, argv, moduleName);
+  // If validateArgs() returned (without exit()ing) then moduleName
+  // must be properly filled in.
+  assert (moduleName);
+  doFile (moduleName);
+  exit (0);
+}
