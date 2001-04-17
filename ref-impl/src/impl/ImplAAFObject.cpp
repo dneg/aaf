5c8b83ff9a4083588016fde786245427c6c9a6f7
tbingham <>
2001-04-17 09:34:06 +0000

    Bugfix: comment out assert(_pProperties[i]) [line 109] since it incorrectly accessing uninitialized memory.

--- ref-impl/src/impl/ImplAAFObject.cpp
+++ ref-impl/src/impl/ImplAAFObject.cpp
@@ -106,7 +106,7 @@ AAFRESULT ImplPropertyCollection::Initialize
 	   i < _count;
 	   i++)
 	{
-	  assert (_pProperties[i]);
+//	  assert (_pProperties[i]); // danger will robinson: accessing initialized memory !
 	  _pProperties[i] = NULL;
 	}
 
