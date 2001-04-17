1cbeb425188fcf91fddb199346301dfb2e12e552
tbingham <>
2001-04-17 03:03:15 +0000

    More incr decr functions to static so that they can be ported and used in CAAFUnknown et. al

--- ref-impl/src/com-api/com-dll/CAAFServer.h
+++ ref-impl/src/com-api/com-dll/CAAFServer.h
@@ -23,6 +23,9 @@ public:
 	aafUInt32 GetLockCount();
 	aafUInt32 GetActiveObjectCount();
 
+	static aafUInt32 InterlockedIncrement(aafUInt32 *value);
+	static aafUInt32 InterlockedDecrement(aafUInt32 *value);
+
 protected:
 	aafUInt32 _lockCount;
 	aafUInt32 _activeCount;
