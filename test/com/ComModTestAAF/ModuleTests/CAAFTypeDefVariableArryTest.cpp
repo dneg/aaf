53754b842ccfd550bbb5458cfaa85451e298158d
tbingham <>
2001-04-17 07:44:49 +0000

    Removing CoCreateInstance from the module tests.

--- test/com/ComModTestAAF/ModuleTests/CAAFTypeDefVariableArryTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFTypeDefVariableArryTest.cpp
@@ -20,51 +20,10 @@
 #endif
 
 
-#include <iostream.h>
-
-// Temporarily necessary global declarations.
-extern "C" const CLSID CLSID_AAFTypeDefVariableArray; // generated
+#include "AAFResult.h"
 
 
 HRESULT CAAFTypeDefVariableArray::test()
 {
-  HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
-  IAAFTypeDefVariableArray *pObject = NULL;
-
-  try
-    {
-      // Attempt to create an AAFTypeDefVariableArray.
-      hr =  CoCreateInstance(
-                             CLSID_AAFTypeDefVariableArray,
-                             NULL, 
-                             CLSCTX_INPROC_SERVER, 
-                             IID_IAAFTypeDefVariableArray, (void **)&pObject);
-      if (FAILED(hr))
-        {
-          cerr << "CAAFTypeDefVariableArray::test...FAILED!";
-          cerr << hr;
-          cerr << "\tCoCreateInstance(&CLSID_AAFTypeDefVariableArray, NULL,"
-            " CLSCTX_INPROC_SERVER, &IID_IAAFTypeDefVariableArray, ...);" <<
-              endl;
-          return hr;
-        }
-
-      // module-specific tests go here
-
-      if (pObject)
-        pObject->Release();
-      return AAFRESULT_NOT_IMPLEMENTED;
-
-    }
-  catch (...)
-    {
-      cerr << "CAAFTypeDefVariableArray::test...Caught general C++"
-        " exception!" << endl; 
-    }
-
-  // Cleanup our object if it exists.
-  if (pObject)
-    pObject->Release();
-
-  return hr;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
