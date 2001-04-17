6162fa0e2dcd8b1ed2b41c9722d687e4292fa480
tbingham <>
2001-04-17 07:30:37 +0000

    Add two more weak references

--- ref-impl/src/impl/ImplAAFTypeDefRecord.h
+++ ref-impl/src/impl/ImplAAFTypeDefRecord.h
@@ -282,7 +282,7 @@ public:
 
          // @parm [in, size_is(numMembers)] array of member types to
 		 // be represented in this record type
-         aafUID_t ** pMemberTypeIDs,
+         ImplAAFTypeDef ** pMemberTypeIDs,
 
          // @parm [in, size_is(numMembers)] array of member names to
 		 // be represented in this enumerated  type
@@ -303,7 +303,8 @@ private:
   // types of members in this record
   //
   // BobT Note!!! This should be weak reference vector property...
-  OMVariableSizeProperty<aafUID_t> _memberTypes;
+  //OMVariableSizeProperty<aafUID_t> _memberTypes;
+  OMWeakReferenceVectorProperty<ImplAAFTypeDef> _memberTypes;
 
   // names of members in this record; stored as single aafCharacter
   // array with embedded nulls
