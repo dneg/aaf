56e255ef23111ab804749f705abeab493e85686b
tbingham <>
2001-04-17 07:26:04 +0000

    Add autoduck comments for OMStoredVectorIndex. Some code polishing.

--- ref-impl/src/OM/OMStoredVectorIndex.cpp
+++ ref-impl/src/OM/OMStoredVectorIndex.cpp
@@ -1,15 +1,17 @@
+// @doc OMINTERNAL
 #include "OMStoredVectorIndex.h"
 #include "OMAssertions.h"
 
 OMStoredVectorIndex::OMStoredVectorIndex(size_t size)
-: _highWaterMark(0), _size(size), _entries(0), _names(0)
+: _highWaterMark(0), _size(size), _entries(0), _keys(0)
 {
   TRACE("OMStoredVectorIndex::OMStoredVectorIndex");
 
-  _names = new OMUInt32[_size];
-  ASSERT("Valid heap pointer", _names != 0);
+  _keys = new OMUInt32[_size];
+  ASSERT("Valid heap pointer", _keys != 0);
+
   for (size_t i = 0; i < _size; i++) {
-    _names[i] = 0;
+    _keys[i] = 0;
   }
 }
 
@@ -17,39 +19,64 @@ OMStoredVectorIndex::~OMStoredVectorIndex(void)
 {
   TRACE("OMStoredVectorIndex::~OMStoredVectorIndex");
 
-  delete [] _names;
-  _names = 0;
+  delete [] _keys;
+  _keys = 0;
 }
 
+  // @mfunc The high water mark in the set of keys assigned to
+  //        this <c OMStoredVectorIndex>.
+  //   @rdesc The highest previously allocated key.
+  //   @this const
 OMUInt32 OMStoredVectorIndex::highWaterMark(void) const
 {
   TRACE("OMStoredVectorIndex::highWaterMark");
+
   return _highWaterMark;
 }
 
-void OMStoredVectorIndex::insert(size_t position, OMUInt32 name)
+  // @mfunc Insert a new element in this <c OMStoredVectorIndex>
+  //        at position <p position> with key <p key>.
+  //   @parm The position at which the new element should be inserted.
+  //   @parm The key assigned to the element.
+void OMStoredVectorIndex::insert(size_t position, OMUInt32 key)
 {
   TRACE("OMStoredVectorIndex::insert");
   PRECONDITION("Valid position", position < _size);
-  _names[position] = name;
+
+  // Assumes sequential insertion.
+
+  _keys[position] = key;
   _highWaterMark = _highWaterMark + 1;
   _entries = _entries + 1;
 }
 
+  // @mfunc The number of elements in this <c OMStoredVectorIndex>.
+  //   @rdesc The number of elements.
+  //   @this const
 size_t OMStoredVectorIndex::entries(void) const
 {
   TRACE("OMStoredVectorIndex::entries");
+
   return _entries;
 }
 
-void OMStoredVectorIndex::iterate(size_t& context, OMUInt32& name) const
+  // @mfunc Iterate over the elements in this <c OMStoredVectorIndex>.
+  //   @parm Iteration context. Set this to 0 to start with the
+  //         "first" element.
+  //   @parm The key of the "current" element.
+  //   @this const
+void OMStoredVectorIndex::iterate(size_t& context, OMUInt32& key) const
 {
   TRACE("OMStoredVectorIndex::iterate");
   PRECONDITION("Valid context", context < _size);
-  name = _names[context];
+
+  key = _keys[context];
   context = context + 1;
 }
 
+  // @mfunc Is this <c OMStoredVectorIndex> valid ?
+  //   @rdesc True if this is a valid <c OMStoredVectorIndex>, false otherwise.
+  //   @this const
 bool OMStoredVectorIndex::isValid(void) const
 {
   // No checks yet.
--- ref-impl/src/OM/OMStoredVectorIndex.h
+++ ref-impl/src/OM/OMStoredVectorIndex.h
@@ -1,23 +1,49 @@
+// @doc OMINTERNAL
 #ifndef OMSTOREDVECTORINDEX_H
 #define OMSTOREDVECTORINDEX_H
 
 #include "OMPortability.h"
 #include "OMTypes.h"
 
+  // @class The in-memory representation of the on-disk index for a
+  //        stored object vector.
 class OMStoredVectorIndex {
 public:
+  // @access Public members.
+
+    // @cmember Constructor.
   OMStoredVectorIndex(size_t size);
+
+    // @cmember Destructor.
   ~OMStoredVectorIndex(void);
+
+    // @cmember The high water mark in the set of keys assigned to
+    //          this <c OMStoredVectorIndex>.
+    // @this const
   OMUInt32 highWaterMark(void) const;
-  void insert(size_t position, OMUInt32 name);
+
+    // @cmember Insert a new element in this <c OMStoredVectorIndex>
+    //          at position <p position> with key <p key>.
+  void insert(size_t position, OMUInt32 key);
+
+    // @cmember The number of elements in this <c OMStoredVectorIndex>.
+    // @this const
   size_t entries(void) const;
-  void iterate(size_t& context, OMUInt32& name) const;
+
+    // @cmember Iterate over the elements in this <c OMStoredVectorIndex>.
+    // @this const
+  void iterate(size_t& context, OMUInt32& key) const;
+
+    // @cmember Is this <c OMStoredVectorIndex> valid ?
+    // @this const
   bool isValid(void) const;
+
 private:
+
   OMUInt32 _highWaterMark;
   size_t _size;
   size_t _entries;
-  OMUInt32* _names;
+  OMUInt32* _keys;
 };
 
 #endif
