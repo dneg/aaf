8f27d629fc64a13552c6d92a70be4141556d0d06
tbingham <>
2001-04-17 06:27:02 +0000

    Implemented a simple module test

---
+++ test/com/ComModTestAAF/ModuleTests/CAAFFileTest.cpp
@@ -0,0 +1,224 @@
+// @doc INTERNAL
+// @com This file implements the module test for CAAFDefinitionObject
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+
+#ifndef __CAAFFile_h__
+#include "CAAFFile.h"
+#endif
+
+#include <iostream.h>
+#include "AAFResult.h"
+
+static aafUID_t		newUID;
+
+static HRESULT CreateAAFFile(aafWChar * pFileName)
+{
+	IAAFSession *				pSession = NULL;
+	IAAFFile *					pFile = NULL;
+	IAAFHeader *				pHeader = NULL;
+	aafProductIdentification_t	ProductInfo;
+	HRESULT						hr;
+
+	ProductInfo.companyName = L"AAF Developers Desk";
+	ProductInfo.productName = L"Make AVR Example";
+	ProductInfo.productVersion.major = 1;
+	ProductInfo.productVersion.minor = 0;
+	ProductInfo.productVersion.tertiary = 0;
+	ProductInfo.productVersion.patchLevel = 0;
+	ProductInfo.productVersion.type = kVersionUnknown;
+	ProductInfo.productVersionString = NULL;
+	ProductInfo.productID = -1;
+	ProductInfo.platform = NULL;
+
+	hr = CoCreateInstance(CLSID_AAFSession,
+						   NULL, 
+						   CLSCTX_INPROC_SERVER, 
+						   IID_IAAFSession, 
+						   (void **)&pSession);
+	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+
+	hr = pSession->SetDefaultIdentification(&ProductInfo);
+	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+
+	hr = pSession->CreateFile(pFileName, kAAFRev1, &pFile);
+	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+  
+  	hr = pFile->GetHeader(&pHeader);
+	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+ 	
+	//Make the MOB
+	IAAFMob			*pMob;
+
+	// Create a Mob
+	hr = CoCreateInstance(CLSID_AAFMob,
+							NULL, 
+							CLSCTX_INPROC_SERVER, 
+							IID_IAAFMob, 
+							(void **)&pMob);
+
+
+	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+
+	newUID.Data1 = 0;
+	hr = pMob->SetMobID(&newUID);
+	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+
+	hr = pMob->SetName(L"MOBTest");
+	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+
+	hr = pHeader->AppendMob(pMob);
+ 	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+
+	hr = pFile->Close();
+ 	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+
+	hr = pSession->EndSession();
+ 	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+
+	pMob->Release();
+	if (pFile) pFile->Release();
+	if (pSession) pSession->Release();
+
+	return AAFRESULT_SUCCESS;
+}
+
+static HRESULT ReadAAFFile(aafWChar * pFileName)
+{
+	IAAFSession *				pSession = NULL;
+	IAAFFile *					pFile = NULL;
+	IAAFHeader *				pHeader = NULL;
+	aafProductIdentification_t	ProductInfo;
+	aafNumSlots_t				numMobs, n;
+	HRESULT						hr;
+	aafWChar					name[500];
+	aafUID_t					mobID;
+
+	ProductInfo.companyName = L"AAF Developers Desk. NOT!";
+	ProductInfo.productName = L"Make AVR Example. NOT!";
+	ProductInfo.productVersion.major = 1;
+	ProductInfo.productVersion.minor = 0;
+	ProductInfo.productVersion.tertiary = 0;
+	ProductInfo.productVersion.patchLevel = 0;
+	ProductInfo.productVersion.type = kVersionUnknown;
+	ProductInfo.productVersionString = NULL;
+	ProductInfo.productID = -1;
+	ProductInfo.platform = NULL;
+	  
+	hr = CoCreateInstance(CLSID_AAFSession,
+						   NULL, 
+						   CLSCTX_INPROC_SERVER, 
+						   IID_IAAFSession, 
+						   (void **)&pSession);
+	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+
+	hr = pSession->SetDefaultIdentification(&ProductInfo);
+	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+
+	hr = pSession->OpenReadFile(pFileName, &pFile);
+	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+  
+  	hr = pFile->GetHeader(&pHeader);
+	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+
+	hr = pHeader->GetNumMobs(kAllMob, &numMobs);
+	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+
+	IEnumAAFMobs *mobIter;
+
+    hr = pHeader->EnumAAFAllMobs (NULL, &mobIter);
+	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+	for(n = 0; n < numMobs; n++)
+	{
+		IAAFMob			*aMob;
+
+		hr = mobIter->NextOne (&aMob);
+		if (AAFRESULT_SUCCESS != hr)
+			return hr;
+		hr = aMob->GetName (name, sizeof(name));
+		if (AAFRESULT_SUCCESS != hr)
+			return hr;
+		hr = aMob->GetMobID (&mobID);
+		if (AAFRESULT_SUCCESS != hr)
+			return hr;
+		if (wcscmp( name, L"MOBTest") != 0)
+			return AAFRESULT_TEST_FAILED;
+		if ( memcmp(&mobID, &newUID, sizeof(mobID)) != 0)
+			return AAFRESULT_TEST_FAILED;
+	}
+
+	//!!! Problem deleting, let it leak -- 	delete mobIter;
+	hr = pFile->Close();
+	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+
+	hr = pSession->EndSession();
+	if (AAFRESULT_SUCCESS != hr)
+		return hr;
+
+	if (pHeader) pHeader->Release();
+	if (pFile) pFile->Release();
+	if (pSession) pSession->Release();
+
+	return 	AAFRESULT_SUCCESS;
+}
+
+HRESULT CAAFFile::test()
+{
+	HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
+	IAAFMob *pObject = NULL;
+ 	aafWChar * pFileName = L"FileTest.aaf";
+
+	try
+	{
+		hr = CreateAAFFile(	pFileName );
+
+		hr = ReadAAFFile( pFileName );
+	}
+	catch (...)
+	{
+	  cerr << "CAAFMob::test...Caught general C++"
+		" exception!" << endl; 
+	  hr = AAFRESULT_TEST_FAILED;
+	}
+
+  // Cleanup our object if it exists.
+  if (pObject)
+	pObject->Release();
+
+  return hr;
+}
+
+
+
+
+
+
+
+
+
+
