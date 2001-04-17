b3ab76219f6589a05d8d38eddbc76a1078914d3e
tbingham <>
2001-04-17 07:18:40 +0000

    More setValueAt(0, index) -> clearValueAt(index).

--- ref-impl/include/OM/OMReferenceVector.h
+++ ref-impl/include/OM/OMReferenceVector.h
@@ -63,6 +63,10 @@ public:
   ReferencedObject* setValueAt(const ReferencedObject* object,
                                const size_t index);
 
+    // @cmember Set the value of this <c OMReferenceVector>
+    //          at position <p index> to 0.
+  ReferencedObject* clearValueAt(const size_t index);
+
     // @cmember The value of this <c OMReferenceVector> at position <p index>.
   ReferencedObject* valueAt(const size_t index) const;
 
