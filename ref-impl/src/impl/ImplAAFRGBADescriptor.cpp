02debe2a53bdb58ec2dd3615e0e3e45c08cc575f
tbingham <>
2001-04-17 04:24:56 +0000

    Correct spelling of palette.

--- ref-impl/src/impl/ImplAAFRGBADescriptor.cpp
+++ ref-impl/src/impl/ImplAAFRGBADescriptor.cpp
@@ -9,7 +9,7 @@
 * notice appear in all copies of the software and related documentation,
 * and (ii) the name Avid Technology, Inc. may not be used in any
 * advertising or publicity relating to the software without the specific,
-*  prior written permission of Avid Technology, Inc.
+* prior written permission of Avid Technology, Inc.
 *
 * THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
 * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
@@ -136,7 +136,7 @@ AAFRESULT STDMETHODCALLTYPE
 
 
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFRGBADescriptor::SetPallete (
+    ImplAAFRGBADescriptor::SetPalette (
       aafUInt32  numberElements,
       aafUInt8*  pPalette)
 {
