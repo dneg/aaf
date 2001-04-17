b3b4609fc379f7e8d87fa1d5802acbb8acb1a858
tbingham <>
2001-04-17 02:50:17 +0000

    Update the definition of the macro EXTERN(type) extern type to use extern "C" type if __cplusplus is defined. Without this change the jpeglib.h file could not be included in our c++ jpeg code files.

--- ref-impl/libjpeg/jmorecfg.h
+++ ref-impl/libjpeg/jmorecfg.h
@@ -187,7 +187,12 @@ typedef unsigned int JDIMENSION;
 /* a function referenced thru EXTERNs: */
 #define GLOBAL(type)		type
 /* a reference to a GLOBAL function: */
+/* Updated for inclusion by C++ files. */
+#ifdef __cplusplus
+#define EXTERN(type)		extern "C" type
+#else
 #define EXTERN(type)		extern type
+#endif
 
 
 /* This macro is used to declare a "method", that is, a function pointer.
