72789be079989f86c3bd80ff4102af51c43682e2
tbingham <>
2001-04-17 04:21:05 +0000

    1. Added proper support for creating objects with delegating (aggregated object) IUnknown and non-delegating IUnknown (stand-alone object) implemenation.

--- ref-impl/src/com-api/com-dll/CAAFUnknown.h
+++ ref-impl/src/com-api/com-dll/CAAFUnknown.h
@@ -37,7 +37,7 @@
 // IUnknown super-class that will support aggregation.
 // @end
 //
-//	Modified 1998-06-30 by TRR for use by AAF.
+//  Modified 1998-06-30 by TRR for use by AAF.
 //
 //=--------------------------------------------------------------------------=
 
@@ -51,7 +51,7 @@
 // AAF_DECLARE_STANDARD_UNKNOWN
 //=--------------------------------------------------------------------------=
 // @func
-//	Macro to insert standard implementation if delegating <i IUnknown> interface.
+//  Macro to insert standard implementation if delegating <i IUnknown> interface.
 //
 // @comm
 // All objects that are going to inherit from CAAFUnknown for their IUnknown
@@ -60,18 +60,18 @@
 // macro. (See the example for <f AAF_DECLARE_FACTORY>)
 //
 #define AAF_DECLARE_STANDARD_UNKNOWN() \
-	STDMETHOD(QueryInterface)(REFIID riid, void **ppvObjOut) \
-	{ \
-		return ExternalQueryInterface(riid, ppvObjOut); \
-	} \
-	STDMETHOD_(ULONG, AddRef)(void) \
-	{ \
-		return ExternalAddRef(); \
-	} \
-	STDMETHOD_(ULONG, Release)(void) \
-	{ \
-		return ExternalRelease(); \
-	} 
+  STDMETHOD(QueryInterface)(REFIID riid, void **ppvObjOut) \
+  { \
+    return ExternalQueryInterface(riid, ppvObjOut); \
+  } \
+  STDMETHOD_(ULONG, AddRef)(void) \
+  { \
+    return ExternalAddRef(); \
+  } \
+  STDMETHOD_(ULONG, Release)(void) \
+  { \
+    return ExternalRelease(); \
+  } 
 
 
 
@@ -79,10 +79,10 @@
 // AAF_DECLARE_FACTORY
 //=--------------------------------------------------------------------------=
 // @func
-//	Macro to declare the interface for object class factory.
+//  Macro to declare the interface for object class factory.
 //
 // @comm
-//	All COM objects will need to use this macro within their class declaration
+//  All COM objects will need to use this macro within their class declaration
 // so that <f DllGetClassObject> can correctly instantiate an appropriate 
 // <c CAAFClassFactory> that will use the defined factory method to implement
 // the <i IClassFactory> interface. This macro is usually embedded within the 
@@ -91,52 +91,52 @@
 // @ex The Following example will declare the factory method of Foo |
 // // module CFoo.h
 // class CFoo : 
-// 	public IFoo,
-// 	public CAAFUnknown
+//   public IFoo,
+//   public CAAFUnknown
 // {
 // public:
-// 	CFoo(IUnknown *pUnkOuter);
-// 	virtual ~CFoo();
+//   CFoo(IUnknown *pUnkOuter);
+//   virtual ~CFoo();
 // 
-// 	// Declare the factory for this class.
-// 	AAF_DECLARE_FACTORY();
-// 	
-// 	// Declare the standard delegating unknown methods
-// 	AAF_DECLARE_STANDARD_UNKNOWN();
-// 	
-// 	// IFoo methods
-// 	//...
+//   // Declare the factory for this class.
+//   AAF_DECLARE_FACTORY();
+//   
+//   // Declare the standard delegating unknown methods
+//   AAF_DECLARE_STANDARD_UNKNOWN();
+//   
+//   // IFoo methods
+//   //...
 // };
 //
 // 
 // // will generate the following method declarations 
 // class CFoo : 
-// 	public IFoo,
-// 	public CAAFUnknown
+//   public IFoo,
+//   public CAAFUnknown
 // {
 // public:
-// 	CFoo(IUnknown *pUnkOuter);
-// 	virtual ~CFoo();
+//   CFoo(IUnknown *pUnkOuter);
+//   virtual ~CFoo();
 // 
-// 	// Declare the factory for this class.
-// 	static HRESULT AAFCreate(IUnknown *pUnkOuter, void **ppvObjOut);
+//   // Declare the factory for this class.
+//   static HRESULT AAFCreate(IUnknown *pUnkOuter, void **ppvObjOut);
 //
-// 	// Declare the standard delegating unknown methods
-// 	STDMETHOD(QueryInterface)(REFIID riid, void **ppvObjOut)
-// 	{
-// 		return ExternalQueryInterface(riid, ppvObjOut);
-// 	}
-// 	STDMETHOD_(ULONG, AddRef)(void)
-// 	{
-// 		return ExternalAddRef();
-// 	}
-// 	STDMETHOD_(ULONG, Release)(void)
-// 	{
-// 		return ExternalRelease();
-// 	} 
-// 	
-// 	// IFoo methods
-// 	//...
+//   // Declare the standard delegating unknown methods
+//   STDMETHOD(QueryInterface)(REFIID riid, void **ppvObjOut)
+//   {
+//     return ExternalQueryInterface(riid, ppvObjOut);
+//   }
+//   STDMETHOD_(ULONG, AddRef)(void)
+//   {
+//     return ExternalAddRef();
+//   }
+//   STDMETHOD_(ULONG, Release)(void)
+//   {
+//     return ExternalRelease();
+//   } 
+//   
+//   // IFoo methods
+//   //...
 // };
 // 
 #define AAF_DECLARE_FACTORY() \
@@ -146,10 +146,10 @@
 // AAF_DEFINE_FACTORY
 //=--------------------------------------------------------------------------=
 // @func
-//	Macro to define the implementation for object class factory.
+//  Macro to define the implementation for object class factory.
 //
 // @parm 
-//	Base name of the com class implemenation. This should be the expected
+//  Base name of the com class implemenation. This should be the expected
 // COM co-class name. 
 //
 // @comm
@@ -166,60 +166,59 @@
 // // will generate the following method definition
 // HRESULT CFoo::AAFCreate(IUnknown *pUnkOuter, void **ppvObjOut)
 // {
-// 	*ppvObjOut = NULL;
-// 	CFoo *pFoo new CFoo(pUnkOuter);
-// 	if (NULL == pFoo)
-// 		return E_OUTOFMEMORY;
-// 	*ppvObjOut = static_cast<IFoo *>(pFoo);
-// 	((IUnknown *)(*ppvObjOut))->AddRef();
-// 	return S_OK;
+//   *ppvObjOut = NULL;
+//   CFoo *pFoo new CFoo(pUnkOuter);
+//   if (NULL == pFoo)
+//     return E_OUTOFMEMORY;
+//   *ppvObjOut = static_cast<IFoo *>(pFoo);
+//   ((IUnknown *)(*ppvObjOut))->AddRef();
+//   return S_OK;
 // }
 //
 #define AAF_DEFINE_FACTORY(xclass) \
-	HRESULT C##xclass##::COMCreate(IUnknown *pUnkOuter, void **ppvObjOut) \
-	{ \
-		*ppvObjOut = NULL; \
-		C##xclass *p##xclass = new C##xclass(pUnkOuter); \
-		if (NULL == p##xclass) \
-			return E_OUTOFMEMORY; \
-		*ppvObjOut = static_cast<I##xclass *>(p##xclass); \
-		((IUnknown *)(*ppvObjOut))->AddRef(); \
-		return S_OK; \
-	}
+  HRESULT C##xclass##::COMCreate(IUnknown *pUnkOuter, void **ppvObjOut) \
+  { \
+    *ppvObjOut = NULL; \
+    C##xclass *p##xclass = new C##xclass(pUnkOuter); \
+    if (NULL == p##xclass) \
+      return E_OUTOFMEMORY; \
+    *ppvObjOut = static_cast<CAAFUnknown *>(p##xclass); \
+    return S_OK; \
+  }
 
  
 //=--------------------------------------------------------------------------=
 // AAF_DECLARE_CONCRETE
 //=--------------------------------------------------------------------------=
 // @func
-//	Macro to declare the interface for object class factory.
+//  Macro to declare the interface for object class factory.
 //
 // @comm
-//	All COM objects will need to use this macro within their class declaration
+//  All COM objects will need to use this macro within their class declaration
 // so that <f DllGetClassObject> can correctly instantiate an appropriate 
 // <c CAAFClassFactory> that will use the defined factory method to implement
 // the <i IClassFactory> interface. Note: this is just a short cut for calling
 // <f AAF_DECLARE_STANDARD_UNKNOWN>(); followed by <f AAF_DECLARE_FACTORY>();
 //
 #define AAF_DECLARE_CONCRETE() \
-	AAF_DECLARE_STANDARD_UNKNOWN() \
-	AAF_DECLARE_FACTORY() 
+  AAF_DECLARE_STANDARD_UNKNOWN() \
+  AAF_DECLARE_FACTORY() 
 
 //=--------------------------------------------------------------------------=
 // AAF_DEFINE_CONCRETE
 //=--------------------------------------------------------------------------=
 // @func
-//	Macro to define the implementation for object class factory.
+//  Macro to define the implementation for object class factory.
 //
 // @comm
-//	All COM objects will need to use this macro within their class definition
+//  All COM objects will need to use this macro within their class definition
 // so that <f DllGetClassObject> can correctly instantiate an appropriate 
 // <c CAAFClassFactory> that will use the defined factory method to implement
 // the <i IClassFactory> interface.Note: this is just a alias for calling
 // <f AAF_DEFINE_FACTORY>(xclass); .
 //
 #define AAF_DEFINE_CONCRETE(xclass) \
-	AAF_DEFINE_FACTORY(xclass) 
+  AAF_DEFINE_FACTORY(xclass) 
 
 
 //=--------------------------------------------------------------------------=
@@ -239,113 +238,114 @@ class CAAFUnknown
 {
 //@access Public Members
 public:
-	//@cmember Constructor, create with controlling unknown.
-    CAAFUnknown(IUnknown *pUnkOuter);
-	
-	//@cmember Destructor
-    virtual ~CAAFUnknown();
+  //@cmember Constructor, create with controlling unknown.
+  CAAFUnknown(IUnknown *pUnkOuter);
+  
+  //@cmember Destructor
+  virtual ~CAAFUnknown();
 
 //@access Protected Members
-    // these are all protected so that classes that inherit from this can
-    // at get at them.
-    //
 protected:
-	// @cmember,mfunc Delagates <om IUnknown.QueryInterface> to controlling unknown.
-    HRESULT ExternalQueryInterface(REFIID riid, void **ppvObjOut)
-	{
-        return m_pUnkOuter->QueryInterface(riid, ppvObjOut);
-    }
+  // @cmember,mfunc Delagates <om IUnknown.QueryInterface> to controlling unknown.
+  HRESULT ExternalQueryInterface(REFIID riid, void **ppvObjOut)
+  {
+    return m_pUnkOuter->QueryInterface(riid, ppvObjOut);
+  }
 
-	// @cmember,mfunc Delagates <om IUnknown.AddRef> to controlling unknown.
-    ULONG ExternalAddRef(void)
-	{
-        return m_pUnkOuter->AddRef();
-    }
+  // @cmember,mfunc Delagates <om IUnknown.AddRef> to controlling unknown.
+  ULONG ExternalAddRef(void)
+  {
+    return m_pUnkOuter->AddRef();
+  }
 
-	// @cmember,mfunc Delagates <om IUnknown.Release> to controlling unknown.
-    ULONG ExternalRelease(void)
-	{
-        return m_pUnkOuter->Release();
-    }
+  // @cmember,mfunc Delagates <om IUnknown.Release> to controlling unknown.
+  ULONG ExternalRelease(void)
+  {
+    return m_pUnkOuter->Release();
+  }
 
 
-    // @cmember,mfunc People should use this during creation to return their private
-    // unknown
-    //
-    inline IUnknown *GetPrivateUnknown (void)
-	{
-        return &m_UnkPrivate;
-    }
+  // @cmember,mfunc People should use this during creation to return their private
+  // unknown
+  //
+  inline IUnknown *GetPrivateUnknown (void)
+  {
+    return &m_UnkPrivate;
+  }
 
-    // @cmember Called by <om CAAFUnknown::QueryInterface>.
-	// @comm This method is overridden by all derived classes to check for 
-	//		 specific inteface implmentations.
-	virtual HRESULT InternalQueryInterface(REFIID riid, void **ppvObjOut);
-	
-	// @cmember,mfunc
-	// @comm Allows access to the non-aggregating IUnknown implementation
-	// for this class.
-	ULONG InternalAddRef(void)
-	{
-		return GetPrivateUnknown()->AddRef();
-	}
+  // @cmember Called by <om CAAFUnknown::QueryInterface>.
+  // @comm This method is overridden by all derived classes to check for 
+  // specific inteface implmentations.
+  STDMETHOD(InternalQueryInterface)(REFIID riid, void **ppvObjOut);
+  
+  // @cmember,mfunc
+  // @comm Allows access to the non-aggregating IUnknown implementation
+  // for this class.
+  STDMETHOD_(ULONG, InternalAddRef)(void);
 
-	// @cmember,mfunc
-	// @comm Allows access to the non-aggregating IUnknown implementation
-	// for this class.
-	ULONG InternalRelease(void)
-	{
-		return GetPrivateUnknown()->Release();
-	}
+  // @cmember,mfunc
+  // @comm Allows access to the non-aggregating IUnknown implementation
+  // for this class.
+  STDMETHOD_(ULONG, InternalRelease)(void);
 
-	//@cmember Outer controlling Unknown
-    IUnknown *m_pUnkOuter;
-
-	//@access Private Members
+  //@access Private Members
 private:
 
-	// @class CAAFPrivateUnknown |
-	// The inner, private unknown implementation is for the aggregator
-    // to control the lifetime of this object, and for those cases where
-    // this object isn't aggregated.
-    //
-    class CAAFPrivateUnknown : public IUnknown
-	{
-		//@access Public Members
-    public:
-		//@cmember Implements non-delegating <om IUnknown.QueryInterface>.
-        STDMETHOD(QueryInterface)(REFIID riid, void **ppvObjOut);
-		//@cmember Implements non-delegating <om IUnknown.AddRef>.
-        STDMETHOD_(ULONG, AddRef)(void);
-		//@cmember Implements non-delegating <om IUnknown.Release>.
-        STDMETHOD_(ULONG, Release)(void);
+  // @class CAAFPrivateUnknown |
+  // The inner, private unknown implementation is for the aggregator
+  // to control the lifetime of this object, and for those cases where
+  // this object isn't aggregated.
+  //
+  class CAAFPrivateUnknown : public IUnknown
+  {
+    //@access Public Members
+  public:
+    //@cmember Implements non-delegating <om IUnknown.QueryInterface>.
+    STDMETHOD(QueryInterface)(REFIID riid, void **ppvObjOut)
+		{
+			return This()->InternalQueryInterface(riid, ppvObjOut);
+		}
 
-        // @cmember Constructor, initializes the reference count.
-        CAAFPrivateUnknown()
-			: m_cRef(0) {}
+    //@cmember Implements non-delegating <om IUnknown.AddRef>.
+    STDMETHOD_(ULONG, AddRef)(void)
+		{
+			return This()->InternalAddRef();
+		}
 
-		//@access Private Members
+    //@cmember Implements non-delegating <om IUnknown.Release>.
+    STDMETHOD_(ULONG, Release)(void)
+		{
+			return This()->InternalRelease();
+		}
+
+
+    //@access Private Members
     private:
-		//@cmember Return pointer to outer object's this pointer.
-        CAAFUnknown *This();
+      //@cmember Return pointer to outer object's this pointer.
+      CAAFUnknown *This();
+  };
 
-		//@cmember Current reference count for this object.
-        ULONG m_cRef;
-    };
+  // so they can reference themselves in CAAFUnknown from pMainUnknown()
+  //
+  friend class CAAFPrivateUnknown;
 
-	// cmember Nested class instance that implements the non-delegating <i IUnknown> interface.
-	CAAFPrivateUnknown m_UnkPrivate;
+  // so the class factory's CreateInstance method can call the private
+  // nondelegating methods.
+  //
+  friend class CAAFClassFactory;
 
-    // so they can reference themselves in CAAFUnknown from pMainUnknown()
-    //
-    friend class CAAFPrivateUnknown;
 
-    // by overriding this, people inheriting from this unknown can implement
-    // additional interfaces; declared as private here so they have to use their
-    // own version.
-    //
+	// Member data:
+
+	//@cmember Current reference count for this object.
+  ULONG m_cRef;
+
+  //@cmember Outer controlling Unknown
+  IUnknown *m_pUnkOuter;
+
+  //@cmember Nested class instance that implements the non-delegating <i IUnknown> interface.
+  CAAFPrivateUnknown m_UnkPrivate;
 };
-
 
 #endif // __CAAFUnknown_h__
 
