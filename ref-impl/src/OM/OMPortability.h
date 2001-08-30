f86336e25da2e4705e08d22ca7c193f42c295343
tbingham <>
2001-08-30 15:56:52 +0000

    Add OM_OS_* symbols identifying the host operating system.

--- ref-impl/src/OM/OMPortability.h
+++ ref-impl/src/OM/OMPortability.h
@@ -28,25 +28,34 @@
 //         of the Object Manager.
 //   @mauthor Tim Bingham | tjb | Avid Technology, Inc.
 
-// Figure out the compiler and define a symbol of the
-// form OM_COMPILER_<compiler name>_<processor>_<operating system>
+// Figure out the compiler and define symbols of the form
+// OM_COMPILER_<compiler name>_<processor>_<operating system>
+// OM_OS_<operating system>
 //
 #if defined (_MSC_VER) && defined(_M_IX86) && defined(_WIN32)
 #define OM_COMPILER_MSC_INTEL_WINDOWS
+#define OM_OS_WINDOWS
 #elif defined(__MWERKS__) && defined(__POWERPC__) && defined(macintosh)
 #define OM_COMPILER_MWERKS_PPC_MACOS
+#define OM_OS_MACOS
 #elif defined(__GNUC__) && defined(__mips__) && defined(__sgi__)
 #define OM_COMPILER_GCC_MIPS_SGI
+#define OM_OS_UNIX
 #elif defined(__GNUC__) && defined(__i386__) && defined(__linux__)
 #define OM_COMPILER_GCC_INTEL_LINUX
+#define OM_OS_UNIX
 #elif defined(__GNUC__) && defined(__i386__) && defined(__FreeBSD__)
 #define OM_COMPILER_GCC_INTEL_FREEBSD
+#define OM_OS_UNIX
 #elif defined(mips) && defined(sgi)
 #define OM_COMPILER_SGICC_MIPS_SGI
+#define OM_OS_UNIX
 #elif defined(__GNUC__) && defined(__ppc__) && defined(__APPLE__)
 #define OM_COMPILER_GCC_PPC_MACOSX
+#define OM_OS_MACOSX
 #elif defined(__GNUC__) && defined(__sparc__) && defined(__sun__)
 #define OM_COMPILER_GCC_SPARC_SUNOS
+#define OM_OS_UNIX
 #else
 #error "Unknown compiler"
 #endif
