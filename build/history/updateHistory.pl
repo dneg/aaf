7b4b059c40748631dff032113e5082790a2ad26a
whoopy <>
2001-09-08 13:25:53 +0000

    inital attempt at script to extract CVS history for HTML summary

---
+++ build/history/updateHistory.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+# Program to summarise CVS history in a format suitable for HTML build page
+# (c) Copyright AAF Association Inc 2001
+#
+
+
+use Time::Local;
+
+$cvs2cl = $ENV{CVS2CL} or die "need Environment VAR CVS2CL to point at cvs2cl.pl";
+
+
+# get current timestamp into CVS style '2001-8-22 12:15:00'
+
+sub getTimeStamp
+	{
+	my ($sec,$min,$hour,$day,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+	$mon++;
+	$year += 1900;
+	$timeStamp = sprintf "%d/%02d/%02d %02d:%02d:%02d", ($year, $mon, $day, $hour, $min, $sec);
+	}
+	
+sub writeTimeStamp
+	{
+	local($timestamp) = @_[0];
+	open(TSTAMP, "> .timestamp") or die "Can't create .timestamp file";
+	print TSTAMP "$timestamp\n";
+	close TSTAMP;
+	}
+
+# Fetch last timestamp
+
+sub getLastTimeStamp
+	{
+	if(!open(TSTAMP, ".timestamp"))
+		{
+		print "No existing Timestamp\n";
+		$lastTimeStamp = "2000-1-1 00:00:00";
+		}
+	else {
+		($lastTimeStamp) = <TSTAMP>;
+		chomp $lastTimeStamp;
+		close (TSTAMP);
+		}
+	}
+
+
+#-----------------------------
+
+getLastTimeStamp();
+getTimeStamp();
+
+print "Current Time $timeStamp\n";
+print "Last    Time $lastTimeStamp\n";
+
+# run the commmand
+
+$lastTimeStamp = "-d\'\>$lastTimeStamp\'";
+print "$cvs2cl -l $lastTimeStamp\n";
+exec($cvs2cl, "-l", $lastTimeStamp) == 0 or die "Can't run cvs2cl";
+
+	open(LOG, "ChangeLog");
+	(@logfile) = <LOG>;
+	close (LOG);
+
+
+# Save current timestamp
+
+writeTimeStamp($timeStamp);
