cd140186ec67d6420bed5e7e5c245d4f4885bd11
tbingham <>
2001-11-29 21:48:10 +0000

    Add implementation for OMPageCache.

--- ref-impl/include/OM/OMPageCache.h
+++ ref-impl/include/OM/OMPageCache.h
@@ -24,5 +24,110 @@
 // @author Tim Bingham | tjb | Avid Technology, Inc. | OMPageCache
 #ifndef OMPAGECACHE_H
 #define OMPAGECACHE_H
-// Nothing yet.
+
+#include "OMDataTypes.h"
+#include "OMSet.h"
+#include "OMSetIterator.h"
+#include "OMList.h"
+#include "OMListIterator.h"
+
+  // @class Page caches.
+  //   @cauthor Tim Bingham | tjb | Avid Technology, Inc.
+class OMPageCache {
+public:
+  // @access Public members.
+
+    // @cmember Constructor.
+  OMPageCache(OMUInt32 pageSize, OMUInt32 pageCount);
+
+    // @cmember Destructor.
+  virtual ~OMPageCache(void);
+
+    // @cmember Attempt to read the number of bytes given by <p byteCount>
+    //          from offset <p position> into the buffer at address <p bytes>
+    //          from this <c OMPageCache>.
+    //          The actual number of bytes read is returned in <p bytesRead>.
+  virtual void readCachedAt(OMUInt64 position,
+                            OMByte* bytes,
+                            OMUInt32 byteCount,
+                            OMUInt32& bytesRead);
+
+    // @cmember Attempt to write the number of bytes given by <p byteCount>
+    //          to offset <p position> from the buffer at address <p bytes>
+    //          to this <c OMPageCache>.
+    //          The actual number of bytes written is returned in
+    //          <p bytesWritten>.
+  virtual void writeCachedAt(OMUInt64 position,
+                             const OMByte* bytes,
+                             OMUInt32 byteCount,
+                             OMUInt32& bytesWritten);
+
+    // @cmember Flush this <c OMPageCache>.
+  virtual void flush(void);
+
+private:
+  // @access Private members.
+
+    // @cmember Read a page or partial page using the cache.
+  virtual void readCachedPage(OMUInt64 page,
+                              OMUInt32 offset,
+                              OMUInt32 byteCount,
+                              OMByte* destination);
+
+    // @cmember Write a page or partial page using the cache.
+  virtual void writeCachedPage(OMUInt64 page,
+                               OMUInt32 offset,
+                               OMUInt32 byteCount,
+                               const OMByte* source);
+
+    // @cmember Read a page or partial page without using the cache.
+  virtual void readPage(OMUInt64 position,
+                        OMUInt32 byteCount,
+                        OMByte* destination) = 0;
+
+    // @cmember Write a page or partial page without using the cache.
+  virtual void writePage(OMUInt64 position,
+                         OMUInt32 byteCount,
+                         const OMByte* source) = 0;
+
+  struct CacheEntry;
+
+  typedef OMSet<OMUInt64, CacheEntry*> Cache;
+  typedef OMSetIterator<OMUInt64, CacheEntry*> CacheIterator;
+
+  typedef OMList<CacheEntry*> CacheList;
+  typedef OMListIterator<CacheEntry*> CacheListIterator;
+
+  struct CacheEntry {
+    OMByte* _page;
+    OMUInt64 _pageNumber;
+    CacheListIterator _position;
+    bool _isDirty;
+  };
+
+  virtual CacheEntry* cacheEntry(OMUInt64 page);
+
+    // @cmember The <t CacheEntry> for page number <p page>, if
+    //          present 0 otherwise.
+  virtual CacheEntry* findEntry(OMUInt64 page);
+
+    // @cmember Allocate a <t CacheEntry> for page number <p page> either
+    //          by creating a new entry or by replacing an existing entry.
+  virtual CacheEntry* allocateEntry(OMUInt64 page);
+
+    // @cmember Create a new <t CacheEntry> for page number <p page>.
+  virtual CacheEntry* newEntry(OMUInt64 page);
+
+    // @cmember Allocate a <t CacheEntry> for page number <p page>
+    //          by replacing an existing entry.
+  virtual CacheEntry* replaceEntry(OMUInt64 page);
+
+  OMUInt32 _pageSize;
+  OMUInt32 _pageCount;
+  OMUInt32 _validPageCount;
+  Cache _cache;
+  CacheList _mru;
+
+};
+
 #endif
--- ref-impl/src/OM/OMPageCache.cpp
+++ ref-impl/src/OM/OMPageCache.cpp
@@ -24,4 +24,294 @@
 // @author Tim Bingham | tjb | Avid Technology, Inc. | OMPageCache 
 #include "OMPageCache.h"
 
-// Nothing yet.
+#include "OMDataTypes.h"
+#include "OMAssertions.h"
+#include "OMSetIterator.h"
+
+#include <string.h>
+
+  // @mfunc Constructor.
+  //   @parm TBS
+  //   @parm TBS
+OMPageCache::OMPageCache(OMUInt32 pageSize,
+                         OMUInt32 pageCount)
+: _pageSize(pageSize),
+  _pageCount(pageCount),
+  _validPageCount(0)
+{
+  TRACE("OMPageCache::OMPageCache");
+
+  PRECONDITION("Valid page size", _pageSize > 0);
+  PRECONDITION("Valid page count", _pageCount > 0);
+}
+
+  // @mfunc Destructor.
+OMPageCache::~OMPageCache(void)
+{
+  TRACE("OMPageCache::~OMPageCache");
+
+  CacheIterator iterator(_cache, OMBefore);
+  while (++iterator) {
+    CacheEntry* entry = iterator.value();
+    ASSERT("Page is clean", !entry->_isDirty);
+    delete [] entry->_page;
+    entry->_page = 0;
+    delete entry;
+  }
+}
+
+void OMPageCache::readCachedAt(OMUInt64 position,
+                               OMByte* bytes,
+                               OMUInt32 byteCount,
+                               OMUInt32& bytesRead)
+{
+  TRACE("OMPageCache::readCachedAt");
+
+  PRECONDITION("Valid buffer", bytes != 0);
+  PRECONDITION("Valid byte count", byteCount > 0);
+
+  OMByte* destination = bytes;
+  OMUInt32 remaining = byteCount;
+  OMUInt64 page = position / _pageSize;
+  OMUInt32 offset = static_cast<OMUInt32>(position - (page * _pageSize));
+  OMUInt32 free = _pageSize - offset;
+  OMUInt32 readSize;
+  if (remaining < free) {
+    readSize = remaining;
+  } else {
+    readSize = free;
+  }
+  while (remaining > 0) {
+    readCachedPage(page, offset, readSize, destination);
+    remaining = remaining - readSize;
+    destination = destination + readSize;
+    page = page + 1;
+    offset = 0;
+    if (remaining < _pageSize) {
+      readSize = remaining;
+    } else {
+      readSize = _pageSize;
+    }
+  }
+  bytesRead = byteCount;
+}
+
+void OMPageCache::writeCachedAt(OMUInt64 position,
+                                const OMByte* bytes,
+                                OMUInt32 byteCount,
+                                OMUInt32& bytesWritten)
+{
+  TRACE("OMPageCache::writeCachedAt");
+
+  PRECONDITION("Valid buffer", bytes != 0);
+  PRECONDITION("Valid byte count", byteCount > 0);
+
+  const OMByte* source = bytes;
+  OMUInt32 remaining = byteCount;
+  OMUInt64 page = position / _pageSize;
+  OMUInt32 offset = static_cast<OMUInt32>(position - (page * _pageSize));
+  OMUInt32 free = _pageSize - offset;
+  OMUInt32 writeSize;
+  if (remaining < free) {
+    writeSize = remaining;
+  } else {
+    writeSize = free;
+  }
+  while (remaining > 0) {
+    writeCachedPage(page, offset, writeSize, source);
+    remaining = remaining - writeSize;
+    source = source + writeSize;
+    page = page + 1;
+    offset = 0;
+    if (remaining < _pageSize) {
+      writeSize = remaining;
+    } else {
+      writeSize = _pageSize;
+    }
+  }
+  bytesWritten = byteCount;
+}
+
+  // @mfunc Flush this <c OMPageCache>.
+void OMPageCache::flush(void)
+{
+  TRACE("OMPageCache::flush");
+
+  CacheIterator iterator(_cache, OMBefore);
+  while (++iterator) {
+    CacheEntry* entry = iterator.value();
+    OMUInt64 pageNumber = iterator.key();
+    ASSERT("Consistent page numbers", pageNumber == entry->_pageNumber);
+    if (entry->_isDirty) {
+      writePage(pageNumber * _pageSize, _pageSize, entry->_page);
+      entry->_isDirty = false;
+    }
+  }
+}
+
+  // @mfunc Read a page or partial page using the cache.
+  //   @parm The page to be read from.
+  //   @parm The starting offset within the page.
+  //   @parm the number of bytes to read.
+  //   @parm The buffer into which the bytes are to be read.
+void OMPageCache::readCachedPage(OMUInt64 page,
+                                 OMUInt32 offset,
+                                 OMUInt32 byteCount,
+                                 OMByte* destination)
+{
+  TRACE("OMPageCache::readCachedPage");
+
+  PRECONDITION("Valid offset", offset < _pageSize);
+  PRECONDITION("Valid count", ((byteCount > 0) && (byteCount <= _pageSize)));
+  PRECONDITION("Valid buffer", destination != 0);
+
+  CacheEntry* entry = cacheEntry(page);
+  ASSERT("Page found", entry != 0);
+  ASSERT("Consistent page numbers", page == entry->_pageNumber);
+  OMByte* p = entry->_page;
+  ASSERT("Valid page", p != 0);
+  memcpy(destination, p + offset, byteCount);
+}
+
+  // @mfunc Write a page or partial page using the cache.
+  //   @parm The page to be written to.
+  //   @parm The starting offset within the page.
+  //   @parm the number of bytes to write.
+  //   @parm The buffer from which the bytes are to be written.
+void OMPageCache::writeCachedPage(OMUInt64 page,
+                                  OMUInt32 offset,
+                                  OMUInt32 byteCount,
+                                  const OMByte* source)
+{
+  TRACE("OMPageCache::writeCachedPage");
+
+  PRECONDITION("Valid offset", offset < _pageSize);
+  PRECONDITION("Valid count", ((byteCount > 0) && (byteCount <= _pageSize)));
+  PRECONDITION("Valid buffer", source != 0);
+
+  CacheEntry* entry = cacheEntry(page);
+  ASSERT("Page found", entry != 0);
+  ASSERT("Consistent page numbers", page == entry->_pageNumber);
+  OMByte* p = entry->_page;
+  ASSERT("Valid page", p != 0);
+  memcpy(p + offset, source, byteCount);
+  entry->_isDirty = true;
+}
+
+OMPageCache::CacheEntry* OMPageCache::cacheEntry(OMUInt64 page)
+{
+  TRACE("OMPageCache::cacheEntry");
+
+  CacheEntry* entry = findEntry(page);
+  if (entry == 0) {
+    // Page not in cache
+    entry = allocateEntry(page);
+  }
+  ASSERT("Page found", entry != 0);
+  ASSERT("Consistent page numbers", page == entry->_pageNumber);
+
+  return entry;
+}
+
+  // @mfunc The <t CacheEntry> for page number <p page>, if
+  //        present 0 otherwise.
+  //   @parm TBS
+  //   @rdesc TBS
+OMPageCache::CacheEntry* OMPageCache::findEntry(OMUInt64 page)
+{
+  TRACE("OMPageCache::findEntry");
+
+  CacheEntry* result;
+  CacheEntry* entry;
+  if (_cache.find(page, entry)) {
+    result = entry;
+    ASSERT("Consistent page numbers", page == entry->_pageNumber);
+    // Promote to most recently used
+    _mru.remove(entry->_position);
+    _mru.prepend(entry);
+     entry->_position = _mru.first();
+  } else {
+    result = 0;
+  }
+  return result;
+}
+
+  // @mfunc Allocate a <t CacheEntry> for page number <p page> either
+  //        by creating a new entry or by replacing an existing entry.
+  //   @parm TBS
+  //   @rdesc TBS
+OMPageCache::CacheEntry* OMPageCache::allocateEntry(OMUInt64 page)
+{
+  TRACE("OMPageCache::allocateEntry");
+
+  CacheEntry* result;
+  if (_validPageCount < _pageCount) {
+    result = newEntry(page);
+    ASSERT("Consistent page numbers", page == result->_pageNumber);
+    _validPageCount++;
+  } else {
+    result = replaceEntry(page);
+  }
+  ASSERT("Page found", result != 0);
+  OMByte* p = result->_page;
+  ASSERT("Valid page", p != 0);
+#if defined(OM_CLEAR_PAGES)
+  for (OMUInt32 i = 0; i < _pageSize; i++) {
+    p[i] = "deadbeef"[i % 8];
+  }
+#endif
+  readPage(page * _pageSize, _pageSize, p);
+  return result;
+}
+
+  // @mfunc Create a new <t CacheEntry> for page number <p page>.
+  //   @parm TBS
+  //   @rdesc TBS
+OMPageCache::CacheEntry* OMPageCache::newEntry(OMUInt64 page)
+{
+  TRACE("OMPageCache::newEntry");
+
+  OMByte* p = new OMByte[_pageSize];
+  ASSERT("Valid heap pointer", p != 0);
+
+  CacheEntry* entry = new CacheEntry;
+  ASSERT("Valid heap pointer", entry != 0);
+
+  // Add new entry to head of most recently used list
+  _mru.prepend(entry);
+  entry->_pageNumber = page;
+  entry->_page = p;
+  entry->_isDirty = false;
+  entry->_position = _mru.first();
+  _cache.insert(page, entry);
+
+  return entry;
+}
+
+  // @mfunc Allocate a <t CacheEntry> for page number <p page>
+  //        by replacing an existing entry.
+  //   @parm TBS
+  //   @rdesc TBS
+OMPageCache::CacheEntry* OMPageCache::replaceEntry(OMUInt64 page)
+{
+  TRACE("OMPageCache::replaceEntry");
+
+  // Victim is at the tail of the most recently used list
+  // Add new entry to head of most recently used list
+  CacheListIterator iterator = _mru.last();
+  CacheEntry* victim = iterator.value();
+  OMUInt64 victimPage = victim->_pageNumber;
+  _mru.remove(iterator);
+  if (victim->_isDirty) {
+    writePage(victimPage * _pageSize, _pageSize, victim->_page);
+    victim->_isDirty = false;
+  }
+  _cache.remove(victimPage);
+  CacheEntry* newEntry = victim;
+  _mru.prepend(newEntry);
+
+  newEntry->_pageNumber = page;
+  newEntry->_position = _mru.first();
+  _cache.insert(page, newEntry);
+  return newEntry;
+}
