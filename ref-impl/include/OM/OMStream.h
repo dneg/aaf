1c52c84fd9f4624c0fbb20a063318f0bf7189e60
tbingham <>
2001-11-29 21:24:52 +0000

    Add implementation for OMStream.

--- ref-impl/include/OM/OMStream.h
+++ ref-impl/include/OM/OMStream.h
@@ -24,5 +24,60 @@
 // @author Tim Bingham | tjb | Avid Technology, Inc. | OMStream
 #ifndef OMSTREAM_H
 #define OMSTREAM_H
-// Nothing yet.
+
+#include <stdio.h>
+#include "OMFile.h"
+#include "OMDataTypes.h"
+
+  // @class Wrappers for ISO FILE*s.
+  //        Additionally supporting wchar_t file names and 64-bit positions.
+  //   @cauthor Tim Bingham | tjb | Avid Technology, Inc.
+class OMStream {
+public:
+
+    // @cmember Create an <c OMStream> object by opening an existing
+    //          file for read-only access, the file is named <p fileName>.
+    //          The file must already exist.
+  static OMStream* openExistingRead(const wchar_t* fileName);
+
+    // @cmember Create an <c OMStream> object by opening an existing
+    //          file for modify access, the file is named <p fileName>.
+    //          The file must already exist.
+  static OMStream* openExistingModify(const wchar_t* fileName);
+
+    // @cmember Create an <c OMStream> object by creating a new
+    //          file for modify access, the file is named <p fileName>.
+    //          The file must not already exist.
+  static OMStream* openNewModify(const wchar_t* fileName);
+
+  void read(OMByte* bytes,
+            OMUInt32 byteCount,
+            OMUInt32& bytesRead) const;
+
+  void write(const OMByte* bytes,
+             OMUInt32 byteCount,
+             OMUInt32& bytesWritten);
+
+  OMUInt64 size(void) const;
+
+  void setSize(OMUInt64 newSize);
+
+  OMUInt64 position(void) const;
+
+  void setPosition(OMUInt64 newPosition);
+
+  void synchronize(void);
+
+  bool isWritable(void) const;
+
+  virtual ~OMStream(void);
+
+private:
+
+  OMStream(FILE* file, bool isWritable);
+
+  FILE* _file;
+  bool _isWritable;
+};
+
 #endif
--- ref-impl/src/OM/OMStream.cpp
+++ ref-impl/src/OM/OMStream.cpp
@@ -25,4 +25,219 @@
 
 #include "OMStream.h"
 
-// Nothing yet.
+#include <limits.h>
+#include <errno.h>
+
+#include "OMUtilities.h"
+#include "OMExceptions.h"
+#include "OMRawStorage.h"
+
+  // @mfunc Create an <c OMStream> object by opening an existing
+  //        file for read-only access, the file is named <p fileName>.
+  //        The file must already exist.
+  //   @parm The file name.
+  //   @rdesc The newly created <c OMStream> object.
+OMStream*
+OMStream::openExistingRead(const wchar_t* fileName)
+{
+  TRACE("OMStream::openExistingRead");
+
+  PRECONDITION("Valid file name", validWideString(fileName));
+
+  FILE* file = wfopen(fileName, L"rb");
+  ASSERT("File successfully opened", file != 0); // tjb - error
+
+  OMStream* result = new OMStream(file, false);
+  ASSERT("Valid heap pointer", result != 0);
+
+  return result;
+}
+
+  // @mfunc Create an <c OMStream> object by opening an existing
+  //        file for modify access, the file is named <p fileName>.
+  //        The file must already exist.
+  //   @parm The file name.
+  //   @rdesc The newly created <c OMStream> object.
+OMStream*
+OMStream::openExistingModify(const wchar_t* fileName)
+{
+  TRACE("OMStream::openExistingModify");
+
+  PRECONDITION("Valid file name", validWideString(fileName));
+
+  FILE* file = wfopen(fileName, L"r+b");
+  ASSERT("File successfully opened", file != 0); // tjb - error
+
+  OMStream* result = new OMStream(file, true);
+  ASSERT("Valid heap pointer", result != 0);
+
+  return result;
+}
+
+  // @mfunc Create an <c OMStream> object by creating a new
+  //        file for modify access, the file is named <p fileName>.
+  //        The file must not already exist.
+  //   @parm The file name.
+  //   @rdesc The newly created <c OMStream> object.
+OMStream*
+OMStream::openNewModify(const wchar_t* fileName)
+{
+  TRACE("OMStream::openNewModify");
+
+  PRECONDITION("Valid file name", validWideString(fileName));
+
+  // Check that the file doesn't already exist.
+  // This is the ISO way to do it.
+  //
+  FILE* f = wfopen(fileName, L"r");
+  if (f != 0) {
+    fclose(f);
+    f = 0;
+    throw OMException("File already exists.");
+  }
+
+  FILE* file = wfopen(fileName, L"w+b");
+  ASSERT("File successfully opened", file != 0); // tjb - error
+
+  OMStream* result = new OMStream(file, true);
+  ASSERT("Valid heap pointer", result != 0);
+
+  return result;
+}
+
+void OMStream::read(OMByte* bytes,
+                    OMUInt32 byteCount,
+                    OMUInt32& bytesRead) const
+{
+  TRACE("OMStream::read");
+  PRECONDITION("No error on stream", ferror(_file) == 0);
+
+  size_t actualByteCount = fread(bytes, 1, byteCount, _file);
+  ASSERT("Successful read", ferror(_file) == 0);
+
+  bytesRead = actualByteCount;
+  POSTCONDITION("All bytes read", actualByteCount == byteCount);
+}
+
+void OMStream::write(const OMByte* bytes,
+                     OMUInt32 byteCount,
+                     OMUInt32& bytesWritten)
+{
+  TRACE("OMStream::write");
+  PRECONDITION("Stream is writable", isWritable());
+  PRECONDITION("No error on stream", ferror(_file) == 0);
+
+  size_t actualByteCount = fwrite(bytes, 1, byteCount, _file);
+  ASSERT("Successful write", ferror(_file) == 0);
+
+  bytesWritten = actualByteCount;
+  POSTCONDITION("All bytes written", actualByteCount == byteCount);
+}
+
+OMUInt64 OMStream::size(void) const
+{
+  TRACE("OMStream::size");
+  PRECONDITION("No error on stream", ferror(_file) == 0);
+
+  errno = 0;
+  long int oldPosition = ftell(_file);
+  ASSERT("Successful tell", IMPLIES(oldPosition == -1L, errno == 0));
+
+  long int status = fseek(_file, 0, SEEK_END);
+  ASSERT("Successful seek", status == 0); // tjb - error
+
+  errno = 0;
+  long int position = ftell(_file);
+  ASSERT("Successful tell", IMPLIES(position == -1L, errno == 0));
+
+  status = fseek(_file, oldPosition, SEEK_SET);
+  ASSERT("Successful seek", status == 0); // tjb - error
+
+  OMUInt64 result = position;
+  return result;
+}
+
+void OMStream::setSize(OMUInt64 newSize)
+{
+  TRACE("OMStream::setSize");
+  PRECONDITION("Stream is writable", isWritable());
+
+  OMUInt64 currentSize = size();
+
+  if (newSize > currentSize) {
+
+    // Extend by writing a single byte.
+    //
+    OMUInt64 oldPosition = position(); // Save position
+    OMByte nullByte = 0;
+
+    setPosition(newSize - 1);
+    OMUInt32 bw;
+    write(&nullByte, 1, bw);
+
+    ASSERT("Size properly changed", size() == newSize);
+    setPosition(oldPosition); // Restore position
+  }
+  // else no ISO/ANSI way to truncate the file in place
+}
+
+OMUInt64 OMStream::position(void) const
+{
+  TRACE("OMStream::position");
+  PRECONDITION("No error on stream", ferror(_file) == 0);
+
+  errno = 0;
+  long int position = ftell(_file);
+  ASSERT("Successful tell", IMPLIES(position == -1L, errno == 0));
+
+  OMUInt64 result = position;
+  return result;
+}
+
+void OMStream::setPosition(OMUInt64 newPosition)
+{
+  TRACE("OMStream::setPosition");
+
+  ASSERT("Supported position", newPosition <= LONG_MAX); // tjb - limit
+
+  ASSERT("No error on stream", ferror(_file) == 0);
+  long int liNewPosition = static_cast<long int>(newPosition);
+  int status = fseek(_file, liNewPosition, SEEK_SET);
+  ASSERT("Successful seek", status == 0); // tjb - error
+}
+
+bool OMStream::isWritable(void) const
+{
+  TRACE("OMStream::isWritable");
+  return _isWritable;
+}
+
+void OMStream::synchronize(void)
+{
+  TRACE("OMStream::synchronize");
+  PRECONDITION("Stream is writable", isWritable());
+  PRECONDITION("No error on stream", ferror(_file) == 0);
+
+  int status = fflush(_file);
+  ASSERT("Successful flush", status == 0);
+}
+
+OMStream::~OMStream(void)
+{
+  TRACE("OMStream::~OMStream");
+
+  if (isWritable()) {
+    synchronize();
+  }
+  ASSERT("No error on stream", ferror(_file) == 0);
+  int status = fclose(_file);
+  ASSERT("Successful close", status == 0);
+  _file = 0;
+}
+
+OMStream::OMStream(FILE* file, bool isWritable)
+: _file(file),
+  _isWritable(isWritable)
+{
+  TRACE("OMStream::OMStream");
+}
