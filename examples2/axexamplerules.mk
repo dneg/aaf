0a9a7cedd2bcfe6e8a826a03777ebe5ea8166645
jptrainor <>
2002-02-04 19:54:41 +0000

    UNIX makefiles

---
+++ examples2/axexamplerules.mk
@@ -0,0 +1,56 @@
+###############################################################################
+#
+# The contents of this file are subject to the AAF SDK Public
+# Source License Agreement (the "License"); You may not use this file
+# except in compliance with the License.  The License is available in
+# AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+# Association or its successor.
+# 
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+# the License for the specific language governing rights and limitations
+# under the License.
+# 
+# The Original Code of this file is Copyright 1998-2001, Licensor of the
+# AAF Association.
+# 
+# The Initial Developer of the Original Code of this file and the
+# Licensor of the AAF Association is Avid Technology.
+# All rights reserved.
+#
+###############################################################################
+
+PACKAGE = axExamples
+
+AAFBASE ?= ../..
+
+# Common definitions
+include $(AAFBASE)/build/common.mk
+
+# Include directories
+INCLUDES = -I$(AAFSDKINCLUDEDIR) \
+	   -I../axLib
+
+BINTARGET = $(AAFSDKBINDIR)/$(PROGNAME)$(EXE)
+
+.PHONY : all
+all : $(OBJDIR) $(BINTARGET)
+
+$(BINTARGET) : $(CXXOBJS)
+	$(LD) $(CXXOBJS) $(RPATH_OPT) \
+	-L$(AAFSDKLIBDIR) -L$(OBJDIR) -laxLib -laaflib -laafiid -o $@
+
+.PHONY : clean
+clean :
+	@$(RM) $(CXXOBJS)
+	@$(RM) $(DEPS)
+	@$(RM) $(BINTARGET)
+	@$(RMDIR) $(OBJDIR)/ii_files
+
+
+.PHONY : realclean
+realclean : clean
+
+
+include $(AAFBASE)/build/rules.mk
+
