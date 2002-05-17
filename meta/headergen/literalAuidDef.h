5b6db49ba8e559bb500290d57604b84e803b06ed
tbingham <>
2002-05-17 22:02:04 +0000

    Print GUIDs all on one line.

--- meta/headergen/literalAuidDef.h
+++ meta/headergen/literalAuidDef.h
@@ -4,10 +4,10 @@
 
 // macro to help parsing AAFMetaDictionary.h
 #define AAF_LITERAL_AUID(L, W1, W2, B1, B2, B3, B4, B5, B6, B7, B8) \
-cout << "{" << endl; \
-cout << "  " << #L << ", " << #W1 << ", " << #W2 << "," << endl; \
-cout << "    {" << endl; \
-cout << "       " #B1 ", " #B2 ", " #B3 ", " #B4 ", "; \
-cout <<  #B5 ", " #B6 ", " #B7 ", " #B8  << endl; \
-cout << "    }" << endl; \
-cout << "};" << endl;
+cout << "{"; \
+cout << #L << ", " << #W1 << ", " << #W2 << ", "; \
+cout << "{"; \
+cout << #B1 ", " #B2 ", " #B3 ", " #B4 ", "; \
+cout <<  #B5 ", " #B6 ", " #B7 ", " #B8; \
+cout << "};" << endl; \
+
