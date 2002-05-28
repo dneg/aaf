a0782e56890b1e0e4fa937d4ae2efd5042d39d34
tbingham <>
2002-05-28 19:10:33 +0000

    Add copyright.

--- meta/headergen/storedObjectUid.cpp
+++ meta/headergen/storedObjectUid.cpp
@@ -1,3 +1,28 @@
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+//
+// The Original Code of this file is Copyright 1998-2002, Licensor of the
+// AAF Association.
+//
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
+
+// Author : Tim Bingham (Tim_Bingham@avid.com) - after an initial
+// implementation by Bob Tillman.
+
 #include <iostream.h>
 #include <assert.h>
 #include <stdlib.h>
