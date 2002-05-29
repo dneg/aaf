8d0519999ba4388420b934299f33670625351b67
tbingham <>
2002-05-29 19:19:07 +0000

    Use generated header.

--- AAFi686LinuxSDK/g++/include/AAFExtEnum.h
+++ AAFi686LinuxSDK/g++/include/AAFExtEnum.h
@@ -1,37 +1,59 @@
-#ifndef __AAFClassDefUIDs_h__
-#define __AAFClassDefUIDs_h__
+#ifndef __AAFExtEnum_h__
+#define __AAFExtEnum_h__
+
 //=---------------------------------------------------------------------=
+//
+// This file was GENERATED for the AAF SDK
 //
 // The contents of this file are subject to the AAF SDK Public
 // Source License Agreement (the "License"); You may not use this file
 // except in compliance with the License.  The License is available in
 // AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
 // Association or its successor.
-// 
+//
 // Software distributed under the License is distributed on an "AS IS"
 // basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
 // the License for the specific language governing rights and limitations
 // under the License.
-// 
-// The Original Code of this file is Copyright 1998-2001, Licensor of the
+//
+// The Original Code of this file is Copyright 1998-2002, Licensor of the
 // AAF Association.
-// 
+//
 // The Initial Developer of the Original Code of this file and the
 // Licensor of the AAF Association is Avid Technology.
 // All rights reserved.
 //
 //=---------------------------------------------------------------------=
 
-//
-// GUIDs for built-in class definitions
+
+#include "AAFTypes.h"
+
+// AAF extensible enumeration member UIDs.
 //
 
-// Define the AAF_CLASS macro to create class GUID definitions.
-// Include the meta dictionary.
+// Members of OperationCategory
 //
-#define AAF_TYPE_DEFINITION_EXTENDIBLE_ENUMERATION_MEMBER(name, guid, container) \
-const aafUID_t name = guid;
+//{0d010102-0101-0100-060e-2b3404010101}
+const aafUID_t kAAFOperationEffect =
+{0x0d010102, 0x0101, 0x0100, {0x06, 0x0e, 0x2b, 0x34, 0x04, 0x01, 0x01, 0x01}};
 
-#include "AAFMetaDictionary.h"
+// Members of GammaType
+//
+// None currently defined
 
-#endif // ! __AAFClassDefUIDs_h__
+// Members of CategoryType
+//
+//{0d010102-0101-0200-060e-2b3404010101}
+const aafUID_t kAAFPluginCatagoryEffect =
+{0x0d010102, 0x0101, 0x0200, {0x06, 0x0e, 0x2b, 0x34, 0x04, 0x01, 0x01, 0x01}};
+
+//{0d010102-0101-0300-060e-2b3404010101}
+const aafUID_t kAAFPluginCatagoryCodec =
+{0x0d010102, 0x0101, 0x0300, {0x06, 0x0e, 0x2b, 0x34, 0x04, 0x01, 0x01, 0x01}};
+
+//{0d010102-0101-0400-060e-2b3404010101}
+const aafUID_t kAAFPluginCatagoryInterpolation =
+{0x0d010102, 0x0101, 0x0400, {0x06, 0x0e, 0x2b, 0x34, 0x04, 0x01, 0x01, 0x01}};
+
+
+#endif // ! __AAFExtEnum_h__
