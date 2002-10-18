e1508d365cac5e2cc2cd581df3d162fc05e486e2
jptrainor <>
2002-10-18 00:16:03 +0000

    this file is for unix platform specific code

---
+++ examples2/axPlugin/AxPluginUnix.cpp
@@ -0,0 +1,28 @@
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+//=---------------------------------------------------------------------=
+
+#include <AAFPlatform.h>
+
+#if defined(OS_UNIX)
+
+// No platform specific unix (Linux, Irix) code is required.
+
+// If ever the need arises to implement unix platform specific library
+// entry points, they can go here.
+
+#endif
