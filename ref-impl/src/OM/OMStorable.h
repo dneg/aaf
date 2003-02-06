5e3225bd4c3e0f8ccf5203b141300c4430ed51bb
tbingham <>
2003-02-06 17:02:05 +0000

    Update copyright dates.

--- ref-impl/src/OM/OMStorable.h
+++ ref-impl/src/OM/OMStorable.h
@@ -11,7 +11,7 @@
 // the License for the specific language governing rights and limitations
 // under the License.
 //
-// The Original Code of this file is Copyright 1998-2002, Licensor of the
+// The Original Code of this file is Copyright 1998-2003, Licensor of the
 // AAF Association.
 //
 // The Initial Developer of the Original Code of this file and the
