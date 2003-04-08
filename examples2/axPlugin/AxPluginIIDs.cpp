be7d9bda1f05d3c8157dc113c8b7dff5b5e75cd9
jptrainor <>
2003-04-08 14:50:01 +0000

    Separate file to define AAF plugin interface IIDs.

---
+++ examples2/axPlugin/AxPluginIIDs.cpp
@@ -0,0 +1,22 @@
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+//=---------------------------------------------------------------------=
+
+// Include the definitions for all of the interface IID's.
+// This defines the IID's for the AAF plugin interfaces.  This
+// should only be included in one of the library'ss files.
+#include <AAFPlugin_i.c>
