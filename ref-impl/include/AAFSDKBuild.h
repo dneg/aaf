ee0eecb3122785db1623e4b63ae9a77f73571a75
jptrainor <>
2003-07-09 14:42:33 +0000

    Manually bump the build number from 520 to 521 to get it in sync with the Build-0521 tag.

--- ref-impl/include/AAFSDKBuild.h
+++ ref-impl/include/AAFSDKBuild.h
@@ -46,7 +46,7 @@
  
 // these next two lines are monitored/incremented by newbuild.pl
 
-#define AAF_BUILD_NUMBER 520
+#define AAF_BUILD_NUMBER 521
 #define AAF_CURRENT_YEAR "2003"
 
 //   The lines below shouldn't normally need to be modified
