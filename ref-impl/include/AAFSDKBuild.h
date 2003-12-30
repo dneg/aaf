1cd0ba91004cf168256d36577ebc4c328cbdb545
jptrainor <>
2003-12-30 06:57:57 +0000

    BuildNumber script automated edit. Changed build number from 536 to 537.

--- ref-impl/include/AAFSDKBuild.h
+++ ref-impl/include/AAFSDKBuild.h
@@ -46,7 +46,7 @@
  
 // these next two lines are monitored/incremented by newbuild.pl
 
-#define AAF_BUILD_NUMBER 536
+#define AAF_BUILD_NUMBER 537
 #define AAF_CURRENT_YEAR "2003"
 
 //   The lines below shouldn't normally need to be modified
