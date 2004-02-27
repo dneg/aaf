490693ea7e8c304b604dcdc1c9a2eaac8cd5779f
stuart_hc <>
2004-02-27 16:41:24 +0000

    Add $Id$ and $Name$ CVS keywords to aid identification of version and branch. Change license to GPL from LGPL.

--- Utilities/aafviewer/src/aaf2dot/Utilities.h
+++ Utilities/aafviewer/src/aaf2dot/Utilities.h
@@ -2,22 +2,24 @@
 #define __UTILITIES_H__
 
 /*
+ * $Id$ $Name$
+ *
  *      Copyright (c) 2003, Philip de Nier (philipn@users.sourceforge.net)
  *
  *
  *      This file is part of aaf2dot.
  *
  *  aaf2dot is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation; either version 2.1 of the License, or
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
  *  aaf2dot is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
+ *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public License
+ *  You should have received a copy of the GNU General Public License
  *  along with aaf2dot; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
