c1ad659fa3d1f29b43433716355926a1e084a545
jptrainor <>
2004-04-01 13:32:06 +0000

    Added AppendNewStaticSlot and AppendNewEventSlot methods.

--- ref-impl/src/impl/ImplAAFMob.h
+++ ref-impl/src/impl/ImplAAFMob.h
@@ -52,6 +52,9 @@ class ImplAAFSegment;
 
 class ImplAAFMobSlot;
 
+class ImplAAFEventMobSlot;
+class ImplAAFStaticMobSlot;
+
 class ImplAAFFile;
 
 template <class T> 
@@ -407,6 +410,100 @@ public:
     // @parm [out] aafUID_t* | pUsageCode | Pointer to usage code.
     aafUID_t*  pUsageCode
   );
+
+
+  //***********************************************************
+  // METHOD NAME: AppendNewStaticSlot()
+  //
+  // DESCRIPTION:
+  // @mfunc AAFRESULT | AAFMob2 | AppendNewStaticSlot |
+  // This method creates a new static mob slot with the given
+  // property values and appends it to the input mob.
+  // 
+  // The returned mob slot is AddRef()ed before it is returned.
+  //
+  // Succeeds if all of the following are true:
+  // - the pSegment pointer is valid.
+  // - the pSlotName pointer is valid.
+  // - the ppNewSlot pointer is valid.
+  // 
+  // If this method fails no state will be changed.
+  // 
+  // This method will return the following codes.  If more than one of
+  // the listed errors is in effect, it will return the first one
+  // encountered in the order given below:
+  // 
+  // AAFRESULT_SUCCESS
+  //   - succeeded.  (This is the only code indicating success.)
+  //
+  // AAFRESULT_NULL_PARAM
+  //   - any of pSegment, pSlotName, or ppNewSlot arguments is null.
+  // @end
+  // 
+  virtual AAFRESULT STDMETHODCALLTYPE
+  AppendNewStaticSlot (
+    // @parm [in] AAFSegment | pSegment | Segment to append as slot component
+    ImplAAFSegment * pSegment,
+
+    // @parm [in] aafSlotID_t | slotID | new slot ID
+    aafSlotID_t  slotID,
+
+    // @parm [in, string] aafCharacter_constptr | pSlotName | new slot name
+    aafCharacter_constptr  pSlotName,
+
+    // @parm [out] AAFStaticMobSlot | ppNewSlot | Newly created slot
+    ImplAAFStaticMobSlot ** ppNewSlot
+  );
+
+  //***********************************************************
+  // METHOD NAME: AppendNewEventSlot()
+  //
+  // DESCRIPTION:
+  // @mfunc AAFRESULT | AAFMob2 | AppendNewEventSlot |
+  // This method creates a new event mob slot with the given
+  // property values and appends it to the input mob.
+  // 
+  // The returned mob slot is AddRef()ed before it is returned.
+  //
+  // Succeeds if all of the following are true:
+  // - the pSegment pointer is valid.
+  // - the pSlotName pointer is valid.
+  // - the ppNewSlot pointer is valid.
+  // 
+  // If this method fails no state will be changed.
+  // 
+  // This method will return the following codes.  If more than one of
+  // the listed errors is in effect, it will return the first one
+  // encountered in the order given below:
+  // 
+  // AAFRESULT_SUCCESS
+  //   - succeeded.  (This is the only code indicating success.)
+  //
+  // AAFRESULT_NULL_PARAM
+  //   - any of pSegment, pSlotName, or ppNewSlot arguments is null.
+  // @end
+  // 
+  virtual AAFRESULT STDMETHODCALLTYPE
+  AppendNewEventSlot (
+    // @parm [in] aafRational_t | editRate | Edit rate property value
+    aafRational_t  editRate,
+
+    // @parm [in] AAFSegment | pSegment | Segment to append as slot component
+    ImplAAFSegment * pSegment,
+
+    // @parm [in] aafSlotID_t | slotID | new slot ID
+    aafSlotID_t  slotID,
+
+    // @parm [in, string] aafCharacter_constptr | pSlotName | new slot name
+    aafCharacter_constptr  pSlotName,
+
+    // @parm [in] aafPosition_t | origin | The slot origin
+    aafPosition_t  origin,
+
+    // @parm [out] AAFEventMobSlot | ppNewSlot | Newly created slot
+    ImplAAFEventMobSlot ** ppNewSlot
+  );
+
 
 
   //****************
