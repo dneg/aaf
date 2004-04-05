272bcabf3b69f20d96c2c141bbdb0f4d36db5d15
stuart_hc <>
2004-04-05 10:49:20 +0000

    Restore Id and Name CVS keywords. Restore original license text (this file was derived from original SDK code). Whitespace cleanup and typo fixed.

--- build/pdefs-PPCDarwin.mk
+++ build/pdefs-PPCDarwin.mk
@@ -1,21 +1,23 @@
 ###############################################################################
 #
+# $Id$ $Name$
+#
 # The contents of this file are subject to the AAF SDK Public
 # Source License Agreement (the "License"); You may not use this file
 # except in compliance with the License.  The License is available in
 # AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
 # Association or its successor.
-# 
+#
 # Software distributed under the License is distributed on an "AS IS"
 # basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
 # the License for the specific language governing rights and limitations
 # under the License.
-# 
-# The Original Code of this file is Copyright 2003-2004, Licensor of the
+#
+# The Original Code of this file is Copyright 1998-2004, Licensor of the
 # AAF Association.
-# 
+#
 # The Initial Developer of the Original Code of this file and the
-# Licensor of the AAF Association is Apple Computer, Inc.
+# Licensor of the AAF Association is Avid Technology.
 # All rights reserved.
 #
 ###############################################################################
@@ -24,18 +26,18 @@
 #
 # pdefs-PPCDarwin.mk
 #
-#	This file contains makefile definitions for the Darwn (Mac OS X) 
+#	This file contains makefile definitions for the Darwin (Mac OS X)
 #	platform.
 #
 #
 # Uses:
 #	COMPILER, XL, RPATH, CC
-#	
+#
 # Requires:
 #	XL, RPATH, CC
 #
 # Sets:
-#	COMPILER, PLATFORM_CFLAGS, RPATH_OPT, 
+#	COMPILER, PLATFORM_CFLAGS, RPATH_OPT,
 #	LD, LD_STAT_LIB, LD_DYN_LIB, U_OPTS, OBJ, EXE, LIB, DLL, BYTE_ORDER,
 #	UUIDLIB, PLATFORMLIBS
 #
@@ -47,7 +49,7 @@
 #------------------------------------------------------------------------------
 include $(AAFBASE)/build/pdefs-Unix.mk
 
-## Now redifine copy so we don't blow out the TOC on static .a libs. 
+## Now redifine copy so we don't blow out the TOC on static .a libs.
 CP = ditto
 
 #------------------------------------------------------------------------------
@@ -66,10 +68,10 @@ PLATFORM_CFLAGS = -arch ppc
 #------------------------------------------------------------------------------
 # Linker command and options
 #------------------------------------------------------------------------------
-RPATH_OPT = 
+RPATH_OPT =
 
 # Command to link executable.
-LD = $(COMPILER) 
+LD = $(COMPILER)
 
 # Command to link static library
 ifndef LD_STAT_LIB
@@ -83,7 +85,7 @@ endif
 
 # UUID library to use
 ifndef UUIDLIB
-    UUIDLIB = 
+    UUIDLIB =
 endif
 
 
@@ -99,7 +101,7 @@ U_OPTS=no_unicode
 # Binary file extensions
 #------------------------------------------------------------------------------
 OBJ ?= .o
-EXE ?= 
+EXE ?=
 LIB ?= .a
 DLL ?= .dylib
 
@@ -112,13 +114,12 @@ DLL ?= .dylib
 BYTE_ORDER = -DBIGENDIAN=1
 
 #------------------------------------------------------------------------------
-#  Get OS revision and add version specific libs as needed. 
+#  Get OS revision and add version specific libs as needed.
 #------------------------------------------------------------------------------
 DARWIN_REV = $(shell uname -r  | cut -c 1 )
 ifeq ($(DARWIN_REV),6)
     PLATFORMLIBS = -ldl -lwchar
-else 
-    ## assuming >6
-    PLATFORMLIBS = 
+else
+    # assuming >6
+    PLATFORMLIBS =
 endif
-
