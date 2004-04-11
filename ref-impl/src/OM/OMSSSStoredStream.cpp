b17c80cce1e0df31c30c15e7d55a417eeb99437a
bakerian <>
2004-04-11 07:30:41 +0000

    reinstated precondition so that writing 0 bytes to essence data stream is prohibited

--- ref-impl/src/OM/OMSSSStoredStream.cpp
+++ ref-impl/src/OM/OMSSSStoredStream.cpp
@@ -100,11 +100,7 @@ void OMSSSStoredStream::write(const OMByte* data,
   TRACE("OMSSSStoredStream::write");
   PRECONDITION("Valid stream", _stream != 0);
   PRECONDITION("Valid data", data != 0);
-  //Ian Baker 3.Mar.2004
-  //precondition removed as a 0 length property is allowed and 
-  //with the SS library actually writing a 0 length property 
-  //prevents an exception with requried properites. 
- // PRECONDITION("Valid size", bytes > 0);
+  PRECONDITION("Valid size", bytes > 0);
 
 	bytesWritten = bytes;
   sresult resultCode = streamWrite( _stream, data, &bytesWritten);
@@ -202,6 +198,7 @@ void OMSSSStoredStream::close(void)
 {
   TRACE("OMSSSStoredStream::close");
   PRECONDITION("Valid stream", _stream != 0);
+
 
   	sresult status = SSTG_OK;
 
