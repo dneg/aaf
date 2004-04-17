023c749bb8f455909e69ac473ee16eeac8a0cfb0
jptrainor <>
2004-04-17 20:32:20 +0000

    Enumerators for new KLVDataDef and TaggedValueDataDef

---
+++ ref-impl/src/impl/ImplEnumAAFKLVDataDefs.h
@@ -0,0 +1,40 @@
+//@doc
+//@class    EnumAAFKLVDataDefs | Implementation class for EnumAAFKLVDataDefs
+#ifndef __ImplEnumAAFKLVDataDefs_h__
+#define __ImplEnumAAFKLVDataDefs_h__
+
+//=---------------------------------------------------------------------=
+//
+// This file was GENERATED for the AAF SDK
+//
+// $Id$ $Name$
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+//
+// The Original Code of this file is Copyright 1998-2004, Licensor of the
+// AAF Association.
+//
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
+
+#include "ImplAAFEnumerator.h"
+
+#include "ImplAAFKLVDataDefinition.h"
+
+typedef ImplAAFEnumerator<ImplAAFKLVDataDefinition> ImplEnumAAFKLVDataDefs;
+
+#endif // ! __ImplEnumAAFKLVDataDefs_h__
+
+
