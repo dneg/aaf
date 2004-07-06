7037c8beafed167b7245968e2b6dd1adafc8c4d4
tbingham <>
2004-07-06 20:19:19 +0000

    Second round of support for arrays and sets of scalars (latent).

--- ref-impl/src/OM/OMDataSet.cpp
+++ ref-impl/src/OM/OMDataSet.cpp
@@ -1,1 +1,62 @@
-// Nothing yet.
+//=---------------------------------------------------------------------=
+//
+// $Id$ $Name$
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+//
+// The Original Code of this file is Copyright 1998-2004, Licensor of the
+// AAF Association.
+//
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
+
+// @doc OMEXTERNAL
+// @author Tim Bingham | tjb | Avid Technology, Inc. | OMDataSet
+
+#include "OMDataSet.h"
+
+#include "OMAssertions.h"
+#include "OMStoredObject.h"
+
+OMDataSet::OMDataSet(const OMPropertyId propertyId,
+                     const OMStoredForm storedForm,
+                     const wchar_t* name)
+: OMProperty(propertyId, storedForm, name)
+{
+  TRACE("OMDataSet::OMDataSet");
+}
+
+OMDataSet::~OMDataSet(void)
+{
+  TRACE("OMDataSet::~OMDataSet");
+}
+
+void OMDataSet::save(void) const
+{
+  TRACE("OMDataSet::save");
+#if 0 // tjb - Not yet
+  store()->save(*this);
+#endif
+}
+
+void OMDataSet::restore(size_t externalSize)
+{
+  TRACE("OMDataSet::restore");
+#if 0 // tjb - Not yet
+  // tjb -- resize ?
+  store()->restore(*this, externalSize);
+  setPresent();
+#endif
+}
