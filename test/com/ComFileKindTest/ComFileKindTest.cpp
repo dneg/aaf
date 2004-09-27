375e35c163e287c56f0726f77d13896fde49a670
stuart_hc <>
2004-09-27 09:41:40 +0000

    Fix missing include for pathconf() call.

--- test/com/ComFileKindTest/ComFileKindTest.cpp
+++ test/com/ComFileKindTest/ComFileKindTest.cpp
@@ -33,6 +33,7 @@ using namespace std;
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>		// for pathconf()
 
 #include "AAFStoredObjectIDs.h"
 #include "AAFResult.h"
