2ac052731ded9101be1b49670238a4d5cfa4acd0
jptrainor <>
2005-01-07 03:17:45 +0000

    update license

--- Utilities/aaf2xtl/aif2xtllib/Aif2XtlObjectRemoveVisitor.h
+++ Utilities/aaf2xtl/aif2xtllib/Aif2XtlObjectRemoveVisitor.h
@@ -1,7 +1,26 @@
-// Copyright 2003 BBC Technology
-
-// $Id$
-// $Name$
+//=---------------------------------------------------------------------=
+//
+// $Id$ $Name$
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+//
+// The Original Code of this file is Copyright 1998-2004, Licensor of the
+// AAF Association.
+//
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Siemens SBS Media
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 #ifndef _Aif2XtlObjectRemove_h_
 #define _Aif2XtlObjectRemove_h_
