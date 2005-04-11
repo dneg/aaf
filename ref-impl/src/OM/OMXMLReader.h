dffa81508f640f573261c18e63c654981546818b
philipn <>
2005-04-11 15:06:40 +0000

    Added missing functionality

--- ref-impl/src/OM/OMXMLReader.h
+++ ref-impl/src/OM/OMXMLReader.h
@@ -27,8 +27,116 @@
 #define __OMXMLREADER_H__
 
 
-class OMXMLReader
+#include "XMLReader.h"
+#include "OMRawStorage.h"
+#include "OMWString.h"
+#include "OMList.h"
+
+
+class OMXMLIStream;
+class OMXMLAttribute;
+
+class OMXMLReader : public XMLReaderListener
 {
+public:
+    OMXMLReader(OMRawStorage* storage);
+    virtual ~OMXMLReader();
+
+    enum EventType
+    {
+        NONE,
+        NOTATION_DECL,
+        UNPARSED_ENTITY_DECL,
+        START_ELEMENT,
+        END_ELEMENT,
+        CHARACTERS
+    };
+
+    bool next();
+    bool nextElement();
+    bool nextEndElement();
+    void skipContent();
+    EventType getEventType();
+    bool elementEquals(const wchar_t* uri, const wchar_t* localName);
+
+    void getNotationDecl(const wchar_t*& name, const wchar_t*& publicID, const wchar_t*& systemID);
+    void getUnparsedEntityDecl(const wchar_t*& name, const wchar_t*& publicID, 
+        const wchar_t*& systemID, const wchar_t*& notationName);
+    void getStartElement(const wchar_t*& uri, const wchar_t*& localName, 
+        const OMList<OMXMLAttribute*>*& attributes);
+    void getEndElement(const wchar_t*& uri, const wchar_t*& localName);
+    void getCharacters(const char*& data, size_t& length);
+
+    const wchar_t* getPositionString();
+    
+    OMXMLAttribute* getAttribute(const OMList<OMXMLAttribute*>* attributes,
+        const wchar_t* nmspace, const wchar_t* localName);
+    
+public:
+    virtual void NotationDecl(const char* name, const char* publicID, const char* systemID);
+    virtual void UnparsedEntityDecl(const char* name, const char* publicID, const char* systemID, 
+        const char* notationName);
+    virtual void StartElement(const char* uri, const char* localName, 
+        const XMLAttribute* attributes);
+    virtual void EndElement(const char* uri, const char* localName);
+    virtual void Characters(const char* data, size_t length);
+
+private:
+    void cleanUp();
+
+    XMLReader*  _xmlReader;
+    
+    EventType   _eventType;
+    OMWString   _name;
+    OMWString   _publicID;
+    OMWString   _systemID;
+    OMWString   _notationName;
+    OMWString   _uri;
+    OMWString   _localName;
+    const char* _data;
+    size_t      _dataLength;
+    OMList<OMXMLAttribute*>   _attributes;
+    OMWString   _positionString;
 };
+
+
+class OMXMLAttribute
+{
+public:
+    friend class OMXMLReader;
+    
+public:
+    ~OMXMLAttribute();
+
+    const wchar_t* getNamespace() const;
+    const wchar_t* getLocalName() const;
+    const wchar_t* getValue() const;
+    
+protected:    
+    OMXMLAttribute(const char* nmspace, const char* localName, const char* value);
+
+private:
+    OMWString       _nmspace;
+    OMWString       _localName;
+    OMWString       _value;
+    
+};
+
+
+class OMXMLIStream : public XMLIStream
+{
+public:
+    OMXMLIStream(OMRawStorage* storage);
+    virtual ~OMXMLIStream();
+    
+    virtual OMUInt64 Read(OMByte* data, OMUInt64 count);
+    
+private:
+    OMRawStorage*   _storage;
+    OMUInt64        _position;
+    OMUInt64        _size;
+};
+
+
 
 #endif
