80bf9135e1bafae8d6fa6586bc3826aa297336af
philipn <>
2005-05-03 10:25:50 +0000

    Using the general purpose string conversion functions

--- ref-impl/src/OM/OMXMLWriter.cpp
+++ ref-impl/src/OM/OMXMLWriter.cpp
@@ -24,6 +24,7 @@
 
 #include "OMXMLWriter.h"
 #include "OMUtilities.h"
+#include "OMXMLUtilities.h"
 #include "OMAssertions.h"
 
 
@@ -78,8 +79,8 @@ OMXMLWriter::writeDocumentEnd()
 void 
 OMXMLWriter::writeElementStart(const wchar_t* ns, const wchar_t* localName)
 {
-    char* ns8 = convertWideString(ns);    
-    char* localName8 = convertWideString(localName);
+    char* ns8 = convertFromWideString(ns);    
+    char* localName8 = convertFromWideString(localName);
     
     _xmlWriter->WriteElementStart(ns8, localName8);
     
@@ -90,11 +91,11 @@ OMXMLWriter::writeElementStart(const wchar_t* ns, const wchar_t* localName)
 void 
 OMXMLWriter::declareNamespace(const wchar_t* ns, const wchar_t* prefix)
 {
-    char* ns8 = convertWideString(ns);    
+    char* ns8 = convertFromWideString(ns);    
     char* prefix8 = 0;
     if (prefix != 0)
     {
-        prefix8 = convertWideString(prefix);
+        prefix8 = convertFromWideString(prefix);
     }
     
     _xmlWriter->DeclareNamespace(ns8, prefix8);
@@ -112,10 +113,10 @@ OMXMLWriter::writeAttribute(const wchar_t* ns, const wchar_t* localName, const w
     char* ns8 = 0;
     if (ns != 0)
     {
-        ns8 = convertWideString(ns);
+        ns8 = convertFromWideString(ns);
     }
-    char* localName8 = convertWideString(localName);
-    char* value8 = convertWideString(value);
+    char* localName8 = convertFromWideString(localName);
+    char* value8 = convertFromWideString(value);
     
     _xmlWriter->WriteAttribute(ns8, localName8, value8);
     
@@ -133,9 +134,9 @@ OMXMLWriter::writeAttributeStart(const wchar_t* ns, const wchar_t* localName)
     char* ns8 = 0;
     if (ns != 0)
     {
-        ns8 = convertWideString(ns);
+        ns8 = convertFromWideString(ns);
     }
-    char* localName8 = convertWideString(localName);
+    char* localName8 = convertFromWideString(localName);
     
     _xmlWriter->WriteAttributeStart(ns8, localName8);
     
@@ -149,7 +150,7 @@ OMXMLWriter::writeAttributeStart(const wchar_t* ns, const wchar_t* localName)
 void 
 OMXMLWriter::writeAttributeContent(const wchar_t* value)
 {
-    char* value8 = convertWideString(value);
+    char* value8 = convertFromWideString(value);
     
     _xmlWriter->WriteAttributeContent(value8);
     
@@ -165,7 +166,7 @@ OMXMLWriter::writeAttributeEnd(void)
 void 
 OMXMLWriter::writeElementContent(const wchar_t* content, size_t length)
 {
-    char* content8 = convertWideString(content);    
+    char* content8 = convertFromWideString(content);    
     
     _xmlWriter->WriteElementContent(content8, strlen(content8));
     
@@ -181,7 +182,7 @@ OMXMLWriter::writeElementEnd(void)
 void 
 OMXMLWriter::writeComment(const wchar_t* comment)
 {
-    char* comment8 = convertWideString(comment);    
+    char* comment8 = convertFromWideString(comment);    
     
     _xmlWriter->WriteComment(comment8);
     
@@ -191,8 +192,8 @@ OMXMLWriter::writeComment(const wchar_t* comment)
 void 
 OMXMLWriter::writeProcInstruction(const wchar_t* target, const wchar_t* instruction)
 {
-    char* target8 = convertWideString(target);    
-    char* instruction8 = convertWideString(instruction);
+    char* target8 = convertFromWideString(target);    
+    char* instruction8 = convertFromWideString(instruction);
     
     _xmlWriter->WriteProcInstruction(target8, instruction8);
     
@@ -203,7 +204,7 @@ OMXMLWriter::writeProcInstruction(const wchar_t* target, const wchar_t* instruct
 void 
 OMXMLWriter::writeText(const wchar_t* text)
 {
-    char* text8 = convertWideString(text);    
+    char* text8 = convertFromWideString(text);    
     
     _xmlWriter->WriteComment(text8);
     
