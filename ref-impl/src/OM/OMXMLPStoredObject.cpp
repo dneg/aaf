22d322218c915e087ca732d871fc04f30e5a44ec
philipn <>
2005-07-21 17:02:38 +0000

    Changed saveString() to check character string length and missing terminator starting from position 0

--- ref-impl/src/OM/OMXMLPStoredObject.cpp
+++ ref-impl/src/OM/OMXMLPStoredObject.cpp
@@ -1804,26 +1804,20 @@ OMXMLPStoredObject::saveString(const OMByte* internalBytes, OMUInt16 internalSiz
         OMByteArray modifiedBytes; 
         const wchar_t* str = reinterpret_cast<const wchar_t*>(internalBytes);
         size_t size = internalSize / sizeof(wchar_t);
-        size_t count = size;
-        while (count > 0 && str[count - 1] != L'\0')
+        size_t len = 0;
+        while (len < size && str[len] != L'\0')
         {
-            count--;
+            len++;
         }
-        if (count == 0)
+        if (len == size)
         {
             const wchar_t n = L'\0';
             modifiedBytes.append(internalBytes, internalSize);
             modifiedBytes.append(reinterpret_cast<const OMByte*>(&n), sizeof(wchar_t));
             str = reinterpret_cast<const wchar_t*>(modifiedBytes.bytes());
-            size++;
             printf("Invalid string value encountered ('%ls') - string was not null terminated\n", str);
         }
-        else
-        {
-            size = count;
-        }
-            
-        if (size > 1)
+        if (len > 0)
         {
             if (stringRequiresEscaping(str))
             {
@@ -1840,7 +1834,7 @@ OMXMLPStoredObject::saveString(const OMByte* internalBytes, OMUInt16 internalSiz
             {
                 if (isElementContent)
                 {
-                    getWriter()->writeElementContent(str, size - 1); 
+                    getWriter()->writeElementContent(str, wcslen(str)); 
                 }
                 else
                 {
