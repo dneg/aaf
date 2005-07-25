d323184c0c0fecda27bb436c75ace5b82d964680
montrowe <>
2005-07-25 21:03:56 +0000

    Merge changes from Avid to enhance testing helper routines and extend testing interface -- more changes to follow

---
+++ test/com/ComModTestAAF/AAFWideString.cpp
@@ -0,0 +1,101 @@
+//=---------------------------------------------------------------------=
+//
+// $Id$ $Name$
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+//
+// The Original Code of this file is Copyright 1998-2005, Licensor of the
+// AAF Association.
+//
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
+
+#include "AAFWideString.h"
+
+#if defined(OS_MACOS10)
+
+#pragma warn_possunwant off
+
+int wcslen(const wchar_t*aString)
+{
+	int	len = -1;
+
+	wchar_t * p = (wchar_t *) aString - 1;
+	
+	do
+		len++;
+	while (*++p);
+	
+	return len;
+}
+
+int wcscmp(const wchar_t*string1, const wchar_t*strign2)
+{
+	const	wchar_t 		* p1 = (wchar_t *) string1 - 1;
+	const	wchar_t 		* p2 = (wchar_t *) strign2 - 1;
+	unsigned long		c1, c2;
+		
+	while ((c1 = *++p1) == (c2 = *++p2))
+		if (!c1)
+			return(0);
+
+	return(c1 - c2);
+}
+
+wchar_t* wcscpy(wchar_t*stringDest, const wchar_t*stringSrc)
+{
+	const	wchar_t * p = (wchar_t *) stringSrc - 1;
+			wchar_t * q = (wchar_t *) stringDest - 1;
+	
+	while (*++q = *++p);
+	
+	return(stringDest);
+}
+
+int wcsncmp(const wchar_t*stringDest, const wchar_t*stringSrc, const int length)
+{
+	const	wchar_t 	* p1 = (wchar_t *) stringDest - 1;
+	const	wchar_t 	* p2 = (wchar_t *) stringSrc - 1;
+	unsigned long	c1, c2;
+	int	n;
+	
+	n = length;
+	n++;
+	
+	while (--n)
+		if ((c1 = *++p1) != (c2 = *++p2))
+			return(c1 - c2);
+		else if (!c1)
+			break;
+	
+	return(0);
+}
+
+wchar_t* wcscat(wchar_t*stringDest, const wchar_t*stringSrc)
+{
+	const	wchar_t * p = (wchar_t *) stringSrc - 1;
+				wchar_t * q = (wchar_t *) stringDest - 1;
+	
+	while (*++q);
+	
+	q--;
+	
+	while (*++q = *++p);
+	
+	return(stringDest);
+}
+
+
+#endif  // if defined(OS_MACOS10)
---
+++ test/com/ComModTestAAF/AAFWideString.h
@@ -0,0 +1,47 @@
+//=---------------------------------------------------------------------=
+//
+// $Id$ $Name$
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+//
+// The Original Code of this file is Copyright 1998-2005, Licensor of the
+// AAF Association.
+//
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
+
+#include "AAFPlatform.h"
+
+
+#if defined(OS_MACOS10)
+// Do not declare the following functions when building with
+// CodeWarrior 9 or later.
+#if defined(COMPILER_MWERKS) && (__MWERKS__ < 0x3200)
+int wcslen(const wchar_t*aString);
+int wcscmp(const wchar_t*string1, const wchar_t*strign2);
+wchar_t *wcscpy(wchar_t*stringDest, const wchar_t*stringSrc);
+wchar_t *wcscat(wchar_t*stringDest, const wchar_t*stringSrc);
+int wcsncmp(const wchar_t*stringDest, const wchar_t*stringSrc, const int length);
+//#include <stdlib.h>
+//int wcstombs(char *s, wchar_t *pwcs, unsigned long n);
+//int mbstowcs(wchar_t *s, char *pwcs, unsigned long n);
+#else
+#include <wchar.h>
+#endif
+#else
+#include <wchar.h>
+//#undef _MSL_NO_WCHART_C_SUPPORT
+#endif
+
