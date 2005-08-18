8f5ee54767f43af0ee01d528e44a2d0f968399fc
tbingham <>
2005-08-18 21:09:44 +0000

    @Add isOperationalPattern().@ [483 - tbingham]

--- DevUtils/MXFDump/MXFDump.cpp
+++ DevUtils/MXFDump/MXFDump.cpp
@@ -1199,6 +1199,18 @@ void dumpMxfOperationalPattern(const char* label, mxfFile infile)
   fprintf(stdout, "\n");
 }
 
+mxfByte opPrefix[] =
+{0x06, 0x0e, 0x2b, 0x34, 0x04, 0x01, 0x01, 0x01, 0x0d, 0x01, 0x02, 0x01};
+
+bool isOperationalPattern(mxfKey& k)
+{
+  bool result = false;
+  if (memcmp(&k, &opPrefix, sizeof(opPrefix)) == 0) {
+    result = true;
+  }
+  return result;
+}
+
 void printGeneralizedOperationalPattern(mxfKey& k, FILE* outfile)
 {
   mxfByte itemComplexity = k[12];
