832c329dd952491b24ad2391ee3f8fbdd512b70d
tbingham <>
2005-08-18 20:25:25 +0000

    @Add copyright notices (some generated).@ [28 - tbingham]

--- DevUtils/MXFDump/MXFMetaDictionary.h
+++ DevUtils/MXFDump/MXFMetaDictionary.h
@@ -1,8 +1,34 @@
+//=---------------------------------------------------------------------=
+//
+// $Id$ $Name$
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+//
+// The Original Code of this file is Copyright 1998-2005, Licensor of the
+// AAF Association.
+//
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
+
 // This is an automatically generated file.
+//
+// Source data (xmldict.xml) by BBC R&D, Matt Beard and Metaglue Corporation.
 //
 // Meta definition of the MXF object model.
 //
-// This file was generated on Wed Dec 15 17:52:45 EST 2004
+// This file was generated on Fri Aug 12 15:44:31 EDT 2005
 
 //
 // MXF_LABEL(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p)
