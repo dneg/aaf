f3ed73e0ef1b71da8af39d99000bbc0f88d58464
tbingham <>
2005-08-18 21:28:32 +0000

    @Checkpoint code for dumping extension-only dictionary.@ [669 - tbingham]

--- DevUtils/MXFDump/MXFDump.cpp
+++ DevUtils/MXFDump/MXFDump.cpp
@@ -1337,6 +1337,107 @@ void dumpMxfRational(const char* label, mxfFile infile)
   fprintf(stdout, "\n");
 }
 
+void dumpMxfBoolean(const char* label,
+                    const char* trueLabel,
+                    const char* falseLabel,
+                    mxfFile infile)
+{
+  mxfUInt08 b;
+  readMxfUInt08(b, infile);
+  if (b == 0x00) {
+    fprintf(stdout, "%20s = %s\n", label, falseLabel);
+  } else {
+    fprintf(stdout, "%20s = %s\n", label, trueLabel);
+    if (b != 0xff) {
+      mxfWarning(currentKey,
+                 keyPosition,
+                 "Invalid value for boolean, "
+                 "0x%"MXFPRIx08" interpreted as true",
+                 b);
+    }
+  }
+}
+
+void printString(FILE* f, mxfUInt16* str)
+{
+  fprintf(stdout, "\"");
+  mxfUInt16 character;
+  while ((character = *str++) != 0) {
+    fprintf(stdout, "%c", (char)(character)); // tjb - hack
+  }
+  fprintf(stdout, "\"\n");
+}
+
+void readString(mxfUInt16* buffer, mxfUInt32 bufferSize, mxfFile infile)
+{
+  for (mxfUInt32 i = 0; i < bufferSize; i++) {
+    readMxfUInt16(buffer[i], infile);
+  }
+}
+
+void dumpMxfString(const char* label, mxfLength& remaining, mxfFile infile)
+{
+  mxfUInt16 length; // in bytes, including terminator
+  readMxfUInt16(length, infile);
+  remaining = remaining - sizeof(mxfUInt16);
+  mxfUInt32 characterCount = length / sizeof(mxfUInt16);
+
+  mxfUInt16* buffer = new mxfUInt16[characterCount];
+  readString(buffer, characterCount, infile);
+  remaining = remaining - length; 
+
+  fprintf(stdout, "%20s = ", label);
+  printString(stdout, buffer);
+
+  delete [] buffer;
+}
+
+void dumpExtensionDefinition(const char* /* label */,
+                             mxfLength& remaining,
+                             mxfFile infile)
+{
+  dumpMxfLabel("identification", infile);
+  remaining = remaining - sizeof(mxfKey);
+
+  dumpMxfString("name", remaining, infile);
+
+  dumpMxfString("description", remaining, infile);
+}
+
+void dumpExtensionClass(const char* label,
+                        mxfLength& remaining,
+                        mxfFile infile)
+{
+  dumpExtensionDefinition(label, remaining, infile);
+
+  dumpMxfLabel("parent class", infile);
+  remaining = remaining - sizeof(mxfKey);
+
+  dumpMxfBoolean("concrete/abstract", "concrete", "abstract", infile);
+  remaining = remaining - sizeof(mxfUInt08);
+}
+
+void dumpExtensionProperty(const char* label,
+                           mxfLength& remaining,
+                           mxfFile infile)
+{
+  dumpExtensionDefinition(label, remaining, infile);
+
+  dumpMxfLabel("type", infile);
+  remaining = remaining - sizeof(mxfKey);
+
+  dumpMxfBoolean("optional/required", "optional", "required", infile);
+  remaining = remaining - sizeof(mxfUInt08);
+}
+
+void dumpExtensionType(const char* label,
+                       mxfLength& remaining,
+                       mxfFile infile)
+{
+  dumpExtensionDefinition(label, remaining, infile);
+
+}
+
 void dumpMxfLabel(const char* label, mxfFile infile)
 {
   mxfKey k;
@@ -1690,6 +1791,18 @@ const mxfKey ObjectDirectory =
   {0x96, 0x13, 0xb3, 0x8a, 0x87, 0x34, 0x87, 0x46,
    0xf1, 0x02, 0x96, 0xf0, 0x56, 0xe0, 0x4d, 0x2a};
 
+const mxfKey ClassExtensions = 
+  {0x87 ,0x3D ,0xAC ,0x1D ,0xEA ,0xBA ,0x35 ,0x46,
+   0x6F ,0xFB ,0x42 ,0xAE ,0x75 ,0xDA ,0x7A ,0x4D};
+
+const mxfKey PropertyExtensions =
+  {0x84, 0xb1, 0x3f, 0xa2, 0xfd, 0x65, 0x80, 0x13,
+   0x03, 0x6d, 0x56, 0x90, 0xaa, 0x97, 0xca, 0x43};
+
+const mxfKey TypeExtensions =
+  {0x82, 0x3B, 0x0B, 0xEA, 0x9E, 0xA3, 0x55, 0xDD,
+   0x4A, 0x40, 0xDC, 0x02, 0x7A, 0x75, 0x87, 0x45};
+
 // Define map key <-> key name
 
 #define MXF_DEFINE_PACK_KEY(n, k)     {#n, &n},
@@ -1712,6 +1825,9 @@ struct Key {
   {"MXFAES3AudioEssenceDescriptor", &MXFAES3AudioEssenceDescriptor},
   //
   {"ObjectDirectory", &ObjectDirectory},
+  {"ClassExtensions", &ClassExtensions},
+  {"PropertyExtensions", &PropertyExtensions},
+  {"TypeExtensions", &TypeExtensions},
   //
   {"bogus", 0}
 };
@@ -5169,6 +5285,45 @@ void printObjectDirectory(mxfKey& /* k */,
   }
 }
 
+void printClassExtensions(mxfKey& k, mxfLength& len, mxfFile infile);
+
+void printClassExtensions(mxfKey& /* k */,
+                          mxfLength& len,
+                          mxfFile infile)
+{
+  mxfLength remaining = len;
+  while (remaining > 0) {
+    fprintf(stdout, "\n");
+    dumpExtensionClass("class", remaining, infile);
+  }
+}
+
+void printPropertyExtensions(mxfKey& k, mxfLength& len, mxfFile infile);
+
+void printPropertyExtensions(mxfKey& /* k */,
+                             mxfLength& len,
+                             mxfFile infile)
+{
+  mxfLength remaining = len;
+  while (remaining > 0) {
+    fprintf(stdout, "\n");
+    dumpExtensionProperty("property", remaining, infile);
+  }
+}
+
+void printTypeExtensions(mxfKey& k, mxfLength& len, mxfFile infile);
+
+void printTypeExtensions(mxfKey& /* k */,
+                         mxfLength& len,
+                         mxfFile infile)
+{
+  mxfLength remaining = len;
+  while (remaining > 0) {
+    fprintf(stdout, "\n");
+    dumpExtensionType("type", remaining, infile);
+  }
+}
+
 void printRandomIndex(mxfKey& k, mxfLength& len, mxfFile infile);
 
 void printRandomIndex(mxfKey& /* k */, mxfLength& len, mxfFile infile)
@@ -5306,6 +5461,12 @@ void mxfDumpKLV(mxfKey& k, mxfLength& len, mxfFile infile)
     printSystemMetadata(k, len, infile);
   } else if (memcmp(&ObjectDirectory, &k, sizeof(mxfKey)) == 0) {
     printObjectDirectory(k, len, infile);
+  } else if (memcmp(&ClassExtensions, &k, sizeof(mxfKey)) == 0) {
+    printClassExtensions(k, len, infile);
+  } else if (memcmp(&PropertyExtensions, &k, sizeof(mxfKey)) == 0) {
+    printPropertyExtensions(k, len, infile);
+  } else if (memcmp(&TypeExtensions, &k, sizeof(mxfKey)) == 0) {
+    printTypeExtensions(k, len, infile);
   } else if (memcmp(&V10IndexTableSegment, &k, sizeof(mxfKey)) == 0) {
     printIndexTable(k, len, infile);
   } else if (memcmp(&IndexTableSegment, &k, sizeof(mxfKey)) == 0) {
