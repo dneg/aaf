540dec477e84c5cd6b203a99d74e58d9f90de448
tbingham <>
2005-08-18 20:41:09 +0000

    @Add local key for the primary package property of the preface.@ [180 - tbingham]

--- DevUtils/MXFDump/MXFDump.cpp
+++ DevUtils/MXFDump/MXFDump.cpp
@@ -778,6 +778,7 @@ struct {
   // All objects
   {"InstanceUID",          0x3c0a},
   // Preface
+  {"Primary Package",      0x3b08},
   {"Operational Pattern",  0x3b09},
   {"Essence Containers",   0x3b0a},
   {"DM Schemes",           0x3b0b},
