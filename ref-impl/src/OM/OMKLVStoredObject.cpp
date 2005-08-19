91ba804924cc40237422afcd87d0c177238da5b0
tbingham <>
2005-08-19 19:23:04 +0000

    @Default KAGSize for body partition is 0x200 (akharkev).@ [59 - tbingham]

--- ref-impl/src/OM/OMKLVStoredObject.cpp
+++ ref-impl/src/OM/OMKLVStoredObject.cpp
@@ -1732,7 +1732,7 @@ void OMKLVStoredObject::writeBodyPartition(OMRawStorage* store)
   OMKLVKey ClosedBodyPartitionPackKey =
     {0x06, 0x0e, 0x2b, 0x34, 0x02, 0x05, 0x01, 0x01,
      0x0d, 0x01, 0x02, 0x01, 0x01, 0x03, 0x02, 0x00};
-  OMUInt32 KAGSize = 0x100;
+  OMUInt32 KAGSize = 0x200;
   writePartition(store, ClosedBodyPartitionPackKey, KAGSize, reorderBytes);
   OMUInt64 currentPosition = store->position();
   fill(store, currentPosition, KAGSize);
