188bff5406de296f35eee089582ed71af862b608
tbingham <>
2005-08-19 19:11:18 +0000

    @Checkpoint work to implement stored objects in KLV.@ [11 - tbingham]

--- ref-impl/include/OM/OMKLVStoredObject.h
+++ ref-impl/include/OM/OMKLVStoredObject.h
@@ -11,7 +11,7 @@
 // the License for the specific language governing rights and limitations
 // under the License.
 // 
-// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// The Original Code of this file is Copyright 1998-2002, Licensor of the
 // AAF Association.
 // 
 // The Initial Developer of the Original Code of this file and the
@@ -211,6 +211,8 @@ public:
     // @cmember Create an <c OMStoredStream> representing the property
     //          <p stream> contained within this <c OMKLVStoredObject>.
   virtual OMStoredStream* createStoredStream(const OMDataStream& property);
+
+  static bool readHeaderPartition(OMRawStorage* store);
 
 private:
   // @access Private members.
