9c120fa58be1b61c9d21309334b2e91827d6132d
tbingham <>
2005-08-26 20:25:30 +0000

    Add id classification and mepping functions.

--- ref-impl/src/OM/OMDictionary.cpp
+++ ref-impl/src/OM/OMDictionary.cpp
@@ -146,3 +146,135 @@ void OMDictionary::finalize(void)
   }
   delete _propertyDefinitions;
 }
+
+bool OMDictionary::isAbstractMeta(const OMObjectIdentification& id) const
+{
+  TRACE("OMDictionary::isAbstractMeta");
+  bool result;
+  if (id == Class_Definition) {
+    result = true;
+  } else if (id == Class_TypeDefinition) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
+}
+
+bool OMDictionary::isConcreteMeta(const OMObjectIdentification& id) const
+{
+  TRACE("OMDictionary::isConcreteMeta");
+  bool result;
+  if (id == Class_MetaDictionary) {
+    result = true;
+  } else if (id == Class_ClassDefinition) {
+    result = true;
+  } else if (id == Class_PropertyDefinition) {
+    result = true;
+  } else if (id == Class_TypeDefinitionStrongObjectReference) {
+    result = true;
+  } else if (id == Class_TypeDefinitionWeakObjectReference) {
+    result = true;
+  } else if (id == Class_TypeDefinitionSet) {
+    result = true;
+  } else if (id == Class_TypeDefinitionFixedArray) {
+    result = true;
+  } else if (id == Class_TypeDefinitionVaryingArray) {
+    result = true;
+  } else if (id == Class_TypeDefinitionRecord) {
+    result = true;
+  } else if (id == Class_TypeDefinitionStream) {
+    result = true;
+  } else if (id == Class_TypeDefinitionRename) {
+    result = true;
+  } else if (id == Class_TypeDefinitionIndirect) {
+    result = true;
+  } else if (id == Class_TypeDefinitionOpaque) {
+    result = true;
+  } else if (id == Class_TypeDefinitionCharacter) {
+    result = true;
+  } else if (id == Class_TypeDefinitionString) {
+    result = true;
+  } else if (id == Class_TypeDefinitionInteger) {
+    result = true;
+  } else if (id == Class_TypeDefinitionEnumerated) {
+    result = true;
+  } else if (id == Class_TypeDefinitionExtendibleEnumerated) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
+}
+
+bool OMDictionary::isMeta(const OMObjectIdentification& id) const
+{
+  TRACE("OMDictionary::isMeta");
+  bool result;
+  if (isAbstractMeta(id)) {
+    result = true;
+  } else if (isConcreteMeta(id)) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
+}
+
+void OMDictionary::mapFromKLV(OMPropertyId& id)
+{
+  TRACE("OMDictionary::mapFromKLV");
+
+  if (id == 0x3f06) {        // MXFEssenceContainerData::IndexSID
+    id = 0x2b01;             // AAFEssenceData::SampleIndex
+  } else if (id == 0x3f07){  // MXFEssenceContainerData::BodySID
+    id =  0x2702;            // AAFEssenceData::Data
+  }
+}
+
+void OMDictionary::mapToKLV(OMPropertyId& id)
+{
+  TRACE("OMDictionary::mapToKLV");
+
+  if (id == 0x2b01) {        // AAFEssenceData::SampleIndex
+    id = 0x3f06;             // MXFEssenceContainerData::IndexSID
+  } else if (id == 0x2702) { // AAFEssenceData::Data
+    id = 0x3f07;             // MXFEssenceContainerData::BodySID
+  }
+}
+
+//{04070200-0000-0000-060e-2b3401010102}
+static const OMUniqueObjectIdentification kAAFPropID_EssenceData_Data =
+{0x04070200, 0x0000, 0x0000, {0x06, 0x0e, 0x2b, 0x34, 0x01, 0x01, 0x01, 0x02}};
+//{06010102-0100-0000-060e-2b3401010102}
+static const OMUniqueObjectIdentification kAAFPropID_EssenceData_SampleIndex =
+{0x06010102, 0x0100, 0x0000, {0x06, 0x0e, 0x2b, 0x34, 0x01, 0x01, 0x01, 0x02}};
+
+// {01030404-0000-0000-060E-2B3401010104}
+static const OMUniqueObjectIdentification kAAFPropID_EssenceData_BodySID =
+{0x01030404, 0x0000, 0x0000, {0x06, 0x0e, 0x2b, 0x34, 0x01, 0x01, 0x01, 0x04}};
+// {01030405-0000-0000-060E-2B3401010104}
+static const OMUniqueObjectIdentification kAAFPropID_EssenceData_IndexSID =
+{0x01030405, 0x0000, 0x0000, {0x06, 0x0e, 0x2b, 0x34, 0x01, 0x01, 0x01, 0x04}};
+
+void OMDictionary::mapFromKLV(OMObjectIdentification& id)
+{
+  TRACE("OMDictionary::mapFromKLV");
+
+  if (id == kAAFPropID_EssenceData_IndexSID) {
+    id = kAAFPropID_EssenceData_SampleIndex;
+  } else if (id == kAAFPropID_EssenceData_BodySID) {
+    id = kAAFPropID_EssenceData_Data;
+  }
+}
+
+void OMDictionary::mapToKLV(OMObjectIdentification & id)
+{
+  TRACE("OMDictionary::mapToKLV");
+
+  if (id == kAAFPropID_EssenceData_SampleIndex) {
+    id = kAAFPropID_EssenceData_IndexSID;
+  } else if (id == kAAFPropID_EssenceData_Data) {
+    id = kAAFPropID_EssenceData_BodySID;
+  }
+}
