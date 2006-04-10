1f7444e7a65d94462cd6404715b1b33a18ec45d0
tbingham <>
2006-04-10 15:53:15 +0000

    Provide a definition for LD_DYN_LIB. Fixes cross-linking.

--- build/pdefs-UniversalDarwin.mk
+++ build/pdefs-UniversalDarwin.mk
@@ -1,3 +1,4 @@
 include $(AAFBASE)/build/pdefs-PPCDarwin.mk
 
 PLATFORM_CFLAGS = -arch ppc -arch i386
+LD_DYN_LIB = $(COMPILER) -arch ppc -arch i386 -dynamiclib
--- build/pdefs-i386Darwin.mk
+++ build/pdefs-i386Darwin.mk
@@ -1,3 +1,4 @@
 include $(AAFBASE)/build/pdefs-PPCDarwin.mk
 
 PLATFORM_CFLAGS = -arch i386
+LD_DYN_LIB = $(COMPILER) -arch i386 -dynamiclib
