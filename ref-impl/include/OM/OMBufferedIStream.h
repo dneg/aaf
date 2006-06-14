d03fed187309b91cf41a3f0f5e71a52024674f21
tbingham <>
2006-06-14 20:25:27 +0000

    Port to 64-bit Windows.

--- ref-impl/include/OM/OMBufferedIStream.h
+++ ref-impl/include/OM/OMBufferedIStream.h
@@ -13,7 +13,7 @@
 // the License for the specific language governing rights and limitations
 // under the License.
 //
-// The Original Code of this file is Copyright 1998-2004, Licensor of the
+// The Original Code of this file is Copyright 1998-2006, Licensor of the
 // AAF Association.
 //
 // The Initial Developer of the Original Code of this file and the
@@ -83,7 +83,7 @@ public:
   virtual HRESULT STDMETHODCALLTYPE Clone(IStream **ppstm);
 
     // @cmember Constructor.
-  OMBufferedIStream(IStream* stream, size_t bufferSize);
+  OMBufferedIStream(IStream* stream, OMUInt32 bufferSize);
 
     // @cmember Destructor.
   virtual ~OMBufferedIStream(void);
@@ -104,6 +104,7 @@ private:
 
 };
 
-#endif // ! OM_NO_STRUCTURED_STORAGE
+#endif // !OM_NO_STRUCTURED_STORAGE
 
 #endif // !OMBUFFEREDISTREAM_H
+
--- ref-impl/src/OM/OMBufferedIStream.cpp
+++ ref-impl/src/OM/OMBufferedIStream.cpp
@@ -13,7 +13,7 @@
 // the License for the specific language governing rights and limitations
 // under the License.
 //
-// The Original Code of this file is Copyright 1998-2004, Licensor of the
+// The Original Code of this file is Copyright 1998-2006, Licensor of the
 // AAF Association.
 //
 // The Initial Developer of the Original Code of this file and the
@@ -161,7 +161,7 @@ OMBufferedIStream::Clone(IStream **ppstm)
   return _stream->Clone(ppstm);
 }
 
-OMBufferedIStream::OMBufferedIStream(IStream* stream, size_t bufferSize)
+OMBufferedIStream::OMBufferedIStream(IStream* stream, OMUInt32 bufferSize)
 : OMBufferedStream(bufferSize),
   _stream(stream),
   _referenceCount(1)
