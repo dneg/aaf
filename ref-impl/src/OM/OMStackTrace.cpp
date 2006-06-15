f178e51985c9fcc7325c254377f0753a0ceccd07
tbingham <>
2006-06-15 03:34:53 +0000

    Port to 64-bit Windows.

--- ref-impl/src/OM/OMStackTrace.cpp
+++ ref-impl/src/OM/OMStackTrace.cpp
@@ -13,7 +13,7 @@
 // the License for the specific language governing rights and limitations
 // under the License.
 //
-// The Original Code of this file is Copyright 1998-2004, Licensor of the
+// The Original Code of this file is Copyright 1998-2006, Licensor of the
 // AAF Association.
 //
 // The Initial Developer of the Original Code of this file and the
@@ -32,7 +32,7 @@
 #include "OMPortability.h"
 #include "OMOStream.h"
 
-#if defined(OM_OS_WINDOWS)
+#if defined(OM_OS_WINDOWS) && defined(_M_IX86)
 
 #include "OMUtilities.h"
 #include "OMAssertions.h"
