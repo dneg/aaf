58e38ce9cfef5069e32f728a1a08b98e5d76db2f
heydowns <>
2006-06-27 20:32:21 +0000

    Add back the accidentally removed Concrete-ness of DataEssenceDescriptor

--- ref-impl/include/ref-api/AAF.h
+++ ref-impl/include/ref-api/AAF.h
@@ -5321,6 +5321,29 @@ DECLARE_INTERFACE_(IAAFDataEssenceDescriptor, IUnknown)
   /* *** IAAFDataEssenceDescriptor methods *** */
 
 
+
+  //***********************************************************
+  //
+  // Initialize()
+  //
+  /// Initializes a newly allocated, empty IAAFDataEssenceDescriptor-supporting
+  /// object.  This method must be called after allocation, and before
+  /// any other method can be called.
+  ///
+  /// Succeeds if:
+  /// - Initialize() has not yet been called on this object.
+  ///
+  /// This method will return the following codes.  If more than one of
+  /// the listed errors is in effect, it will return the first one
+  /// encountered in the order given below:
+  /// 
+  /// AAFRESULT_SUCCESS
+  ///   - succeeded.  (This is the only code indicating success.)
+  ///
+  /// AAFRESULT_ALREADY_INITIALIZED
+  ///   - Initialize() has already been called on this object.
+  STDMETHOD(Initialize) (THIS) PURE;
+
   //***********************************************************
   //
   // SetDataEssenceCoding()
@@ -5372,6 +5395,7 @@ DECLARE_INTERFACE_(IAAFDataEssenceDescriptor, IUnknown)
   END_INTERFACE
 };
 #endif // __IAAFDataEssenceDescriptor_INTERFACE_DEFINED__
+
 
 
 // IAAFDefObject
