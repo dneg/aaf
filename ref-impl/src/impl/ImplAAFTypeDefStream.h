462c3a8b0de8be939a28b96ff2fefbb06de0b346
akharkev <>
2007-01-17 22:14:21 +0000

    Make GetStreamPropertyValue() available to derived classes.

--- ref-impl/src/impl/ImplAAFTypeDefStream.h
+++ ref-impl/src/impl/ImplAAFTypeDefStream.h
@@ -15,7 +15,7 @@
 // the License for the specific language governing rights and limitations
 // under the License.
 //
-// The Original Code of this file is Copyright 1998-2006, Licensor of the
+// The Original Code of this file is Copyright 1998-2007, Licensor of the
 // AAF Association.
 //
 // The Initial Developer of the Original Code of this file and the
@@ -300,7 +300,7 @@ public:
         (ImplAAFTypeDef ** ppRawTypeDef);
 
 
-private:
+protected:
 
 
   //****************
