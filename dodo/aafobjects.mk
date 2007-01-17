60686e142c6b3120f462e263101bc06b0f799e3c
akharkev <>
2007-01-17 21:59:22 +0000

    Add AAFTypeDefMXFEssenceStream.

--- dodo/aafobjects.mk
+++ dodo/aafobjects.mk
@@ -13,7 +13,7 @@
 # the License for the specific language governing rights and limitations
 # under the License.
 #
-# The Original Code of this file is Copyright 1998-2006, Licensor of the
+# The Original Code of this file is Copyright 1998-2007, Licensor of the
 # AAF Association.
 #
 # The Initial Developer of the Original Code of this file and the
@@ -166,6 +166,7 @@ HUMAN_TYPED_IMPL="\
 	AAFTypeDefFixedArray \
 	AAFTypeDefIndirect \
 	AAFTypeDefInt \
+	AAFTypeDefMXFEssenceStream \
 	AAFTypeDefObjectRef \
 	AAFTypeDefOpaque \
 	AAFTypeDefRecord \
