6709aec28a003f40efa9349b397492a1ea472c3c
akharkev <>
2007-01-26 18:40:48 +0000

    Rename AAFTypeDefStream2 to AAFTypeDefStream3.

--- dodo/aafobjects.mk
+++ dodo/aafobjects.mk
@@ -56,7 +56,7 @@ AAFCOMINTERFACESONLY="\
 	AAFStreamAccess \
 	AAFTimelineMobSlot2 \
 	AAFTypeDefStreamEx \
-	AAFTypeDefStream2 \
+	AAFTypeDefStream3 \
 	AAFTypeDefVariableArrayEx "
 
 # These are the impl files that are typed by humans.  Be sure to
