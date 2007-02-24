6557c7973e4904deee1b8e7c2dbb90a743d7474f
akharkev <>
2007-02-24 02:00:47 +0000

    One more time, fix ensureAbsent() to pass a key to setValue() via a void pointer.

--- ref-impl/src/OM/OMWeakRefSetPropertyT.h
+++ ref-impl/src/OM/OMWeakRefSetPropertyT.h
@@ -340,7 +340,7 @@ bool OMWeakReferenceSetProperty<Key, ReferencedObject>::ensureAbsent(
   SetElement* element = 0;
   bool result = _set.find(identification, &element);
   if (result) {
-    element->setValue(nullOMUniqueObjectIdentification, 0);
+    element->setValue(&nullOMUniqueObjectIdentification, 0);
     _set.remove(identification);
   }
 
