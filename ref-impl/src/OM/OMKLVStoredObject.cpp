f6326027deaf67bed46aaa063a79cc1fc06105f5
akharkev <>
2007-03-14 21:12:54 +0000

    Temporary hack - ignore the Preface::PrimaryPackage property - we don't yet have its definition in the dictionary.

--- ref-impl/src/OM/OMKLVStoredObject.cpp
+++ ref-impl/src/OM/OMKLVStoredObject.cpp
@@ -1686,6 +1686,11 @@ void OMKLVStoredObject::flatRestore(const OMPropertySet& properties)
     } else if ((!properties.isAllowed(pid)) && (pid >= 0x8000)) { // HACK4MEIP2
       // Dark extension
       _storage->skipV(length);  // discard value !! tjb
+    } else if (pid == 0x3b08) { // Preface::PrimaryPackage
+      // HACK4MXFLIB
+      // Temporary hack - ignore this property - we don't yet have
+      // compiled-in knowlege of its 'type'
+      _storage->skipV(length);
     } else {
     OMProperty* p = properties.get(pid);
     ASSERT("Valid property", p != 0);
