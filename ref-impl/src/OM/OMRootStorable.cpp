e6b741679819e684d095618b9a7e20a1b3a21488
akharkev <>
2007-03-30 18:03:55 +0000

    Dictionaryless files hack - do not close the dictionary if not present.

--- ref-impl/src/OM/OMRootStorable.cpp
+++ ref-impl/src/OM/OMRootStorable.cpp
@@ -13,7 +13,7 @@
 // the License for the specific language governing rights and limitations
 // under the License.
 //
-// The Original Code of this file is Copyright 1998-2004, Licensor of the
+// The Original Code of this file is Copyright 1998-2007, Licensor of the
 // AAF Association.
 //
 // The Initial Developer of the Original Code of this file and the
@@ -74,7 +74,11 @@ void OMRootStorable::close(void)
   TRACE("OMRootStorable::close");
 
   _clientRoot->close();
-  _dictionary->close();
+  // HACK4MEIP2
+  OMStorable* d = _dictionary;
+  if (d != 0) {
+    d->close();
+  }
 }
 
   // @mfunc Restore the contents of an <c OMRootStorable>.
