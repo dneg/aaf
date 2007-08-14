75d920df5d9d478586a45c7df91592c6fbd09efb
stuart_hc <>
2007-08-14 16:08:19 +0000

    Use 64bit safe seeking on WIN32 platform - needed by Win-g++ build

--- ref-impl/plugins/CAAFEssenceRIFFWAVEStream.cpp
+++ ref-impl/plugins/CAAFEssenceRIFFWAVEStream.cpp
@@ -46,21 +46,45 @@ typedef off64_t off_t;
 #define ftello ftello64
 
 #elif defined (_WIN32)		// Win32 differences
+
+#if defined (COMPILER_MSC)
 typedef __int64 off_t;
 // __OFF_T_DEFINED prevents MSVC redefining off_t in wchar.h
 // which is included by utf8.h.
 typedef long _off_t;
 #define _OFF_T_DEFINED
+#endif
 
-#define fseeko(fp, off, whence) fsetpos(fp, &off)
-static off_t ftello (FILE* fp)
+#define fseeko(fp, off, whence) win_fseeko(fp, off, whence)
+static int win_fseeko(FILE *fp, off_t off, int whence)
 {
-  off_t position;
+        if (whence == SEEK_SET) {
+                fpos_t pos = off;
+                return fsetpos(fp, &pos);
+        }
+        if (whence == SEEK_CUR) {
+                // First get current pos, then add required offset
+                fpos_t pos = 0;
+                if (0 != fgetpos(fp, &pos))
+                        return -1;
+                pos += off;
+                return fsetpos(fp, &pos);
+        }
 
-  if(0 != fgetpos(fp, &position))
+        // To reach here, whence is SEEK_END
+        return fseek(fp, off, whence);
+}
+
+#define ftello(fp) win_ftello(fp)
+static off_t win_ftello(FILE* fp)
+{
+  fpos_t pos;
+
+  if(0 != fgetpos(fp, &pos))
      return -1;
 
-  return position;
+  off_t result = pos;
+  return result;
 }
 
 #else
