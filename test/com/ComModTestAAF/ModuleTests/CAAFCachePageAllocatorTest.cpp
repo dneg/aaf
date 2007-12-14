dae543928b157166e6ed08e0a8dee51de60f2c4b
akharkev <>
2007-12-14 16:05:08 +0000

    Use GenerateTestFileName() to generate test file names.

--- test/com/ComModTestAAF/ModuleTests/CAAFCachePageAllocatorTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFCachePageAllocatorTest.cpp
@@ -13,7 +13,7 @@
 // the License for the specific language governing rights and limitations
 // under the License.
 //
-// The Original Code of this file is Copyright 1998-2005, Licensor of the
+// The Original Code of this file is Copyright 1998-2007, Licensor of the
 // AAF Association.
 //
 // The Initial Developer of the Original Code of this file and the
@@ -162,7 +162,9 @@ HRESULT CAAFCachePageAllocator_test(testMode_t /* mode */,
     testRawStorageType_t /* rawStorageType */,
     aafProductIdentification_t productID)
 {
-  const aafCharacter * pFileName = L"CAAFCachePageAllocatorTest.aaf";
+  const size_t fileNameBufLen = 128;
+  aafWChar pFileName[ fileNameBufLen ] = L"";
+  GenerateTestFileName( productID.productName, fileKind, fileNameBufLen, pFileName );
 
   (void)RemoveTestFile(pFileName );
 
