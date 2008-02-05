4ad8d723c888edfd196ad9e76784f4f50eacc9df
akharkev <>
2008-02-05 22:10:21 +0000

    Change AddMasterSlot to reference new SourceClip directly, without wrapping it in a Sequence. This reverts the previous changes 1.61 and 1.62, which were made in attempt to comply with MXF structural metadata requirements. This altered the behavior of existing COM interfaces breaking COM rules and affecting some toolkit clients.

--- ref-impl/src/impl/ImplAAFMasterMob.cpp
+++ ref-impl/src/impl/ImplAAFMasterMob.cpp
@@ -13,7 +13,7 @@
 // the License for the specific language governing rights and limitations
 // under the License.
 //
-// The Original Code of this file is Copyright 1998-2004, Licensor of the
+// The Original Code of this file is Copyright 1998-2007, Licensor of the
 // AAF Association.
 //
 // The Initial Developer of the Original Code of this file and the
@@ -124,7 +124,6 @@ AAFRESULT STDMETHODCALLTYPE
 	ImplAAFStaticMobSlot* pStaticMobSlot = NULL;
 	aafUID_t	segDataDef;
 	ImplAAFSegment*	pSegment = NULL;
-	ImplAAFSequence*	pSequence = NULL;
 	ImplAAFSourceClip*	pSrcClip = NULL;
 	aafSourceRef_t		ref;
 	aafPosition_t		zeroPos;
@@ -181,29 +180,18 @@ AAFRESULT STDMETHODCALLTYPE
 			CHECK(GetDictionary(&pDictionary));
 			CHECK(pDictionary->GetBuiltinDefs()->cdSourceClip()->
 				CreateInstance((ImplAAFObject**) &pSrcClip));
-			CHECK(pSrcClip->Initialize(pDataDef, slotLength, ref));
-
-		// For MXF it is mandatory that we use a sequence here.
-		// For AAF it is optional.
-			CHECK(pDictionary->GetBuiltinDefs()->cdSequence()->
-			    CreateInstance((ImplAAFObject **) &pSequence));
-			CHECK(pSequence->Initialize(pDataDef));
-			CHECK(pSequence->AppendComponent(pSrcClip));
-
-			CHECK(AppendNewStaticSlot(pSequence, masterSlotID, pSlotName, 
-										&pNewStaticSlot));
 
 			pDictionary->ReleaseReference();
 			pDictionary = NULL;
+
+			CHECK(pSrcClip->Initialize(pDataDef, slotLength, ref));
+			CHECK(AppendNewStaticSlot(pSrcClip, masterSlotID, pSlotName, 
+										&pNewStaticSlot));
 
 			pNewStaticSlot->ReleaseReference();
 			pNewStaticSlot = NULL;
 
 			pSrcClip->ReleaseReference();
-			pSrcClip = NULL;
-
-			pSequence->ReleaseReference();
-			pSequence = NULL;
 			
 		}
 		else
@@ -239,29 +227,18 @@ AAFRESULT STDMETHODCALLTYPE
 			CHECK(pDictionary->GetBuiltinDefs()->cdSourceClip()->
 				CreateInstance((ImplAAFObject**) &pSrcClip));
 
-			CHECK(pSrcClip->Initialize(pDataDef, slotLength, ref));
-
-		// For MXF it is mandatory that we use a sequence here.
-		// For AAF it is optional.
-			CHECK(pDictionary->GetBuiltinDefs()->cdSequence()->
-			    CreateInstance((ImplAAFObject **) &pSequence));
-			CHECK(pSequence->Initialize(pDataDef));
-			CHECK(pSequence->AppendComponent(pSrcClip));
-
-			CHECK(AppendNewTimelineSlot(editRate,pSequence, masterSlotID, pSlotName, 
-										zeroPos,&pNewTimelineSlot));
-
 			pDictionary->ReleaseReference();
 			pDictionary = NULL;
+
+			CHECK(pSrcClip->Initialize(pDataDef, slotLength, ref));
+			CHECK(AppendNewTimelineSlot(editRate,pSrcClip, masterSlotID, pSlotName, 
+										zeroPos,&pNewTimelineSlot));
 
 			pNewTimelineSlot->ReleaseReference();
 			pNewTimelineSlot = NULL;
 
 			pSrcClip->ReleaseReference();
 			pSrcClip = NULL;
-
-			pSequence->ReleaseReference();
-			pSequence = NULL;
 		}
 	}
 	XEXCEPT
@@ -276,8 +253,6 @@ AAFRESULT STDMETHODCALLTYPE
 			pMobSlot->ReleaseReference();
 		if(pSrcClip != NULL)
 			pSrcClip->ReleaseReference();
-		if(pSequence != NULL)
-			pSequence->ReleaseReference();
 		if(pDictionary != NULL)
 			pDictionary->ReleaseReference();
 	}
