435cb6ccaa55411092180234031dfd69765c6fd6
terabrit <>
2008-08-01 01:54:41 +0000

    update to AAF PSL license

--- examples/com-api/ExportVC3/framebuffer.h
+++ examples/com-api/ExportVC3/framebuffer.h
@@ -1,31 +1,26 @@
-/*! \file	framebuffer.h
- *	\brief	functions to fill a buffer
- *
- *	\version $Id$
- *
- */
-/*
- *	Copyright (c) 2008, Metaglue Corporation
- *
- *	This software is provided 'as-is', without any express or implied warranty.
- *	In no event will the authors be held liable for any damages arising from
- *	the use of this software.
- *
- *	Permission is granted to anyone to use this software for any purpose,
- *	including commercial applications, and to alter it and redistribute it
- *	freely, subject to the following restrictions:
- *
- *	  1. The origin of this software must not be misrepresented; you must
- *	     not claim that you wrote the original software. If you use this
- *	     software in a product, an acknowledgment in the product
- *	     documentation would be appreciated but is not required.
- *	
- *	  2. Altered source versions must be plainly marked as such, and must
- *	     not be misrepresented as being the original software.
- *	
- *	  3. This notice may not be removed or altered from any source
- *	     distribution.
- */
+//=---------------------------------------------------------------------=
+//
+// $Id$
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+//
+// The Original Code of this file is Copyright 2008, Licensor of the
+// AAF Association.
+//
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Metaglue Corporation.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 #ifndef _FRAMEBUFFER_H_
 #define _FRAMEBUFFER_H_
