887317e1a4ae47377ad0a5facb580d07967471cc
stuart_hc <>
2009-06-02 04:23:07 +0000

    Update AAF SDK license boilerplate text to Version 2.0

--- AAFWinSDK/vs7/update.cmd
+++ AAFWinSDK/vs7/update.cmd
@@ -1,29 +1,39 @@
 @echo off
 rem  --------------------------------------------------------------------------
 rem
-rem   The contents of this file are subject to the AAF SDK Public
-rem   Source License Agreement (the "License"); You may not use this file
-rem   except in compliance with the License.  The License is available in
-rem   AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-rem   Association or its successor.
-rem   
+rem   $Id$ $Name$
+rem
+rem   The contents of this file are subject to the AAF SDK Public Source
+rem   License Agreement Version 2.0 (the "License"); You may not use this
+rem   file except in compliance with the License.  The License is available
+rem   in AAFSDKPSL.TXT, or you may obtain a copy of the License from the
+rem   Advanced Media Workflow Association, Inc., or its successor.
+rem
 rem   Software distributed under the License is distributed on an "AS IS"
 rem   basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
 rem   the License for the specific language governing rights and limitations
-rem   under the License.
-rem   
-rem   The Original Code of this file is Copyright 1998-2001, Licensor of the
-rem   AAF Association.
-rem   
+rem   under the License.  Refer to Section 3.3 of the License for proper use
+rem   of this Exhibit.
+rem
+rem   WARNING:  Please contact the Advanced Media Workflow Association,
+rem   Inc., for more information about any additional licenses to
+rem   intellectual property covering the AAF Standard that may be required
+rem   to create and distribute AAF compliant products.
+rem   (http://www.amwa.tv/policies).
+rem
+rem   Copyright Notices:
+rem   The Original Code of this file is Copyright 1998-2009, licensor of the
+rem   Advanced Media Workflow Association.  All rights reserved.
+rem
 rem   The Initial Developer of the Original Code of this file and the
-rem   Licensor of the AAF Association is Avid Technology.
+rem   licensor of the Advanced Media Workflow Association is Avid Technology.
 rem   All rights reserved.
 rem
 rem  --------------------------------------------------------------------------
 
 rem  ----------------------------------------------------------------
 rem  File: update.cmd
-rem  
+rem
 rem  Usage:  update.cmd <source> <destination>
 rem
 rem  This script checks if destination file is identical to
