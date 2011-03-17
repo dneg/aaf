05c74147050272a8b42e085f0e5a46afd8598c2a
philipn <>
2011-03-17 19:06:40 +0000

    use ffmpeg avcodec_decode_video2 when avcodec_decode_video is deprecated

--- ref-impl/plugins/CAAFCDCICodec.cpp
+++ ref-impl/plugins/CAAFCDCICodec.cpp
@@ -1583,9 +1583,20 @@ HRESULT STDMETHODCALLTYPE CAAFCDCICodec::ReadSamples(
 
 			// Decode one frame
 			int dec_picture_ok = 0;
-			int bytes_dec = avcodec_decode_video(_decoder->codec_context, _decoder->outputFrame, &dec_picture_ok,
-													_decoder->inputBuffer, params->storedSize);
-    
+			int bytes_dec;
+#if LIBAVCODEC_VERSION_INT >= ((52<<16)+(25<<8)+0)
+			// avcodec_decode_video was deprecated in favour of avcodec_decode_video2
+			AVPacket packet;
+			av_init_packet(&packet);
+			packet.data = _decoder->inputBuffer;
+			packet.size = params->storedSize;
+			bytes_dec = avcodec_decode_video2(_decoder->codec_context, _decoder->outputFrame, &dec_picture_ok,
+												&packet);
+#else
+			bytes_dec = avcodec_decode_video(_decoder->codec_context, _decoder->outputFrame, &dec_picture_ok,
+												_decoder->inputBuffer, params->storedSize);
+#endif
+
 			if (bytes_dec <= 0 || !dec_picture_ok) 
 			{
 				fprintf(stderr, "avcodec_decode_video() Error decoding video\n");
