f3862c5aaac79aae66fec4e1a3fe4c56d94e8251
phil_tudor <>
2012-03-19 13:24:09 +0000

    Change version to 1.1.4 Released

--- ref-impl/include/AAFSDKBuild.h
+++ ref-impl/include/AAFSDKBuild.h
@@ -48,7 +48,7 @@
 #define AAF_MINOR_VERSION 1
 #define AAF_MAINT_RELEASE 4
 
-#define AAF_RELEASE_STAGE kAAFVersionBeta
+#define AAF_RELEASE_STAGE kAAFVersionReleased
  
 // these next two lines are monitored/incremented by newbuild.pl
 
